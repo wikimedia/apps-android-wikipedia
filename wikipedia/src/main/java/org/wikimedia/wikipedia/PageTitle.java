0809ca3364cfdb08b34786f853629a04471502f3
YuviPanda <yuvipanda@gmail.com>
2013-10-31 19:02:07 -0700

    Make PageTitle object parcelable
    Change-Id: If236c119178f325b9eea7ae47c66858359c64a7b

--- wikipedia/src/main/java/org/wikimedia/wikipedia/PageTitle.java
+++ wikipedia/src/main/java/org/wikimedia/wikipedia/PageTitle.java
@@ -1,12 +1,15 @@
 package org.wikimedia.wikipedia;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
- * Value object representing the title of a page.
+ * Immutable value object representing the title of a page.
  *
  * Points to a specific page in a specific namespace on a specific site.
  * Is immutable.
  */
-public class PageTitle {
+public class PageTitle implements Parcelable {
     private final String namespace;
     private final String title;
 
@@ -21,5 +24,32 @@ public class PageTitle {
 
     public String getTitle() {
         return title;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Parcelable.Creator<PageTitle> CREATOR
+            = new Parcelable.Creator<PageTitle>() {
+        public PageTitle createFromParcel(Parcel in) {
+            return new PageTitle(in);
+        }
+
+        public PageTitle[] newArray(int size) {
+            return new PageTitle[size];
+        }
+    };
+
+    private PageTitle(Parcel in) {
+        namespace = in.readString();
+        title = in.readString();
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int flags) {
+        parcel.writeString(namespace);
+        parcel.writeString(title);
     }
 }
