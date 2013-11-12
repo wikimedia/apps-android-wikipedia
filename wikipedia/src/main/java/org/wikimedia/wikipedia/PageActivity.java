5ed27956cb232d7482f203205039373139886ebc
Yuvi Panda <yuvipanda@gmail.com>
2013-11-12 12:49:33 +0530

    Set app name to Wikipedia & kill unused menus
    Change-Id: Iedab69121583d4bde898c807598c7cc85dcbef65

--- wikipedia/src/main/java/org/wikimedia/wikipedia/PageActivity.java
+++ wikipedia/src/main/java/org/wikimedia/wikipedia/PageActivity.java
@@ -39,18 +39,4 @@ public class PageActivity extends FragmentActivity {
     public void onBackPressed() {
         getSupportFragmentManager().popBackStack();
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onMenuItemSelected(int featureId, MenuItem item) {
-//        PageViewFragment viewFragment = (PageViewFragment) getSupportFragmentManager().findFragmentById(R.id.testFragment);
-//        viewFragment.displayPage(new PageTitle(null, "India"));
-        return super.onMenuItemSelected(featureId, item);
-    }
 }
