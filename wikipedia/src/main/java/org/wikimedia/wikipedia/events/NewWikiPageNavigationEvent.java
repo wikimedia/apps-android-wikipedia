792f8529c3fc8d3f19543bbccebd548f42388674
Yuvi Panda <yuvipanda@gmail.com>
2013-11-27 17:29:49 +0530

    Fix spacing for NewWikiPageNavigationEvent
    Change-Id: Iaf80754a1e78f4ec9973062b3f152574f4cc02ab

--- wikipedia/src/main/java/org/wikimedia/wikipedia/events/NewWikiPageNavigationEvent.java
+++ wikipedia/src/main/java/org/wikimedia/wikipedia/events/NewWikiPageNavigationEvent.java
@@ -3,13 +3,13 @@ package org.wikimedia.wikipedia.events;
 import org.wikimedia.wikipedia.PageTitle;
 
 public class NewWikiPageNavigationEvent {
-        private final PageTitle title;
+    private final PageTitle title;
 
-        public NewWikiPageNavigationEvent(PageTitle title) {
-            this.title = title;
-        }
+    public NewWikiPageNavigationEvent(PageTitle title) {
+        this.title = title;
+    }
 
-        public PageTitle getTitle() {
-            return title;
-        }
+    public PageTitle getTitle() {
+        return title;
+    }
 }
