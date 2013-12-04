fd44aba35ffb1fb90ffe6e30631ba8c2375a7c9c
Yuvi Panda <yuvipanda@gmail.com>
2013-12-05 01:05:30 +0530

    Make NavDrawer use a ListView
    And thus we get free click feedback!
    
    Change-Id: Id732c0b1b9d8941262ef0e80915148fd485a440b

--- wikipedia/src/main/java/org/wikimedia/wikipedia/NavDrawerFragment.java
+++ wikipedia/src/main/java/org/wikimedia/wikipedia/NavDrawerFragment.java
@@ -6,32 +6,73 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.*;
 import org.wikimedia.wikipedia.history.HistoryActivity;
 
-public class NavDrawerFragment extends Fragment implements View.OnClickListener {
-    private static final int[] ACTION_ITEMS = {
-            R.id.nav_item_history
+public class NavDrawerFragment extends Fragment implements AdapterView.OnItemClickListener {
+    private static final int[] ACTION_ITEMS_TEXT = {
+            R.string.nav_item_history
     };
+    private static final int[] ACTION_ITEM_IMAGES = {
+            android.R.drawable.ic_menu_recent_history
+    };
+
+    private ListView navList;
+    private NavListAdapter adapter;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View parentView = inflater.inflate(R.layout.fragment_navdrawer, container, false);
-        for (int id: ACTION_ITEMS) {
-            parentView.findViewById(id).setOnClickListener(this);
-        }
+        navList = (ListView) parentView.findViewById(R.id.nav_list);
+        adapter = new NavListAdapter();
+
+        navList.setAdapter(adapter);
+        navList.setOnItemClickListener(this);
+
         return parentView;
     }
 
     @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.nav_item_history:
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        switch ((Integer)view.getTag()) {
+            case R.string.nav_item_history:
                 Intent intent = new Intent();
                 intent.setClass(this.getActivity(), HistoryActivity.class);
                 getActivity().startActivity(intent);
                 break;
             default:
                 throw new RuntimeException("Unknown ID clicked!");
+        }
+    }
+
+    private class NavListAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return ACTION_ITEMS_TEXT.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return ACTION_ITEMS_TEXT[position];
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = getActivity().getLayoutInflater().inflate(R.layout.item_nav_item, parent, false);
+            }
+            TextView navText = (TextView)convertView.findViewById(R.id.nav_item_text);
+            ImageView navImage = (ImageView)convertView.findViewById(R.id.nav_item_image);
+            navText.setText(ACTION_ITEMS_TEXT[position]);
+            navImage.setImageResource(ACTION_ITEM_IMAGES[position]);
+            convertView.setTag(ACTION_ITEMS_TEXT[position]);
+
+            return convertView;
         }
     }
 }
