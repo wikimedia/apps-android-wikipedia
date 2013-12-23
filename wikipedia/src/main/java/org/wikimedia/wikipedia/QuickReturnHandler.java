da3b0c87daf4b470a0dbf7294fd5ffbc4f05647a
Yuvi Panda <yuvipanda@gmail.com>
2013-12-24 05:14:26 +0530

    Make sure that the Search bar is never in 'indeterminate' state
    - Is always hidden or shown when you stop scrolling
    - Does not trigger any hiding behavior for first 'screen'
    
    Change-Id: I566169854810cf861fcabe262c314e33f0148fe4

--- wikipedia/src/main/java/org/wikimedia/wikipedia/QuickReturnHandler.java
+++ wikipedia/src/main/java/org/wikimedia/wikipedia/QuickReturnHandler.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 
-public class QuickReturnHandler implements  ObservableWebView.OnScrollChangeListener {
+public class QuickReturnHandler implements  ObservableWebView.OnScrollChangeListener, ObservableWebView.OnUpOrCancelMotionEventListener, ObservableWebView.OnDownMotionEventListener {
     private final ObservableWebView webview;
     private final View quickReturnView;
 
@@ -17,10 +17,19 @@ public class QuickReturnHandler implements  ObservableWebView.OnScrollChangeList
         this.quickReturnView =  quickReturnView;
 
         webview.setOnScrollChangeListener(this);
+        webview.setOnDownMotionEventListener(this);
+        webview.setOnUpOrCancelMotionEventListener(this);
     }
 
     @Override
     public void onScrollChanged(int oldScrollY, int scrollY) {
+        if (scrollY <= webview.getHeight()) {
+            // For the first screenful, ensure it always exists.
+            if (quickReturnView.getTranslationY() != 0) {
+                quickReturnView.animate().translationY(0).setDuration(WikipediaApp.SHORT_ANIMATION_DURATION).start();
+            }
+            return;
+        }
         int animMargin;
         if (oldScrollY > scrollY) {
             int minMargin = 0;
@@ -35,5 +44,25 @@ public class QuickReturnHandler implements  ObservableWebView.OnScrollChangeList
             animMargin = Math.max(minMargin, newMargin);
         }
         quickReturnView.setTranslationY(animMargin);
+    }
+
+    @Override
+    public void onUpOrCancelMotionEvent() {
+        int transY = (int)quickReturnView.getTranslationY();
+        int height = quickReturnView.getHeight();
+        if (transY != 0 && transY > -height) {
+            if (transY > -height / 2 ) {
+                // Fully display it
+                quickReturnView.animate().translationY(0).setDuration(WikipediaApp.SHORT_ANIMATION_DURATION).start();
+            } else {
+                // Fully hide it
+                quickReturnView.animate().translationY(-height).setDuration(WikipediaApp.SHORT_ANIMATION_DURATION).start();
+            }
+        }
+    }
+
+    @Override
+    public void onDownMotionEvent() {
+        // Don't do anything for now
     }
 }
