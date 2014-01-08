2088b3df274fd31c28baa6193c937046c04b98a6
Yuvi Panda <yuvipanda@gmail.com>
2014-01-08 12:14:48 +0530

    Modify language generation script to make JSON for iOS
    Change-Id: Ib5aec2f6cfcb5bd1187cf8863ecd50f1b1a2d20c

--- scripts/generate_wiki_languages.py
+++ scripts/generate_wiki_languages.py
@@ -1,5 +1,6 @@
 from urllib2 import urlopen
 import csv
+import json
 import lxml.builder as lb
 from lxml import etree
 
@@ -8,16 +9,21 @@ URL = "https://wikistats.wmflabs.org/api.php?action=dump&table=wikipedias&format
 
 data = csv.reader(urlopen(URL))
 
-# Column 2 is the language code
-lang_keys = [row[2] for row in data]
+lang_keys = []
+lang_local_names = []
+lang_eng_names = []
+for row in data:
+    lang_keys.append(row[2])
+    lang_local_names.append(row[10])
+    lang_eng_names.append(row[1])
 
-del lang_keys[0] # Get rid of the headers
-
-# Generate the XML
+# Generate the XML, for Android
 x = lb.E
 
 keys = [x.item(k) for k in lang_keys]
 
+# Skip the headers!
+del keys[0]
 resources = x.resources(
     getattr(x, 'string-array')(*keys, name="preference_language_keys"),
 )
@@ -26,3 +32,15 @@ open("languages_list.xml", "w").write(
     etree.tostring(resources, pretty_print=True, encoding="utf-8", xml_declaration=True)
 )
 
+# Generate the JSON, for iOS
+langs_json = []
+
+# Start from 1, to skip the headers
+for i in xrange(1, len(lang_keys)):
+    langs_json.append({
+        "code": lang_keys[i],
+        "name": lang_local_names[i],
+        "canonical_name": lang_eng_names[i]
+    })
+
+open("languages_list.json", "w").write(json.dumps(langs_json, indent=4))
