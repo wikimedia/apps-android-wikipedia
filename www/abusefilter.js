558a59f81a787e72ea37062b9a5d0c26a46093fe
Yuvi Panda <yuvipanda@gmail.com>
2014-01-31 00:26:09 +0530

    Clear all inline styling from AF warning messages
    They are at least readable now.
    
    Change-Id: I89f48f580308fde3f9a7af3194b4840455c456b9

--- www/abusefilter.js
+++ www/abusefilter.js
@@ -1,11 +1,27 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var bridge = require('./bridge');
+var transformer = require('./transformer');
+
+transformer.register( 'clearInlineStyling', function( content ) {
+    var styledElements = content.querySelectorAll( "*[style]" );
+    for ( var i = 0; i < styledElements.length; i++ ) {
+        styledElements[i].removeAttribute( "style" );
+    }
+    return content;
+} );
 
 bridge.registerListener( 'displayWarning', function( payload ) {
     var content = document.getElementById( 'content' );
-    content.innerHTML = payload.html;
+
+    var warning = document.createElement( 'div' );
+    warning.innerHTML = payload.html;
+
+    warning = transformer.transform( 'clearInlineStyling', warning );
+
+    content.appendChild( warning );
 } );
-},{"./bridge":2}],2:[function(require,module,exports){
+
+},{"./bridge":2,"./transformer":3}],2:[function(require,module,exports){
 function Bridge() {
 }
 
@@ -43,4 +59,28 @@ module.exports = new Bridge();
 window.onload = function() {
     module.exports.sendMessage( "DOMLoaded", {} );
 };
+},{}],3:[function(require,module,exports){
+function Transformer() {
+}
+
+var transforms = {};
+
+Transformer.prototype.register = function( transform, fun ) {
+    if ( transform in transforms ) {
+        transforms[transform].append( fun );
+    } else {
+        transforms[transform] = [ fun ];
+    }
+};
+
+Transformer.prototype.transform = function( transform, element ) {
+    var functions = transforms[transform];
+    for ( var i = 0; i < functions.length; i++ ) {
+        element = functions[i](element);
+    }
+    return element;
+}
+
+module.exports = new Transformer();
+
 },{}]},{},[2,1])
--- www/js/abusefilter.js
+++ www/js/abusefilter.js
@@ -1,6 +1,21 @@
 var bridge = require('./bridge');
+var transformer = require('./transformer');
+
+transformer.register( 'clearInlineStyling', function( content ) {
+    var styledElements = content.querySelectorAll( "*[style]" );
+    for ( var i = 0; i < styledElements.length; i++ ) {
+        styledElements[i].removeAttribute( "style" );
+    }
+    return content;
+} );
 
 bridge.registerListener( 'displayWarning', function( payload ) {
     var content = document.getElementById( 'content' );
-    content.innerHTML = payload.html;
+
+    var warning = document.createElement( 'div' );
+    warning.innerHTML = payload.html;
+
+    warning = transformer.transform( 'clearInlineStyling', warning );
+
+    content.appendChild( warning );
 } );
