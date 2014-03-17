23ac15bea51b56307c23b77244d94c6d847c3a32
YuviPanda <yuvipanda@gmail.com>
2014-03-17 14:45:28 +0530

    Add python script that generates pngs from svgs
    - Requires rsvg-convert and the python sh module
    - Has svgs in subfolders of icon-svgs/, where the name of the
      subfolder is taken to be the size of the (square) icon in dp
    - Also add ldpi support. 8% of devices, and it is trivial for us to
      support anyway
    - Maintains aspect ratio by fixing height
    
    Change-Id: Icd9a8fbc9ff698036c28f53060c120ebe46165af

---
+++ scripts/convert-icons.py
@@ -0,0 +1,40 @@
+import os
+import sh
+
+from glob import glob
+
+DENSITIES = {
+    "ldpi": 0.75,
+    "mdpi": 1,
+    "hdpi": 1.5,
+    "xhdpi": 2,
+    "xxhdpi": 3
+}
+
+OUTPUT_PATH_PREFIX = os.path.abspath(os.path.join(os.path.dirname(__file__), "../wikipedia/res/"))
+
+
+class ImagesBatch(object):
+    def __init__(self, path):
+        self.dp = int(os.path.basename(path))
+        self.path = path
+        self.svgs = [os.path.abspath(p) for p in glob(os.path.join(path, "*.svg"))]
+
+    def _do_export(self, density, input_path):
+        file_name = os.path.basename(os.path.splitext(input_path)[0] + ".png")
+        output_file_path = os.path.join(OUTPUT_PATH_PREFIX, "drawable-" + density, file_name)
+        px = int(DENSITIES[density] * self.dp)
+        sh.rsvg_convert(input_path, "-a", h=px, o=output_file_path)
+
+    def convert(self):
+        for svg in self.svgs:
+            for density in DENSITIES.keys():
+                self._do_export(density, svg)
+            print u"\u2713 %s" % os.path.basename(svg)
+
+        
+if __name__ == "__main__":
+    paths = glob(os.path.join(os.path.dirname(__file__), "../icon-svgs/*"))
+    for path in paths:
+        ib = ImagesBatch(path)
+        ib.convert()
