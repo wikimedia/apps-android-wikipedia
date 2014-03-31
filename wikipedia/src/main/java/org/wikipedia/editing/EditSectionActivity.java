f3d7132cd546104e16d3b73454abeccebcbb6483
YuviPanda <yuvipanda@gmail.com>
2014-04-01 03:32:59 +0530

    Set edit text box's directionality
    Bug: 63264
    Change-Id: I2e3907156ced1850f0e2f729bbd044cdd4485f38

--- wikipedia/src/main/java/org/wikipedia/editing/EditSectionActivity.java
+++ wikipedia/src/main/java/org/wikipedia/editing/EditSectionActivity.java
@@ -136,6 +136,8 @@ public class EditSectionActivity extends ActionBarActivity {
             }
         });
 
+        Utils.setTextDirection(sectionText, title.getSite().getLanguage());
+
         fetchSectionText();
     }
 
