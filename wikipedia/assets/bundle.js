b1c443e3ca739c2a4729684d651253c71867e089
YuviPanda <yuvipanda@gmail.com>
2014-04-11 03:08:15 +0530

    Support hiding edit button for anon users
    Based on remoteConfig - if it is tweaked to be 'true' then
    anon users won't be getting edit buttons in a day or so.
    
    Change-Id: I956d77190035742528e432cca14483cf0995aa1a

--- wikipedia/assets/bundle.js
+++ wikipedia/assets/bundle.js
@@ -107,6 +107,10 @@ bridge.registerListener( "requestImagesList", function () {
     bridge.sendMessage( "imagesListResponse", { "images": imageURLs });
 } );
 
+bridge.registerListener( "hideEditButtons", function() {
+    document.getElementsByTagName( "html" )[0].classList.add( "no-editing" );
+} );
+
 },{"./bridge":2}],5:[function(require,module,exports){
 var bridge = require("./bridge");
 
