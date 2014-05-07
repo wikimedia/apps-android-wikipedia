9d0e633751a2f6056ca0d827ed80ae699f82bda3
dbrant <dbrant@wikimedia.org>
2014-05-07 12:38:21 -0400

    Fixed clicking on links that have inner tags.
    e.g. <a><strong>foo</strong></a>
    
    Change-Id: Id3faf0c870dacefbf1760a46d3685e8dab64743c

--- wikipedia/assets/bundle.js
+++ wikipedia/assets/bundle.js
@@ -15,15 +15,26 @@ ActionsHandler.prototype.register = function( action, fun ) {
 };
 
 document.onclick = function() {
-    if ( event.target.tagName === "A" ) {
-        if ( event.target.hasAttribute( "data-action" ) ) {
-            var action = event.target.getAttribute( "data-action" );
+    var linkTarget = null;
+    var curNode = event.target;
+    // If an element was clicked, check if it or any of its parents are <a>
+    // This handles cases like <a>foo</a>, <a><strong>foo</strong></a>, etc.
+    while (curNode) {
+        if (curNode.tagName === "A") {
+            linkTarget = curNode;
+            break;
+        }
+        curNode = curNode.parentNode;
+    }
+    if (linkTarget) {
+        if ( linkTarget.hasAttribute( "data-action" ) ) {
+            var action = linkTarget.getAttribute( "data-action" );
             var handlers = actionHandlers[ action ];
             for ( var i = 0; i < handlers.length; i++ ) {
-                handlers[i]( event.target, event );
+                handlers[i]( linkTarget, event );
             }
         } else {
-            var href = event.target.getAttribute( "href" );
+            var href = linkTarget.getAttribute( "href" );
             if ( href[0] === "#" ) {
                 // If it is a link to an anchor in the current page, just scroll to it
                 document.getElementById( href.substring( 1 ) ).scrollIntoView();
