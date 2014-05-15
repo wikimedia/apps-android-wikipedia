fb18fd2b556c7388c421667ba4eb2c48173c7836
bearND <bsitzmann@wikimedia.org>
2014-05-15 17:02:40 -0600

    Fix PreviewTaskTests
    The server response now includes more HTML classes:
    icon icon-32px icon-edit enabled
    
    Change-Id: I5a1cc787309a2bec4fbed663625b745f4ff9c89f

--- wikipedia-it/src/main/java/org/wikipedia/test/PreviewTaskTests.java
+++ wikipedia-it/src/main/java/org/wikipedia/test/PreviewTaskTests.java
@@ -20,8 +20,7 @@ public class PreviewTaskTests extends ActivityUnitTestCase<TestDummyActivity> {
         final PageTitle title = new PageTitle(null, "Test_page_for_app_testing/Section1", new Site("test.wikipedia.org"));
         long randomTime = System.currentTimeMillis();
         final String wikitext = "== Section 2 ==\n\nEditing section INSERT RANDOM & HERE test at " + randomTime;
-        final String expected = "<div></div><h2><span class=\"mw-headline\" id=\"Section_2\">Section 2</span><a href=\"#editor/1\" data-section=\"1\" class=\"edit-page\">Edit</a></h2><div>\n<p>Editing section INSERT RANDOM &amp; HERE test at " + randomTime + "</p>\n\n\n\n</div>";
-        final WikipediaApp app = (WikipediaApp)getInstrumentation().getTargetContext().getApplicationContext();
+        final String expected = "<div></div><h2><span class=\"mw-headline\" id=\"Section_2\">Section 2</span><a href=\"#editor/1\" data-section=\"1\" class=\"edit-page icon icon-32px icon-edit enabled\">Edit</a></h2><div>\n<p>Editing section INSERT RANDOM &amp; HERE test at " + randomTime + "</p>\n\n\n\n</div>";
         final CountDownLatch completionLatch = new CountDownLatch(1);
         runTestOnUiThread(new Runnable() {
             @Override
