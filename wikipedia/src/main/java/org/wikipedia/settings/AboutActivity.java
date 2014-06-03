b048476b3c4d3d266344dd54c61e14aeff6b1d21
dbrant <dbrant@wikimedia.org>
2014-06-02 23:05:36 -0400

    Add feedback link to About page.
    Change-Id: I984459340a8e0adc350563ef519e9621339f9cfe

--- wikipedia/src/main/java/org/wikipedia/settings/AboutActivity.java
+++ wikipedia/src/main/java/org/wikipedia/settings/AboutActivity.java
@@ -2,6 +2,7 @@ package org.wikipedia.settings;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.text.Html;
 import android.text.method.LinkMovementMethod;
 import android.view.MenuItem;
 import android.view.ViewGroup;
@@ -16,8 +17,14 @@ public class AboutActivity extends ActionBarActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         ((TextView) findViewById(R.id.about_version_text)).setText(WikipediaApp.APP_VERSION_STRING);
+        ((TextView) findViewById(R.id.send_feedback_text)).setText(Html.fromHtml(
+                "<a href=\"mailto:mobile-android-wikipedia@wikimedia.org?subject=Android App " +
+                WikipediaApp.APP_VERSION_STRING +
+                " Feedback\">" +
+                getString(R.string.send_feedback) +
+                "</a>"));
 
-        makeEverythingClickable((ViewGroup)getWindow().getDecorView());
+        makeEverythingClickable((ViewGroup) getWindow().getDecorView());
     }
 
     @Override
