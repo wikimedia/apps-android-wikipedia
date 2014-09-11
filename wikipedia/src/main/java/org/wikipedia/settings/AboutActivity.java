e03f15703092a01f61fcd52981cb02fc8f7651d8
Eric Cochran <smartandmellow@gmail.com>
2014-09-11 11:49:38 -0400

    remove configChange hack from AboutActivity
    Change-Id: I951ad77c633e91dadbdf72d874e7eb75726ead48

--- wikipedia/src/main/java/org/wikipedia/settings/AboutActivity.java
+++ wikipedia/src/main/java/org/wikipedia/settings/AboutActivity.java
@@ -1,12 +1,14 @@
 package org.wikipedia.settings;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import org.wikipedia.R;
 import org.wikipedia.ThemedActionBarActivity;
@@ -14,10 +16,17 @@ import org.wikipedia.Utils;
 import org.wikipedia.WikipediaApp;
 
 public class AboutActivity extends ThemedActionBarActivity {
+
+    private static final String KEY_SCROLL_X = "KEY_SCROLL_X";
+    private static final String KEY_SCROLL_Y = "KEY_SCROLL_Y";
+
+    private ScrollView mScrollView;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
+        mScrollView = (ScrollView) findViewById(R.id.about_scrollview);
         ((TextView) findViewById(R.id.about_translators)).setText(Html.fromHtml(getString(R.string.about_translators_translatewiki)));
         ((TextView) findViewById(R.id.about_wmf)).setText(Html.fromHtml(getString(R.string.about_wmf)));
         ((TextView) findViewById(R.id.about_version_text)).setText(WikipediaApp.getInstance().getAppVersionString());
@@ -50,6 +59,26 @@ public class AboutActivity extends ThemedActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putInt(KEY_SCROLL_X, mScrollView.getScrollX());
+        outState.putInt(KEY_SCROLL_Y, mScrollView.getScrollY());
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        final int x = savedInstanceState.getInt(KEY_SCROLL_X);
+        final int y = savedInstanceState.getInt(KEY_SCROLL_Y);
+        mScrollView.post(new Runnable() {
+            @Override
+            public void run() {
+                mScrollView.scrollTo(x, y);
+            }
+        });
+    }
+
     private void makeEverythingClickable(ViewGroup vg) {
         for (int i = 0; i < vg.getChildCount(); i++) {
             if (vg.getChildAt(i) instanceof ViewGroup) {
@@ -59,6 +88,5 @@ public class AboutActivity extends ThemedActionBarActivity {
                 tv.setMovementMethod(LinkMovementMethod.getInstance());
             }
         }
-
     }
 }
