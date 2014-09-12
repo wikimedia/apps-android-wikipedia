e355df55eda935500a6f5b658fd8624bcc78ddfc
dbrant <dbrant@wikimedia.org>
2014-09-12 09:06:25 -0400

    Correctly urldecode links from Disambiguation dialog.
    Bug: 70769
    Change-Id: I58a2177915b533a2fa01c44041e6b80fc7bb5266

--- wikipedia/src/main/java/org/wikipedia/page/LinkMovementMethodExt.java
+++ wikipedia/src/main/java/org/wikipedia/page/LinkMovementMethodExt.java
@@ -6,6 +6,8 @@ import android.text.method.LinkMovementMethod;
 import android.text.style.URLSpan;
 import android.view.MotionEvent;
 import android.widget.TextView;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 
 /**
  * Intercept web links and add special behavior for external links.
@@ -28,9 +30,13 @@ public class LinkMovementMethodExt extends LinkMovementMethod {
             final int off = layout.getOffsetForHorizontal(line, x);
             final URLSpan[] links = buffer.getSpans(off, off, URLSpan.class);
             if (links.length != 0) {
-                String url = links[0].getURL();
-                handler.onUrlClick(url);
-                return true;
+                try {
+                    String url = URLDecoder.decode(links[0].getURL(), "utf-8");
+                    handler.onUrlClick(url);
+                    return true;
+                } catch (UnsupportedEncodingException e) {
+                    // won't happen
+                }
             }
         }
         return super.onTouchEvent(widget, buffer, event);
