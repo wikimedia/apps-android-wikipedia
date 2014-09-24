e0ba88c5e4ee5b537d5c253aa56ecf70072dd44a
YuviPanda <yuvipanda@gmail.com>
2014-09-24 16:06:44 +0100

    Add xxxhdpi resolution launcher icons
    Change-Id: I13af17bfa2b761cfcb9dcc488a9c9a527522ebf1

--- scripts/convert-icons.py
+++ scripts/convert-icons.py
@@ -8,7 +8,8 @@ DENSITIES = {
     "mdpi": 1,
     "hdpi": 1.5,
     "xhdpi": 2,
-    "xxhdpi": 3
+    "xxhdpi": 3,
+    "xxxhdpi": 4
 }
 
 OUTPUT_PATH_PREFIX = os.path.abspath(os.path.join(os.path.dirname(__file__), "../wikipedia/res/"))
@@ -24,7 +25,9 @@ class ImagesBatch(object):
         nonspecific = ".nonspecific." in input_path
         noflip = nonspecific or input_path.endswith(".noflip.svg")
         file_name = os.path.basename(os.path.splitext(input_path)[0].split(".")[0] + ".png")
-        output_file_path = os.path.join(OUTPUT_PATH_PREFIX, drawable, file_name)
+        folder_path = os.path.join(OUTPUT_PATH_PREFIX, drawable)
+        sh.mkdir("-p", folder_path)
+        output_file_path = os.path.join(folder_path, file_name)
         px = int(DENSITIES[density] * self.dp)
         sh.rsvg_convert(input_path, "-a", h=px, o=output_file_path)
         return (output_file_path, noflip)
@@ -43,7 +46,9 @@ class ImagesBatch(object):
                 self._do_export("xxhdpi", svg, "drawable")
             else:
                 for density in DENSITIES.keys():
-                    self._do_flop(density, self._do_export(density, svg, "drawable-" + density))
+                    # For xxxhdpi only generate launcher icons
+                    if not (density == 'xxxhdpi' and 'launcher' not in svg):
+                        self._do_flop(density, self._do_export(density, svg, "drawable-" + density))
             print(u"\u2713 %s" % os.path.basename(svg))
 
 
