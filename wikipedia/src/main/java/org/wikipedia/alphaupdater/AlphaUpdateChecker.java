e2b7561b98778f867043f97532f9cfa8296580be
bearND <bsitzmann@wikimedia.org>
2014-10-17 12:48:42 -0600

    Alpha updater update
    ;) Update the URLs for alpha builds.
    
    Change-Id: I8303a446229d464a4c79f63143bec4761c72e9bd

--- wikipedia/src/main/java/org/wikipedia/alphaupdater/AlphaUpdateChecker.java
+++ wikipedia/src/main/java/org/wikipedia/alphaupdater/AlphaUpdateChecker.java
@@ -1,5 +1,10 @@
 package org.wikipedia.alphaupdater;
 
+import org.wikipedia.R;
+import org.wikipedia.recurring.RecurringTask;
+import com.github.kevinsawicki.http.HttpRequest;
+import org.json.JSONException;
+import org.json.JSONObject;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -9,16 +14,9 @@ import android.content.SharedPreferences;
 import android.net.Uri;
 import android.preference.PreferenceManager;
 import android.support.v4.app.NotificationCompat;
-import com.github.kevinsawicki.http.HttpRequest;
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.wikipedia.R;
-import org.wikipedia.recurring.RecurringTask;
-
 import java.util.Date;
 
 public class AlphaUpdateChecker extends RecurringTask {
-    // The 'l' suffix is needed because stupid Java overflows constants otherwise
     private static final long RUN_INTERVAL_MILLI = 24L * 60L * 60L * 1000L; // Once a day!
 
     private static final String PREFERENCE_KEY_ALPHA_COMMIT = "alpha_last_checked_commit";
@@ -36,7 +34,7 @@ public class AlphaUpdateChecker extends RecurringTask {
         // Check for updates!
         String responseJSON;
         try {
-            HttpRequest request = HttpRequest.get("https://tools.wmflabs.org/wikipedia-android-builds/runs/latest/meta.json");
+            HttpRequest request = HttpRequest.get("http://android-builds.wmflabs.org/runs/latest/meta.json");
             responseJSON = request.body();
         } catch (HttpRequest.HttpRequestException e) {
             // It's ok, we can do nothing.
@@ -60,7 +58,7 @@ public class AlphaUpdateChecker extends RecurringTask {
     }
 
     private void showNotification() {
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://tools.wmflabs.org/wikipedia-android-builds/runs/latest/wikipedia.apk"));
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://android-builds.wmflabs.org/runs/latest/wikipedia.apk"));
         PendingIntent pintent = PendingIntent.getActivity(getContext(), 0, intent, 0);
 
         Notification notification = new NotificationCompat.Builder(getContext()).setSmallIcon(R.drawable.launcher_alpha)
