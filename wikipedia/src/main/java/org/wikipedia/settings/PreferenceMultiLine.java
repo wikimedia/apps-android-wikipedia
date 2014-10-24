e23811b9a35375ccbaba3a3be4c0c8c853cd0ab1
dbrant <dbrant@wikimedia.org>
2014-10-24 12:04:13 -0400

    Catch possible exception when clicking Preference with intent.
    If the device doesn't have a Browser app installed, it will throw an
    exception when trying to launch a VIEW intent from a Preference.
    
    Bug: 70528
    Change-Id: I06e791f4eb5126ac5ddb0d850a2a91c84b18c058

--- wikipedia/src/main/java/org/wikipedia/settings/PreferenceMultiLine.java
+++ wikipedia/src/main/java/org/wikipedia/settings/PreferenceMultiLine.java
@@ -1,10 +1,14 @@
 package org.wikipedia.settings;
 
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.preference.Preference;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.wikipedia.R;
 
 public class PreferenceMultiLine extends Preference {
 
@@ -27,5 +31,21 @@ public class PreferenceMultiLine extends Preference {
         if (textView != null) {
             textView.setSingleLine(false);
         }
+        // Intercept the click listener for this preference, and if the preference has an intent,
+        // launch the intent ourselves, so that we can catch the exception if the intent fails.
+        this.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                if (preference.getIntent() != null) {
+                    try {
+                        getContext().startActivity(preference.getIntent());
+                    } catch (ActivityNotFoundException e) {
+                        Toast.makeText(getContext(), getContext().getString(R.string.error_browser_not_found), Toast.LENGTH_LONG).show();
+                    }
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 }
