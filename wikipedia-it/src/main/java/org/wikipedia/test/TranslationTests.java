4cc966ea11e27b699255f2333ff101a14fc0fcd4
bearND <bsitzmann@wikimedia.org>
2014-11-13 13:45:24 -0700

    Fix translations and add more tests for them
    Checking now if there are HTML tags iff there are any in en.
    Better failure output.
    
    Change-Id: I3664c56cb6282b3b60b312b1f27e7d26f87f8d01
    (cherry picked from commit fdb420ade518b886818381e71d170acc1c76318b)

--- wikipedia-it/src/main/java/org/wikipedia/test/TranslationTests.java
+++ wikipedia-it/src/main/java/org/wikipedia/test/TranslationTests.java
@@ -11,6 +11,9 @@ import org.wikipedia.R;
 import org.wikipedia.page.PageActivity;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
 
 /**
@@ -25,7 +28,7 @@ import java.util.Locale;
 public class TranslationTests extends ActivityInstrumentationTestCase2<PageActivity> {
     public static final String TAG = "TrTest";
     private PageActivity activity;
-    private StringBuilder paramMismatches = new StringBuilder();
+    private StringBuilder mismatches = new StringBuilder();
 
     public TranslationTests() {
         super(PageActivity.class);
@@ -42,6 +45,13 @@ public class TranslationTests extends ActivityInstrumentationTestCase2<PageActiv
     }
 
     public void testAllTranslations() throws Exception {
+        setLocale(Locale.ROOT.toString());
+        List<Res> tagRes = new ResourceCollector("<", "&lt;").collectParameterResources();
+        List<Res> noTagRes = new ResourceCollector("<", "&lt;").not().collectParameterResources();
+        List<Res> stringParamRes = new ResourceCollector("%s", "%1$s").collectParameterResources();
+        List<Res> decimalParamRes = new ResourceCollector("%d", "%1$d").collectParameterResources();
+        List<Res> floatParamRes = new ResourceCollector("%f", "%.2f").collectParameterResources();
+
         AssetManager assetManager = getInstrumentation().getTargetContext().getResources().getAssets();
         for (String lang : assetManager.getLocales()) {
             Log.i(TAG, "----locale=" + (lang.equals("") ? "DEFAULT" : lang));
@@ -49,21 +59,55 @@ public class TranslationTests extends ActivityInstrumentationTestCase2<PageActiv
 //            checkAllStrings(); // might take too long
 
             // commented out during the transition from 1 param to 0
-//            checkOneStringWith0Parameter(R.string.saved_pages_search_empty_message);
-//            checkOneStringWith0Parameter(R.string.history_search_empty_message);
+//            checkTranslationHasNoParameter(R.string.saved_pages_search_empty_message);
+//            checkTranslationHasNoParameter(R.string.history_search_empty_message);
 
             if (!lang.startsWith("qq")) {
-                checkOneStringWithParameter(R.string.last_updated_text);
-                if (!lang.startsWith("ak") && !lang.startsWith("el")) {
-                    // taking forever to get those fixed :(
-                    checkOneStringWithParameter(R.string.editing_error_spamblacklist);
+                // tag (html) parameters
+                for (Res res : tagRes) {
+                    if (res.id == R.string.wp_stylized
+                        && (lang.startsWith("iw") || lang.startsWith("he"))) {
+                        // exceptions of the rule
+                        continue;
+                    }
+                    expectContains(res, "<", "&lt;");
                 }
-                checkOneStringWithParameter(R.string.page_protected_other);
-                checkOneStringWithParameter(R.string.search_redirect_title);
-                // TODO: build a list of all parameterized string resources from default strings dynamically first
+
+                for (Res res : noTagRes) {
+                    expectNotContains(res, "<", "&lt;");
+                }
+
+                // string parameters
+                for (Res res : stringParamRes) {
+                    if (res.id == R.string.editing_error_spamblacklist
+                        && (lang.startsWith("ak") || lang.startsWith("el"))) {
+                        // taking forever to get those fixed :(
+                        continue;
+                    }
+                    checkTranslationHasParameter(res, "[stringParam]", null);
+                }
+
+                if (lang.startsWith("fa") || lang.startsWith("ar") || lang.equals("bn")
+                    || lang.equals("mr") || lang.equals("my") || lang.startsWith("ne") || lang.equals("ps")) {
+                    // don't check the number params since those languages have different string representations for numbers
+                    continue;
+                }
+
+                // decimal parameters
+                for (Res res : decimalParamRes) {
+                    final int param1 = 42;
+                    checkTranslationHasParameter(res, param1, null);
+                }
+
+                // float parameters
+                for (Res res : floatParamRes) {
+                    final float param1 = .27f;
+                    checkTranslationHasParameter(res, param1, "0,27");
+//                // TODO: build a list of all parameterized string resources from default strings dynamically first
+                }
             }
         }
-        assertTrue(paramMismatches.toString(), paramMismatches.length() == 0);
+        assertTrue("\n" + mismatches.toString(), mismatches.length() == 0);
     }
 
     private Locale myLocale;
@@ -79,62 +123,133 @@ public class TranslationTests extends ActivityInstrumentationTestCase2<PageActiv
     }
 
     private void checkAllStrings() {
-        final R.string stringResources = new R.string();
-        final Class<R.string> c = R.string.class;
-        final Field[] fields = c.getDeclaredFields();
-
-        for (int i = 0, max = fields.length; i < max; i++) {
-            final String name;
-            final int resourceId;
-            try {
-                name = fields[i].getName();
-                resourceId = fields[i].getInt(stringResources);
-            } catch (Exception e) {
-                Log.e(TAG, myLocale + "-" + i + "; failed: " + e.getMessage());
-                continue;
-            }
-            try {
-                String value = getInstrumentation().getTargetContext().getResources().getString(resourceId);
-//                Log.i(TAG, buildLogString(i, name) + " = " + value);
-            } catch (Resources.NotFoundException e) {
-                Log.w(TAG, buildLogString(i, name) + "; <not found>");
-            } catch (RuntimeException e) {
-                Log.e(TAG, buildLogString(i, name) + "; --- " + e.getMessage());
-            }
-        }
+        new ResourceCollector().collectParameterResources();
     }
 
     private String buildLogString(int i, String name) {
         return myLocale + "-" + i + "; name = " + name;
     }
 
-    private void checkOneStringWith0Parameter(int resourceId) {
+    private void expectNotContains(Res res, String... examples) {
+        String translatedString = getInstrumentation().getTargetContext().getString(res.id);
+//        Log.i(TAG, myLocale + ":" + translatedString);
+        for (String example : examples) {
+            if (translatedString.contains(example)) {
+                final String msg = myLocale + ":" + res.name + " = " + translatedString + "' contains " + example;
+                Log.e(TAG, msg);
+                mismatches.append(msg).append("\n");
+                break;
+            }
+        }
+    }
+
+    private void expectContains(Res res, Object... examples) {
+        String translatedString = getInstrumentation().getTargetContext().getString(res.id);
+//        Log.i(TAG, myLocale + ":" + translatedString);
+        boolean found = false;
+        for (Object example : examples) {
+            if (translatedString.contains(example.toString())) {
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            final String msg = myLocale + ":" + res.name + " = " + translatedString + "' does not contain " + Arrays.toString(examples);
+            Log.e(TAG, msg);
+            mismatches.append(msg).append("\n");
+        }
+    }
+
+    private void checkTranslationHasNoParameter(Res res) {
         final String param1 = "[param1]";
-        String translatedString = getInstrumentation().getTargetContext().getString(resourceId, param1);
+        String translatedString = getInstrumentation().getTargetContext().getString(res.id, param1);
 //        Log.i(TAG, myLocale + ":" + translatedString);
         if (translatedString.contains(param1)) {
-            final String msg = myLocale + ":" + translatedString + "' contains " + param1;
+            final String msg = myLocale + ":" + res.name + " = " + translatedString + "' contains " + param1;
             Log.e(TAG, msg);
-            paramMismatches.append(msg).append("\n");
+            mismatches.append(msg).append("\n");
         }
     }
 
-    public void checkOneStringWithParameter(int resourceId) throws Exception {
-        final String param1 = "[param1]";
-        String translatedString = getInstrumentation().getTargetContext().getString(resourceId, param1);
+    public void checkTranslationHasParameter(Res res, Object param1, String alternate) throws Exception {
+        String translatedString = getInstrumentation().getTargetContext().getString(res.id, param1);
 //        Log.i(TAG, myLocale + ":" + translatedString);
-        if (!translatedString.contains(param1)) {
-            final String msg = myLocale + ":" + translatedString + "' doesn't contain " + param1;
+        if (!translatedString.contains(param1.toString())
+            && (alternate == null || !translatedString.contains(alternate))) {
+            final String msg = myLocale + ":" + res.name + " = " + translatedString + "' doesn't contain " + param1;
             Log.e(TAG, msg);
-            paramMismatches.append(msg).append("\n");
+            mismatches.append(msg).append("\n");
         }
-//        assertTrue(myLocale + ":'" + translatedString + "' doesn't contain " + param1, translatedString.contains(param1));
     }
 
-//    public void testOneStringWithParameter() throws Exception {
-//        setLocale("de");
-//        assertEquals("Zuletzt aktualisiert foo", getInstrumentation().getTargetContext().getString(R.string.last_updated_text, "foo"));
-//        setLocale("fa");
-//        assertEquals("", getInstrumentation().getTargetContext().getString(R.string.last_updated_text, "foo"));
-//    }
+    private class ResourceCollector {
+        private boolean negate;
+        private final String[] paramExamples;
+
+        ResourceCollector(String... paramExamples) {
+            this.paramExamples = paramExamples;
+        }
+
+        private ResourceCollector not() {
+            negate = true;
+            return this;
+        }
+
+        private List<Res> collectParameterResources() {
+            final List<Res> resources = new ArrayList<Res>();
+
+            final R.string stringResources = new R.string();
+            final Class<R.string> c = R.string.class;
+            final Field[] fields = c.getDeclaredFields();
+
+            for (int i = 0, max = fields.length; i < max; i++) {
+                final String name;
+                final int resourceId;
+                try {
+                    name = fields[i].getName();
+                    resourceId = fields[i].getInt(stringResources);
+                } catch (Exception e) {
+                    Log.e(TAG, myLocale + "-" + i + "; failed: " + e.getMessage());
+                    continue;
+                }
+                try {
+                    String value = getInstrumentation().getTargetContext().getResources().getString(resourceId);
+//                Log.i(TAG, buildLogString(i, name) + " = " + value);
+                    if (!name.startsWith("abc_") && !name.startsWith("preference_")) {
+                        boolean found = false;
+                        if (value.contains("wikimediafoundation.org") && negate) {
+                            System.out.println("found = " + found);
+                        }
+                        for (String paramExample : paramExamples) {
+                            if (value.contains(paramExample)) {
+                                found = true;
+                                if (!negate) {
+                                    break;
+                                }
+                            }
+                        }
+                        if ((!negate && found) || (negate && !found)) {
+                            resources.add(new Res(resourceId, name));
+                        }
+                    }
+                } catch (Resources.NotFoundException e) {
+                    Log.w(TAG, buildLogString(i, name) + "; <not found>");
+                } catch (RuntimeException e) {
+                    Log.e(TAG, buildLogString(i, name) + "; --- " + e.getMessage());
+                }
+            }
+
+            return resources;
+        }
+    }
+
+    class Res {
+        private int id;
+        private String name;
+
+        public Res(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+    }
 }
