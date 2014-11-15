e41c6a4817adfe00a5ddfeaeb5a5dfba8d27b275
bearND <bsitzmann@wikimedia.org>
2014-11-15 16:42:38 -0700

    Full text search results in correct order
    Also avoiding an extra request per full text search to get the thumbnail URLs.
    This should make the thumbnails appear much faster.
    
    Using similar request as iOS app.
    
    Bug: 72781
    Change-Id: Ic6742bd8ae796c1c0ee4879e22cae0063efd564b

--- wikipedia/src/main/java/org/wikipedia/search/FullSearchArticlesTask.java
+++ wikipedia/src/main/java/org/wikipedia/search/FullSearchArticlesTask.java
@@ -3,19 +3,24 @@ package org.wikipedia.search;
 import org.wikipedia.ApiTask;
 import org.wikipedia.PageTitle;
 import org.wikipedia.Site;
+import org.wikipedia.WikipediaApp;
 import org.mediawiki.api.json.Api;
 import org.mediawiki.api.json.ApiException;
 import org.mediawiki.api.json.ApiResult;
 import org.mediawiki.api.json.RequestBuilder;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 public class FullSearchArticlesTask extends ApiTask<FullSearchArticlesTask.FullSearchResults> {
-    private static final String NUM_RESULTS_PER_QUERY = "12";
+    private static final int MAX_RESULTS = 12;
+    private static final String NUM_RESULTS_PER_QUERY = Integer.toString(MAX_RESULTS);
 
     private final Site site;
     private final String searchTerm;
@@ -30,17 +35,29 @@ public class FullSearchArticlesTask extends ApiTask<FullSearchArticlesTask.FullS
 
     @Override
     public RequestBuilder buildRequest(Api api) {
+        final String offset = Integer.toString(continueOffset);
         return api.action("query")
-                .param("prop", "pageprops")
+                .param("prop", "pageprops|pageimages")
                 .param("ppprop", "wikibase_item") // only interested in wikibase_item
                 .param("generator", "search")
                 .param("gsrsearch", searchTerm)
                 .param("gsrnamespace", "0")
                 .param("gsrwhat", "text")
-                .param("gsrinfo", "totalhits|suggestion")
+                .param("gsrinfo", "")
                 .param("gsrprop", "redirecttitle")
-                .param("gsroffset", Integer.toString(continueOffset))
-                .param("gsrlimit", NUM_RESULTS_PER_QUERY);
+                .param("gsroffset", offset)
+                .param("gsrlimit", NUM_RESULTS_PER_QUERY)
+                .param("list", "search") // for correct order
+                .param("srsearch", searchTerm)
+                .param("srnamespace", "0")
+                .param("srwhat", "text")
+                .param("srinfo", "suggestion")
+                .param("srprop", "")
+                .param("sroffset", offset)
+                .param("srlimit", NUM_RESULTS_PER_QUERY)
+                .param("piprop", "thumbnail") // for thumbnail URLs
+                .param("pithumbsize", Integer.toString(WikipediaApp.PREFERRED_THUMB_SIZE))
+                .param("pilimit", NUM_RESULTS_PER_QUERY);
     }
 
     @Override
@@ -77,7 +94,19 @@ public class FullSearchArticlesTask extends ApiTask<FullSearchArticlesTask.FullS
             return emptyResults();
         }
 
-        ArrayList<FullSearchResult> resultList = new ArrayList<FullSearchResult>();
+        /*
+        So here's what we're doing here:
+        We're requesting two sets of results with our API query. They both contain the same titles,
+        but in different orders.  The results given by "list=search" give us the results in
+        the correct order, but with no thumbnails or wikidata ID. The results given by "generator=search"
+        give the results in the wrong order, but with thumbnails and wikidata IDs!
+        So, all we have to do is use the first list, and correlate the titles with the second list to
+        extract the thumbnails. Unfortunately, the search generator only gives us titles and not pageids.
+        This is why we need a Map of titles to results.
+        */
+
+        // build a map of full result objects
+        Map<String, FullSearchResult> map = new HashMap<String, FullSearchResult>(MAX_RESULTS + 1, 1.0f);
         Iterator<String> keys = pages.keys();
         while (keys.hasNext()) {
             String key = keys.next();
@@ -89,7 +118,20 @@ public class FullSearchArticlesTask extends ApiTask<FullSearchArticlesTask.FullS
                 JSONObject pageProps = pageData.getJSONObject("pageprops");
                 wikiBaseId = pageProps.optString("wikibase_item", null);
             }
-            resultList.add(new FullSearchResult(pageTitle, wikiBaseId));
+            String thumbUrl = null;
+            if (pageData.has("thumbnail")) {
+                JSONObject thumbnail = pageData.getJSONObject("thumbnail");
+                thumbUrl = thumbnail.optString("source", null);
+            }
+
+            map.put(titleString, new FullSearchResult(pageTitle, thumbUrl, wikiBaseId));
+        }
+
+        // put them into the list in the correct order
+        ArrayList<FullSearchResult> resultList = new ArrayList<FullSearchResult>();
+        JSONArray search = queryResult.getJSONArray("search");
+        for (int i = 0; i < search.length(); i++) {
+            resultList.add(map.get(search.getJSONObject(i).getString("title")));
         }
 
         return new FullSearchResults(resultList, newOffset, suggestion);
