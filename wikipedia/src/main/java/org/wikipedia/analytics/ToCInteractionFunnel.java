d683ba8e01a60c23d31bb1fb8144b0178be38b62
Deskana <dgarry@wikimedia.org>
2015-01-19 20:53:03 -0800

    Add 1:100 sampling rate for table of contents interaction.
    EventLogging has a pretty strict limit for how many events it can write to its
    database per second. Since the ToC data isn't being actively used right now
    and is one of our biggest schemas in event throughput, this patch adds 1:100
    sampling to the ToC data to ease the load on EventLogging.
    
    Change-Id: Ib4ca1f570fd18aa288ad003b5759b42541d8097f

--- wikipedia/src/main/java/org/wikipedia/analytics/ToCInteractionFunnel.java
+++ wikipedia/src/main/java/org/wikipedia/analytics/ToCInteractionFunnel.java
@@ -7,7 +7,7 @@ import org.wikipedia.WikipediaApp;
 
 public class ToCInteractionFunnel extends Funnel {
     private static final String SCHEMA_NAME = "MobileWikiAppToCInteraction";
-    private static final int REV_ID = 10375484;
+    private static final int REV_ID = 11014396;
 
     private final String appInstallID;
     private final Site site;
@@ -31,7 +31,25 @@ public class ToCInteractionFunnel extends Funnel {
         return eventData;
     }
 
-    protected void log(Object... params) { super.log(site, params); }
+    protected void log(Object... params) {
+        final int defaultSampleRate = 100;
+
+        //get our sampling rate from remote config
+        int sampleRate = WikipediaApp.getInstance().getRemoteConfig().getConfig()
+                .optInt("tocLogSampleRate", defaultSampleRate);
+
+        if (sampleRate != 0) {
+            //take the last 4 hex digits of the uuid, modulo the sampling coefficient.
+            //if the result is 0, then we're one of the Chosen.
+            final int uuidSubstrLen = 4;
+            final int hexBase = 16;
+            boolean chosen = Integer.parseInt(appInstallID.substring(appInstallID.length() - uuidSubstrLen), hexBase) % sampleRate == 0;
+
+            if (chosen) {
+                super.log(site, params);
+            }
+        }
+    }
 
     public void logOpen() {
         log(
