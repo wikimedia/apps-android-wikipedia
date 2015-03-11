eaeea585726c6b810ad3e01111e764ac3ebdbd37
dbrant <dbrant@wikimedia.org>
2015-03-10 22:27:03 -0400

    Patch support-v4 crash in SwipeRefreshLayout.
    This seems to have been fixed already in support-v4, but not released yet.
    
    We're already extending SwipeRefreshLayout ourselves, so why not.
    
    Bug: T88904
    Change-Id: I9695741bc289e17c36ab7ba3fe97f27995859cf3

--- wikipedia/src/main/java/org/wikipedia/views/SwipeRefreshLayoutWithScroll.java
+++ wikipedia/src/main/java/org/wikipedia/views/SwipeRefreshLayoutWithScroll.java
@@ -3,6 +3,7 @@ package org.wikipedia.views;
 import android.content.Context;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class SwipeRefreshLayoutWithScroll extends SwipeRefreshLayout {
@@ -26,5 +27,21 @@ public class SwipeRefreshLayoutWithScroll extends SwipeRefreshLayout {
             return false;
         }
         return scrollableView.getScrollY() > 0;
+    }
+
+    /**
+     * TODO: Remove this override when it's fixed in support-v4.
+     * https://phabricator.wikimedia.org/T88904
+     *
+     * This seems to have been fixed in the Support library, but not released yet:
+     * https://github.com/android/platform_frameworks_support/commit/07a4db40e79aae23694b205f99b013ee2e4f2307
+     */
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        try {
+            return super.onTouchEvent(event);
+        } catch (RuntimeException e) {
+            return false;
+        }
     }
 }
