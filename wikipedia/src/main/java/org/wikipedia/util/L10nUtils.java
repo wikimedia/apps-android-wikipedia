573e13d0f64520121846756556bba37e8c2bfc76
bearND <bsitzmann@wikimedia.org>
2015-04-06 21:00:20 -0600

    Add private L10nUtils constructor
    to please checkstyle
    
    Change-Id: I2fbc04d9368fa493d0f0af2267dc3efdb01f4135

--- wikipedia/src/main/java/org/wikipedia/util/L10nUtils.java
+++ wikipedia/src/main/java/org/wikipedia/util/L10nUtils.java
@@ -8,6 +8,9 @@ import org.wikipedia.R;
  * A collection of localization `related methods.
  */
 public class L10nUtils {
+    private L10nUtils() {
+    }
+
     public static boolean canLangUseImageForWikipediaWordmark(Context context) {
         return "<big>W</big>IKIPEDI<big>A</big>".equals(context.getString(R.string.wp_stylized));
     }
