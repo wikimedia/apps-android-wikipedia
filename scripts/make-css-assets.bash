6f17499d37b7277828fe5d3f49d4bc04c91aae72
Dmitry Brant <dbrant@wikimedia.org>
2015-05-24 23:19:08 +0200

    Update style fetcher script to work with MobileApp extension update.
    Also includes a refresh of styles from upstream MFE.
    
    Refer to https://gerrit.wikimedia.org/r/213431
    
    Change-Id: I73735d2142faec11d90dc42251c1b35db558e7a1

--- scripts/make-css-assets.bash
+++ scripts/make-css-assets.bash
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-PREFIX="http://bits.beta.wmflabs.org/en.wikipedia.beta.wmflabs.org"
-#PREFIX="http://127.0.0.1:8080/w"
+#PREFIX="http://bits.beta.wmflabs.org/en.wikipedia.beta.wmflabs.org"
+PREFIX="http://127.0.0.1:8080/w"
 BASE_PATH="`dirname $0`/.."
 
-wget "$PREFIX/load.php?debug=true&lang=en&modules=mobile.app.pagestyles.android&only=styles&skin=vector&version=&*" -O "$BASE_PATH/wikipedia/assets/styles.css"
-wget "$PREFIX/load.php?debug=true&lang=en&modules=mobile.app.preview&only=styles&skin=vector&version=&*" -O "$BASE_PATH/wikipedia/assets/preview.css"
+wget "$PREFIX/load.php?debug=true&lang=en&modules=skins.minerva.base.reset|skins.minerva.content.styles|mobile.app.pagestyles.android&only=styles&skin=vector&version=&*" -O "$BASE_PATH/wikipedia/assets/styles.css"
+wget "$PREFIX/load.php?debug=true&lang=en&modules=skins.minerva.base.reset|skins.minerva.content.styles|mobile.app.preview&only=styles&skin=vector&version=&*" -O "$BASE_PATH/wikipedia/assets/preview.css"
 wget "$PREFIX/load.php?debug=true&lang=en&modules=mobile.app.pagestyles.android.night&only=styles&skin=vector&version=&*" -O "$BASE_PATH/wikipedia/assets/night.css"
