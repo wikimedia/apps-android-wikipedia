98193742d9371090980a15ec4f5f55731778ccf6
bearND <bsitzmann@wikimedia.org>
2015-06-15 17:40:56 -0600

    Hygiene: use DimenUtil when calculating display height and density
    Also renamed displayHeight to displayHeightDp since that value is in dp.
    
    Change-Id: Ife84b0181f3ef729c0343ffec7ea721b0b75bd8b

--- wikipedia/src/main/java/org/wikipedia/util/DimenUtil.java
+++ wikipedia/src/main/java/org/wikipedia/util/DimenUtil.java
@@ -1,8 +1,12 @@
 package org.wikipedia.util;
 
+import android.annotation.TargetApi;
 import android.content.res.Resources;
+import android.graphics.Point;
+import android.os.Build;
 import android.util.DisplayMetrics;
 import android.util.TypedValue;
+import android.view.Display;
 
 public final class DimenUtil {
     public static float dpToPx(float dp) {
@@ -21,12 +25,39 @@ public final class DimenUtil {
         return Math.round(pxToDp(px));
     }
 
-    private static float getDensityScalar() {
+    public static float getDensityScalar() {
         return getDisplayMetrics().density;
     }
 
     private static DisplayMetrics getDisplayMetrics() {
         return Resources.getSystem().getDisplayMetrics();
+    }
+
+    /**
+     * Gets the screen height in pixels.
+     * There is a simpler DimenUtil.getDisplayMetrics()#heightPixels but that doesn't work on 2.3
+     * devices since it always returns 0.
+     * @param display the screen to measure
+     * @return screen height in pixels
+     */
+    public static int getDisplayHeight(Display display) {
+        if (ApiUtil.hasHoneyCombMr2()) {
+            return getDisplayHeightNewer(display);
+        } else {
+            return getDisplayHeightOlder(display);
+        }
+    }
+
+    private static int getDisplayHeightOlder(Display display) {
+        //noinspection deprecation
+        return display.getHeight();
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private static int getDisplayHeightNewer(Display display) {
+        Point size = new Point();
+        display.getSize(size);
+        return size.y;
     }
 
     private DimenUtil() {
