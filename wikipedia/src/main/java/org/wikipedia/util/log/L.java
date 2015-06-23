d2122439af101fcf5fff9ee9967de057142cfdd1
Stephen Niedzielski <stephen@niedzielski.com>
2015-06-23 14:54:11 -0600

    Hygiene: accept throwable instead of exception
    Change-Id: Iebde762ee7026b52cfe64af32c8917b6585f5724

--- wikipedia/src/main/java/org/wikipedia/util/log/L.java
+++ wikipedia/src/main/java/org/wikipedia/util/log/L.java
@@ -6,36 +6,36 @@ import android.util.Log;
 public final class L {
     private static final LogLevel LEVEL_V = new LogLevel() {
         @Override
-        public void logLevel(String tag, String msg, Exception e) {
-            Log.v(tag, msg, e);
+        public void logLevel(String tag, String msg, Throwable t) {
+            Log.v(tag, msg, t);
         }
     };
 
     private static final LogLevel LEVEL_D = new LogLevel() {
         @Override
-        public void logLevel(String tag, String msg, Exception e) {
-            Log.d(tag, msg, e);
+        public void logLevel(String tag, String msg, Throwable t) {
+            Log.d(tag, msg, t);
         }
     };
 
     private static final LogLevel LEVEL_I = new LogLevel() {
         @Override
-        public void logLevel(String tag, String msg, Exception e) {
-            Log.i(tag, msg, e);
+        public void logLevel(String tag, String msg, Throwable t) {
+            Log.i(tag, msg, t);
         }
     };
 
     private static final LogLevel LEVEL_W = new LogLevel() {
         @Override
-        public void logLevel(String tag, String msg, Exception e) {
-            Log.w(tag, msg, e);
+        public void logLevel(String tag, String msg, Throwable t) {
+            Log.w(tag, msg, t);
         }
     };
 
     private static final LogLevel LEVEL_E = new LogLevel() {
         @Override
-        public void logLevel(String tag, String msg, Exception e) {
-            Log.e(tag, msg, e);
+        public void logLevel(String tag, String msg, Throwable t) {
+            Log.e(tag, msg, t);
         }
     };
 
@@ -59,24 +59,24 @@ public final class L {
         LEVEL_E.log(msg, null);
     }
 
-    public static void v(Exception e) {
-        LEVEL_V.log(null, e);
+    public static void v(Throwable t) {
+        LEVEL_V.log(null, t);
     }
 
-    public static void d(Exception e) {
-        LEVEL_D.log(null, e);
+    public static void d(Throwable t) {
+        LEVEL_D.log(null, t);
     }
 
-    public static void i(Exception e) {
-        LEVEL_I.log(null, e);
+    public static void i(Throwable t) {
+        LEVEL_I.log(null, t);
     }
 
-    public static void w(Exception e) {
-        LEVEL_W.log(null, e);
+    public static void w(Throwable t) {
+        LEVEL_W.log(null, t);
     }
 
-    public static void e(Exception e) {
-        LEVEL_E.log(null, e);
+    public static void e(Throwable t) {
+        LEVEL_E.log(null, t);
     }
 
     public static void v(CharSequence msg, Exception e) {
@@ -102,11 +102,11 @@ public final class L {
     private abstract static class LogLevel {
         private static final int STACK_INDEX = 4;
 
-        public abstract void logLevel(String tag, String msg, Exception e);
+        public abstract void logLevel(String tag, String msg, Throwable t);
 
-        public final void log(CharSequence msg, Exception e) {
+        public final void log(CharSequence msg, Throwable t) {
             StackTraceElement element = Thread.currentThread().getStackTrace()[STACK_INDEX];
-            logLevel(element.getClassName(), stackTraceElementToMessagePrefix(element) + msg, e);
+            logLevel(element.getClassName(), stackTraceElementToMessagePrefix(element) + msg, t);
         }
 
         private String stackTraceElementToMessagePrefix(StackTraceElement element) {
