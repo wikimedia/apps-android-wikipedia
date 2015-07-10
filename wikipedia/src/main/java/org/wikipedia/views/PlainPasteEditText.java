2c5a3e442363fd421296ea2bc0c5381f20cb3f20
dbrant <dbrant@wikimedia.org>
2015-07-10 16:22:03 -0400

    Use TextInputLayout in Login and CreateAccount activities.
    Wrapped EditText's in the Login and CreateAccount activities in
    TextInputLayout, as well as the EditText in the Captcha entry view, and
    the edit preview summary.
    
    Also made PlainPasteEditText inherit from AppCompatEditText, to make sure
    that any styling that we apply to EditText in the future will be applied
    properly by AppCompat.
    
    Note: did *not* modify the EditText in the History and Saved Pages
    fragments, since they're being affected by a known bug in AppCompat:
    https://code.google.com/p/android/issues/detail?id=175228
    Once the above issue is fixed, we can finish those updates.
    
    Bug: T102549
    Change-Id: I72b1acd858c47ecb20dfc0b7b269c11aad5a99a9

--- wikipedia/src/main/java/org/wikipedia/views/PlainPasteEditText.java
+++ wikipedia/src/main/java/org/wikipedia/views/PlainPasteEditText.java
@@ -1,15 +1,17 @@
 package org.wikipedia.views;
 
+import android.annotation.TargetApi;
 import android.content.ClipData;
 import android.content.ClipDescription;
 import android.content.Context;
 import android.content.ClipboardManager;
+import android.os.Build;
+import android.support.v7.widget.AppCompatEditText;
 import android.util.AttributeSet;
-import android.widget.EditText;
 
 import org.wikipedia.util.ApiUtil;
 
-public class PlainPasteEditText extends EditText {
+public class PlainPasteEditText extends AppCompatEditText {
     private Context context;
 
     public PlainPasteEditText(Context context) {
@@ -34,24 +36,7 @@ public class PlainPasteEditText extends EditText {
                 break;
             case android.R.id.paste:
                 if (ApiUtil.hasHoneyComb()) {
-                    // Do not allow pasting of formatted text!
-                    // We do this by intercepting the clipboard and temporarily replacing its
-                    // contents with plain text.
-                    ClipboardManager clipboard = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
-                    if (clipboard.hasPrimaryClip()) {
-                        ClipData oldClipData = clipboard.getPrimaryClip();
-                        String lastClipText = oldClipData.getItemAt(oldClipData.getItemCount() - 1).coerceToText(context).toString();
-                        // create a new text-only clip data
-                        ClipData newClipData = new ClipData(new ClipDescription("text", new String[]{ClipDescription.MIMETYPE_TEXT_PLAIN}),
-                                new ClipData.Item(lastClipText));
-                        // temporarily set the new clip data as the primary
-                        clipboard.setPrimaryClip(newClipData);
-                        // execute the paste!
-                        boolean ret = super.onTextContextMenuItem(id);
-                        // restore the clip data back to the old one.
-                        clipboard.setPrimaryClip(oldClipData);
-                        return ret;
-                    }
+                    return pastePlainTextFromClipboard();
                 }
                 break;
             case android.R.id.copy:
@@ -59,6 +44,29 @@ public class PlainPasteEditText extends EditText {
             default:
         }
         return super.onTextContextMenuItem(id);
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    private boolean pastePlainTextFromClipboard() {
+        // Do not allow pasting of formatted text!
+        // We do this by intercepting the clipboard and temporarily replacing its
+        // contents with plain text.
+        boolean ret = false;
+        ClipboardManager clipboard = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
+        if (clipboard.hasPrimaryClip()) {
+            ClipData oldClipData = clipboard.getPrimaryClip();
+            String lastClipText = oldClipData.getItemAt(oldClipData.getItemCount() - 1).coerceToText(context).toString();
+            // create a new text-only clip data
+            ClipData newClipData = new ClipData(new ClipDescription("text", new String[]{ClipDescription.MIMETYPE_TEXT_PLAIN}),
+                                                new ClipData.Item(lastClipText));
+            // temporarily set the new clip data as the primary
+            clipboard.setPrimaryClip(newClipData);
+            // execute the paste!
+            ret = super.onTextContextMenuItem(android.R.id.paste);
+            // restore the clip data back to the old one.
+            clipboard.setPrimaryClip(oldClipData);
+        }
+        return ret;
     }
 
 }
