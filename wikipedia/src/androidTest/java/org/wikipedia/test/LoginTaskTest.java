56932f32c44e6d1ebd651e237e745ff1189fe0be
Stephen Niedzielski <stephen@niedzielski.com>
2015-07-27 19:44:02 -0600

    Add support for release tests
    * Flip testBuildType to debug or release depending on command line
      tasks. This change adds support for running tests against release
      builds. Previously, it was assumed that connectedAndroidTest*Release
      existed which caused certain tasks to fail to execute, namely the
      tasks task. This change fixes that too.
    * Don't suppress LoginTaskTest. @Suppress is currently ProGuarded away
      and we can fix the test by specifying a dummy account. This patch
      accomplishes the latter.
    * Publicize package protected method in ImageUrlMap to work around
      ProGuard stripping.
    
    Change-Id: I1dd6e0b01012e28a87967ba7d215114cb83d0099

--- wikipedia/src/androidTest/java/org/wikipedia/test/LoginTaskTest.java
+++ wikipedia/src/androidTest/java/org/wikipedia/test/LoginTaskTest.java
@@ -1,9 +1,7 @@
-
 package org.wikipedia.test;
 
 import android.content.Intent;
 import android.test.ActivityUnitTestCase;
-import android.test.suitebuilder.annotation.Suppress;
 
 import org.wikipedia.Site;
 import org.wikipedia.WikipediaApp;
@@ -21,23 +19,12 @@ public class LoginTaskTest extends ActivityUnitTestCase<TestDummyActivity> {
         super(TestDummyActivity.class);
     }
 
-    /**
-     * Don't run automatically since this requires some setup, see method body.
-     * Remove the @Suppress annotation when we've implemented a way to configure the user
-     * name/password without having to store a file inside the Git repo.
-     * This makes this test a hassle.
-     */
-    @Suppress
     public void testLogin() throws Throwable {
         startActivity(new Intent(), null, null);
         final Site testWiki = new Site("test.wikipedia.org");
         final String username = getInstrumentation().getContext().getString(R.string.test_username);
         final String password = getInstrumentation().getContext().getString(R.string.test_password);
         final WikipediaApp app = (WikipediaApp)getInstrumentation().getTargetContext().getApplicationContext();
-
-        if (username.equals("Insert-your-test-username-here")) {
-            throw new RuntimeException("Use a custom username and password in wikipedia-it/res/values/credentials.xml");
-        }
 
         final CountDownLatch completionLatch = new CountDownLatch(1);
         runTestOnUiThread(new Runnable() {
@@ -69,4 +56,3 @@ public class LoginTaskTest extends ActivityUnitTestCase<TestDummyActivity> {
         assertTrue(completionLatch.await(TASK_COMPLETION_TIMEOUT, TimeUnit.MILLISECONDS));
     }
 }
-
