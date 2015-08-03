b179c28fbd0f939aa2b34ed1a87a1a72010043e9
Stephen Niedzielski <stephen@niedzielski.com>
2015-08-03 17:21:24 -0600

    Add crash button to dev settings
    Add button for intentionally crashing the app to the developer settings
    screen. This button is used to test the crash handler.
    
    Change-Id: I00b99a9b724acfe2d828b09a2e1e55f22886184f

--- app/src/main/java/org/wikipedia/settings/DeveloperSettingsPreferenceLoader.java
+++ app/src/main/java/org/wikipedia/settings/DeveloperSettingsPreferenceLoader.java
@@ -1,5 +1,9 @@
 package org.wikipedia.settings;
 
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 import android.support.annotation.NonNull;
@@ -7,16 +11,48 @@ import android.support.annotation.NonNull;
 import org.wikipedia.R;
 
 /*package*/ class DeveloperSettingsPreferenceLoader extends BasePreferenceLoader {
+    @NonNull private Context context;
+
     /*package*/ DeveloperSettingsPreferenceLoader(@NonNull PreferenceActivity activity) {
         super(activity);
+        init(activity);
     }
 
-    /*package*/ DeveloperSettingsPreferenceLoader(@NonNull PreferenceFragment fragment) {
+    /*package*/
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    DeveloperSettingsPreferenceLoader(@NonNull PreferenceFragment fragment) {
         super(fragment);
+        init(fragment.getActivity());
     }
 
     @Override
     public void loadPreferences() {
         loadPreferences(R.xml.developer_preferences);
+        setupCrashButton(findPreference(getCrashButtonKey()));
+    }
+
+    private void setupCrashButton(Preference crashButton) {
+        crashButton.setOnPreferenceClickListener(buildCrashButtonClickListener());
+    }
+
+    private Preference.OnPreferenceClickListener buildCrashButtonClickListener() {
+        return new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                throw new NullPointerException(buildCrashMessage());
+            }
+        };
+    }
+
+    private String buildCrashMessage() {
+        return "Crash test from " + getClass().getName();
+    }
+
+    private String getCrashButtonKey() {
+        return context.getString(R.string.preferences_developer_crash_key);
+    }
+
+    private void init(@NonNull Context context) {
+        this.context = context.getApplicationContext();
     }
 }
