b8e562a51e6dd876a90c4bac7787f2a79ad9bab9
Stephen Niedzielski <stephen@niedzielski.com>
2015-08-03 15:10:31 -0600

    Catch Gson unmarshalling errors and report them
    - Catch errors during tab unmarshalling and report them. For non-prod
      builds, the user is prompted via ACRA. For prod builds, exceptions are
      printed to logcat.
    - Light clean up of Gson classes.
    
    Gson can throw more than JsonSyntaxException. This patch adds a safety
    mechanism for catching any Exception produce during unmarshalling of
    tabs and reports it.
    
    Change-Id: I06bf864e8e4dbd9d9c4ff4bc725edd81274ec120

--- wikipedia/src/main/java/org/wikipedia/data/GsonUnmarshaller.java
+++ wikipedia/src/main/java/org/wikipedia/data/GsonUnmarshaller.java
@@ -1,6 +1,7 @@
 package org.wikipedia.data;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -8,25 +9,25 @@ import com.google.gson.reflect.TypeToken;
 import java.util.Collection;
 
 public final class GsonUnmarshaller {
-    private static final Gson DEFAULT_GSON = new Gson();
-
     /** @return Unmarshalled object. */
-    public static <T> T unmarshal(Class<T> clazz, @NonNull String json) {
-        return unmarshal(DEFAULT_GSON, clazz, json);
+    public static <T> T unmarshal(Class<T> clazz, @Nullable String json) {
+        return unmarshal(GsonUtil.getDefaultGson(), clazz, json);
     }
 
     /** @return Unmarshalled collection of objects. */
-    public static <T extends Collection<?>> T unmarshal(TypeToken<T> typeToken, @NonNull String json) {
-        return unmarshal(DEFAULT_GSON, typeToken, json);
+    public static <T extends Collection<?>> T unmarshal(TypeToken<T> typeToken, @Nullable String json) {
+        return unmarshal(GsonUtil.getDefaultGson(), typeToken, json);
     }
 
     /** @return Unmarshalled object. */
-    public static <T> T unmarshal(@NonNull Gson gson, Class<T> clazz, @NonNull String json) {
+    public static <T> T unmarshal(@NonNull Gson gson, Class<T> clazz, @Nullable String json) {
         return gson.fromJson(json, clazz);
     }
 
     /** @return Unmarshalled collection of objects. */
-    public static <T extends Collection<?>> T unmarshal(@NonNull Gson gson, TypeToken<T> typeToken, @NonNull String json) {
+    public static <T extends Collection<?>> T unmarshal(@NonNull Gson gson,
+                                                        TypeToken<T> typeToken,
+                                                        @Nullable String json) {
         // From the manual: "Fairly hideous...  Unfortunately, no way to get around this in Java".
         return gson.fromJson(json, typeToken.getType());
     }
