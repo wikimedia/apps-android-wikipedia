7be7f59b98862092d8a7c441122cd690df115add
bearND <bsitzmann@wikimedia.org>
2015-11-16 22:15:01 -0700

    Rename RESTBase routes to mobile-sections*
    The service has dropped the "html" from the name of the
    mobile-html-section* routes.
    
    Bug: T118187
    Change-Id: I452e168343cda0c63244ea19514b511c49bcc8de

--- app/src/main/java/org/wikipedia/server/restbase/RbPageService.java
+++ app/src/main/java/org/wikipedia/server/restbase/RbPageService.java
@@ -102,7 +102,7 @@ public class RbPageService implements PageService {
          * @param noImages add the noimages flag to the request if true
          * @param cb a Retrofit callback which provides the populated RbPageLead object in #success
          */
-        @GET("/page/mobile-html-sections-lead/{title}")
+        @GET("/page/mobile-sections-lead/{title}")
         void pageLead(@Path("title") String title, @Query("noimages") Boolean noImages,
                       Callback<RbPageLead> cb);
 
@@ -113,7 +113,7 @@ public class RbPageService implements PageService {
          * @param noImages add the noimages flag to the request if true
          * @param cb a Retrofit callback which provides the populated RbPageRemaining object in #success
          */
-        @GET("/page/mobile-html-sections-remaining/{title}")
+        @GET("/page/mobile-sections-remaining/{title}")
         void pageRemaining(@Path("title") String title, @Query("noimages") Boolean noImages,
                            Callback<RbPageRemaining> cb);
 
@@ -125,7 +125,7 @@ public class RbPageService implements PageService {
          * @param noImages add the noimages flag to the request if true
          * @param cb a Retrofit callback which provides the populated RbPageCombo object in #success
          */
-        @GET("/page/mobile-html-sections/{title}")
+        @GET("/page/mobile-sections/{title}")
         void pageCombo(@Path("title") String title, @Query("noimages") Boolean noImages,
                        Callback<RbPageCombo> cb);
     }
