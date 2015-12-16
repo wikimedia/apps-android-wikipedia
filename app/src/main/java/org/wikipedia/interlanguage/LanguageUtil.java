8e9125be725e25ad580d4bcd0b033455d409e53c
bearND <bsitzmann@wikimedia.org>
2015-12-16 13:48:20 -0700

    Show Norwegian when device language is Norwegian
    Was falling back to English before.
    
    Bug: T114042
    Change-Id: I08cb4c3a073709d334d8450e450a11c09bcb685e

--- app/src/main/java/org/wikipedia/interlanguage/LanguageUtil.java
+++ app/src/main/java/org/wikipedia/interlanguage/LanguageUtil.java
@@ -26,6 +26,8 @@ public final class LanguageUtil {
                 return "he"; // Hebrew
             case "in":
                 return "id"; // Indonesian
+            case "nb":
+                return "no"; // Norsk bokmål T114042
             case "ji":
                 return "yi"; // Yiddish
             case "zh":
