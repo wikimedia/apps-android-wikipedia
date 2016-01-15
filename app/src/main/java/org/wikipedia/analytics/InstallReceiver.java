026b8ed03996d0683c62ff4d301a7181055d19b1
Stephen Niedzielski <stephen@niedzielski.com>
2016-01-15 16:23:02 -0700

    Retain preload channel ID
    We now keep the same package ID for OEM installs as the Play Store. This
    is good for users and developers because everyone gets the same apk when
    updates are made. This is bad for trackers because the preload channel
    ID, a property of the original preloaded apk and different for upgraded
    apks, was lost as soon as the phone was connected to the internet and
    upgrades are downloaded. We now record the channel ID on first boot.
    
    Bug: T115396
    Change-Id: Iec363ee4b011dd70e457dcea733fc21551244def

--- app/src/main/java/org/wikipedia/analytics/InstallReceiver.java
+++ app/src/main/java/org/wikipedia/analytics/InstallReceiver.java
@@ -2,17 +2,41 @@ package org.wikipedia.analytics;
 
 import org.wikipedia.WikipediaApp;
 import org.wikipedia.page.PageActivity;
+import org.wikipedia.util.ReleaseUtil;
 import org.wikipedia.util.ShareUtil;
 import org.wikipedia.util.log.L;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.text.TextUtils;
 
-public final class InstallReferrerReceiver extends BroadcastReceiver {
+public final class InstallReceiver extends BroadcastReceiver {
+    private static final String BOOT_ACTION = "android.intent.action.BOOT_COMPLETED";
     private static final String INSTALL_ACTION = "com.android.vending.INSTALL_REFERRER";
     private static final String REFERRER_KEY = "referrer";
+
+    public void onReceive(Context ctx, Intent intent) {
+        String action = intent.getAction();
+        switch (action) {
+            case INSTALL_ACTION:
+                // for play store devices only:
+                // * invoke the receiver and open the page:
+                //     `adb shell am broadcast -a com.android.vending.INSTALL_REFERRER -n org.wikipedia.dev/org.wikipedia.analytics.InstallReceiver --es "referrer" "referrer_url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FWombat&campaign_id=foo&install_id=bar"`
+                // * invoke the receiver but don't open the app (bad url):
+                //     `adb shell am broadcast -a com.android.vending.INSTALL_REFERRER -n org.wikipedia.dev/org.wikipedia.analytics.InstallReceiver --es "referrer" "referrer_url=gibberish&campaign_id=foo&install_id=bar"`
+                installReferrer(ctx, intent);
+                break;
+            case BOOT_ACTION:
+                // `adb shell am broadcast -a android.intent.action.BOOT_COMPLETED`
+                recordChannelId(ctx);
+                break;
+            default:
+                L.d("action=" + action);
+                break;
+        }
+    }
 
     /**
      * Receives an Intent from the Play Store app upon first launch after the app has been
@@ -26,10 +50,10 @@ public final class InstallReferrerReceiver extends BroadcastReceiver {
      * @param ctx Context in which this intent is received.
      * @param intent Intent that contains referrer data from the Play Store.
      */
-    public void onReceive(Context ctx, Intent intent) {
+    private void installReferrer(@NonNull Context ctx, @NonNull Intent intent) {
         String referrerStr = intent.getStringExtra(REFERRER_KEY);
         L.d("Received install referrer: " + referrerStr);
-        if (!INSTALL_ACTION.equals(intent.getAction()) || TextUtils.isEmpty(referrerStr)) {
+        if (TextUtils.isEmpty(referrerStr)) {
             return;
         }
 
@@ -55,6 +79,11 @@ public final class InstallReferrerReceiver extends BroadcastReceiver {
         if (!TextUtils.isEmpty(refUrl) && ShareUtil.canOpenUrlInApp(ctx, refUrl)) {
             openPageFromUrl(ctx, refUrl);
         }
+    }
+
+    private void recordChannelId(@NonNull Context ctx) {
+        String channel = ReleaseUtil.getChannel(ctx);
+        L.v("channel=" + channel);
     }
 
     private void openPageFromUrl(Context context, String url) {
