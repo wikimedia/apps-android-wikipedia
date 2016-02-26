f38dfa3d0e0df6421f92c8caee95a1e87b6111e2
Paladox <thomasmulhall410@yahoo.com>
2016-02-26 19:29:08 +0000

    Update npm configuations to be able to run npm install
    This allows us to run grunt from inside www/ instead of having to move it
    to parent directory.
    
    Also update packages
    
    Update grunt to 0.4.5
    
    Update grunt-contrib-copy to 1.0.0
    
    Update grunt-contrib-jshint to 1.0.0
    
    Update browserify to 13.0.0
    
    Add package grunt-jsonlint for linting json files.
    
    Change-Id: Iaf286b1dc4af2948896d2dbd37697f06b440850d

--- www/Gruntfile.js
+++ www/Gruntfile.js
@@ -1,100 +1,116 @@
+/*jshint node:true */
 module.exports = function ( grunt ) {
-    var allScriptFiles = [
-        "js/bridge.js",
-        "js/main.js",
-        "js/utilities.js",
-        "js/transformer.js",
-        "js/transforms/*.js",
-        "js/transforms/service/*.js",
-        "js/actions.js",
-        "js/disambig.js",
-        "js/editaction.js",
-        "js/issues.js",
-        "js/loader.js",
-        "js/night.js",
-        "js/preview.js",
-        "js/rtlsupport.js",
-        "js/sections.js",
-        "tests/*.js"
-    ];
-    var allHTMLFiles = [
-        "index.html",
-        "tests/index.html"
-    ];
-    var distFolder = "../app/src/main/assets/";
+	var allScriptFiles = [
+		"js/bridge.js",
+		"js/main.js",
+		"js/utilities.js",
+		"js/transformer.js",
+		"js/transforms/*.js",
+		"js/transforms/service/*.js",
+		"js/actions.js",
+		"js/disambig.js",
+		"js/editaction.js",
+		"js/issues.js",
+		"js/loader.js",
+		"js/night.js",
+		"js/preview.js",
+		"js/rtlsupport.js",
+		"js/sections.js",
+		"tests/*.js"
+	];
+	var allHTMLFiles = [
+		"index.html",
+		"tests/index.html"
+	];
+	var distFolder = "../app/src/main/assets/";
 
-    grunt.loadNpmTasks( 'grunt-browserify' );
-    grunt.loadNpmTasks( 'grunt-contrib-jshint' );
-    grunt.loadNpmTasks( 'grunt-contrib-copy' );
-    grunt.loadNpmTasks( 'grunt-contrib-watch' );
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+	grunt.loadNpmTasks( 'grunt-jsonlint' );
+	grunt.loadNpmTasks( 'grunt-browserify' );
+	grunt.loadNpmTasks( 'grunt-contrib-copy' );
+	grunt.loadNpmTasks( 'grunt-contrib-watch' );
 
-    grunt.initConfig( {
-        pkg: grunt.file.readJSON( "package.json" ),
-        browserify: {
-            dist: {
-                files: {
-                    "bundle.js": [
-                        "js/bridge.js",
-                        "js/main.js",
-                        "js/utilities.js",
-                        "js/transformer.js",
-                        "js/transforms/*.js",
-                        "js/transforms/service/*.js",
-                        "js/actions.js",
-                        "js/disambig.js",
-                        "js/editaction.js",
-                        "js/issues.js",
-                        "js/loader.js",
-                        "js/night.js",
-                        "js/preview.js",
-                        "js/rtlsupport.js",
-                        "js/sections.js"
-                    ],
-                    "bundle-test.js": [
-                        "js/loader.js",
-                        "js/main.js",
-                        "js/bridge.js",
-                        "tests/*.js"
-                    ],
-                    "preview.js": [
-                        "js/loader.js",
-                        "js/bridge.js",
-                        "js/night.js",
-                        "js/actions.js",
-                        "js/preview.js",
-                        "js/rtlsupport.js",
-                        "js/util.js"
-                    ]
-                }
-            }
-        },
-        jshint: {
-            allFiles: allScriptFiles,
-            options: {
-                jshintrc: ".jshintrc"
-            }
-        },
-        copy: {
-            main: {
-                files: [
-                    // App files
-                    { src: [ "bundle.js", "index.html" ], dest: distFolder },
+	grunt.initConfig( {
+		pkg: grunt.file.readJSON( "package.json" ),
+		jshint: {
+			options: {
+				jshintrc: true
+			},
+			all: [
+				'../*.js',
+				'**/*.js',
+				'!node_modules/**',
+				'!lib/**'
+			]
+		},
+		jsonlint: {
+			all: [
+				'../**/*.json',
+				'**/*.json',
+				'!../www/node_modules/**',
+				'!node_modules/**'
+			]
+		},
+		browserify: {
+			dist: {
+				files: {
+					"bundle.js": [
+						"js/bridge.js",
+						"js/main.js",
+						"js/utilities.js",
+						"js/transformer.js",
+						"js/transforms/*.js",
+						"js/transforms/service/*.js",
+						"js/actions.js",
+						"js/disambig.js",
+						"js/editaction.js",
+						"js/issues.js",
+						"js/loader.js",
+						"js/night.js",
+						"js/preview.js",
+						"js/rtlsupport.js",
+						"js/sections.js"
+					],
+					"bundle-test.js": [
+						"js/loader.js",
+						"js/main.js",
+						"js/bridge.js",
+						"tests/*.js"
+					],
+					"preview.js": [
+						"js/loader.js",
+						"js/bridge.js",
+						"js/night.js",
+						"js/actions.js",
+						"js/preview.js",
+						"js/rtlsupport.js",
+						"js/util.js"
+					]
+				}
+			}
+		},
+		copy: {
+			main: {
+				files: [
+					// App files
+					{ src: [ "bundle.js", "index.html" ], dest: distFolder },
 
-                    // Test files
-                    { src: [ "bundle-test.js", "tests/index.html" ], dest: distFolder },
+					// Test files
+					{ src: [ "bundle-test.js", "tests/index.html" ], dest: distFolder },
 
-                    // Preview files
-                    { src: [ "preview.js", "preview.html" ], dest: distFolder },
-                ]
-            }
-        },
-        watch: {
-            scripts: {
-                files: allScriptFiles.concat( allHTMLFiles ),
-                tasks: [ "default" ]
-            }
-        }
-    } );
+					// Preview files
+					{ src: [ "preview.js", "preview.html" ], dest: distFolder },
+				]
+			}
+		},
+		watch: {
+			scripts: {
+				files: allScriptFiles.concat( allHTMLFiles ),
+				tasks: [ "default" ]
+			}
+		}
+	} );
 
-    grunt.registerTask( 'default', [ 'browserify', 'copy' ] );
+	grunt.registerTask( 'test', [ 'jshint', 'jsonlint', 'browserify', 'copy' ] );
+	grunt.registerTask( 'default', 'test' );
 };
