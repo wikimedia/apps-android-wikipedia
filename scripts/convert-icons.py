f7f8a4e24c5c2e10c30331d137302e160b5f2101
Marco Ippolito <maroloccio@gmail.com>
2016-04-12 07:50:22 +0200

    scripts/convert-icons.py clean-up
    Change-Id: I290f233a0bb9cc49d793f63f3401adef87dbb892

--- scripts/convert-icons.py
+++ scripts/convert-icons.py
@@ -1,96 +1,116 @@
-#!/usr/bin/env python2
-import os
-import sh
+#!/usr/bin/env python
+
 import sys
+import os
+import re
+import glob
+import inspect
+import sh
 
-from glob import glob
+TRACE = False
 
-DENSITIES = {
-    "mdpi": 1,
-    "hdpi": 1.5,
-    "xhdpi": 2,
-    "xxhdpi": 3,
-    "xxxhdpi": 4
-}
+DENSITIES = {'mdpi': 1, 'hdpi': 1.5, 'xhdpi': 2, 'xxhdpi': 3, 'xxxhdpi': 4}
 
-OUTPUT_PATH_PREFIX = os.path.abspath(os.path.join(os.path.dirname(__file__), "../app/src/main/res/"))
+OUTPUT_PATH_PREFIX = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                                  '../app/src/main/res/'))
 
 
-class ImagesBatch(object):
-    def __init__(self, path, filters):
-        self.dp = int(os.path.basename(path))
-        self.path = path
-        self.svgs = []
-        all_svgs = self.find_svg_files(path)
-        filtered_svgs = self.filter_filenames(all_svgs, filters)
-        self.svgs = self.abspath(filtered_svgs)
+def trace_value_of(variable_name):
+    """Format and output current value of named local variable."""
+    if not TRACE:
+        return
+    value = inspect.currentframe().f_back.f_locals[variable_name]
+    value_type = type(value)
+    if value_type in set([int, str]):
+        print('{}: {}'.format(variable_name, value))
+    elif value_type in set([list, set]) and value:
+        print(variable_name + ':')
+        print('\n'.join('- ' + x for x in value))
 
-    @staticmethod
-    def find_svg_files(path):
-        return [p for p in glob(os.path.join(path, "*.svg"))]
 
-    @staticmethod
-    def filter_filenames(all_svg_files, filters=None):
-        relative_svg_files = []
-        if filters:
-            for filter in filters:
-                if os.path.join(source_path, filter) in all_svg_files:
-                    relative_svg_files.append(os.path.join(source_path, filter))
+def validate_filter_argument(filter_argument):
+    if not filter_argument.endswith('.svg') or os.sep in filter_argument:
+        raise SystemExit('Only svg file names allowed in arguments.')
+    return filter_argument
+
+
+def export(dp, density, filepath, drawable):
+    noflip = '.nonspecific.' in filepath or filepath.endswith('.noflip.svg')
+    filename = os.path.basename(filepath)
+    new_filename = filename[:filename.index('.')] + '.png'
+    folder_path = os.path.join(OUTPUT_PATH_PREFIX, drawable)
+    sh.mkdir('-p', folder_path)
+    output_file_path = os.path.join(folder_path, new_filename)
+    output_precrush_path = output_file_path + '_'
+    px = int(DENSITIES[density] * dp)
+    sh.rsvg_convert(filepath, '-a', h=px, o=output_precrush_path)
+    sh.pngcrush('-q', '-reduce', output_precrush_path, output_file_path)
+    sh.rm(output_precrush_path)
+    return output_file_path, noflip
+
+
+def flop(density, (filepath, noflip)):
+    if noflip:
+        return
+    folder_name = os.path.join(OUTPUT_PATH_PREFIX, 'drawable-ldrtl-' + density)
+    output_file_path = os.path.join(folder_name, os.path.basename(filepath))
+    sh.mkdir('-p', folder_name)
+    sh.convert(filepath, '-flop', output_file_path)
+
+
+def convert(icon_path, svg_filters):
+    print('\n* icon_path: {}'.format(icon_path))
+
+    dp = int(os.path.basename(icon_path))
+    trace_value_of('dp')
+
+    svg_glob = glob.glob(os.path.join(icon_path, '*.svg'))
+    svg_files = ([x for x in svg_glob if os.path.basename(x) in svg_filters]
+                 if svg_filters else svg_glob)
+
+    if not svg_files:
+        return
+    print('converted:')
+    for svg_file in svg_files:
+        if '.nonspecific.' in svg_file:
+            export(dp, 'xxxhdpi', svg_file, 'drawable')
         else:
-            relative_svg_files = all_svg_files
-        return relative_svg_files
-
-    @staticmethod
-    def abspath(filenames):
-        output = []
-        for filename in filenames:
-            output.append(os.path.abspath(filename))
-        return output
-
-    def _do_export(self, density, input_path, drawable):
-        nonspecific = ".nonspecific." in input_path
-        noflip = nonspecific or input_path.endswith(".noflip.svg")
-        file_name = os.path.basename(os.path.splitext(input_path)[0].split(".")[0] + ".png")
-        folder_path = os.path.join(OUTPUT_PATH_PREFIX, drawable)
-        sh.mkdir("-p", folder_path)
-        output_file_path = os.path.join(folder_path, file_name)
-        output_precrush_path = output_file_path + "_"
-        px = int(DENSITIES[density] * self.dp)
-        sh.rsvg_convert(input_path, "-a", h=px, o=output_precrush_path)
-        sh.pngcrush("-q", "-reduce", output_precrush_path, output_file_path)
-        sh.rm(output_precrush_path)
-        return output_file_path, noflip
-
-    def _do_flop(self, density, (input_path, noflip)):
-        if noflip:
-            return
-        folder_name = os.path.join(OUTPUT_PATH_PREFIX, "drawable-ldrtl-" + density)
-        output_file_path = os.path.join(folder_name, os.path.basename(input_path))
-        sh.mkdir("-p", folder_name)
-        sh.convert(input_path, "-flop", output_file_path)
-
-    def convert(self):
-        for svg in self.svgs:
-            if ".nonspecific." in svg:
-                self._do_export("xxxhdpi", svg, "drawable")
-            else:
-                for density in DENSITIES.keys():
-                    self._do_flop(density, self._do_export(density, svg, "drawable-" + density))
-            print(u"\u2713 %s" % os.path.basename(svg))
+            for density in DENSITIES:
+                flop(density,
+                     export(dp, density, svg_file, 'drawable-' + density))
+        print('+ {}'.format(svg_file))
 
 
-def validate_filters(filter_set):
-    for filter in filter_set:
-        if not filter.endswith(".svg") or "/" in filter:
-            print >> sys.stderr, 'Only svg file names allowed in arguments.'
-            sys.exit(-1)
-    return filter_set
+def is_a_size(name):
+    return re.match(r'[1-9]\d*', name)
 
-if __name__ == "__main__":
-    svg_filters = None
-    if len(sys.argv) > 1:
-        svg_filters = validate_filters(set(sys.argv[1:]))
-    source_density_paths = glob(os.path.join(os.path.dirname(__file__), "../icon-svgs/*"))
-    for source_path in source_density_paths:
-        ib = ImagesBatch(source_path, svg_filters)
-        ib.convert()
+
+def main():
+    executed_via = os.path.abspath(__file__)
+    trace_value_of('executed_via')
+
+    script_dir = os.path.dirname(executed_via)
+    trace_value_of('script_dir')
+
+    icons_dir = os.path.abspath(
+        os.path.join(script_dir, os.pardir, 'icon-svgs'))
+    trace_value_of('icons_dir')
+
+    svg_filters = set(validate_filter_argument(x) for x in sys.argv[1:])
+    trace_value_of('svg_filters')
+
+    source_densities = list()
+    for name in os.listdir(icons_dir):
+        if not is_a_size(name):
+            continue
+        icon_path = os.path.join(icons_dir, name)
+        if os.path.isdir(icon_path):
+            source_densities.append(icon_path)
+    trace_value_of('source_densities')
+
+    for icon_path in source_densities:
+        convert(icon_path, svg_filters)
+
+
+if __name__ == '__main__':
+    main()
