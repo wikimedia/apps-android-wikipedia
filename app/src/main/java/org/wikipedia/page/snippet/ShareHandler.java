6bb7bec991184a569d3258f3b2935cb1d5cbafd1
Dmitry Brant <dbrant@wikimedia.org>
2016-04-28 11:09:45 -0400

    [facepalm] Enable Wiktionary popups in production.
    http://i.imgur.com/iWKad22.jpg?fb
    
    Change-Id: I5c7556fb504ba991ff8c3aae6ba4764282c1a86a

--- app/src/main/java/org/wikipedia/page/snippet/ShareHandler.java
+++ app/src/main/java/org/wikipedia/page/snippet/ShareHandler.java
@@ -212,8 +212,7 @@ public class ShareHandler {
     }
 
     private boolean shouldEnableWiktionaryDialog() {
-        return Prefs.useRestBase() && WikipediaApp.getInstance().isPreProdRelease()
-            && isWiktionaryDialogEnabledForArticleLanguage();
+        return Prefs.useRestBase() && isWiktionaryDialogEnabledForArticleLanguage();
     }
 
     private boolean isWiktionaryDialogEnabledForArticleLanguage() {
