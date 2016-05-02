wikimedia/apps-android-wikipedia

commit f38dfa3d0e0df6421f92c8caee95a1e87b6111e2

Update npm configuations to be able to run npm install
This allows us to run grunt from inside www/ instead of having to move it
to parent directory.

Also update packages

Update grunt to 0.4.5

Update grunt-contrib-copy to 1.0.0

Update grunt-contrib-jshint to 1.0.0

Update browserify to 13.0.0

Add package grunt-jsonlint for linting json files.

Change-Id: Iaf286b1dc4af2948896d2dbd37697f06b440850d

www/Gruntfile.js
@@ -1,100 +1,116 @@
+/*jshint node:true */
 module.exports = function ( grunt ) {
-    var allScriptFiles = [
-        "js/bridge.js",
-        "js/main.js",
-        "js/utilities.js",
-        "js/transformer.js",
-        "js/transforms/*.js",
-        "js/transforms/service/*.js",
-        "js/actions.js",
-        "js/disambig.js",
-        "js/editaction.js",
-        "js/issues.js",
-        "js/loader.js",
-        "js/night.js",
-        "js/preview.js",
-        "js/rtlsupport.js",
-        "js/sections.js",
-        "tests/*.js"
-    ];
-    var allHTMLFiles = [
-        "index.html",
-        "tests/index.html"
-    ];
-    var distFolder = "../app/src/main/assets/";
+	var allScriptFiles = [
+		"js/bridge.js",
+		"js/main.js",
+		"js/utilities.js",
+		"js/transformer.js",
+		"js/transforms/*.js",
+		"js/transforms/service/*.js",
+		"js/actions.js",
+		"js/disambig.js",
+		"js/editaction.js",
+		"js/issues.js",
+		"js/loader.js",
+		"js/night.js",
+		"js/preview.js",
+		"js/rtlsupport.js",
+		"js/sections.js",
+		"tests/*.js"
+	];
+	var allHTMLFiles = [
+		"index.html",
+		"tests/index.html"
+	];
+	var distFolder = "../app/src/main/assets/";
 
-    grunt.loadNpmTasks( 'grunt-browserify' );
-    grunt.loadNpmTasks( 'grunt-contrib-jshint' );
-    grunt.loadNpmTasks( 'grunt-contrib-copy' );
-    grunt.loadNpmTasks( 'grunt-contrib-watch' );
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+	grunt.loadNpmTasks( 'grunt-jsonlint' );
+	grunt.loadNpmTasks( 'grunt-browserify' );
+	grunt.loadNpmTasks( 'grunt-contrib-copy' );
+	grunt.loadNpmTasks( 'grunt-contrib-watch' );
 
-    grunt.initConfig( {
-        pkg: grunt.file.readJSON( "package.json" ),
-        browserify: {
-            dist: {
-                files: {
-                    "bundle.js": [
-                        "js/bridge.js",
-                        "js/main.js",
-                        "js/utilities.js",
-                        "js/transformer.js",
-                        "js/transforms/*.js",
-                        "js/transforms/service/*.js",
-                        "js/actions.js",
-                        "js/disambig.js",
-                        "js/editaction.js",
-                        "js/issues.js",
-                        "js/loader.js",
-                        "js/night.js",
-                        "js/preview.js",
-                        "js/rtlsupport.js",
-                        "js/sections.js"
-                    ],
-                    "bundle-test.js": [
-                        "js/loader.js",
-                        "js/main.js",
-                        "js/bridge.js",
-                        "tests/*.js"
-                    ],
-                    "preview.js": [
-                        "js/loader.js",
-                        "js/bridge.js",
-                        "js/night.js",
-                        "js/actions.js",
-                        "js/preview.js",
-                        "js/rtlsupport.js",
-                        "js/util.js"
-                    ]
-                }
-            }
-        },
-        jshint: {
-            allFiles: allScriptFiles,
-            options: {
-                jshintrc: ".jshintrc"
-            }
-        },
-        copy: {
-            main: {
-                files: [
-                    // App files
-                    { src: [ "bundle.js", "index.html" ], dest: distFolder },
+	grunt.initConfig( {
+		pkg: grunt.file.readJSON( "package.json" ),
+		jshint: {
+			options: {
+				jshintrc: true
+			},
+			all: [
+				'../*.js',
+				'**/*.js',
+				'!node_modules/**',
+				'!lib/**'
+			]
+		},
+		jsonlint: {
+			all: [
+				'../**/*.json',
+				'**/*.json',
+				'!../www/node_modules/**',
+				'!node_modules/**'
+			]
+		},
+		browserify: {
+			dist: {
+				files: {
+					"bundle.js": [
+						"js/bridge.js",
+						"js/main.js",
+						"js/utilities.js",
+						"js/transformer.js",
+						"js/transforms/*.js",
+						"js/transforms/service/*.js",
+						"js/actions.js",
+						"js/disambig.js",
+						"js/editaction.js",
+						"js/issues.js",
+						"js/loader.js",
+						"js/night.js",
+						"js/preview.js",
+						"js/rtlsupport.js",
+						"js/sections.js"
+					],
+					"bundle-test.js": [
+						"js/loader.js",
+						"js/main.js",
+						"js/bridge.js",
+						"tests/*.js"
+					],
+					"preview.js": [
+						"js/loader.js",
+						"js/bridge.js",
+						"js/night.js",
+						"js/actions.js",
+						"js/preview.js",
+						"js/rtlsupport.js",
+						"js/util.js"
+					]
+				}
+			}
+		},
+		copy: {
+			main: {
+				files: [
+					// App files
+					{ src: [ "bundle.js", "index.html" ], dest: distFolder },
 
-                    // Test files
-                    { src: [ "bundle-test.js", "tests/index.html" ], dest: distFolder },
+					// Test files
+					{ src: [ "bundle-test.js", "tests/index.html" ], dest: distFolder },
 
-                    // Preview files
-                    { src: [ "preview.js", "preview.html" ], dest: distFolder },
-                ]
-            }
-        },
-        watch: {
-            scripts: {
-                files: allScriptFiles.concat( allHTMLFiles ),
-                tasks: [ "default" ]
-            }
-        }
-    } );
+					// Preview files
+					{ src: [ "preview.js", "preview.html" ], dest: distFolder },
+				]
+			}
+		},
+		watch: {
+			scripts: {
+				files: allScriptFiles.concat( allHTMLFiles ),
+				tasks: [ "default" ]
+			}
+		}
+	} );
 
-    grunt.registerTask( 'default', [ 'browserify', 'copy' ] );
+	grunt.registerTask( 'test', [ 'jshint', 'jsonlint', 'browserify', 'copy' ] );
+	grunt.registerTask( 'default', 'test' );
 };

.gitignore
@@ -28,7 +28,8 @@ scripts/apps
 scripts/backup.ab
 
 # Node modules for the js
-www/node_modules
+node_modules/
+www/node_modules/
 www/bundle*.js
 www/abusefilter.js
 www/preview.js

.jshintignore
@@ -1,2 +1,4 @@
 www/lib/*
 app/src/main/assets/*
+node_modules/
+www/node_modules/

www/.jshintignore
@@ -1,0 +1,2 @@
+lib/*
+node_modules/

www/.jshintrc
@@ -1,0 +1,18 @@
+{
+	"globals": {
+		"console": true,
+        "require": true,
+        "module": true,
+        "marshaller": true
+	},
+
+	"browser": true,
+	"curly": true,
+	"eqeqeq": true,
+	"forin": false,
+	"onevar": false,
+	"trailing": true,
+	"undef" : true,
+	"unused": true,
+	"supernew": true
+}

commit eaaa5538a3fee35afa3267c08a25a2b02a0d7d4e

Hygiene: tabs to spaces
f38dfa3 mistakenly swapped spaces for tabs. Replace tabs with spaces. No
functional changes intended.

Change-Id: If468131f1efc3cb3200767066498eba12e489307

www/Gruntfile.js
@@ -1,116 +1,116 @@
 /*jshint node:true */
 module.exports = function ( grunt ) {
-	var allScriptFiles = [
-		"js/bridge.js",
-		"js/main.js",
-		"js/utilities.js",
-		"js/transformer.js",
-		"js/transforms/*.js",
-		"js/transforms/service/*.js",
-		"js/actions.js",
-		"js/disambig.js",
-		"js/editaction.js",
-		"js/issues.js",
-		"js/loader.js",
-		"js/night.js",
-		"js/preview.js",
-		"js/rtlsupport.js",
-		"js/sections.js",
-		"tests/*.js"
-	];
-	var allHTMLFiles = [
-		"index.html",
-		"tests/index.html"
-	];
-	var distFolder = "../app/src/main/assets/";
+    var allScriptFiles = [
+        "js/bridge.js",
+        "js/main.js",
+        "js/utilities.js",
+        "js/transformer.js",
+        "js/transforms/*.js",
+        "js/transforms/service/*.js",
+        "js/actions.js",
+        "js/disambig.js",
+        "js/editaction.js",
+        "js/issues.js",
+        "js/loader.js",
+        "js/night.js",
+        "js/preview.js",
+        "js/rtlsupport.js",
+        "js/sections.js",
+        "tests/*.js"
+    ];
+    var allHTMLFiles = [
+        "index.html",
+        "tests/index.html"
+    ];
+    var distFolder = "../app/src/main/assets/";
 
-	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
-	grunt.loadNpmTasks( 'grunt-jsonlint' );
-	grunt.loadNpmTasks( 'grunt-browserify' );
-	grunt.loadNpmTasks( 'grunt-contrib-copy' );
-	grunt.loadNpmTasks( 'grunt-contrib-watch' );
+    grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+    grunt.loadNpmTasks( 'grunt-jsonlint' );
+    grunt.loadNpmTasks( 'grunt-browserify' );
+    grunt.loadNpmTasks( 'grunt-contrib-copy' );
+    grunt.loadNpmTasks( 'grunt-contrib-watch' );
 
-	grunt.initConfig( {
-		pkg: grunt.file.readJSON( "package.json" ),
-		jshint: {
-			options: {
-				jshintrc: true
-			},
-			all: [
-				'js/*.js',
-				'js/transforms/*.js',
-				'!node_modules/**',
-				'!lib/**'
-			]
-		},
-		jsonlint: {
-			all: [
-				'../**/*.json',
-				'**/*.json',
-				'!../www/node_modules/**',
-				'!node_modules/**'
-			]
-		},
-		browserify: {
-			dist: {
-				files: {
-					"bundle.js": [
-						"js/bridge.js",
-						"js/main.js",
-						"js/utilities.js",
-						"js/transformer.js",
-						"js/transforms/*.js",
-						"js/transforms/service/*.js",
-						"js/actions.js",
-						"js/disambig.js",
-						"js/editaction.js",
-						"js/issues.js",
-						"js/loader.js",
-						"js/night.js",
-						"js/preview.js",
-						"js/rtlsupport.js",
-						"js/sections.js"
-					],
-					"bundle-test.js": [
-						"js/loader.js",
-						"js/main.js",
-						"js/bridge.js",
-						"tests/*.js"
-					],
-					"preview.js": [
-						"js/loader.js",
-						"js/bridge.js",
-						"js/night.js",
-						"js/actions.js",
-						"js/preview.js",
-						"js/rtlsupport.js",
-						"js/util.js"
-					]
-				}
-			}
-		},
-		copy: {
-			main: {
-				files: [
-					// App files
-					{ src: [ "bundle.js", "index.html" ], dest: distFolder },
+    grunt.initConfig( {
+        pkg: grunt.file.readJSON( "package.json" ),
+        jshint: {
+            options: {
+                jshintrc: true
+            },
+            all: [
+                'js/*.js',
+                'js/transforms/*.js',
+                '!node_modules/**',
+                '!lib/**'
+            ]
+        },
+        jsonlint: {
+            all: [
+                '../**/*.json',
+                '**/*.json',
+                '!../www/node_modules/**',
+                '!node_modules/**'
+            ]
+        },
+        browserify: {
+            dist: {
+                files: {
+                    "bundle.js": [
+                        "js/bridge.js",
+                        "js/main.js",
+                        "js/utilities.js",
+                        "js/transformer.js",
+                        "js/transforms/*.js",
+                        "js/transforms/service/*.js",
+                        "js/actions.js",
+                        "js/disambig.js",
+                        "js/editaction.js",
+                        "js/issues.js",
+                        "js/loader.js",
+                        "js/night.js",
+                        "js/preview.js",
+                        "js/rtlsupport.js",
+                        "js/sections.js"
+                    ],
+                    "bundle-test.js": [
+                        "js/loader.js",
+                        "js/main.js",
+                        "js/bridge.js",
+                        "tests/*.js"
+                    ],
+                    "preview.js": [
+                        "js/loader.js",
+                        "js/bridge.js",
+                        "js/night.js",
+                        "js/actions.js",
+                        "js/preview.js",
+                        "js/rtlsupport.js",
+                        "js/util.js"
+                    ]
+                }
+            }
+        },
+        copy: {
+            main: {
+                files: [
+                    // App files
+                    { src: [ "bundle.js", "index.html" ], dest: distFolder },
 
-					// Test files
-					{ src: [ "bundle-test.js", "tests/index.html" ], dest: distFolder },
+                    // Test files
+                    { src: [ "bundle-test.js", "tests/index.html" ], dest: distFolder },
 
-					// Preview files
-					{ src: [ "preview.js", "preview.html" ], dest: distFolder },
-				]
-			}
-		},
-		watch: {
-			scripts: {
-				files: allScriptFiles.concat( allHTMLFiles ),
-				tasks: [ "default" ]
-			}
-		}
-	} );
+                    // Preview files
+                    { src: [ "preview.js", "preview.html" ], dest: distFolder },
+                ]
+            }
+        },
+        watch: {
+            scripts: {
+                files: allScriptFiles.concat( allHTMLFiles ),
+                tasks: [ "default" ]
+            }
+        }
+    } );
 
-	grunt.registerTask( 'test', [ 'jshint', 'jsonlint', 'browserify', 'copy' ] );
-	grunt.registerTask( 'default', 'test' );
+    grunt.registerTask( 'test', [ 'jshint', 'jsonlint', 'browserify', 'copy' ] );
+    grunt.registerTask( 'default', 'test' );
 };