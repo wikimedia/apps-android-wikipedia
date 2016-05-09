79fb80c67faa3e228685f6d3a8d2982f274af760
Michael Holloway <mholloway@wikimedia.org>
2016-05-09 14:46:59 -0400

    Workaround: Prevent OkHttpClient from opening an HTTP/2 connection
    OkHttp does not play nicely with nginx over HTTP/2 and this is causing
    our connection to fail when trying to log in or make an edit.
    
    This workaround will fix our issue while Square works on resolving
    https://github.com/square/okhttp/issues/2543 .
    
    Bug: T134758
    Bug: T134759
    Change-Id: Ifc2ec3eb75e2e54b4789914f43ff9104f1ca11f9

--- app/src/main/java/org/wikipedia/OkHttpConnectionFactory.java
+++ app/src/main/java/org/wikipedia/OkHttpConnectionFactory.java
@@ -5,11 +5,14 @@ import com.github.kevinsawicki.http.HttpRequest;
 import com.squareup.okhttp.Cache;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.OkUrlFactory;
+import com.squareup.okhttp.Protocol;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.Proxy;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class OkHttpConnectionFactory implements HttpRequest.ConnectionFactory {
     private static final long HTTP_CACHE_SIZE = 16 * 1024 * 1024;
@@ -34,6 +37,15 @@ public class OkHttpConnectionFactory implements HttpRequest.ConnectionFactory {
         OkHttpClient client = new OkHttpClient();
         client.setCookieHandler(((WikipediaApp)context.getApplicationContext()).getCookieManager());
         client.setCache(new Cache(context.getCacheDir(), HTTP_CACHE_SIZE));
+
+        // Create a custom set of protocols that excludes HTTP/2, since OkHttp doesn't play
+        // nicely with nginx over HTTP/2.
+        // TODO: Remove when https://github.com/square/okhttp/issues/2543 is fixed.
+        List<Protocol> protocolList = new ArrayList<>();
+        protocolList.add(Protocol.SPDY_3);
+        protocolList.add(Protocol.HTTP_1_1);
+        client.setProtocols(protocolList);
+
         return client;
     }
 }
