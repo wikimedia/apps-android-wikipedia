5ef1ca7f462864386c56a7b9e602872e4337723b
Dmitry Brant <dbrant@wikimedia.org>
2016-06-06 17:48:00 -0400

    Hygiene: remove non-functional code.
    This removes the code for interfacing with the Gather API :..(
    
    Change-Id: Ic0cbd19a1e0c58a4d4afc92c68e66434c9affcbb

--- app/src/main/java/org/wikipedia/readinglist/api/ReadingListPageTitle.java
+++
@@ -1,8 +0,0 @@
-package org.wikipedia.readinglist.api;
-
-/**
- * An entry in a collection representing a page title in a reading list.
- */
-public interface ReadingListPageTitle {
-    String getPrefixedTitle();
-}
