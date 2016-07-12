dfa6711b7af9a669f61549fe80155dbed80a186e
Stephen Niedzielski <stephen@niedzielski.com>
2016-07-12 13:31:45 -0600

    Hygiene: update test to JUnit 4
    Change-Id: I57ea4b3e389684717017ba7b4972d75d99491b1e

--- app/src/test/java/org/wikipedia/page/PageTitleTest.java
+++ app/src/test/java/org/wikipedia/page/PageTitleTest.java
@@ -1,118 +1,118 @@
 package org.wikipedia.page;
 
-import junit.framework.TestCase;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.wikipedia.Site;
 import org.wikipedia.staticdata.MainPageNameData;
 import org.wikipedia.test.TestRunner;
 
-@RunWith(TestRunner.class)
-public class PageTitleTest extends TestCase {
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
 
-    @Test public void testEquals() throws Exception {
-        assertTrue(new PageTitle(null, "India", new Site("en.wikipedia.org")).equals(new PageTitle(null, "India", new Site("en.wikipedia.org"))));
-        assertTrue(new PageTitle("Talk", "India",  new Site("en.wikipedia.org")).equals(new PageTitle("Talk", "India", new Site("en.wikipedia.org"))));
+@RunWith(TestRunner.class) public class PageTitleTest {
+    @Test public void testEquals() throws Throwable {
+        assertThat(new PageTitle(null, "India", new Site("en.wikipedia.org")).equals(new PageTitle(null, "India", new Site("en.wikipedia.org"))), is(true));
+        assertThat(new PageTitle("Talk", "India",  new Site("en.wikipedia.org")).equals(new PageTitle("Talk", "India", new Site("en.wikipedia.org"))), is(true));
 
-        assertFalse(new PageTitle(null, "India",  new Site("ta.wikipedia.org")).equals(new PageTitle(null, "India", new Site("en.wikipedia.org"))));
-        assertFalse(new PageTitle("Talk", "India",  new Site("ta.wikipedia.org")).equals(new PageTitle("Talk", "India", new Site("en.wikipedia.org"))));
-        assertFalse(new PageTitle("Talk", "India",  new Site("ta.wikipedia.org")).equals("Something else"));
+        assertThat(new PageTitle(null, "India",  new Site("ta.wikipedia.org")).equals(new PageTitle(null, "India", new Site("en.wikipedia.org"))), is(false));
+        assertThat(new PageTitle("Talk", "India",  new Site("ta.wikipedia.org")).equals(new PageTitle("Talk", "India", new Site("en.wikipedia.org"))), is(false));
+        assertThat(new PageTitle("Talk", "India",  new Site("ta.wikipedia.org")).equals("Something else"), is(false));
     }
 
-    @Test public void testJSONSerialization() throws Exception {
+    @Test public void testJsonSerialization() throws Throwable {
         Site enwiki = new Site("en.wikipedia.org");
         PageTitle title = new PageTitle(null, "Test title",  enwiki);
-        assertEquals(title, new PageTitle(title.toJSON()));
+        assertThat(title, is(new PageTitle(title.toJSON())));
 
         title = new PageTitle("Talk", "Test title",  enwiki);
-        assertEquals(title, new PageTitle(title.toJSON()));
+        assertThat(new PageTitle(title.toJSON()), is(title));
     }
 
-    @Test public void testPrefixedText() throws Exception {
+    @Test public void testPrefixedText() throws Throwable {
         Site enwiki = new Site("en.wikipedia.org");
 
-        assertEquals(new PageTitle(null, "Test  title",  enwiki).getPrefixedText(), "Test__title");
-        assertEquals(new PageTitle(null, "Test title",  enwiki).getPrefixedText(), "Test_title");
-        assertEquals(new PageTitle("Talk", "Test title",  enwiki).getPrefixedText(), "Talk:Test_title");
-        assertEquals(new PageTitle(null, "Test title",  enwiki).getText(), "Test_title");
+        assertThat(new PageTitle(null, "Test  title",  enwiki).getPrefixedText(), is("Test__title"));
+        assertThat(new PageTitle(null, "Test title",  enwiki).getPrefixedText(), is("Test_title"));
+        assertThat(new PageTitle("Talk", "Test title",  enwiki).getPrefixedText(), is("Talk:Test_title"));
+        assertThat(new PageTitle(null, "Test title",  enwiki).getText(), is("Test_title"));
     }
 
-    @Test public void testFromInternalLink() throws Exception {
+    @Test public void testFromInternalLink() throws Throwable {
         Site enwiki = new Site("en.wikipedia.org");
 
-        assertEquals(enwiki.titleForInternalLink("/wiki/India").getPrefixedText(), "India");
-        assertNull(enwiki.titleForInternalLink("/wiki/India").getNamespace());
+        assertThat(enwiki.titleForInternalLink("/wiki/India").getPrefixedText(), is("India"));
+        assertThat(enwiki.titleForInternalLink("/wiki/India").getNamespace(), nullValue());
 
-        assertEquals(enwiki.titleForInternalLink("/wiki/Talk:India").getNamespace(), "Talk");
-        assertEquals(enwiki.titleForInternalLink("/wiki/Talk:India").getText(), "India");
-        assertEquals(enwiki.titleForInternalLink("/wiki/Talk:India").getFragment(), null);
+        assertThat(enwiki.titleForInternalLink("/wiki/Talk:India").getNamespace(), is("Talk"));
+        assertThat(enwiki.titleForInternalLink("/wiki/Talk:India").getText(), is("India"));
+        assertThat(enwiki.titleForInternalLink("/wiki/Talk:India").getFragment(), nullValue());
 
-        assertEquals(enwiki.titleForInternalLink("/wiki/Talk:India#").getNamespace(), "Talk");
-        assertEquals(enwiki.titleForInternalLink("/wiki/Talk:India#").getText(), "India");
-        assertEquals(enwiki.titleForInternalLink("/wiki/Talk:India#").getFragment(), "");
+        assertThat(enwiki.titleForInternalLink("/wiki/Talk:India#").getNamespace(), is("Talk"));
+        assertThat(enwiki.titleForInternalLink("/wiki/Talk:India#").getText(), is("India"));
+        assertThat(enwiki.titleForInternalLink("/wiki/Talk:India#").getFragment(), is(""));
 
-        assertEquals(enwiki.titleForInternalLink("/wiki/Talk:India#History").getNamespace(), "Talk");
-        assertEquals(enwiki.titleForInternalLink("/wiki/Talk:India#History").getText(), "India");
-        assertEquals(enwiki.titleForInternalLink("/wiki/Talk:India#History").getFragment(), "History");
+        assertThat(enwiki.titleForInternalLink("/wiki/Talk:India#History").getNamespace(), is("Talk"));
+        assertThat(enwiki.titleForInternalLink("/wiki/Talk:India#History").getText(), is("India"));
+        assertThat(enwiki.titleForInternalLink("/wiki/Talk:India#History").getFragment(), is("History"));
     }
 
-    @Test public void testCanonicalURL() throws Exception {
+    @Test public void testCanonicalURL() throws Throwable {
         Site enwiki = new Site("en.wikipedia.org");
 
-        assertEquals(enwiki.titleForInternalLink("/wiki/India").getCanonicalUri(), "https://en.wikipedia.org/wiki/India");
-        assertEquals(enwiki.titleForInternalLink("/wiki/India Gate").getCanonicalUri(), "https://en.wikipedia.org/wiki/India_Gate");
-        assertEquals(enwiki.titleForInternalLink("/wiki/India's Gate").getCanonicalUri(), "https://en.wikipedia.org/wiki/India%27s_Gate");
+        assertThat(enwiki.titleForInternalLink("/wiki/India").getCanonicalUri(), is("https://en.wikipedia.org/wiki/India"));
+        assertThat(enwiki.titleForInternalLink("/wiki/India Gate").getCanonicalUri(), is("https://en.wikipedia.org/wiki/India_Gate"));
+        assertThat(enwiki.titleForInternalLink("/wiki/India's Gate").getCanonicalUri(), is("https://en.wikipedia.org/wiki/India%27s_Gate"));
     }
 
-    @Test public void testSite() throws Exception {
+    @Test public void testSite() throws Throwable {
         Site enwiki = new Site("en.wikipedia.org");
 
-        assertEquals(enwiki, new PageTitle(null, "Test", enwiki).getSite());
-        assertEquals(enwiki, new Site("en.wikipedia.org"));
+        assertThat(new PageTitle(null, "Test", enwiki).getSite(), is(enwiki));
+        assertThat(new Site("en.wikipedia.org"), is(enwiki));
     }
 
-    @Test public void testParsing() throws Exception {
+    @Test public void testParsing() throws Throwable {
         Site enwiki = new Site("en.wikipedia.org");
 
-        assertEquals("Hello", new PageTitle("Hello", enwiki).getDisplayText());
-        assertEquals("Talk:Hello", new PageTitle("Talk:Hello", enwiki).getDisplayText());
-        assertEquals("Hello", new PageTitle("Talk:Hello", enwiki).getText());
-        assertEquals("Talk", new PageTitle("Talk:Hello", enwiki).getNamespace());
-        assertEquals("Wikipedia talk:Hello world", new PageTitle("Wikipedia_talk:Hello world", enwiki).getDisplayText());
+        assertThat(new PageTitle("Hello", enwiki).getDisplayText(), is("Hello"));
+        assertThat(new PageTitle("Talk:Hello", enwiki).getDisplayText(), is("Talk:Hello"));
+        assertThat(new PageTitle("Talk:Hello", enwiki).getText(), is("Hello"));
+        assertThat(new PageTitle("Talk:Hello", enwiki).getNamespace(), is("Talk"));
+        assertThat(new PageTitle("Wikipedia_talk:Hello world", enwiki).getDisplayText(), is("Wikipedia talk:Hello world"));
     }
 
-    @Test public void testSpecial() throws Exception {
-        assertTrue(new PageTitle("Special:Version", new Site("en.wikipedia.org")).isSpecial());
-        assertTrue(new PageTitle("特別:Version", new Site("ja.wikipedia.org")).isSpecial());
-        assertFalse(new PageTitle("Special:Version", new Site("ja.wikipedia.org")).isSpecial());
-        assertFalse(new PageTitle("特別:Version", new Site("en.wikipedia.org")).isSpecial());
+    @Test public void testSpecial() throws Throwable {
+        assertThat(new PageTitle("Special:Version", new Site("en.wikipedia.org")).isSpecial(), is(true));
+        assertThat(new PageTitle("特別:Version", new Site("ja.wikipedia.org")).isSpecial(), is(true));
+        assertThat(new PageTitle("Special:Version", new Site("ja.wikipedia.org")).isSpecial(), is(false));
+        assertThat(new PageTitle("特別:Version", new Site("en.wikipedia.org")).isSpecial(), is(false));
     }
 
-    @Test public void testFile() throws Exception {
-        assertTrue(new PageTitle("File:SomethingSomething", new Site("en.wikipedia.org")).isFilePage());
-        assertTrue(new PageTitle("ファイル:Version", new Site("ja.wikipedia.org")).isFilePage());
-        assertFalse(new PageTitle("File:SomethingSomething", new Site("ja.wikipedia.org")).isFilePage());
-        assertFalse(new PageTitle("ファイル:Version", new Site("en.wikipedia.org")).isFilePage());
+    @Test public void testFile() throws Throwable {
+        assertThat(new PageTitle("File:SomethingSomething", new Site("en.wikipedia.org")).isFilePage(), is(true));
+        assertThat(new PageTitle("ファイル:Version", new Site("ja.wikipedia.org")).isFilePage(), is(true));
+        assertThat(new PageTitle("File:SomethingSomething", new Site("ja.wikipedia.org")).isFilePage(), is(false));
+        assertThat(new PageTitle("ファイル:Version", new Site("en.wikipedia.org")).isFilePage(), is(false));
     }
 
-    @Test public void testMainpage() throws Exception {
+    @Test public void testMainPage() throws Throwable {
         Site enwiki = new Site("en.wikipedia.org");
-        assertEquals(new PageTitle("", enwiki), new PageTitle(MainPageNameData.valueFor("en"), enwiki));
+        assertThat(new PageTitle("", enwiki), is(new PageTitle(MainPageNameData.valueFor("en"), enwiki)));
     }
 
     /** https://bugzilla.wikimedia.org/66151 */
     @Test public void testHashChar() {
         PageTitle pageTitle = new PageTitle("#", new Site("en.wikipedia.org"));
-        assertEquals(null, pageTitle.getNamespace());
-        assertEquals("", pageTitle.getText());
-        assertEquals("", pageTitle.getFragment());
+        assertThat(pageTitle.getNamespace(), nullValue());
+        assertThat(pageTitle.getText(), is(""));
+        assertThat(pageTitle.getFragment(), is(""));
     }
 
     @Test public void testColonChar() {
         PageTitle pageTitle = new PageTitle(":", new Site("en.wikipedia.org"));
-        assertEquals("", pageTitle.getNamespace());
-        assertEquals("", pageTitle.getText());
-        assertEquals(null, pageTitle.getFragment());
+        assertThat(pageTitle.getNamespace(), is(""));
+        assertThat(pageTitle.getText(), is(""));
+        assertThat(pageTitle.getFragment(), nullValue());
     }
 }
