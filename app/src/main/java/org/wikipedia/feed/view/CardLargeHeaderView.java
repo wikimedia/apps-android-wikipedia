1bace5110499f32f9d7c917ef645de38eebd73be
Stephen Niedzielski <stephen@niedzielski.com>
2016-09-01 14:31:43 -0600

    Hygiene: simplify CardLargeHeaderView
    • Fix title for RTL.
    
    • Remove unused subtitle.
    
    • Reduce View hierarchy depth.
    
    • Rename pageTitle to title. This View is generic and shouldn't assume
      an article title will be shown.
    
    Change-Id: Ie0bae4dd3b918e8e78aebf5c88bc24bf621e5c3b

--- app/src/main/java/org/wikipedia/feed/view/CardLargeHeaderView.java
+++ app/src/main/java/org/wikipedia/feed/view/CardLargeHeaderView.java
@@ -10,30 +10,27 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.view.View;
-import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import org.wikipedia.R;
 import org.wikipedia.views.FaceAndColorDetectImageView;
-import org.wikipedia.views.GoneIfEmptyTextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class CardLargeHeaderView extends FrameLayout {
+public class CardLargeHeaderView extends RelativeLayout {
     @BindView(R.id.view_card_header_large_background) View backgroundView;
-    @BindView(R.id.view_card_header_large_text_container) View textContainerView;
     @BindView(R.id.view_card_header_large_image) FaceAndColorDetectImageView imageView;
-    @BindView(R.id.view_card_header_large_page_title) TextView pageTitleView;
-    @BindView(R.id.view_card_header_large_subtitle) GoneIfEmptyTextView subtitleView;
+    @BindView(R.id.view_card_header_large_title) TextView titleView;
 
     public CardLargeHeaderView(Context context) {
         super(context);
 
+        resetBackgroundColor();
         inflate(getContext(), R.layout.view_card_header_large, this);
         ButterKnife.bind(this);
         imageView.setOnImageLoadListener(new ImageLoadListener());
-        resetBackgroundColor();
     }
 
     @NonNull public CardLargeHeaderView setImage(@Nullable Uri uri) {
@@ -42,32 +39,27 @@ public class CardLargeHeaderView extends FrameLayout {
         return this;
     }
 
-    @NonNull public CardLargeHeaderView setSubtitle(@Nullable CharSequence subtitle) {
-        subtitleView.setText(subtitle == null ? null : subtitle.toString());
-        return this;
-    }
-
-    @NonNull public CardLargeHeaderView setPageTitle(@Nullable CharSequence title) {
-        pageTitleView.setText(title);
+    @NonNull public CardLargeHeaderView setTitle(@Nullable CharSequence title) {
+        titleView.setText(title);
         return this;
     }
 
     @NonNull public CardLargeHeaderView onClickListener(@Nullable OnClickListener listener) {
-        setOnClickListener(listener);
+        backgroundView.setOnClickListener(listener);
         return this;
     }
 
     private void resetBackgroundColor() {
-        backgroundView.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.gray_background));
+        setBackgroundColor(ContextCompat.getColor(getContext(), R.color.gray_background));
     }
 
     private class ImageLoadListener implements FaceAndColorDetectImageView.OnImageLoadListener {
         @Override
         public void onImageLoaded(final int bmpHeight, @Nullable final PointF faceLocation, @ColorInt final int mainColor) {
-            backgroundView.post(new Runnable() {
+            post(new Runnable() {
                 @Override
                 public void run() {
-                    animateBackgroundColor(backgroundView, mainColor);
+                    animateBackgroundColor(CardLargeHeaderView.this, mainColor);
                     if (faceLocation != null) {
                         imageView.getHierarchy().setActualImageFocusPoint(faceLocation);
                     }
