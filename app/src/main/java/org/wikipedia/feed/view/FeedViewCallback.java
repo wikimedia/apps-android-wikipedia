8e9c2100230d62d93b121fef8ffa53577917c3c3
Stephen Niedzielski <stephen@niedzielski.com>
2016-09-21 15:57:47 -0600

    Hygiene: encapsulate NewsListCardView's callback
    Change-Id: I63539c2a7f2c351f8da0b1c0b46e1764fc224419

--- app/src/main/java/org/wikipedia/feed/view/FeedViewCallback.java
+++ app/src/main/java/org/wikipedia/feed/view/FeedViewCallback.java
@@ -1,14 +1,11 @@
 package org.wikipedia.feed.view;
 
-import android.support.annotation.NonNull;
-
 import org.wikipedia.feed.image.FeaturedImageCardView;
-import org.wikipedia.feed.news.NewsItemCard;
+import org.wikipedia.feed.news.NewsListCardView;
 import org.wikipedia.feed.searchbar.SearchCardView;
 import org.wikipedia.views.ItemTouchHelperSwipeAdapter;
 
 public interface FeedViewCallback extends ItemTouchHelperSwipeAdapter.Callback,
         PageTitleListCardItemView.Callback, CardHeaderView.Callback, FeaturedImageCardView.Callback,
-        SearchCardView.Callback {
-    void onNewsItemSelected(@NonNull NewsItemCard card);
+        SearchCardView.Callback, NewsListCardView.Callback {
 }
