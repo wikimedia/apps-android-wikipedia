bb363c1baced3b76d705116e65f52395ed323e4c
Stephen Niedzielski <stephen@niedzielski.com>
2016-09-21 15:54:07 -0600

    Hygiene: move FeaturedImageCardView callback
    Encapsulate FeaturedImageCardView's callback as
    FeaturedImageCardView.Callback.
    
    Change-Id: Ideeed05abecb0de2b662a02d9d65de32aad10558

--- app/src/main/java/org/wikipedia/feed/view/FeedViewCallback.java
+++ app/src/main/java/org/wikipedia/feed/view/FeedViewCallback.java
@@ -2,17 +2,13 @@ package org.wikipedia.feed.view;
 
 import android.support.annotation.NonNull;
 
-import org.wikipedia.feed.image.FeaturedImage;
-import org.wikipedia.feed.image.FeaturedImageCard;
+import org.wikipedia.feed.image.FeaturedImageCardView;
 import org.wikipedia.feed.news.NewsItemCard;
 import org.wikipedia.views.ItemTouchHelperSwipeAdapter;
 
 public interface FeedViewCallback extends ItemTouchHelperSwipeAdapter.Callback,
-        PageTitleListCardItemView.Callback, CardHeaderView.Callback {
+        PageTitleListCardItemView.Callback, CardHeaderView.Callback, FeaturedImageCardView.Callback {
     void onSearchRequested();
     void onVoiceSearchRequested();
     void onNewsItemSelected(@NonNull NewsItemCard card);
-    void onShareImage(@NonNull FeaturedImageCard card);
-    void onDownloadImage(@NonNull FeaturedImage image);
-    void onFeaturedImageSelected(@NonNull FeaturedImageCard card);
 }
