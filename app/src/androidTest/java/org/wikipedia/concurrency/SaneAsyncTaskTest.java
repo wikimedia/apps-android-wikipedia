ed016acc7f08275d8f439129e8cffd2de97e1b8c
Stephen Niedzielski <stephen@niedzielski.com>
2016-10-19 16:58:32 -0600

    Hygiene: update SaneAsyncTaskTest to JUnit 4
    I thought this test was causing problems when I looked at the log but it
    was actually expected to throw (and log) Exceptions. Upgrade to JUnit 4
    and clean up code where easy. Also, use UI thread when referring to
    caller since AsyncTasks require execute() to be called on the main
    thread.
    
    Change-Id: Ie7fb15a9837f8ff71e76531c9c19ec75f9841496

--- app/src/androidTest/java/org/wikipedia/concurrency/SaneAsyncTaskTest.java
+++ app/src/androidTest/java/org/wikipedia/concurrency/SaneAsyncTaskTest.java
@@ -1,37 +1,35 @@
 package org.wikipedia.concurrency;
 
+import android.support.annotation.NonNull;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.filters.SmallTest;
 
 import org.junit.Test;
+import org.wikipedia.WikipediaApp;
 import org.wikipedia.testlib.TestLatch;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.fail;
-import static org.wikipedia.test.TestUtil.runOnMainSync;
 
 @SmallTest public class SaneAsyncTaskTest {
     @Test public void testFinishHandling() {
         final TestLatch latch = new TestLatch();
         final Integer returned = 42;
         runOnMainSync(new Runnable() {
-            @Override
-            public void run() {
+            @Override public void run() {
                 new SaneAsyncTask<Integer>() {
-                    @Override
-                    public void onFinish(Integer result) {
+                    @Override public void onFinish(Integer result) {
                         assertThat(returned, is(result));
                         latch.countDown();
                     }
 
-                    @Override
-                    public void onCatch(Throwable caught) {
+                    @Override public void onCatch(Throwable caught) {
                         fail("Exception called despite success");
                     }
 
-                    @Override
-                    public Integer performTask() throws Throwable {
+                    @Override public Integer performTask() throws Throwable {
                         return returned;
                     }
                 }.execute();
@@ -44,22 +42,18 @@ import static org.wikipedia.test.TestUtil.runOnMainSync;
         final TestLatch latch = new TestLatch();
         final Throwable thrown = new Exception();
         runOnMainSync(new Runnable() {
-            @Override
-            public void run() {
+            @Override public void run() {
                 new SaneAsyncTask<Void>() {
-                    @Override
-                    public void onFinish(Void result) {
+                    @Override public void onFinish(Void result) {
                         fail("onFinish called despite exception");
                     }
 
-                    @Override
-                    public void onCatch(Throwable caught) {
+                    @Override public void onCatch(Throwable caught) {
                         assertThat(caught, is(thrown));
                         latch.countDown();
                     }
 
-                    @Override
-                    public Void performTask() throws Throwable {
+                    @Override public Void performTask() throws Throwable {
                         throw thrown;
                     }
                 }.execute();
@@ -68,29 +62,23 @@ import static org.wikipedia.test.TestUtil.runOnMainSync;
         latch.await();
     }
 
-    @Test public void testAppropriateThreadFinish() throws Throwable {
+    @Test public void testAppropriateThreadFinish() {
         final TestLatch latch = new TestLatch();
         runOnMainSync(new Runnable() {
-            @Override
-            public void run() {
-                final Thread callingThread = Thread.currentThread();
-                new SaneAsyncTask<Thread>() {
-                    @Override
-                    public void onBeforeExecute() {
-                        assertThat(callingThread, is(Thread.currentThread()));
+            @Override public void run() {
+                new SaneAsyncTask<Void>() {
+                    @Override public void onBeforeExecute() {
+                        assertUiThread();
                     }
 
-                    @Override
-                    public void onFinish(Thread result) {
-                        assertThat(result, not(Thread.currentThread()));
-                        assertThat(Thread.currentThread(), is(callingThread));
+                    @Override public void onFinish(Void result) {
+                        assertUiThread();
                         latch.countDown();
                     }
 
-                    @Override
-                    public Thread performTask() throws Throwable {
-                        assertThat(callingThread, not(Thread.currentThread()));
-                        return Thread.currentThread();
+                    @Override public Void performTask() throws Throwable {
+                        assertNotUiThread();
+                        return null;
                     }
                 }.execute();
             }
@@ -98,38 +86,47 @@ import static org.wikipedia.test.TestUtil.runOnMainSync;
         latch.await();
     }
 
-    @Test public void testAppropriateThreadException() throws Throwable {
+    @Test public void testAppropriateThreadException() {
         final TestLatch latch = new TestLatch();
-        final Throwable thrown = new Exception();
         runOnMainSync(new Runnable() {
-            @Override
-            public void run() {
-                final Thread callingThread = Thread.currentThread();
-                new SaneAsyncTask<Thread>() {
-                    @Override
-                    public void onBeforeExecute() {
-                        assertThat(callingThread, is(Thread.currentThread()));
+            @Override public void run() {
+                new SaneAsyncTask<Void>() {
+                    @Override public void onBeforeExecute() {
+                        assertUiThread();
                     }
 
-                    @Override
-                    public void onFinish(Thread result) {
+                    @Override public void onFinish(Void result) {
                         fail("onFinish called even when there is an exception");
                     }
 
-                    @Override
-                    public void onCatch(Throwable caught) {
-                        assertThat(callingThread, is(Thread.currentThread()));
+                    @Override public void onCatch(Throwable caught) {
+                        assertUiThread();
                         latch.countDown();
                     }
 
-                    @Override
-                    public Thread performTask() throws Throwable {
-                        assertThat(callingThread, not(Thread.currentThread()));
-                        throw thrown;
+                    @Override public Void performTask() throws Throwable {
+                        assertNotUiThread();
+                        throw new Exception();
                     }
                 }.execute();
             }
         });
         latch.await();
+    }
+
+    private void assertUiThread() {
+        assertThat(Thread.currentThread(), is(uiThread()));
+    }
+
+    private void assertNotUiThread() {
+        assertThat(Thread.currentThread(), not(uiThread()));
+    }
+
+    @NonNull private Thread uiThread() {
+        return WikipediaApp.getInstance().getMainLooper().getThread();
+    }
+
+    private void runOnMainSync(Runnable runnable) {
+        InstrumentationRegistry.getInstrumentation().runOnMainSync(runnable);
     }
 }
