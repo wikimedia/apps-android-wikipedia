8c6d23ab2817544dab03d4c1dae0b5a4e1bc47cf
Michael Holloway <mholloway@wikimedia.org>
2016-10-20 14:51:10 -0400

    Add tests for field checking with the new @Require annotation
    Bug: T145075
    Change-Id: I2f5add17352ae234ea860be1c7f291909eced67d

--- app/src/main/java/org/wikipedia/json/GsonUtil.java
+++ app/src/main/java/org/wikipedia/json/GsonUtil.java
@@ -1,6 +1,7 @@
 package org.wikipedia.json;
 
 import android.net.Uri;
+import android.support.annotation.VisibleForTesting;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -9,15 +10,23 @@ import org.wikipedia.page.Namespace;
 
 public final class GsonUtil {
     private static final String DATE_FORMAT = "MMM dd, yyyy HH:mm:ss";
-    private static final Gson DEFAULT_GSON = new GsonBuilder()
+
+    private static final GsonBuilder DEFAULT_GSON_BUILDER = new GsonBuilder()
             .setDateFormat(DATE_FORMAT)
             .registerTypeHierarchyAdapter(Uri.class, new UriTypeAdapter().nullSafe())
             .registerTypeHierarchyAdapter(Namespace.class, new NamespaceTypeAdapter().nullSafe())
-            .registerTypeAdapterFactory(new RequiredFieldsCheckOnReadTypeAdapterFactory())
-            .create();
+            .registerTypeAdapterFactory(new RequiredFieldsCheckOnReadTypeAdapterFactory());
+
+    private static final Gson DEFAULT_GSON = DEFAULT_GSON_BUILDER.create();
+
 
     public static Gson getDefaultGson() {
         return DEFAULT_GSON;
+    }
+
+    @VisibleForTesting
+    public static GsonBuilder getDefaultGsonBuilder() {
+        return DEFAULT_GSON_BUILDER;
     }
 
     private GsonUtil() { }
