645c5b847b3a84bb5e1cea0a2100f09a2bb4e210
Anirudh S <anirudh24seven@gmail.com>
2016-10-25 14:18:34 +0530

    Fixes T146725. Theme change will apply to main screen now.
    Added Event listener to MainActivity so that it can recreate itself when theme changes. Similar solution as that of PageActivity.
    
    Bug: T146725
    
    Change-Id: I1297bf59e83eb81182c7b994871334117183ba39

--- app/src/main/java/org/wikipedia/main/MainActivity.java
+++ app/src/main/java/org/wikipedia/main/MainActivity.java
@@ -2,17 +2,37 @@ package org.wikipedia.main;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.view.View;
 
+import com.squareup.otto.Bus;
+import com.squareup.otto.Subscribe;
+
 import org.wikipedia.R;
+import org.wikipedia.WikipediaApp;
 import org.wikipedia.activity.SingleFragmentActivityWithToolbar;
+import org.wikipedia.events.ThemeChangeEvent;
 import org.wikipedia.navtab.NavTab;
+import org.wikipedia.util.log.L;
 
 public class MainActivity extends SingleFragmentActivityWithToolbar<MainFragment>
         implements MainFragment.Callback {
+
+    private WikipediaApp app;
+    private Bus bus;
+    private MainActivity.EventBusMethods busMethods;
+
     public static Intent newIntent(@NonNull Context context) {
         return new Intent(context, MainActivity.class);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        app = (WikipediaApp) getApplicationContext();
+        busMethods = new EventBusMethods();
+        registerBus();
     }
 
     @Override protected MainFragment createFragment() {
@@ -65,5 +85,31 @@ public class MainActivity extends SingleFragmentActivityWithToolbar<MainFragment
             return;
         }
         finish();
+    }
+
+    @Override
+    public void onDestroy() {
+        unregisterBus();
+        super.onDestroy();
+    }
+
+    private void registerBus() {
+        bus = app.getBus();
+        bus.register(busMethods);
+        L.d("Registered bus.");
+    }
+
+    private void unregisterBus() {
+        bus.unregister(busMethods);
+        bus = null;
+        L.d("Unregistered bus.");
+    }
+
+    private class EventBusMethods {
+
+        @Subscribe
+        public void onChangeTheme(ThemeChangeEvent event) {
+            MainActivity.this.recreate();
+        }
     }
 }
