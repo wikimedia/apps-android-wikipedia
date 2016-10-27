112867e9d4674a4b45eed0dea19b85971a0e6ae7
Michael Holloway <mholloway@wikimedia.org>
2016-10-27 13:03:27 -0400

    Add EditTokenClientTest
    Change-Id: Id0f8597f3bbb63f0775cd8b6a0c8738d4e8bab71

--- app/src/main/java/org/wikipedia/editing/EditToken.java
+++ app/src/main/java/org/wikipedia/editing/EditToken.java
@@ -1,11 +1,18 @@
 package org.wikipedia.editing;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.VisibleForTesting;
 
 import org.wikipedia.model.BaseModel;
 
 public class EditToken extends BaseModel {
     @SuppressWarnings("unused,NullableProblems") @NonNull private Query query;
+
+    @VisibleForTesting EditToken(String token) {
+        Tokens tokens = new Tokens(token);
+        this.query = new Query(tokens);
+    }
+
     @NonNull protected String token() {
         return query.tokens().token();
     }
@@ -15,12 +22,20 @@ public class EditToken extends BaseModel {
         @NonNull protected Tokens tokens() {
             return tokens;
         }
+
+        Query(@NonNull Tokens tokens) {
+            this.tokens = tokens;
+        }
     }
 
     private static class Tokens {
         @SuppressWarnings("unused,NullableProblems") @NonNull private String csrftoken;
         @NonNull protected String token() {
             return csrftoken;
+        }
+
+        Tokens(@NonNull String token) {
+            this.csrftoken = token;
         }
     }
 }
---
+++ app/src/test/java/org/wikipedia/editing/EditTokenClientTest.java
@@ -0,0 +1,74 @@
+package org.wikipedia.editing;
+
+import android.support.annotation.NonNull;
+
+import com.google.gson.stream.MalformedJsonException;
+
+import org.junit.Test;
+import org.wikipedia.dataclient.retrofit.RetrofitException;
+import org.wikipedia.editing.EditTokenClient.Callback;
+import org.wikipedia.editing.EditTokenClient.Service;
+import org.wikipedia.test.MockWebServerTest;
+
+import retrofit2.Call;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Matchers.isA;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+public class EditTokenClientTest extends MockWebServerTest {
+    @NonNull private final EditTokenClient subject = new EditTokenClient();
+
+    @Test
+    public void testRequestSuccess() throws Throwable {
+        String expected = "b6f7bd58c013ab30735cb19ecc0aa08258122cba+\\";
+        enqueueFromFile("edittoken.json");
+
+        Callback cb = mock(Callback.class);
+        Call<EditToken> call = request(cb);
+
+        server().takeRequest();
+        assertCallbackSuccess(call, cb, expected);
+    }
+
+    @Test public void testRequestResponseFailure() throws Throwable {
+        enqueue404();
+
+        Callback cb = mock(Callback.class);
+        Call<EditToken> call = request(cb);
+
+        server().takeRequest();
+        assertCallbackFailure(call, cb, RetrofitException.class);
+    }
+
+    @Test public void testRequestResponseMalformed() throws Throwable {
+        server().enqueue("'");
+
+        Callback cb = mock(Callback.class);
+        Call<EditToken> call = request(cb);
+
+        server().takeRequest();
+        assertCallbackFailure(call, cb, MalformedJsonException.class);
+    }
+
+    private void assertCallbackSuccess(@NonNull Call<EditToken> call, @NonNull Callback cb,
+                                       @NonNull String expected) {
+        verify(cb).success(eq(call), eq(expected));
+        //noinspection unchecked
+        verify(cb, never()).failure(any(Call.class), any(Throwable.class));
+    }
+
+    private void assertCallbackFailure(@NonNull Call<EditToken> call, @NonNull Callback cb,
+                                       @NonNull Class<? extends Throwable> throwable) {
+        //noinspection unchecked
+        verify(cb, never()).success(any(Call.class), any(String.class));
+        verify(cb).failure(eq(call), isA(throwable));
+    }
+
+    private Call<EditToken> request(@NonNull Callback cb) {
+        return subject.request(service(Service.class), cb);
+    }
+}
