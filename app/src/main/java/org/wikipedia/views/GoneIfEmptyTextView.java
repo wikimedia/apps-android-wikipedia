4630377a49fe487f61abc5e5a299ca1605a7b014
Dmitry Brant <dbrant@wikimedia.org>
2016-10-31 15:55:22 -0400

    Remove ambiguity when using GoneIfEmptyTextView.
    I've noticed that in the current Feed, we have a regression:
    In the list of trending articles, if an article does not have a
    description, the title should become vertically centered, whereas it now
    incorrectly stays near the top of the list item.
    
    This is because the description field is a GoneIfEmptyTextView. This
    component very deceptively exposes a setText(String) function, which
    does NOT override the super function setText(CharSequence). This can lead
    to all kinds of subtle visual bugs like this.
    
    This patch finally patches this inconsistency once and for all, by making
    this component use a TextWatcher, and update its visibility based on the
    text content that is fed into it.
    
    Change-Id: I0d1675b801b17df36aa5d61d9200d596e1c35c1d

--- app/src/main/java/org/wikipedia/views/GoneIfEmptyTextView.java
+++ app/src/main/java/org/wikipedia/views/GoneIfEmptyTextView.java
@@ -3,33 +3,50 @@ package org.wikipedia.views;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.os.Build;
-import android.support.annotation.Nullable;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.TextView;
 
-import org.wikipedia.util.StringUtil;
-
 public class GoneIfEmptyTextView extends TextView {
     public GoneIfEmptyTextView(Context context) {
         super(context);
+        init();
     }
 
     public GoneIfEmptyTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public GoneIfEmptyTextView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
     }
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     public GoneIfEmptyTextView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+        init();
     }
 
-    public void setText(@Nullable String text) {
-        super.setText(text);
-        setVisibility(StringUtil.emptyIfNull(text).equals("") ? View.GONE : View.VISIBLE);
+    private void init() {
+        addTextChangedListener(new EmptyTextWatcher());
+    }
+
+    private class EmptyTextWatcher implements TextWatcher {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            setVisibility(editable.length() == 0 ? View.GONE : View.VISIBLE);
+        }
     }
 }
