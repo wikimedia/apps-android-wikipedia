0c49cea2557c375416a770becf36aa40ff9b684f
Stephen Niedzielski <stephen@niedzielski.com>
2016-11-08 12:52:27 -0700

    Hygiene: replace Visibility with @TestedOnBool
    Change-Id: I7ea8b1291b134990f0879351a22523da2a90717f

--- app/src/androidTest/java/android/support/design/widget/PasswordTextInputTest.java
+++ app/src/androidTest/java/android/support/design/widget/PasswordTextInputTest.java
@@ -3,13 +3,12 @@ package android.support.design.widget;
 import android.support.annotation.NonNull;
 
 import org.junit.Before;
-import org.junit.experimental.theories.DataPoints;
 import org.junit.experimental.theories.Theory;
+import org.wikipedia.test.theories.TestedOnBool;
 import org.wikipedia.test.view.FontScale;
 import org.wikipedia.test.view.LayoutDirection;
 import org.wikipedia.test.view.NullValue;
 import org.wikipedia.test.view.ViewTest;
-import org.wikipedia.test.view.Visibility;
 import org.wikipedia.theme.Theme;
 
 import static android.support.design.widget.PasswordTextInput.OnShowPasswordClickListener;
@@ -22,30 +21,28 @@ import static org.mockito.Mockito.verify;
 public class PasswordTextInputTest extends ViewTest {
     private PasswordTextInput subject;
 
-    @DataPoints public static final Visibility[] VISIBILITIES = Visibility.values();
-
     @Before public void setUp() {
         setUp(WIDTH_DP_S, LayoutDirection.LOCALE, FontScale.DEFAULT, Theme.LIGHT);
         subject = new PasswordTextInput(ctx());
     }
 
-    @Theory public void testIsPasswordVisible(@NonNull Visibility visibility) {
-        if (visibility.visible()) {
+    @Theory public void testIsPasswordVisible(@TestedOnBool boolean visible) {
+        if (visible) {
             subject.passwordVisibilityToggleRequested();
         }
-        assertThat(subject.isPasswordVisible(), is(visibility.visible()));
+        assertThat(subject.isPasswordVisible(), is(visible));
     }
 
     @Theory public void testSetOnShowPasswordListener(@NonNull NullValue nul,
-                                                      @NonNull Visibility visibility) {
+                                                      @TestedOnBool boolean visible) {
         OnShowPasswordClickListener listener = nul.isNull() ? null : mock(OnShowPasswordClickListener.class);
-        if (visibility.visible()) {
+        if (visible) {
             subject.passwordVisibilityToggleRequested();
         }
         subject.setOnShowPasswordListener(listener);
         subject.passwordVisibilityToggleRequested();
         if (listener != null) {
-            verify(listener).onShowPasswordClick(eq(!visibility.visible()));
+            verify(listener).onShowPasswordClick(eq(!visible));
         }
     }
 }
--- app/src/androidTest/java/org/wikipedia/test/view/Visibility.java
+++
@@ -1,9 +0,0 @@
-package org.wikipedia.test.view;
-
-public enum Visibility {
-    HIDDEN, VISIBLE;
-
-    public boolean visible() {
-        return this == VISIBLE;
-    }
-}
