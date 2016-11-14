1ef60fdb540080742707e1e7c18e3d05c1e4e139
bearND <bsitzmann@wikimedia.org>
2016-11-14 14:25:52 -0700

    Hygiene: add base class for MW responses
    For the Wikidata description editing work I need to have an MW GET response
    without a query (just a centralauthtoken).
    
    Change-Id: I8ca7c888ec4335c63b8c4f9062093bd8746d829e

--- app/src/main/java/org/wikipedia/dataclient/mwapi/MwPostResponse.java
+++ app/src/main/java/org/wikipedia/dataclient/mwapi/MwPostResponse.java
@@ -2,29 +2,8 @@ package org.wikipedia.dataclient.mwapi;
 
 import android.support.annotation.Nullable;
 
-import org.wikipedia.server.mwapi.MwServiceError;
-
-public abstract class MwPostResponse {
-    @SuppressWarnings("unused") @Nullable private String servedby;
-    @SuppressWarnings("unused") @Nullable private MwServiceError error;
-
-    @Nullable public String code() {
-        return error == null ? null : error.getTitle();
-    }
-
-    @Nullable public String info() {
-        return error == null ? null : error.getDetails();
-    }
-
+public abstract class MwPostResponse extends MwResponse {
     public boolean success(@Nullable String result) {
-        return error == null && "success".equals(result);
-    }
-
-    public boolean hasError() {
-        return error != null;
-    }
-
-    public boolean badToken() {
-        return error != null && error.badToken();
+        return super.success() && "success".equals(result);
     }
 }
