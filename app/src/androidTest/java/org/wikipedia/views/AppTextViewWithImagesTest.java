ba65a3cbc491fe6258d9192fb3b116fed8a5a0e2
Stephen Niedzielski <stephen@niedzielski.com>
2016-12-06 12:13:56 -0700

    Update: add screenshot tests to AppTextViewWithImagesTest
    Bug: T152141
    
    	new file:   app/screenshots-ref/org.wikipedia.views.AppTextViewWithImagesTest.testWidth-120dp-en-ltr-font1.0x-light-long_text.png
    
    Change-Id: Ib887d6377b686361c65654e8044af4af443b206d

--- app/src/androidTest/java/org/wikipedia/views/AppTextViewWithImagesTest.java
+++ app/src/androidTest/java/org/wikipedia/views/AppTextViewWithImagesTest.java
@@ -1,66 +1,107 @@
 package org.wikipedia.views;
 
-import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.support.annotation.ColorInt;
 import android.support.annotation.ColorRes;
-import android.support.test.InstrumentationRegistry;
+import android.support.annotation.NonNull;
 import android.support.test.filters.SmallTest;
 import android.support.v4.content.ContextCompat;
 import android.text.Spanned;
 import android.text.style.ImageSpan;
+import android.view.View;
 
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.theories.Theory;
+import org.junit.experimental.theories.suppliers.TestedOn;
 import org.wikipedia.R;
+import org.wikipedia.test.view.FontScale;
+import org.wikipedia.test.view.LayoutDirection;
+import org.wikipedia.test.view.TertiaryTestStr;
+import org.wikipedia.test.view.ViewTest;
+import org.wikipedia.theme.Theme;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-@SmallTest public class AppTextViewWithImagesTest {
+@SmallTest public class AppTextViewWithImagesTest extends ViewTest {
     private AppTextViewWithImages subject;
-    private Context ctx = InstrumentationRegistry.getTargetContext();
 
-    @Before public void setUp() {
-        subject = new AppTextViewWithImages(ctx);
+    @Theory public void testWidth(@TestedOn(ints = {WIDTH_DP_XS, WIDTH_DP_S, WIDTH_DP_M, WIDTH_DP_XL}) int widthDp,
+                                  @NonNull FontScale fontScale, @NonNull TertiaryTestStr text) {
+        setUp(widthDp, LayoutDirection.LOCALE, fontScale, Theme.LIGHT);
+        subject.setTextWithDrawables("^1 " + str(text)  + " ^2", R.drawable.edit, R.drawable.cc_logo);
+        snap(subject, text + "_text");
+    }
+
+    @Theory public void testLayoutDirection(@TestedOn(ints = {WIDTH_DP_XS, WIDTH_DP_M}) int widthDp,
+                                            @NonNull LayoutDirection direction) {
+        setUp(widthDp, direction, FontScale.DEFAULT, Theme.LIGHT);
+        subject.setTextWithDrawables("Every good ^1 does ^2.", R.drawable.edit, R.drawable.cc_logo);
+        snap(subject);
+    }
+
+    @Theory public void testTheme(@NonNull Theme theme) {
+        setUp(WIDTH_DP_L, LayoutDirection.LOCALE, FontScale.DEFAULT, theme);
+        subject.setTextWithDrawables("Every good ^1 does ^2.", R.drawable.edit, R.drawable.cc_logo);
+        snap(subject);
     }
 
     @Test public void testSetTextWithDrawablesWithOneExpectedImageSpan() {
+        setUpTypical();
         subject.setTextWithDrawables("Every good ^1 does fine.", R.drawable.edit);
         ImageSpan[] spans = subject.getSpans(ImageSpan.class);
         assertThat(spans.length, is(1));
     }
 
     @Test public void testSetTextWithDrawablesWithTwoExpectedImageSpans() {
+        setUpTypical();
         subject.setTextWithDrawables("Every good ^1 does ^2.", R.drawable.edit, R.drawable.cc_logo);
         ImageSpan[] spans = subject.getSpans(ImageSpan.class);
         assertThat(spans.length, is(2));
     }
 
     @Test public void testSetTextWithDrawablesWithNoResourcesSpecified() {
+        setUpTypical();
         subject.setTextWithDrawables("Every good ^1 does ^2.");
         CharSequence text = subject.getText();
         assertThat(text.toString(), is("Every good ^1 does ^2."));
     }
 
     @Test public void testSetWithDrawablesWithEmptyString() {
+        setUpTypical();
         subject.setTextWithDrawables("", R.drawable.edit, R.drawable.cc_logo);
         CharSequence text = subject.getText();
         assertThat(text.toString(), is(""));
     }
 
     @Test public void testMakeImageSpan() {
+        setUpTypical();
         Spanned spanned = subject.makeImageSpan(R.drawable.edit, 10, getColor(android.R.color.black));
         assertThat(spanned, notNullValue());
     }
 
     @Test public void testGetFormattedDrawable() {
+        setUpTypical();
         Drawable edit = subject.getFormattedDrawable(R.drawable.edit, 10, getColor(android.R.color.black));
         assertThat(edit, notNullValue());
     }
 
+    @Override protected void setUp(int widthDp, @NonNull LayoutDirection layoutDirection,
+                                   @NonNull FontScale fontScale, @NonNull Theme theme) {
+        super.setUp(widthDp, layoutDirection, fontScale, theme);
+        subject = new AppTextViewWithImages(ctx());
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            subject.setTextAlignment(View.TEXT_ALIGNMENT_VIEW_START);
+        }
+    }
+
+    private void setUpTypical() {
+        setUp(WIDTH_DP_XS, LayoutDirection.LOCALE, FontScale.DEFAULT, Theme.LIGHT);
+    }
+
     @ColorInt private int getColor(@ColorRes int id) {
-        return ContextCompat.getColor(ctx, id);
+        return ContextCompat.getColor(ctx(), id);
     }
 }
