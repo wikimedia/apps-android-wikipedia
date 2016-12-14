bfe03c65e1c30f228df654297afbc93a2425814a
Chris Zhao <xinyue.zhao@immonet.de>
2016-12-14 22:58:20 +0100

    Fixed null-ptr crash if support actionbar retuns null
    Change-Id: Ifa994dd264173c634a2ae457230c8aec28324624

--- app/src/main/java/org/wikipedia/edit/EditSectionActivity.java
+++ app/src/main/java/org/wikipedia/edit/EditSectionActivity.java
@@ -9,6 +9,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.text.Editable;
 import android.text.TextUtils;
@@ -128,7 +129,10 @@ public class EditSectionActivity extends ThemedActionBarActivity {
         progressDialog.setCancelable(false);
         progressDialog.setMessage(getString(R.string.dialog_saving_in_progress));
 
-        getSupportActionBar().setTitle("");
+        final ActionBar supportActionBar = getSupportActionBar();
+        if (supportActionBar != null) {
+            supportActionBar.setTitle("");
+        }
 
         sectionText = (EditText) findViewById(R.id.edit_section_text);
 
