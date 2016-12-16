4e70c62e5075be8becc54bea949c5e34cbb5a3a3
Dmitry Brant <dbrant@wikimedia.org>
2016-12-16 16:45:25 -0500

    Vectorize still more drawables.
    - Abuse filter icons.
    - Map marker icon.
    - Error icon used in Crash dialog and WikiErrorView (no longer using a
      compound drawable in TextView, for compatibility).
    
    Change-Id: Ia07ab53e7aa99c62b2a5e241e0a142ad89861aac

--- app/src/main/java/org/wikipedia/edit/EditSectionActivity.java
+++ app/src/main/java/org/wikipedia/edit/EditSectionActivity.java
@@ -9,6 +9,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.text.Editable;
@@ -483,12 +484,12 @@ public class EditSectionActivity extends ThemedActionBarActivity {
         }
         if (abusefilterEditResult.getType() == EditAbuseFilterResult.TYPE_ERROR) {
             funnel.logAbuseFilterError(abusefilterEditResult.getCode());
-            abuseFilterImage.setImageResource(R.drawable.abusefilter_disallow);
+            abuseFilterImage.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_abusefilter_disallow));
             abusefilterTitle.setText(getString(R.string.abusefilter_title_disallow));
             abusefilterText.setText(StringUtil.fromHtml(getString(R.string.abusefilter_text_disallow)));
         } else {
             funnel.logAbuseFilterWarning(abusefilterEditResult.getCode());
-            abuseFilterImage.setImageResource(R.drawable.abusefilter_warn);
+            abuseFilterImage.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_abusefilter_warn));
             abusefilterTitle.setText(getString(R.string.abusefilter_title_warn));
             abusefilterText.setText(StringUtil.fromHtml(getString(R.string.abusefilter_text_warn)));
         }
