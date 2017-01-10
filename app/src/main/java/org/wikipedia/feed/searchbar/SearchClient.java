6ca2120bedfa28be8be6df4079b251b77af2f4a2
Stephen Niedzielski <stephen@niedzielski.com>
2017-01-10 10:09:00 -0800

    Hygiene: change SearchClient superclass to DummyClient
    Change-Id: I8b41d7190cbeb9cd60bf5371295ca5e6ab322091

--- app/src/main/java/org/wikipedia/feed/searchbar/SearchClient.java
+++ app/src/main/java/org/wikipedia/feed/searchbar/SearchClient.java
@@ -1,22 +1,11 @@
 package org.wikipedia.feed.searchbar;
 
-import android.content.Context;
-import android.support.annotation.NonNull;
-
 import org.wikipedia.dataclient.WikiSite;
-import org.wikipedia.feed.dataclient.FeedClient;
+import org.wikipedia.feed.dataclient.DummyClient;
 import org.wikipedia.feed.model.Card;
 
-import java.util.Collections;
-
-public class SearchClient implements FeedClient {
-    @Override
-    public void request(@NonNull Context context, @NonNull WikiSite wiki, int age,
-                        @NonNull final FeedClient.Callback cb) {
-        cb.success(Collections.singletonList((Card) new SearchCard()));
-    }
-
-    @Override
-    public void cancel() {
+public class SearchClient extends DummyClient {
+    @Override public Card getNewCard(WikiSite wiki) {
+        return new SearchCard();
     }
 }
