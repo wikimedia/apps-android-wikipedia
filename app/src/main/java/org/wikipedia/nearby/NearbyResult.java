fdc3a9c53ab02e24fa0971e9cfe6b3e40bd923eb
Stephen Niedzielski <stephen@niedzielski.com>
2017-01-20 11:51:06 -0700

    Fix: use app language when new nearby results are requested
    Previously, the nearby screen would use a copy of app language from
    NearbyFragment birth. This caused app language changes to have no effect
    on the map until the Fragment was recreated
    
    Change-Id: I445f54e0984be8adaccfa654946fc9c37d971316

--- app/src/main/java/org/wikipedia/nearby/NearbyResult.java
+++ app/src/main/java/org/wikipedia/nearby/NearbyResult.java
@@ -2,13 +2,21 @@ package org.wikipedia.nearby;
 
 import android.support.annotation.NonNull;
 
+import org.wikipedia.dataclient.WikiSite;
+
 import java.util.List;
 
 class NearbyResult {
-    @NonNull private List<NearbyPage> list;
+    @NonNull private final WikiSite wiki;
+    @NonNull private final List<NearbyPage> list;
 
-    NearbyResult(@NonNull List<NearbyPage> list) {
+    NearbyResult(@NonNull WikiSite wiki, @NonNull List<NearbyPage> list) {
+        this.wiki = wiki;
         this.list = list;
+    }
+
+    @NonNull public WikiSite getWiki() {
+        return wiki;
     }
 
     @NonNull public List<NearbyPage> getList() {
