7aa2e83e4f20527cbf2b1ea1c15944df2b29593c
Stephen Niedzielski <stephen@niedzielski.com>
2017-01-27 18:15:26 -0700

    Hygiene: reorder PageBackStackItem methods
    Change-Id: I2f6eca294f69db197e2f94e41c74561d7f544a11

--- app/src/main/java/org/wikipedia/page/PageBackStackItem.java
+++ app/src/main/java/org/wikipedia/page/PageBackStackItem.java
@@ -7,25 +7,27 @@ import org.wikipedia.model.BaseModel;
 
 public class PageBackStackItem extends BaseModel {
     private final PageTitle title;
-    public PageTitle getTitle() {
-        return title;
-    }
-
     @NonNull private final HistoryEntry historyEntry;
-    @NonNull public HistoryEntry getHistoryEntry() {
-        return historyEntry;
-    }
-
     private int scrollY;
-    public int getScrollY() {
-        return scrollY;
-    }
-    public void setScrollY(int scrollY) {
-        this.scrollY = scrollY;
-    }
 
     public PageBackStackItem(PageTitle title, @NonNull HistoryEntry historyEntry) {
         this.title = title;
         this.historyEntry = historyEntry;
+    }
+
+    public PageTitle getTitle() {
+        return title;
+    }
+
+    @NonNull public HistoryEntry getHistoryEntry() {
+        return historyEntry;
+    }
+
+    public int getScrollY() {
+        return scrollY;
+    }
+
+    public void setScrollY(int scrollY) {
+        this.scrollY = scrollY;
     }
 }
