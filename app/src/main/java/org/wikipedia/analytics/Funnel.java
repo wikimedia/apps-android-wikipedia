wikimedia/apps-android-wikipedia

commit cf6101256c97b8962bb8be7ed5edeab7246fee8a

Hygiene: rename Site to WikiSite
Move org.wikipedia.Site to org.wikipedia.dataclient.WikiSite. No user
visible change intended.

Site appears to have originally been intended as a generic wiki URL. If
not, that's what is expected now as class assumptions around URL
structure are wiki specific, especially language. This patch renames
Site to WikiSite and moves it under the dataclient package to help
alleviate confusion over its purpose.

Site is misused to accept unsupported URLs in some cases. A future
refactor should find and fix these instances. There are also some wiki
corner cases that aren't fully handled around language including: meta,
upload, deployment, and test. Maybe a more generic Site super class
would help.

Additional changes:

• Use "wiki" terminology instead of "site". Retaining site nomenclature
  would be keeping some confusion. Where models are thought to be kept
  as JSON, @SerializedName is used. For volatile storage,
  @SerializedName is not used

• Use WikiSite.forLanguageCode instead of hardcoding *.wikipedia.org

• Removed always null Site member from GalleryCollection

• In tests, replace a couple instances of:

  (WikipediaApp) getInstrumentation().getTargetContext().getApplicationContext()`

  With: `WikipediaApp.getInstance()`

My apologies for the unhappily large diff. I recommend viewing through a
few programs including icdiff and grep.

Change-Id: Ic967146a77b1301c87a0e6fa552c2f70663ca2fe

app/src/main/java/org/wikipedia/analytics/Funnel.java
@@ -2,9 +2,12 @@ package org.wikipedia.analytics;
 
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+
+import com.google.gson.annotations.SerializedName;
+
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.wikipedia.Site;
+import org.wikipedia.dataclient.WikiSite;
 import org.wikipedia.WikipediaApp;
 import org.wikipedia.util.log.L;
 
@@ -27,7 +30,8 @@ import java.util.UUID;
     private final int sampleRate;
     private final String sampleRateRemoteParamName;
     private final WikipediaApp app;
-    @Nullable private final Site site;
+    // todo: remove @SerializedName if not pickled
+    @SerializedName("site") @Nullable private final WikiSite wiki;
 
     private final String sessionToken = UUID.randomUUID().toString();
 
@@ -35,20 +39,20 @@ import java.util.UUID;
         this(app, schemaName, revision, SAMPLE_LOG_ALL);
     }
 
-    /*package*/ Funnel(WikipediaApp app, String schemaName, int revision, @Nullable Site site) {
-        this(app, schemaName, revision, SAMPLE_LOG_ALL, site);
+    /*package*/ Funnel(WikipediaApp app, String schemaName, int revision, @Nullable WikiSite wiki) {
+        this(app, schemaName, revision, SAMPLE_LOG_ALL, wiki);
     }
 
     /*package*/ Funnel(WikipediaApp app, String schemaName, int revision, int sampleRate) {
         this(app, schemaName, revision, sampleRate, null);
     }
 
-    /*package*/ Funnel(WikipediaApp app, String schemaName, int revision, int sampleRate, @Nullable Site site) {
+    /*package*/ Funnel(WikipediaApp app, String schemaName, int revision, int sampleRate, @Nullable WikiSite wiki) {
         this.app = app;
         this.schemaName = schemaName;
         this.revision = revision;
         this.sampleRate = sampleRate;
-        this.site = site;
+        this.wiki = wiki;
         sampleRateRemoteParamName = schemaName + "_rate";
     }
 
@@ -89,7 +93,7 @@ import java.util.UUID;
     }
 
     protected void log(Object... params) {
-        log(site, params);
+        log(wiki, params);
     }
 
     /**
@@ -118,7 +122,7 @@ import java.util.UUID;
      *                      The subclass methods should take more explicit parameters
      *                      depending on what they are logging.
      */
-    protected void log(@Nullable Site site, Object... params) {
+    protected void log(@Nullable WikiSite wiki, Object... params) {
         if (!app.isEventLoggingEnabled()) {
             // Do not send events if the user opted out of EventLogging
             return;
@@ -142,7 +146,7 @@ import java.util.UUID;
                 new EventLoggingEvent(
                         schemaName,
                         revision,
-                        getDBNameForSite(site == null ? getApp().getSite() : site),
+                        app.getUserAgent(),
                         preprocessData(eventData)
                 ).log();
             }
@@ -179,14 +183,14 @@ import java.util.UUID;
     }
 
     /**
-     * Returns db name for given site
+     * Returns db name for given wiki
      *
      * WARNING: HARDCODED TO WORK FOR WIKIPEDIA ONLY
      *
-     * @param site Site object to get dbname for
-     * @return dbname for given site object
+     * @param wiki WikiSite object to get dbname for
+     * @return dbname for given wiki object
      */
-    private String getDBNameForSite(Site site) {
-        return site.languageCode() + "wiki";
+    private String getDBNameForWikiSite(WikiSite wiki) {
+        return wiki.languageCode() + "wiki"; // todo: revise with known exceptions
     }
 }

commit cf4cc71c6af296f654466b613a48d90caea4b16f

Fix setting of wiki parameter in eventlogging.
It looks like cf6101256c caused the app to start incorrectly passing the
userAgent into the `wiki` field, instead of the correct db name of the
wiki.

Change-Id: Ibda3ceb21db1049a0a4fba1ca16b605e37d22723

app/src/main/java/org/wikipedia/analytics/Funnel.java
@@ -147,7 +147,7 @@ import java.util.UUID;
                 new EventLoggingEvent(
                         schemaName,
                         revision,
-                        app.getUserAgent(),
+                        getDBNameForWikiSite(wiki == null ? app.getWikiSite() : wiki),
                         preprocessData(eventData)
                 ).log();
             }