eb320b514cf8e48f6d5549d1270206f022af9039
Michael Holloway <mholloway@wikimedia.org>
2017-02-21 12:15:09 -0500

    Update revert help dialog styling
    Bug: T150477
    Change-Id: I832883b56c656cbdfcf484abc45903f2076a6f98

--- app/src/main/java/org/wikipedia/descriptions/DescriptionEditRevertHelpView.java
+++ app/src/main/java/org/wikipedia/descriptions/DescriptionEditRevertHelpView.java
@@ -4,12 +4,17 @@ import android.content.Context;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.StringRes;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextUtils;
 import android.text.method.LinkMovementMethod;
+import android.text.style.BulletSpan;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
 import org.wikipedia.R;
 import org.wikipedia.WikipediaApp;
+import org.wikipedia.util.DimenUtil;
 import org.wikipedia.util.StringUtil;
 
 import butterknife.BindView;
@@ -23,21 +28,24 @@ public class DescriptionEditRevertHelpView extends ScrollView {
         init(qNumber);
     }
 
-    private void init(@NonNull String qNumber) {
+    @SuppressWarnings("checkstyle:magicnumber") private void init(@NonNull String qNumber) {
         inflate(getContext(), R.layout.view_description_edit_revert_help, this);
         ButterKnife.bind(this);
         helpText.setMovementMethod(new LinkMovementMethod());
 
-        String helpStr = getString(R.string.description_edit_revert_help_body)
+        Spanned helpStr = StringUtil.fromHtml(getString(R.string.description_edit_revert_help_body)
                 .replaceAll(":revertSubtitle", getString(R.string.description_edit_revert_subtitle))
                 .replaceAll(":revertIntro", getString(R.string.description_edit_revert_intro))
-                .replaceAll(":revertReason1",
-                        String.format(getString(R.string.description_edit_revert_reason1), getString(R.string.wikidata_description_guide_url)))
-                .replaceAll(":revertReason2", getString(R.string.description_edit_revert_reason2))
                 .replaceAll(":revertHistory",
-                        String.format(getString(R.string.description_edit_revert_history), getHistoryUri(qNumber)));
+                        String.format(getString(R.string.description_edit_revert_history), getHistoryUri(qNumber))));
 
-        helpText.setText(StringUtil.fromHtml(helpStr));
+        int gapWidth = DimenUtil.roundedDpToPx(8);
+        SpannableString revertReason1 = new SpannableString(StringUtil.fromHtml(String.format(getString(R.string.description_edit_revert_reason1), getString(R.string.wikidata_description_guide_url))));
+        SpannableString revertReason2 = new SpannableString(StringUtil.fromHtml(getString(R.string.description_edit_revert_reason2)));
+        revertReason1.setSpan(new BulletSpan(gapWidth), 0, revertReason1.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        revertReason2.setSpan(new BulletSpan(gapWidth), 0, revertReason2.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        SpannableString helpSpan = new SpannableString(TextUtils.expandTemplate(helpStr, revertReason1, revertReason2));
+        helpText.setText(helpSpan);
     }
 
     private Uri getHistoryUri(@NonNull String qNumber) {
