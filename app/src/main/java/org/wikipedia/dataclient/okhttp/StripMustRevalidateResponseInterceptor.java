e35e717af9f85826bd5a09448d66676269e46d44
Stephen Niedzielski <stephen@niedzielski.com>
2017-03-01 16:37:55 -0700

    Hygiene: consolidate RESTBase and MobileView URL comparisons
    Bug: T156917
    
    Change-Id: I2877a67fd2a3955e270fe2590d2bd23d7376e245

--- app/src/main/java/org/wikipedia/dataclient/okhttp/StripMustRevalidateResponseInterceptor.java
+++ app/src/main/java/org/wikipedia/dataclient/okhttp/StripMustRevalidateResponseInterceptor.java
@@ -1,18 +1,19 @@
 package org.wikipedia.dataclient.okhttp;
 
+import org.wikipedia.dataclient.okhttp.util.HttpUrlUtil;
+
 import java.io.IOException;
 
+import okhttp3.HttpUrl;
 import okhttp3.Interceptor;
 import okhttp3.Response;
 
 class StripMustRevalidateResponseInterceptor implements Interceptor {
     @Override public Response intercept(Interceptor.Chain chain) throws IOException {
         Response rsp = chain.proceed(chain.request());
-        String url = rsp.request().url().toString();
+        HttpUrl url = rsp.request().url();
 
-        boolean restBase = url.contains("/rest_v1/");
-        boolean mobileView = url.contains("action=mobileview");
-        if (restBase || mobileView) {
+        if (HttpUrlUtil.isRestBase(url) || HttpUrlUtil.isMobileView(url)) {
             String cacheControl = CacheControlUtil.removeDirective(rsp.cacheControl().toString(),
                     "must-revalidate");
             rsp = rsp.newBuilder().header("Cache-Control", cacheControl).build();
