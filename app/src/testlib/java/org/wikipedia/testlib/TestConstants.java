1b78f09cf293e04a15676f2764a2870b65efa4d1
Stephen Niedzielski <stephen@niedzielski.com>
2017-03-21 23:38:41 -0600

    Hygiene: limit dev test timeout to 5 seconds
    CI is slow but local dev builds probably aren't. Limit test timeout to 5
    seconds instead of 5 minutes for quick feedback.
    
    Change-Id: I1816ce345ee43b2e818df5fdac98fdf7af6aac92

--- app/src/testlib/java/org/wikipedia/testlib/TestConstants.java
+++ app/src/testlib/java/org/wikipedia/testlib/TestConstants.java
@@ -1,10 +1,12 @@
 package org.wikipedia.testlib;
 
+import org.wikipedia.util.ReleaseUtil;
+
 import java.util.concurrent.TimeUnit;
 
 public final class TestConstants {
     public static final int TIMEOUT_DURATION = 5;
-    public static final TimeUnit TIMEOUT_UNIT = TimeUnit.MINUTES;
+    public static final TimeUnit TIMEOUT_UNIT = ReleaseUtil.isDevRelease() ? TimeUnit.SECONDS : TimeUnit.MINUTES;
 
     private TestConstants() { }
 }
