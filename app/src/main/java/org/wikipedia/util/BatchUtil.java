a4685d2dedf3583eae7b624c1fe078d0ef608e86
Michael Holloway <mholloway@wikimedia.org>
2017-04-07 17:18:34 -0400

    Hygiene: Add TODO to BatchUtil.makeBatches
    Add a note about handling query continuation and batchcomplete correctly.
    
    Our current usage of BatchUtil should be fine if I correctly understand
    the API docs, but it's an issue we should be taking into account
    particularly when working with queries using a generator.
    
    Bug: T162497
    Change-Id: I09218f1e4222433a158ced7f99fd6de424a13d0e

--- app/src/main/java/org/wikipedia/util/BatchUtil.java
+++ app/src/main/java/org/wikipedia/util/BatchUtil.java
@@ -9,6 +9,20 @@ import java.util.List;
 import static org.wikipedia.Constants.API_QUERY_MAX_TITLES;
 
 public final class BatchUtil {
+    // Useful for API requests in which we want a number of results that exceeds the limit for at
+    // least one of the modules being used in the query (e.g., 50 for PageImages).
+    //
+    // TODO: This function does not yet handle batchcomplete.  For requests using a generator
+    // together with properties, the API result may signal to continue because there are more
+    // properties to retrieve for the pages so far, or because there are more pages from the
+    // generator, or both.
+    //
+    // https://www.mediawiki.org/wiki/API:Query#batchcomplete
+    //
+    // Implement continuation/batchcomplete handling if we want to batch requests for a query in
+    // which we are using a generator.
+    //
+    // Bug: T162497
     public static <T> void makeBatches(@NonNull final List<PageTitle> titles,
                                        @NonNull Handler<T> handler,
                                        @NonNull final Callback<T> callback) {
