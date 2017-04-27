aae23f7b9c3a9a0f900d6f9cb2628ebe322c29fe
Jorge Casariego <jorgekasa@gmail.com>
2017-04-27 12:07:18 -0400

    Retrofit GetDescriptionsTask
    Port GetDescriptionTask to Retrofit and try to stay as consistent as possible with the existing code.
    
    Bug: T152405
    Change-Id: I857c8f07a8df09590676629fb5f01bc0e4fcb8c7

---
+++ app/src/test/java/org/wikipedia/wikidata/GetDescriptionClientTest.java
@@ -0,0 +1,103 @@
+package org.wikipedia.wikidata;
+
+import android.support.annotation.NonNull;
+
+import com.google.gson.stream.MalformedJsonException;
+
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.wikipedia.dataclient.WikiSite;
+import org.wikipedia.dataclient.mwapi.MwException;
+import org.wikipedia.dataclient.mwapi.MwQueryPage;
+import org.wikipedia.dataclient.mwapi.MwQueryResponse;
+import org.wikipedia.dataclient.okhttp.HttpStatusException;
+import org.wikipedia.page.PageTitle;
+import org.wikipedia.test.MockWebServerTest;
+
+import java.util.Arrays;
+import java.util.List;
+
+import retrofit2.Call;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Matchers.isA;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+public class GetDescriptionClientTest extends MockWebServerTest {
+    private static final WikiSite WIKISITE_TEST = WikiSite.forLanguageCode("test");
+    private static final PageTitle PAGE_TITLE_BIDEN = new PageTitle("Joe Biden", WIKISITE_TEST);
+    private static final PageTitle PAGE_TITLE_OBAMA = new PageTitle("Barack Obama", WIKISITE_TEST);
+
+    @NonNull private final GetDescriptionsClient subject = new GetDescriptionsClient();
+
+    @Test public void testRequestSuccess() throws Throwable {
+        enqueueFromFile("reading_list_page_info.json");
+
+        GetDescriptionsClient.Callback cb = mock(GetDescriptionsClient.Callback.class);
+        Call<MwQueryResponse<GetDescriptionsClient.QueryResult>> call = request(cb);
+
+        server().takeRequest();
+        ArgumentCaptor<List> captor = ArgumentCaptor.forClass(List.class);
+        verify(cb).success(eq(call), captor.capture());
+
+        List<MwQueryPage> result = captor.getValue();
+        MwQueryPage biden = result.get(0);
+        MwQueryPage obama = result.get(1);
+
+        assertThat(biden.title(), is("Joe Biden"));
+        assertThat(biden.description(), is("47th Vice President of the United States"));
+
+        assertThat(obama.title(), is("Barack Obama"));
+        assertThat(obama.description(), is("44th President of the United States of America"));
+
+    }
+
+    @Test public void testRequestResponseApiError() throws Throwable {
+        enqueueFromFile("api_error.json");
+
+        GetDescriptionsClient.Callback cb = mock(GetDescriptionsClient.Callback.class);
+        Call<MwQueryResponse<GetDescriptionsClient.QueryResult>> call = request(cb);
+
+        server().takeRequest();
+        assertCallbackFailure(call, cb, MwException.class);
+    }
+
+    @Test public void testRequestResponseFailure() throws Throwable {
+        enqueue404();
+
+        GetDescriptionsClient.Callback cb = mock(GetDescriptionsClient.Callback.class);
+        Call<MwQueryResponse<GetDescriptionsClient.QueryResult>> call = request(cb);
+
+        server().takeRequest();
+        assertCallbackFailure(call, cb, HttpStatusException.class);
+    }
+
+    @Test public void testRequestResponseMalformed() throws Throwable {
+        server().enqueue("'");
+
+        GetDescriptionsClient.Callback cb = mock(GetDescriptionsClient.Callback.class);
+        Call<MwQueryResponse<GetDescriptionsClient.QueryResult>> call = request(cb);
+
+        server().takeRequest();
+        assertCallbackFailure(call, cb, MalformedJsonException.class);
+    }
+
+    private void assertCallbackFailure(@NonNull Call<MwQueryResponse<GetDescriptionsClient.QueryResult>> call,
+                                       @NonNull GetDescriptionsClient.Callback cb,
+                                       @NonNull Class<? extends Throwable> throwable) {
+        //noinspection unchecked
+        verify(cb, never()).success(any(Call.class), any(List.class));
+        verify(cb).failure(eq(call), isA(throwable));
+    }
+
+    private Call<MwQueryResponse<GetDescriptionsClient.QueryResult>> request(
+            @NonNull GetDescriptionsClient.Callback cb) {
+        return subject.request(WIKISITE_TEST, service(GetDescriptionsClient.Service.class),
+                Arrays.asList(PAGE_TITLE_BIDEN, PAGE_TITLE_OBAMA), cb);
+    }
+}
