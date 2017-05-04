238d1be2af14f6fd801789a0e75d515f8f134c15
Stephen Niedzielski <stephen@niedzielski.com>
2017-05-04 16:56:50 -0600

    Hygiene: remove some dead JavaScript
    These bridge messages aren't sent or received anywhere:
    
    - requestImagesList
    - imagesListResponse
    - replaceImageSrc
    - replaceImageSources
    
    Remove the dead code.
    
    Change-Id: I48ed9cbc3c4bd953867d2e9f279f4219fc9a0191

--- www/js/main.js
+++ www/js/main.js
@@ -1,36 +1,6 @@
 var bridge = require( "./bridge" );
 var transformer = require("./transformer");
 
-bridge.registerListener( "requestImagesList", function( payload ) {
-    var imageURLs = [];
-    var images = document.querySelectorAll( "img" );
-    for ( var i = 0; i < images.length; i++ ) {
-        if (images[i].width < payload.minsize || images[i].height < payload.minsize) {
-            continue;
-        }
-        imageURLs.push( images[i].src );
-    }
-    bridge.sendMessage( "imagesListResponse", { "images": imageURLs });
-} );
-
-// reusing this function
-function replaceImageSrc( payload ) {
-    var images = document.querySelectorAll( "img[src='" + payload.originalURL + "']" );
-    for ( var i = 0; i < images.length; i++ ) {
-        var img = images[i];
-        img.setAttribute( "src", payload.newURL );
-        img.setAttribute( "data-old-src", payload.originalURL );
-        img.removeAttribute( "srcset" );
-    }
-}
-bridge.registerListener( "replaceImageSrc", replaceImageSrc );
-
-bridge.registerListener( "replaceImageSources", function( payload ) {
-    for ( var i = 0; i < payload.img_map.length; i++ ) {
-        replaceImageSrc( payload.img_map[i] );
-    }
-} );
-
 bridge.registerListener( "setPageProtected", function( payload ) {
     var el = document.getElementsByTagName( "html" )[0];
     if (!el.classList.contains("page-protected") && payload.protect) {
