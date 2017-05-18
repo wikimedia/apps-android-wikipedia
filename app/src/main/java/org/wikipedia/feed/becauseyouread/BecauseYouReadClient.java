cf86909f6e540b3348aae3e4938cdc4439730890
Michael Holloway <mholloway@wikimedia.org>
2017-05-18 14:13:30 -0400

    Replace FullSearchArticlesTask with FullTextSearchClient
    Tests to follow.
    
    Bug: T152402
    Change-Id: Ia1db88bfbed488eb31f86e7057eede48c0809b96

--- app/src/main/java/org/wikipedia/feed/becauseyouread/BecauseYouReadClient.java
+++ app/src/main/java/org/wikipedia/feed/becauseyouread/BecauseYouReadClient.java
@@ -4,19 +4,12 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
-import com.google.gson.annotations.SerializedName;
-
-import org.wikipedia.Constants;
 import org.wikipedia.dataclient.WikiSite;
-import org.wikipedia.dataclient.mwapi.MwException;
-import org.wikipedia.dataclient.mwapi.MwQueryPage;
 import org.wikipedia.dataclient.mwapi.MwQueryResponse;
-import org.wikipedia.dataclient.retrofit.MwCachedService;
-import org.wikipedia.dataclient.retrofit.WikiCachedService;
 import org.wikipedia.feed.dataclient.FeedClient;
-import org.wikipedia.feed.model.Card;
 import org.wikipedia.history.HistoryEntry;
 import org.wikipedia.page.bottomcontent.MainPageReadMoreTopicTask;
+import org.wikipedia.search.FullTextSearchClient;
 import org.wikipedia.search.SearchResult;
 import org.wikipedia.search.SearchResults;
 import org.wikipedia.util.log.L;
@@ -27,34 +20,27 @@ import java.util.Collections;
 import java.util.List;
 
 import retrofit2.Call;
-import retrofit2.Response;
-import retrofit2.http.GET;
-import retrofit2.http.Query;
 
-public class BecauseYouReadClient implements FeedClient {
-    private static final String MORELIKE = "morelike:";
+import static org.wikipedia.Constants.SUGGESTION_REQUEST_ITEMS;
 
-    @NonNull private final WikiCachedService<MwApiSearchClient> cachedService = new MwCachedService<>(MwApiSearchClient.class);
-
+public class BecauseYouReadClient extends FullTextSearchClient implements FeedClient {
     @Nullable private MainPageReadMoreTopicTask readMoreTopicTask;
-    @Nullable private Call<MwQueryResponse<Pages>> readMoreCall;
+    @Nullable private Call<MwQueryResponse<MwQueryResponse.Pages>> fullTextSearchCall;
 
     @Override public void request(@NonNull Context context, @NonNull final WikiSite wiki, int age,
-                        @NonNull final FeedClient.Callback cb) {
+                                  @NonNull final FeedClient.Callback cb) {
         cancel();
         readMoreTopicTask = new MainPageReadMoreTopicTask(context, age) {
-            @Override
-            public void onFinish(@Nullable HistoryEntry entry) {
+            @Override public void onFinish(@Nullable HistoryEntry entry) {
                 if (entry == null) {
-                    cb.error(new IOException("Error fetching suggestions"));
+                    cb.error(new IOException("Error retrieving history entry for suggestions"));
                     return;
                 }
-                getSuggestionsForTitle(wiki, entry, cb);
+                getCardForHistoryEntry(wiki, entry, cb);
             }
 
-            @Override
-            public void onCatch(Throwable caught) {
-                L.w("Error fetching 'because you read' suggestions", caught);
+            @Override public void onCatch(Throwable caught) {
+                L.e("Error fetching 'because you read' suggestions", caught);
                 cb.error(caught);
             }
         };
@@ -66,58 +52,36 @@ public class BecauseYouReadClient implements FeedClient {
             readMoreTopicTask.cancel();
             readMoreTopicTask = null;
         }
-        if (readMoreCall != null) {
-            readMoreCall.cancel();
-            readMoreCall = null;
+        if (fullTextSearchCall != null) {
+            fullTextSearchCall.cancel();
+            fullTextSearchCall = null;
         }
     }
 
-    private void getSuggestionsForTitle(@NonNull WikiSite wiki,
-                                        @NonNull final HistoryEntry entry,
+    private void getCardForHistoryEntry(@NonNull WikiSite wiki, @NonNull final HistoryEntry entry,
                                         final FeedClient.Callback cb) {
-        readMoreCall = cachedService.service(wiki).get(MORELIKE + entry.getTitle().getDisplayText());
-        readMoreCall.enqueue(new retrofit2.Callback<MwQueryResponse<Pages>>() {
-            @Override
-            public void onResponse(Call<MwQueryResponse<Pages>> call,
-                                   Response<MwQueryResponse<Pages>> response) {
-                MwQueryResponse<Pages> pages = response.body();
-                if (pages.success() && pages.query().results(entry.getTitle().getWikiSite()) != null) {
-                    SearchResults results = SearchResults.filter(pages.query().results(entry.getTitle().getWikiSite()), entry.getTitle().getText(), false);
-                    List<BecauseYouReadItemCard> itemCards = new ArrayList<>();
-                    for (SearchResult result : results.getResults()) {
-                        itemCards.add(new BecauseYouReadItemCard(result.getPageTitle()));
-                    }
-                    cb.success(Collections.singletonList((Card) new BecauseYouReadCard(entry, itemCards)));
-                } else if (pages.hasError()) {
-                    cb.error(new MwException(pages.getError()));
-                } else {
-                    cb.error(new IOException("Error fetching suggestions."));
-                }
+        request(wiki, entry.getTitle().getDisplayText(), null, null, SUGGESTION_REQUEST_ITEMS,
+                new FullTextSearchClient.Callback() {
+            @Override public void success(@NonNull Call<MwQueryResponse<MwQueryResponse.Pages>> call,
+                                          @NonNull SearchResults results) {
+                cb.success(Collections.singletonList(toBecauseYouReadCard(results, entry)));
             }
 
-            @Override
-            public void onFailure(Call<MwQueryResponse<Pages>> call, Throwable t) {
-                cb.error(t);
+            @Override public void failure(@NonNull Call<MwQueryResponse<MwQueryResponse.Pages>> call,
+                                          @NonNull Throwable caught) {
+                cb.error(caught);
             }
         });
     }
 
-    static class Pages {
-        @SuppressWarnings("unused")
-        @SerializedName("pages")
-        private List<MwQueryPage> pages;
-        public SearchResults results(WikiSite wiki) {
-            return new SearchResults(pages, wiki);
+    @NonNull private BecauseYouReadCard toBecauseYouReadCard(@NonNull SearchResults results,
+                                                             @NonNull HistoryEntry entry) {
+        List<BecauseYouReadItemCard> itemCards = new ArrayList<>();
+        for (SearchResult result : SearchResults
+                .filter(results, entry.getTitle().getText(), false)
+                .getResults()) {
+            itemCards.add(new BecauseYouReadItemCard(result.getPageTitle()));
         }
-    }
-
-    private interface MwApiSearchClient {
-        @GET("w/api.php?format=json&formatversion=2&action=query"
-                + "&prop=pageterms|pageimages|pageprops&ppprop=mainpage|disambiguation"
-                + "&wbptterms=description&generator=search&gsrnamespace=0&gsrwhat=text"
-                + "&gsrinfo=&gsrprop=redirecttitle&gsrlimit=" + Constants.SUGGESTION_REQUEST_ITEMS
-                + "&piprop=thumbnail&pilicense=any&pithumbsize=" + Constants.PREFERRED_THUMB_SIZE
-                + "&pilimit=" + Constants.SUGGESTION_REQUEST_ITEMS)
-        @NonNull Call<MwQueryResponse<Pages>> get(@Query("gsrsearch") String morelikeQuery);
+        return new BecauseYouReadCard(entry, itemCards);
     }
 }
--- app/src/main/java/org/wikipedia/search/SearchResults.java
+++ app/src/main/java/org/wikipedia/search/SearchResults.java
@@ -1,6 +1,7 @@
 package org.wikipedia.search;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import org.wikipedia.Constants;
 import org.wikipedia.dataclient.WikiSite;
@@ -11,73 +12,75 @@ import org.wikipedia.util.log.L;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
+import static org.apache.commons.lang3.StringUtils.defaultString;
 
 /**
  * Simple Data Object to hold search result data for both prefix search and full text search.
  */
 public class SearchResults {
-    private List<SearchResult> results;
-    private ContinueOffset continueOffset;
-    private String suggestion;
+    @NonNull private List<SearchResult> results;
+    @Nullable private Map<String, String> continuation;
+    @NonNull private String suggestion = "";
 
     /**
      * Empty results. Use for fallback when something goes wrong.
      */
     public SearchResults() {
         results = new ArrayList<>();
-        continueOffset = null;
-        suggestion = "";
+        continuation = null;
     }
 
     /**
      * Constructor for a list of Retrofit results from BecauseYouReadTask.
      */
-    public SearchResults(List<MwQueryPage> pages, WikiSite wiki) {
+    public SearchResults(@NonNull List<MwQueryPage> pages, @NonNull WikiSite wiki,
+                         @Nullable Map<String, String> continuation, @Nullable String suggestion) {
         List<SearchResult> searchResults = new ArrayList<>();
         for (MwQueryPage page : pages) {
             searchResults.add(new SearchResult(page, wiki));
         }
         this.results = searchResults;
-        this.continueOffset = null;
-        this.suggestion = "";
+        this.continuation = continuation;
+        this.suggestion = defaultString(suggestion);
     }
 
     /**
      * Constructor for when we get results.
      * @param results the actual results
-     * @param continueOffset for search continuation
+     * @param continuation info for search continuation
      * @param suggestion a search suggestion to show to the user: "Did you mean ...?"
      */
-    public SearchResults(List<SearchResult> results,
-                         ContinueOffset continueOffset,
-                         String suggestion) {
+    public SearchResults(@NonNull List<SearchResult> results,
+                         @Nullable Map<String, String> continuation,
+                         @Nullable String suggestion) {
         this.results = results;
-        this.continueOffset = continueOffset;
-        this.suggestion = suggestion;
+        this.continuation = continuation;
+        this.suggestion = defaultString(suggestion);
     }
 
-    public List<SearchResult> getResults() {
+    @NonNull public List<SearchResult> getResults() {
         return results;
     }
 
-    public String getSuggestion() {
+    @NonNull public String getSuggestion() {
         return suggestion;
     }
 
-    public ContinueOffset getContinueOffset() {
-        return continueOffset;
+    @Nullable public Map<String, String> getContinuation() {
+        return continuation;
     }
 
     /**
-     * Filter the list of suggestions to make sure the original page title isn't one of them,
-     * as well as whether the suggestion contains a thumbnail.
+     * Filter the list of results to make sure the original page title isn't one of them
+     * and the suggestions have thumbnails if required.
      *
      * @param searchResults original results from server
      * @return filtered results
      */
-    @NonNull
-    public static SearchResults filter(SearchResults searchResults, String title,
-                                       boolean requireThumbnail) {
+    @NonNull public static SearchResults filter(SearchResults searchResults, String title,
+                                                boolean requireThumbnail) {
         final boolean verbose = ReleaseUtil.isDevRelease();
         List<SearchResult> filteredResults = new ArrayList<>();
         List<SearchResult> results = searchResults.getResults();
@@ -95,15 +98,6 @@ public class SearchResults {
             }
         }
         return new SearchResults(filteredResults, null, null);
-    }
-
-
-    /**
-     * Continuation markers to pass around between search requests.
-     * All internal to the actual search type, so see implementation.
-     */
-    public static class ContinueOffset {
-
     }
 }
 
