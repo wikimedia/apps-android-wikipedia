08117d2df073778e00d8508fcadf3f4fb06909bd
Michael Holloway <mholloway@wikimedia.org>
2017-05-31 18:08:11 -0400

    [Whoops] Restore search results filtering tests
    There were some nice unit tests for filtering behavior in SuggestionsTask-
    Test that were lumped in with an AsyncTask call test and got deleted in
    I4bde23e.  This restores them in a new test class.
    
    Change-Id: I6a39211ec84b963ab43c664149f6a6a4bee5821d

---
+++ app/src/test/java/org/wikipedia/search/SearchResultsFilterTest.java
@@ -0,0 +1,51 @@
+package org.wikipedia.search;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.wikipedia.Constants;
+import org.wikipedia.dataclient.WikiSite;
+import org.wikipedia.page.PageTitle;
+import org.wikipedia.test.TestRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(TestRunner.class) public class SearchResultsFilterTest {
+    private static final WikiSite WIKI = WikiSite.forLanguageCode("test");
+
+    @Test public void testFilterNoResults() {
+        List<SearchResult> originalResults = new ArrayList<>();
+        checkFilter(0, originalResults);
+    }
+
+    @Test public void testFilter1ResultSameAsTitleIgnoreCase() {
+        List<SearchResult> originalResults = new ArrayList<>();
+        originalResults.add(new SearchResult(new PageTitle("Test", WIKI, null, null)));
+        checkFilter(0, originalResults);
+    }
+
+    @Test public void testFilter1ResultDifferentFromTitle() {
+        List<SearchResult> originalResults = new ArrayList<>();
+        originalResults.add(new SearchResult(new PageTitle("something else", WIKI, null, null)));
+        checkFilter(1, originalResults);
+    }
+
+    @Test public void testFilter4ResultsDifferentFromTitle() {
+        List<SearchResult> originalResults = new ArrayList<>();
+        originalResults.add(new SearchResult(new PageTitle("something else", WIKI, null, null)));
+        originalResults.add(new SearchResult(new PageTitle("something else", WIKI, null, null)));
+        originalResults.add(new SearchResult(new PageTitle("something else", WIKI, null, null)));
+        originalResults.add(new SearchResult(new PageTitle("something else", WIKI, null, null)));
+        checkFilter(Constants.MAX_SUGGESTION_RESULTS, originalResults);
+    }
+
+    private void checkFilter(int expected, List<SearchResult> originalResults) {
+        String title = "test";
+        SearchResults searchResults = new SearchResults(originalResults, null, null);
+        List<SearchResult> filteredList = SearchResults.filter(searchResults, title, false).getResults();
+        assertThat(expected, is(filteredList.size()));
+    }
+}
