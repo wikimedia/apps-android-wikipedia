496d43bcea03f36e27fb1af00800ed3ba41b222f
Dmitry Brant <dbrant@wikimedia.org>
2017-06-13 14:47:49 -0400

    Introduce a general-purpose onboarding view.
    This introduces a view that can be slipped into various portions of our UX
    where any kind of onboarding or one-time acknowledgment is desired.
    
    It allows for a Title, Text, and a Positive and Negative action button,
    with some customizability of each of these elements.
    
    Change-Id: I9ff6ede76fe2b8fd0baf192dc9d04aae098f76b9

---
+++ app/src/main/java/org/wikipedia/onboarding/OnboardingView.java
@@ -0,0 +1,75 @@
+package org.wikipedia.onboarding;
+
+import android.content.Context;
+import android.support.annotation.ColorRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.StringRes;
+import android.support.v4.content.ContextCompat;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.wikipedia.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class OnboardingView extends LinearLayout {
+    public interface Callback {
+        void onPositiveAction();
+        void onNegativeAction();
+    }
+
+    @BindView(R.id.view_onboarding_title) TextView titleView;
+    @BindView(R.id.view_onboarding_text) TextView textView;
+    @BindView(R.id.view_onboarding_action_positive) TextView actionViewPositive;
+    @BindView(R.id.view_onboarding_action_negative) TextView actionViewNegative;
+
+    @Nullable private Callback callback;
+
+    public OnboardingView(@NonNull Context context) {
+        super(context);
+        setOrientation(VERTICAL);
+        inflate(context, R.layout.view_onboarding, this);
+        ButterKnife.bind(this);
+    }
+
+    public void setCallback(@Nullable Callback callback) {
+        this.callback = callback;
+    }
+
+    public void setTitle(@StringRes int id) {
+        titleView.setText(id);
+    }
+
+    public void setText(@StringRes int id) {
+        textView.setText(id);
+    }
+
+    public void setPositiveAction(@StringRes int id) {
+        actionViewPositive.setText(id);
+    }
+
+    public void setPositiveTextColor(@ColorRes int id) {
+        actionViewPositive.setTextColor(ContextCompat.getColor(getContext(), id));
+    }
+
+    public void setNegativeAction(@StringRes int id) {
+        actionViewNegative.setText(id);
+    }
+
+    @OnClick(R.id.view_onboarding_action_positive)
+    void onPositiveActionClick() {
+        if (callback != null) {
+            callback.onPositiveAction();
+        }
+    }
+
+    @OnClick(R.id.view_onboarding_action_negative)
+    void onNegativeActionClick() {
+        if (callback != null) {
+            callback.onNegativeAction();
+        }
+    }
+}
