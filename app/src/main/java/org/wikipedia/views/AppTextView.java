dff97bbbba7dccecf18ce270ee22e3c301a4a1a2
Dmitry Brant <dbrant@wikimedia.org>
2017-06-20 10:44:10 -0400

    Fix possible crash when highlighting text on API 23.
    https://rink.hockeyapp.net/manage/apps/226649/app_versions/204/crash_reasons/173095781
    
    This occurs only on Android 6.0 (API 23):
    To reproduce:
    Find any TextView that has text selection enabled, and that has a
    LinkMovementMethod associated with it. Then long-press to select some text
    inside it, and then click away from the selection (on another part of the
    same TextView). It crashes with an IllegalArgumentException.
    
    This workaround patches the internals of the TextView itself. We already
    have a "custom" patched TextView component, which is AppTextView, so we
    can apply the fix there, and just switch all instances of TextViews that
    have selection enabled to become AppTextViews.
    
    Change-Id: If2f24c4cd3a799c5ecd2e32d29372ba9b2c1fd83

--- app/src/main/java/org/wikipedia/views/AppTextView.java
+++ app/src/main/java/org/wikipedia/views/AppTextView.java
@@ -1,7 +1,9 @@
 package org.wikipedia.views;
 
 import android.content.Context;
+import android.os.Build;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 
 public class AppTextView extends ConfigurableTextView {
 
@@ -15,6 +17,21 @@ public class AppTextView extends ConfigurableTextView {
 
     public AppTextView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        // Workaround for https://code.google.com/p/android/issues/detail?id=191430
+        // which only occurs on API 23
+        if (Build.VERSION.SDK_INT == Build.VERSION_CODES.M) {
+            if (event.getActionMasked() == MotionEvent.ACTION_DOWN
+                    && getSelectionStart() != getSelectionEnd()) {
+                CharSequence text = getText();
+                setText(null);
+                setText(text);
+            }
+        }
+        return super.dispatchTouchEvent(event);
     }
 
     @Override
