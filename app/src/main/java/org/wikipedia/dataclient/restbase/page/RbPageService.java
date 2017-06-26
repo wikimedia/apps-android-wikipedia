ed75cf8fb1ad16705cbf765bf818d77416374aef
Michael Holloway <mholloway@wikimedia.org>
2017-06-26 15:29:40 -0400

    Update MCS content format versions for mobile-sections and definitions
    This will ensure we get the latest version of MCS content, which now
    includes redlink hiding!
    
    Bug: T119266
    Change-Id: I3c281362f847d5bccafa54f5b0da1ea5fd155b02

--- app/src/main/java/org/wikipedia/dataclient/restbase/page/RbPageService.java
+++ app/src/main/java/org/wikipedia/dataclient/restbase/page/RbPageService.java
@@ -22,8 +22,8 @@ import static org.wikipedia.Constants.ACCEPT_HEADER_SUMMARY;
  * Retrofit service for mobile content service endpoints.
  */
 public interface RbPageService {
-    String ACCEPT_HEADER_MOBILE_SECTIONS = ACCEPT_HEADER_PREFIX + "mobile-sections/0.10.2\"";
-    String ACCEPT_HEADER_DEFINITION = ACCEPT_HEADER_PREFIX + "definition/0.7.1\"";
+    String ACCEPT_HEADER_MOBILE_SECTIONS = ACCEPT_HEADER_PREFIX + "mobile-sections/0.11.0\"";
+    String ACCEPT_HEADER_DEFINITION = ACCEPT_HEADER_PREFIX + "definition/0.7.2\"";
 
     /**
      * Gets a page summary for a given title -- for link previews
