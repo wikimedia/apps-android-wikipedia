039a9eba400622851e56b9f4d959f6878ef0ecd5
Stephen Niedzielski <stephen@niedzielski.com>
2017-07-05 17:46:24 -0500

    Update: add / remove dark mode class from root element
    Toggling dark mode now adds a `dark` class to the root HTML node which
    allows for pure CSS presentation changes. The code is modeled after the
    RTL and page protection implementations and required by the lazily
    loaded images implementation.
    
    Bug: T164429
    Change-Id: I222027248a6c399b516a198323d30294f6ab36ec

--- app/src/main/assets/bundle.js
+++ app/src/main/assets/bundle.js
@@ -684,7 +684,10 @@ var transformer = require("../transformer");
 var dark = require("../dark");
 
 transformer.register( "addDarkModeStyles", function( content ) {
+    var html = document.getElementsByTagName( 'html' )[0];
+    html.classList.remove( 'dark' );
     if ( window.isDarkMode ) {
+        html.classList.add( 'dark' );
         dark.setImageBackgroundsForDarkMode ( content );
     }
 } );
