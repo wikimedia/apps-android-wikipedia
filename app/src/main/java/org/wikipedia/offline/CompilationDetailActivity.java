959718e1ddbda1d1b3daf6d54995916727bf9da3
Michael Holloway <mholloway@wikimedia.org>
2017-07-21 13:57:34 -0400

    Add offline compilation detail view
    Next: add the download management component.
    
    Bug: T163594
    Change-Id: I0f31bcb3eaa9625c26c60998004ef64e958d9809

--- app/src/main/java/org/wikipedia/offline/CompilationDetailActivity.java
+++ app/src/main/java/org/wikipedia/offline/CompilationDetailActivity.java
@@ -2,18 +2,35 @@ package org.wikipedia.offline;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.view.WindowManager;
 
 import org.wikipedia.activity.SingleFragmentActivity;
+import org.wikipedia.json.GsonMarshaller;
+import org.wikipedia.json.GsonUnmarshaller;
 
 public class CompilationDetailActivity extends SingleFragmentActivity<CompilationDetailFragment> {
+    protected static final String EXTRA_COMPILATION = "compilation";
 
-    public static Intent newIntent(@NonNull Context context) {
-        return new Intent(context, CompilationDetailActivity.class);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+    }
+
+    public static Intent newIntent(@NonNull Context context, @NonNull Compilation comp) {
+        return new Intent(context, CompilationDetailActivity.class)
+                .putExtra(EXTRA_COMPILATION, GsonMarshaller.marshal(comp));
     }
 
     @Override
     protected CompilationDetailFragment createFragment() {
-        return CompilationDetailFragment.newInstance();
+        return CompilationDetailFragment.newInstance(GsonUnmarshaller.unmarshal(Compilation.class,
+                getIntent().getStringExtra(EXTRA_COMPILATION)));
     }
 }
--- app/src/main/java/org/wikipedia/offline/CompilationDetailFragment.java
+++ app/src/main/java/org/wikipedia/offline/CompilationDetailFragment.java
@@ -1,12 +1,98 @@
 package org.wikipedia.offline;
 
+import android.net.Uri;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.wikipedia.R;
+import org.wikipedia.json.GsonMarshaller;
+import org.wikipedia.json.GsonUnmarshaller;
+import org.wikipedia.util.DimenUtil;
+import org.wikipedia.util.GradientUtil;
+import org.wikipedia.views.FaceAndColorDetectImageView;
+import org.wikipedia.views.ViewUtil;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+import static org.wikipedia.offline.CompilationDetailActivity.EXTRA_COMPILATION;
+import static org.wikipedia.util.DateUtil.getShortDateString;
+import static org.wikipedia.util.DimenUtil.leadImageHeightForDevice;
+import static org.wikipedia.util.FileUtil.bytesToGB;
 
 public class CompilationDetailFragment extends Fragment {
+    @BindView(R.id.compilation_detail_toolbar) Toolbar toolbar;
+    @BindView(R.id.compilation_detail_header_image) FaceAndColorDetectImageView imageView;
+    @BindView(R.id.view_compilation_info_title) TextView nameView;
+    @BindView(R.id.view_compilation_info_date_size) TextView dateSizeView;
+    @BindView(R.id.view_compilation_info_summary) TextView summaryView;
+    @BindView(R.id.view_compilation_info_description) TextView descriptionView;
+    @BindView(R.id.button_compilation_detail_download) TextView downloadButton;
+
+    private Unbinder unbinder;
 
     @NonNull
-    public static CompilationDetailFragment newInstance() {
-        return new CompilationDetailFragment();
+    public static CompilationDetailFragment newInstance(@NonNull Compilation info) {
+        CompilationDetailFragment instance = new CompilationDetailFragment();
+        Bundle args = new Bundle();
+        args.putString(EXTRA_COMPILATION, GsonMarshaller.marshal(info));
+        instance.setArguments(args);
+        return instance;
+    }
+
+    @Nullable @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_compilation_detail, container, false);
+        unbinder = ButterKnife.bind(this, view);
+
+        ViewUtil.setTopPaddingDp(toolbar, (int) DimenUtil.getTranslucentStatusBarHeight(getContext()));
+        ViewUtil.setBackgroundDrawable(toolbar, GradientUtil.getCubicGradient(
+                ContextCompat.getColor(getContext(), R.color.lead_gradient_start), Gravity.TOP));
+        getAppCompatActivity().setSupportActionBar(toolbar);
+        getAppCompatActivity().getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getAppCompatActivity().getSupportActionBar().setTitle("");
+
+        Compilation comp = GsonUnmarshaller.unmarshal(Compilation.class,
+                getActivity().getIntent().getStringExtra(EXTRA_COMPILATION));
+
+        Uri imageUri = comp.thumbUri();
+        int height = imageUri == null ? DimenUtil.getContentTopOffsetPx(getContext()) : leadImageHeightForDevice();
+        if (imageUri == null) {
+            toolbar.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.main_toolbar_background));
+        }
+        DimenUtil.setViewHeight(imageView, height);
+
+        imageView.loadImage(imageUri);
+        nameView.setText(comp.name());
+        dateSizeView.setText(String.format(getString(R.string.offline_compilation_detail_date_size),
+                getShortDateString(comp.timestamp()), bytesToGB(comp.size())));
+        summaryView.setText(comp.summary());
+        descriptionView.setText(comp.description());
+        downloadButton.setText(String.format(getString(R.string.offline_compilation_detail_button_download),
+                bytesToGB(comp.size())));
+
+        return view;
+    }
+
+    @Override public void onDestroyView() {
+        unbinder.unbind();
+        unbinder = null;
+        super.onDestroyView();
+    }
+
+    private AppCompatActivity getAppCompatActivity() {
+        return (AppCompatActivity) getActivity();
     }
 }
