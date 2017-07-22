b39ca15928a1c867c77f739ddefed6608a2b7e71
Stephen Niedzielski <stephen@niedzielski.com>
2017-07-21 23:16:36 -0500

    Update: dim images in dark mode
    Change-Id: Ic78c5b9a56bc6088e067cf2296b383a8c701f2cb

--- app/src/main/assets/bundle.js
+++ app/src/main/assets/bundle.js
@@ -134,12 +134,13 @@ var bridge = require("./bridge");
 var pagelib = require("wikimedia-page-library");
 
 bridge.registerListener( 'toggleDarkMode', function() {
-   var theme;
+    var theme;
 
-   window.isDarkMode = !window.isDarkMode;
+    window.isDarkMode = !window.isDarkMode;
 
-   theme = window.isDarkMode ? pagelib.ThemeTransform.THEME.DARK : pagelib.ThemeTransform.THEME.DEFAULT;
-   pagelib.ThemeTransform.setTheme( document, theme );
+    theme = window.isDarkMode ? pagelib.ThemeTransform.THEME.DARK : pagelib.ThemeTransform.THEME.DEFAULT;
+    pagelib.ThemeTransform.setTheme( document, theme );
+    pagelib.DimImagesTransform.dim( window, window.isDarkMode );
 } );
 },{"./bridge":2,"wikimedia-page-library":20}],4:[function(require,module,exports){
 var transformer = require('./transformer');
