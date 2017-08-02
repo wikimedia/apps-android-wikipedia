d8929db637ba76c76bd7fa5eda77b5a2c7f9c788
Dmitry Brant <dbrant@wikimedia.org>
2017-08-02 12:16:43 -0400

    Update design of compilation layouts for better scrollability.
    Change-Id: Ice2c21355957a7822df68698b6b433ee1c405e63

--- app/src/main/java/org/wikipedia/offline/CompilationDetailFragment.java
+++ app/src/main/java/org/wikipedia/offline/CompilationDetailFragment.java
@@ -1,11 +1,9 @@
 package org.wikipedia.offline;
 
 import android.graphics.Color;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
@@ -18,7 +16,6 @@ import org.wikipedia.R;
 import org.wikipedia.gallery.MediaDownloadReceiver;
 import org.wikipedia.json.GsonMarshaller;
 import org.wikipedia.json.GsonUnmarshaller;
-import org.wikipedia.util.DimenUtil;
 import org.wikipedia.util.GradientUtil;
 import org.wikipedia.views.FaceAndColorDetectImageView;
 import org.wikipedia.views.ViewUtil;
@@ -30,12 +27,10 @@ import butterknife.Unbinder;
 
 import static org.wikipedia.offline.CompilationDetailActivity.EXTRA_COMPILATION;
 import static org.wikipedia.util.DateUtil.getShortDateString;
-import static org.wikipedia.util.DimenUtil.leadImageHeightForDevice;
 import static org.wikipedia.util.FileUtil.bytesToGB;
 
 public class CompilationDetailFragment extends DownloadObserverFragment {
     @BindView(R.id.compilation_detail_toolbar) Toolbar toolbar;
-    @BindView(R.id.compilation_detail_header_container) View headerContainerView;
     @BindView(R.id.compilation_detail_header_image) FaceAndColorDetectImageView imageView;
     @BindView(R.id.compilation_detail_header_gradient) View gradientView;
     @BindView(R.id.view_compilation_info_title) TextView nameView;
@@ -65,7 +60,6 @@ public class CompilationDetailFragment extends DownloadObserverFragment {
         View view = inflater.inflate(R.layout.fragment_compilation_detail, container, false);
         unbinder = ButterKnife.bind(this, view);
 
-        ViewUtil.setTopPaddingDp(toolbar, (int) DimenUtil.getTranslucentStatusBarHeight(getContext()));
         getAppCompatActivity().setSupportActionBar(toolbar);
         getAppCompatActivity().getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getAppCompatActivity().getSupportActionBar().setTitle("");
@@ -73,15 +67,9 @@ public class CompilationDetailFragment extends DownloadObserverFragment {
         compilation = GsonUnmarshaller.unmarshal(Compilation.class,
                 getActivity().getIntent().getStringExtra(EXTRA_COMPILATION));
 
-        Uri featureImageUri = compilation.featureImageUri();
-        int height = featureImageUri == null ? DimenUtil.getContentTopOffsetPx(getContext()) : leadImageHeightForDevice();
-        if (featureImageUri == null) {
-            toolbar.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.main_toolbar_background));
-        }
-        DimenUtil.setViewHeight(headerContainerView, height);
         ViewUtil.setBackgroundDrawable(gradientView, GradientUtil.getCubicGradient(Color.BLACK, Gravity.TOP));
 
-        imageView.loadImage(featureImageUri);
+        imageView.loadImage(compilation.featureImageUri());
         nameView.setText(compilation.name());
         dateSizeView.setText(String.format(getString(R.string.offline_compilation_detail_date_size),
                 getShortDateString(compilation.timestamp()), bytesToGB(compilation.size())));
