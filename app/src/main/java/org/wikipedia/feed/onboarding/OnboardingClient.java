757c67275c26bb090314ccaced3f8f60b2f4ccb3
Cooltey Feng <cfeng@wikimedia.org>
2017-11-13 16:28:49 -0800

    Revise onboarding and user education cards in sideloading Offline Library  - Remove OnboardingClient() from FeedCoordinator  - Add sideloading version text  - Set default value of offline tutorial as false
    Bug: T179203
    Change-Id: I6b0367c8fc6aa9fc8c2c9ced9ee0a40306fa168e

--- app/src/main/java/org/wikipedia/feed/onboarding/OnboardingClient.java
+++ app/src/main/java/org/wikipedia/feed/onboarding/OnboardingClient.java
@@ -3,12 +3,9 @@ package org.wikipedia.feed.onboarding;
 import android.content.Context;
 import android.support.annotation.NonNull;
 
-import org.wikipedia.R;
 import org.wikipedia.dataclient.WikiSite;
-import org.wikipedia.feed.announcement.Announcement;
 import org.wikipedia.feed.dataclient.FeedClient;
 import org.wikipedia.feed.model.Card;
-import org.wikipedia.util.UriUtil;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,15 +15,15 @@ public class OnboardingClient implements FeedClient {
                                   @NonNull FeedClient.Callback cb) {
         List<Card> cards = new ArrayList<>();
 
-        OnboardingCard card = new OfflineOnboardingCard(new Announcement(
-                "offlineOnboardingCard1",
-                context.getString(R.string.offline_library_onboarding_text),
-                "https://upload.wikimedia.org/wikipedia/commons/5/5b/Illustration-OfflineLibraryPromo2_3x.png",
-                new Announcement.Action(context.getString(R.string.offline_library_onboarding_action),
-                        UriUtil.LOCAL_URL_OFFLINE_LIBRARY)));
-        if (card.shouldShow()) {
-            cards.add(card);
-        }
+//        OnboardingCard card = new OfflineOnboardingCard(new Announcement(
+//                "offlineOnboardingCard1",
+//                context.getString(R.string.offline_library_onboarding_text),
+//                "https://upload.wikimedia.org/wikipedia/commons/5/5b/Illustration-OfflineLibraryPromo2_3x.png",
+//                new Announcement.Action(context.getString(R.string.offline_library_onboarding_action),
+//                        UriUtil.LOCAL_URL_OFFLINE_LIBRARY)));
+//        if (card.shouldShow()) {
+//            cards.add(card);
+//        }
 
         cb.success(cards);
     }
