baf532b10342d199f048549724dc85745c4b4044
Dmitry Brant <dbrant@wikimedia.org>
2017-11-17 11:39:24 -0500

    Include syntax highlighting CSS styles.
    This introduces the inclusion of CSS content from the resource module of
    ext.pygments, which ensures that syntax-highlighted code in articles will
    be styles correctly.
    
    Bug: T180332
    Change-Id: I4ef004e96339f767804222308987eadaf2e83d0d

--- scripts/make-css-assets.bash
+++ scripts/make-css-assets.bash
@@ -6,5 +6,5 @@ PREFIX="https://www.mediawiki.org/w"
 
 BASE_PATH="`dirname $0`/.."
 
-wget "$PREFIX/load.php?debug=false&lang=en&modules=skins.minerva.base.reset|skins.minerva.content.styles|ext.cite.style|ext.math.styles|ext.timeline.styles|mediawiki.page.gallery.styles|mobile.app.pagestyles.android&only=styles&version=&*" -O "$BASE_PATH/app/src/main/assets/styles.css"
-wget "$PREFIX/load.php?debug=false&lang=en&modules=skins.minerva.base.reset|skins.minerva.content.styles|ext.cite.style|ext.math.styles|ext.timeline.styles|mediawiki.page.gallery.styles|mobile.app.preview.android|mobile.app.preview&only=styles&version=&*" -O "$BASE_PATH/app/src/main/assets/preview.css"
+wget "$PREFIX/load.php?debug=false&lang=en&modules=skins.minerva.base.reset|skins.minerva.content.styles|ext.cite.style|ext.pygments|ext.math.styles|ext.timeline.styles|mediawiki.page.gallery.styles|mobile.app.pagestyles.android&only=styles&version=&*" -O "$BASE_PATH/app/src/main/assets/styles.css"
+wget "$PREFIX/load.php?debug=false&lang=en&modules=skins.minerva.base.reset|skins.minerva.content.styles|ext.cite.style|ext.pygments|ext.math.styles|ext.timeline.styles|mediawiki.page.gallery.styles|mobile.app.preview.android|mobile.app.preview&only=styles&version=&*" -O "$BASE_PATH/app/src/main/assets/preview.css"
