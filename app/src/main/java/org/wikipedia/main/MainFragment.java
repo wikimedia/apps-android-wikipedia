7e6d25439755ee046c2a8d2c0ae434a2f885fdc8
Dmitry Brant <dbrant@wikimedia.org>
2017-11-27 08:27:25 -0500

    Scroll feed to top when Explore tab clicked again.
    Bug: T181394
    Change-Id: Iaa856e8de921614c4d9962679bbff7e77a34e653

--- app/src/main/java/org/wikipedia/main/MainFragment.java
+++ app/src/main/java/org/wikipedia/main/MainFragment.java
@@ -14,12 +14,10 @@ import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -108,12 +106,13 @@ public class MainFragment extends Fragment implements BackPressedHandler, FeedFr
         unbinder = ButterKnife.bind(this, view);
 
         viewPager.setAdapter(new NavTabFragmentPagerAdapter(getChildFragmentManager()));
-        tabLayout.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                viewPager.setCurrentItem(item.getOrder());
-                return true;
+        tabLayout.setOnNavigationItemSelectedListener(item -> {
+            Fragment fragment = ((NavTabFragmentPagerAdapter) viewPager.getAdapter()).getCurrentFragment();
+            if (fragment instanceof FeedFragment && item.getOrder() == 0) {
+                ((FeedFragment) fragment).scrollToTop();
             }
+            viewPager.setCurrentItem(item.getOrder());
+            return true;
         });
 
         if (savedInstanceState == null) {
