5fff0abd05c915c7f759603452bf6df906ec824e
cooltey <cfeng@wikimedia.org>
2017-12-01 16:46:10 -0800

    [SPIKE] Sample codes of EspressoTest + screenshots comparison
    1. Run a espresso test from Feed page => change search language to "test" => select "test" => search "obama" => go to first result
    2. Do a simple bitmap comparison with its pixel to RGB
    3. Export an image of result of comparison to the device folder
    
    * Setup note: https://www.mediawiki.org/wiki/User:CFeng_(WMF)/EspressoTest_with_Screenshots_Comparison
    * The reference screenshots are device dependant, and could be updated in the assets/espresso folder
    
    Bug: T176860
    Change-Id: I184a359ffc84884f821811b7d652b4ac86480871

---
+++ app/src/androidTest/java/org/wikipedia/espresso/SampleOfTests.java
@@ -0,0 +1,74 @@
+package org.wikipedia.espresso;
+
+
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.wikipedia.R;
+import org.wikipedia.espresso.onboarding.OnBoardingTest;
+import org.wikipedia.espresso.search.SearchTest;
+import org.wikipedia.espresso.util.CompareTools;
+import org.wikipedia.espresso.util.ScreenshotTools;
+import org.wikipedia.main.MainActivity;
+
+import static org.wikipedia.espresso.Constants.WIKIPEDIA_APP_TEST_COMPARE_ALLOWANCE_NUMBER;
+import static org.wikipedia.espresso.util.ViewTools.WAIT_FOR_2000;
+import static org.wikipedia.espresso.util.ViewTools.WAIT_FOR_500;
+import static org.wikipedia.espresso.util.ViewTools.pressBack;
+import static org.wikipedia.espresso.util.ViewTools.rotateScreen;
+import static org.wikipedia.espresso.util.ViewTools.viewIsDisplayed;
+import static org.wikipedia.espresso.util.ViewTools.waitFor;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class SampleOfTests {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void sampleOfTests() {
+
+        // run OnBoarding on every tests
+        OnBoardingTest.runOnBoarding();
+
+        while (!viewIsDisplayed(R.id.fragment_feed_feed)) {
+            // press back until back to the feed page
+            pressBack();
+        }
+
+        ScreenshotTools.snap("FeedPage");
+        waitFor(WAIT_FOR_2000);
+
+        // test orientation
+        rotateScreen(getActivity(), ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        waitFor(WAIT_FOR_2000);
+        ScreenshotTools.snap("FeedPage_Landscape");
+        rotateScreen(getActivity(), ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        waitFor(WAIT_FOR_2000);
+        ScreenshotTools.snap("FeedPage_Portrait");
+
+        SearchTest.searchKeywordAndGo("Cat");
+
+        if (!viewIsDisplayed(R.id.page_toc_drawer)) {
+            waitFor(WAIT_FOR_500);
+        }
+
+        // TODO: find a better way to generate reference images
+        // Reference images should be put under /assets/espresso folder
+        Assert.assertTrue("Matching percentage should be higher than " + WIKIPEDIA_APP_TEST_COMPARE_ALLOWANCE_NUMBER, CompareTools.compare(getActivity(), "FeedPage") > WIKIPEDIA_APP_TEST_COMPARE_ALLOWANCE_NUMBER);
+        Assert.assertTrue("Matching percentage should be higher than " + WIKIPEDIA_APP_TEST_COMPARE_ALLOWANCE_NUMBER, CompareTools.compare(getActivity(), "SearchSuggestionPage") > WIKIPEDIA_APP_TEST_COMPARE_ALLOWANCE_NUMBER);
+        Assert.assertTrue("Matching percentage should be higher than " + WIKIPEDIA_APP_TEST_COMPARE_ALLOWANCE_NUMBER, CompareTools.compare(getActivity(), "SearchPage") > WIKIPEDIA_APP_TEST_COMPARE_ALLOWANCE_NUMBER);
+    }
+
+    private Activity getActivity() {
+        return mActivityTestRule.getActivity();
+    }
+}
---
+++ app/src/androidTest/java/org/wikipedia/espresso/util/CompareTools.java
@@ -0,0 +1,95 @@
+package org.wikipedia.espresso.util;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.os.Environment;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.runner.RunWith;
+import org.wikipedia.util.MathUtil;
+import org.wikipedia.util.log.L;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import static org.wikipedia.espresso.Constants.WIKIPEDIA_APP_TEST_ASSET_FOLDER;
+import static org.wikipedia.espresso.Constants.WIKIPEDIA_APP_TEST_COMPARE_NUMBER;
+import static org.wikipedia.espresso.Constants.WIKIPEDIA_APP_TEST_FOLDER;
+
+@RunWith(AndroidJUnit4.class)
+public final class CompareTools {
+
+    public static int compare(Activity activity, String fileName) {
+
+        try {
+            // source file comes from local folder of device
+            InputStream sourceInputStream = new FileInputStream((Environment.getExternalStorageDirectory().getAbsolutePath() + WIKIPEDIA_APP_TEST_FOLDER + fileName + ".png"));
+            Bitmap sourceBitmap = BitmapFactory.decodeStream(sourceInputStream);
+
+            // reference file comes from asset folder of the app
+            InputStream referenceInputStream = activity.getAssets().open(WIKIPEDIA_APP_TEST_ASSET_FOLDER + fileName + ".png");
+            Bitmap referenceBitmap = BitmapFactory.decodeStream(referenceInputStream);
+
+            int compareResult = compareTwoBitmaps("COMPARISON_OF_" + fileName, sourceBitmap, referenceBitmap);
+            // TODO: Create a formal tests result instead of output a log
+            L.d("===== Compare Result ===== ");
+            L.d("Compare page [" + fileName + "] => Match Percentage => " + compareResult + "%");
+
+            return compareResult;
+
+        } catch (Exception e) {
+            L.d("Compare Error: " + e);
+        }
+
+        return -1;
+    }
+
+    private static int compareTwoBitmaps(String fileName, Bitmap source, Bitmap reference) {
+        if (source.getWidth() != reference.getWidth()
+                || source.getHeight() != reference.getHeight()) {
+            return -1;
+        }
+
+        int totalCount = 0;
+        int diffCount = 0;
+        int sourceWidth = source.getWidth();
+        int sourceHeight = source.getHeight();
+
+        Bitmap compareResult = Bitmap.createBitmap(sourceWidth, sourceHeight, Bitmap.Config.ARGB_8888);
+
+        for (int x = 0; x < sourceWidth; x++) {
+            for (int y = 0; y < sourceHeight; y++) {
+
+                totalCount++;
+
+                int pixelA = source.getPixel(x, y);
+                int pixelB = reference.getPixel(x, y);
+
+                int aA = Color.alpha(pixelA);
+                int aR = Color.red(pixelA);
+                int aG = Color.green(pixelA);
+                int aB = Color.blue(pixelA);
+
+                int bR = Color.red(pixelB);
+                int bG = Color.green(pixelB);
+                int bB = Color.blue(pixelB);
+
+                if (aR != bR || aG != bG || aB != bB) {
+                    diffCount++;
+                    compareResult.setPixel(x, y, Color.argb(aA, aR, aG, aB));
+                }
+
+            }
+        }
+
+        ScreenshotTools.saveImageIntoDisk(fileName, compareResult);
+
+        int percentage = WIKIPEDIA_APP_TEST_COMPARE_NUMBER - MathUtil.percentage(diffCount, totalCount);
+
+        return  (diffCount == 0) ? WIKIPEDIA_APP_TEST_COMPARE_NUMBER : percentage;
+    }
+
+    private CompareTools() { }
+}
