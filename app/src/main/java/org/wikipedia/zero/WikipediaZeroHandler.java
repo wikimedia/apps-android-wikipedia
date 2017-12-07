589d1a50727f1e9899e0cefde19607135423ccf0
cooltey <cfeng@wikimedia.org>
2017-12-06 17:32:38 -0800

    Update targetSdk, compileSdk to 27 and Support Library 27.0.2
    Bug: T182236, T182234
    Change-Id: Ia41a57abfe3bdfe9e36defeaefd3acd4ebd8ba34

--- app/src/main/java/org/wikipedia/zero/WikipediaZeroHandler.java
+++ app/src/main/java/org/wikipedia/zero/WikipediaZeroHandler.java
@@ -14,9 +14,9 @@ import android.os.Looper;
 import android.os.Message;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
-import android.support.v7.app.NotificationCompat;
 
 import org.apache.commons.lang3.StringUtils;
 import org.wikipedia.R;
