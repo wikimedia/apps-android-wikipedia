976dc683425f78180f06b69a8ee529192ee50d96
Dmitry Brant <dbrant@wikimedia.org>
2018-02-01 12:35:41 -0500

    Set correct background color in Settings.
    Bug: T186104
    Change-Id: I44e2a1276fb208b20e7ff54bd7bb19d4ceaae2c7

--- app/src/main/java/org/wikipedia/settings/PreferenceLoaderFragment.java
+++ app/src/main/java/org/wikipedia/settings/PreferenceLoaderFragment.java
@@ -2,11 +2,24 @@ package org.wikipedia.settings;
 
 import android.os.Bundle;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import org.wikipedia.R;
+import org.wikipedia.util.ResourceUtil;
 
 abstract class PreferenceLoaderFragment extends PreferenceFragmentCompat
         implements PreferenceLoader {
     @Override
     public void onCreatePreferences(Bundle bundle, String s) {
         loadPreferences();
+    }
+
+    @Override
+    public RecyclerView onCreateRecyclerView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        RecyclerView v = super.onCreateRecyclerView(inflater, parent, savedInstanceState);
+        v.setBackgroundColor(ResourceUtil.getThemedColor(getContext(), R.attr.paper_color));
+        return v;
     }
 }
