9cfaef10b69700128d5f8284c2f1ac5997ba6864
cooltey <cfeng@wikimedia.org>
2018-02-22 17:17:36 -0800

    Update the testing flow as separate test cases
     - To prevent a failed test case stopping all other test cases
    
    Change-Id: I4bb9103e2223a0c013503848cac37fa5c097601e

--- app/src/androidTest/java/org/wikipedia/espresso/SampleOfTests.java
+++ app/src/androidTest/java/org/wikipedia/espresso/SampleOfTests.java
@@ -8,6 +8,7 @@ import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -31,46 +32,68 @@ import static org.wikipedia.espresso.util.ViewTools.waitFor;
 @RunWith(AndroidJUnit4.class)
 public class SampleOfTests {
 
+    private static boolean HAS_SETUP;
+
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
-    @Test
-    public void sampleOfTests() {
+    @Before
+    public void init() {
 
-        // run OnBoarding on every tests
-        OnBoardingTest.runOnBoarding();
+        if (!HAS_SETUP) {
 
-        while (!viewIsDisplayed(R.id.fragment_feed_feed)) {
-            // press back until back to the feed page
-            pressBack();
+            // run OnBoarding on every tests
+            OnBoardingTest.runOnBoarding();
+
+            while (!viewIsDisplayed(R.id.fragment_feed_feed)) {
+                // press back until back to the feed page
+                pressBack();
+            }
+
+            ScreenshotTools.snap("FeedPage");
+            waitFor(WAIT_FOR_2000);
+
+            // test orientation
+            rotateScreen(getActivity(), ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            waitFor(WAIT_FOR_2000);
+            ScreenshotTools.snap("FeedPage_Landscape");
+            rotateScreen(getActivity(), ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            waitFor(WAIT_FOR_2000);
+            ScreenshotTools.snap("FeedPage_Portrait");
+
+            SearchTest.searchKeywordAndGo("Barack Obama");
+
+            if (!viewIsDisplayed(R.id.page_toc_drawer)) {
+                waitFor(WAIT_FOR_1000);
+            }
+            PageActivityTest.testArticleLoad(getActivity());
+
+            HAS_SETUP = true;
         }
-
-        ScreenshotTools.snap("FeedPage");
-        waitFor(WAIT_FOR_2000);
-
-        // test orientation
-        rotateScreen(getActivity(), ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-        waitFor(WAIT_FOR_2000);
-        ScreenshotTools.snap("FeedPage_Landscape");
-        rotateScreen(getActivity(), ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        waitFor(WAIT_FOR_2000);
-        ScreenshotTools.snap("FeedPage_Portrait");
-
-        SearchTest.searchKeywordAndGo("Barack Obama");
-
-        if (!viewIsDisplayed(R.id.page_toc_drawer)) {
-            waitFor(WAIT_FOR_1000);
-        }
-        PageActivityTest.testArticleLoad(getActivity());
-        // TODO: find a better way to generate reference images
-        // Reference images should be put under androidTest/assets/espresso folder
-        Assert.assertTrue("Matching percentage should be higher than " + WIKIPEDIA_APP_TEST_COMPARE_ALLOWANCE_NUMBER, CompareTools.compare("FeedPage") > WIKIPEDIA_APP_TEST_COMPARE_ALLOWANCE_NUMBER);
-        Assert.assertTrue("Matching percentage should be higher than " + WIKIPEDIA_APP_TEST_COMPARE_ALLOWANCE_NUMBER, CompareTools.compare("SearchSuggestionPage") > WIKIPEDIA_APP_TEST_COMPARE_ALLOWANCE_NUMBER);
-        Assert.assertTrue("Matching percentage should be higher than " + WIKIPEDIA_APP_TEST_COMPARE_ALLOWANCE_NUMBER, CompareTools.compare("SearchPage") > WIKIPEDIA_APP_TEST_COMPARE_ALLOWANCE_NUMBER);
-        Assert.assertTrue("Matching percentage should be higher than " + WIKIPEDIA_APP_TEST_COMPARE_ALLOWANCE_NUMBER, CompareTools.compare("Barack") > WIKIPEDIA_APP_TEST_COMPARE_ALLOWANCE_NUMBER);
-        //Assert.assertTrue("Matching percentage should be higher than " + WIKIPEDIA_APP_TEST_COMPARE_ALLOWANCE_NUMBER, CompareTools.compare(getActivity(), "ArticleSwipeUpNoActionBar") > WIKIPEDIA_APP_TEST_COMPARE_ALLOWANCE_NUMBER);
-        //Assert.assertTrue("Matching percentage should be higher than " + WIKIPEDIA_APP_TEST_COMPARE_ALLOWANCE_NUMBER, CompareTools.compare(getActivity(), "ArticleSwipeDownActionBarAndTabSeen") > WIKIPEDIA_APP_TEST_COMPARE_ALLOWANCE_NUMBER);
     }
+
+
+    @Test
+    public void testFeedPage() {
+        Assert.assertTrue("Matching percentage should be higher than " + WIKIPEDIA_APP_TEST_COMPARE_ALLOWANCE_NUMBER, CompareTools.compare("FeedPage") > WIKIPEDIA_APP_TEST_COMPARE_ALLOWANCE_NUMBER);
+    }
+
+    @Test
+    public void testSuggestionPage() {
+        Assert.assertTrue("Matching percentage should be higher than " + WIKIPEDIA_APP_TEST_COMPARE_ALLOWANCE_NUMBER, CompareTools.compare("SearchSuggestionPage") > WIKIPEDIA_APP_TEST_COMPARE_ALLOWANCE_NUMBER);
+    }
+
+    @Test
+    public void testSearchPage() {
+        Assert.assertTrue("Matching percentage should be higher than " + WIKIPEDIA_APP_TEST_COMPARE_ALLOWANCE_NUMBER, CompareTools.compare("SearchPage") > WIKIPEDIA_APP_TEST_COMPARE_ALLOWANCE_NUMBER);
+    }
+
+    @Test
+    public void testBarack() {
+        Assert.assertTrue("Matching percentage should be higher than " + WIKIPEDIA_APP_TEST_COMPARE_ALLOWANCE_NUMBER, CompareTools.compare("Barack") > WIKIPEDIA_APP_TEST_COMPARE_ALLOWANCE_NUMBER);
+    }
+
+
 
     private Activity getActivity() {
         return mActivityTestRule.getActivity();
