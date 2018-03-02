fce3822831298d160bf88a3e0265de31c75da77f
cooltey <cfeng@wikimedia.org>
2018-03-01 17:28:11 -0800

    Check device configurations when starting EspressoTest
     - Create a configuration tools class to check the device's configurations
     - Check configurations before running tests
    
    Bug: T188578
    Change-Id: I9c20214902e243f9f851f2cd22eba69c18df55f6

--- app/src/androidTest/java/org/wikipedia/espresso/Constants.java
+++ app/src/androidTest/java/org/wikipedia/espresso/Constants.java
@@ -1,5 +1,7 @@
 package org.wikipedia.espresso;
 
+import java.util.Locale;
+
 public final class Constants {
 
     public static final String TEST_OUTPUT_FOLDER = "/wikipedia-app-tests/";
@@ -8,6 +10,10 @@ public final class Constants {
     public static final String TEST_JSON_ASSET_FOLDER = "espresso/json/";
     public static final int SCREENSHOT_COMPRESSION_QUALITY = 100;
     public static final float SCREENSHOT_COMPARE_PERCENT_TOLERANCE = 0f;
+    public static final int HEIGHT_OF_TESTING_DEVICE = 1280;
+    public static final int WIDTH_OF_TESTING_DEVICE = 720;
+    public static final int SDK_VERSION_OF_TESTING_DEVICE = 26;
+    public static final String DEFAULT_LANGUAGE_OF_TESTING_DEVICE = Locale.ENGLISH.getDisplayLanguage();
 
     private Constants() { }
 }
