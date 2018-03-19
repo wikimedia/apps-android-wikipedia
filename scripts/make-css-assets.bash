b2916cb6d1e7a2309cb7240dae75544bdc14f4a5
Michael Holloway <mholloway@wikimedia.org>
2018-03-19 18:35:21 -0400

    Update make-css-assets and bundled HTML/CSS
    Updates make-css-assets.bash to accomplish the following:
    
    * Request mobile.app.parsoid rather than mobile.app.android (see
      I30a2f32423dfad16469b3a6b12f21e711c592acf)
    
    * Begin once again requesting mediawiki.skinning.content.parsoid for
      auto-incrementing external link counter functionality (see T162116).
      This module was removed in T163738 and replaced with a copy/pasted
      snippet in the MobileApp extension since other code from that module was
      breaking image widening. With the changes here it no longer breaks image
      widening, allowing us to get rid of the custom snippet.
    
    * Adds the 'mw-parser-output' class to the bundled HTML files' <body>
      elements.  This is needed as a result of a change in the relevant
      mediawiki.skinning.content.parsoid style.[1]  Note that the 'mw-parser-
      output' class is applied to the body in all Parsoid HTML.[2]
    
    * Also begin requesting mediawiki.page.gallery.styles since that module is
      also needed for correct image gallery rendering.  (Note that 'gallery'
      here refers to MediaWiki image galleries, not the app's native Gallery
      Activity.)
    
    * Change the requested skin to minerva and add target=mobile since these
      affect the styles returned by ResourceLoader in ways that are required
      to make the above work.
    
    * Change the production request domain to en.wikipedia.org since that will
      be the request domain used when switching over to the MCS CSS endpoint.
    
    * Omit debug=false since that's the default.
    
    [1] https://github.com/wikimedia/mediawiki/commit/361c98d0b9f47e8eea443f46073fd9f02b02bd57
    [2] https://github.com/wikimedia/parsoid/blob/master/lib/wt2html/DOMPostProcessor.js#L482-L484
    
    Bug: T188919
    Depends-On: I2ccd81a683002270cd5ec74431bf1d033d4373a0
    Change-Id: I5885693409fad4c69626779a49a5a4885299946b

--- scripts/make-css-assets.bash
+++ scripts/make-css-assets.bash
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-PREFIX="https://www.mediawiki.org/w"
+PREFIX="https://en.wikipedia.org/w"
 
 #For use when importing styles from a local Vagrant instance
 #PREFIX="localhost:8080/w"
 
 BASE_PATH="`dirname $0`/.."
 
-wget "$PREFIX/load.php?debug=false&lang=en&modules=skins.minerva.base.reset|skins.minerva.content.styles|ext.cite.style|ext.pygments|ext.math.styles|ext.timeline.styles|mediawiki.page.gallery.styles|mobile.app|mobile.app.android&only=styles&version=&*" -O "$BASE_PATH/app/src/main/assets/styles.css"
-wget "$PREFIX/load.php?debug=false&lang=en&modules=skins.minerva.base.reset|skins.minerva.content.styles|ext.cite.style|ext.pygments|ext.math.styles|ext.timeline.styles|mediawiki.page.gallery.styles|mobile.app|mobile.app.android&only=styles&version=&*" -O "$BASE_PATH/app/src/main/assets/preview.css"
+wget "$PREFIX/load.php?skin=minerva&target=mobile&modules=skins.minerva.base.reset|skins.minerva.content.styles|mediawiki.page.gallery.styles|ext.cite.style|ext.pygments|ext.math.styles|ext.timeline.styles|mediawiki.skinning.content.parsoid|mobile.app|mobile.app.parsoid&only=styles&version=&*" -O "$BASE_PATH/app/src/main/assets/styles.css"
+wget "$PREFIX/load.php?skin=minerva&target=mobile&modules=skins.minerva.base.reset|skins.minerva.content.styles|mediawiki.page.gallery.styles|ext.cite.style|ext.pygments|ext.math.styles|ext.timeline.styles|mediawiki.skinning.content.parsoid|mobile.app|mobile.app.parsoid&only=styles&version=&*" -O "$BASE_PATH/app/src/main/assets/preview.css"
