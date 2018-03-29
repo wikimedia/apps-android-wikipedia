465f2d99936070428481bb58ba77c21242968369
Dmitry Brant <dbrant@wikimedia.org>
2018-03-29 16:08:54 -0400

    Remove unnecessary cruft from custom TextView classes.
    - Improve text-change detection in GoneIfEmptyTextView (no longer use
      TextWatcher), which actually fixes ellipsization for this view.
    - Remove unnecessary stuff from AppTextView, which was only useful for
      API <19.
    
    Change-Id: Ib11ff1538e02f2ca09c91bb1f0d842544034802e

--- app/src/main/java/org/wikipedia/views/GoneIfEmptyTextView.java
+++ app/src/main/java/org/wikipedia/views/GoneIfEmptyTextView.java
@@ -1,49 +1,25 @@
 package org.wikipedia.views;
 
 import android.content.Context;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.util.AttributeSet;
 import android.view.View;
 
-/**
- * WARNING: This class was created for a very specific purpose: allowing the correct vertical
- * centering of a vertical set of TextViews within a parent view, when the content of one or more of
- * those TextViews was optional.  Don't use this if you don't need it!  It may break other things
- * (for example, it breaks "android:ellipsize=true").
- */
 public class GoneIfEmptyTextView extends AppTextView {
     public GoneIfEmptyTextView(Context context) {
         super(context);
-        init();
     }
 
     public GoneIfEmptyTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init();
     }
 
     public GoneIfEmptyTextView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init();
     }
 
-    private void init() {
-        addTextChangedListener(new EmptyTextWatcher());
-    }
-
-    private class EmptyTextWatcher implements TextWatcher {
-        @Override
-        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-        }
-
-        @Override
-        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-        }
-
-        @Override
-        public void afterTextChanged(Editable editable) {
-            setVisibility(editable.length() == 0 ? View.GONE : View.VISIBLE);
-        }
+    @Override
+    protected void onTextChanged(CharSequence text, int start, int lengthBefore, int lengthAfter) {
+        super.onTextChanged(text, start, lengthBefore, lengthAfter);
+        setVisibility(text == null || text.length() == 0 ? View.GONE : View.VISIBLE);
     }
 }
