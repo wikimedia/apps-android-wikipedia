b2f2bc4bde8a0d7b2919cf7c935c9767a2ae261a
sharvani <sharan@wikimedia.org>
2018-05-09 06:34:56 -0700

    Fix: Announcement card should not be added on loading more cards
    - Announcement card is now added only at age 0.
    
    Change-Id: Ic38d5280ba1ad749fb3ec507e976646064a4f077

--- app/src/main/java/org/wikipedia/feed/FeedCoordinator.java
+++ app/src/main/java/org/wikipedia/feed/FeedCoordinator.java
@@ -34,7 +34,7 @@ public class FeedCoordinator extends FeedCoordinatorBase {
 
         conditionallyAddPendingClient(new SearchClient(), age == 0);
         conditionallyAddPendingClient(new OfflineCompilationClient(), age == 0 && !online && OfflineManager.hasCompilation());
-        addPendingClient(new OnboardingClient());
+        conditionallyAddPendingClient(new OnboardingClient(), age == 0);
         conditionallyAddPendingClient(new AnnouncementClient(), age == 0 && online);
 
         List<FeedContentType> orderedContentTypes = new ArrayList<>();
