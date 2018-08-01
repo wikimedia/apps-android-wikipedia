235b38894d4fc522cea9a012f13f014ca06ee88e
Dmitry Brant <dbrant@wikimedia.org>
2018-08-01 18:00:46 -0400

    Optimization: make list of Tabs less-often serialized/deserialized.
    This is one of several revelatory optimizations that has come out of the
    building of the two new prototypes.
    
    There are several places in the app that deal with the list of Tabs that
    belong to the current user. However, every time the list of tabs is
    queried, it gets read from SharedPreferences and deserialized with Gson,
    which is not a trivial operation.
    
    This patch makes the list of tabs get deserialized *once*, when the app
    itself initializes, and then gets serialized only when PageFragment is
    paused.
    
    No visual or behavioral changes are intended.
    
    Change-Id: I550e58c48d3326f4221083f8b014ab8446b2ee81

--- app/src/main/java/org/wikipedia/feed/continuereading/LastPageReadTask.java
+++ app/src/main/java/org/wikipedia/feed/continuereading/LastPageReadTask.java
@@ -2,11 +2,10 @@ package org.wikipedia.feed.continuereading;
 
 import android.support.annotation.Nullable;
 
+import org.wikipedia.WikipediaApp;
 import org.wikipedia.concurrency.SaneAsyncTask;
 import org.wikipedia.history.HistoryEntry;
 import org.wikipedia.page.PageBackStackItem;
-import org.wikipedia.page.tabs.Tab;
-import org.wikipedia.settings.Prefs;
 
 import java.util.List;
 
@@ -18,9 +17,8 @@ public class LastPageReadTask extends SaneAsyncTask<HistoryEntry> {
     }
 
     @Nullable @Override public HistoryEntry performTask() throws Throwable {
-        List<Tab> tabList = Prefs.getTabs();
-        if (age < tabList.size()) {
-            List<PageBackStackItem> items = tabList.get(tabList.size() - age - 1).getBackStack();
+        if (age < WikipediaApp.getInstance().getTabList().size()) {
+            List<PageBackStackItem> items =  WikipediaApp.getInstance().getTabList().get(WikipediaApp.getInstance().getTabList().size() - age - 1).getBackStack();
             if (!items.isEmpty()) {
                 return items.get(items.size() - 1).getHistoryEntry();
             }
