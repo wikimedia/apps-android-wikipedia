fab106410fdb6f21ed67b94f64d78cee9978261a
sharvani <sharan@wikimedia.org>
2018-08-17 16:31:14 -0700

    Show a message to users about sync the first 3 time(s) they add an article to a reading list
    - Sync dialog shown to users first 3 times when they save an article to reading list.
    
    Bug: T188404
    Change-Id: Icabfe9786b90352729439d72c9fc479cb4c7abff

--- app/src/main/java/org/wikipedia/events/ArticleSavedOrDeletedEvent.java
+++ app/src/main/java/org/wikipedia/events/ArticleSavedOrDeletedEvent.java
@@ -5,12 +5,19 @@ import org.wikipedia.readinglist.database.ReadingListPage;
 public class ArticleSavedOrDeletedEvent {
     private ReadingListPage[] pages;
 
+    private boolean isAdded;
+
     public ReadingListPage[] getPages() {
         return pages;
     }
 
-    public ArticleSavedOrDeletedEvent(ReadingListPage... pages) {
+    public ArticleSavedOrDeletedEvent(boolean isAdded, ReadingListPage... pages) {
         this.pages = pages;
+        this.isAdded = isAdded;
+    }
+
+    public boolean isAdded() {
+        return isAdded;
     }
 
 }
