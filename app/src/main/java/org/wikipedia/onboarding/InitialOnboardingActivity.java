565ae04d3adc47fdc96630c14fc7748fb0acd909
cooltey <cfeng@wikimedia.org>
2018-10-09 14:19:59 -0700

    Follow-up: make the status bar color to be base100 on the onboarding screens
    Change-Id: I68cb17fbe77321887f9daa35f8a6e7d0749285ad

--- app/src/main/java/org/wikipedia/onboarding/InitialOnboardingActivity.java
+++ app/src/main/java/org/wikipedia/onboarding/InitialOnboardingActivity.java
@@ -2,14 +2,21 @@ package org.wikipedia.onboarding;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 
+import org.wikipedia.R;
 import org.wikipedia.activity.SingleFragmentActivity;
 import org.wikipedia.settings.Prefs;
 
 public class InitialOnboardingActivity
         extends SingleFragmentActivity<InitialOnboardingFragment>
         implements InitialOnboardingFragment.Callback {
+
+    @Override public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStatusBarColor(R.color.base100);
+    }
 
     @NonNull public static Intent newIntent(@NonNull Context context) {
         return new Intent(context, InitialOnboardingActivity.class);
