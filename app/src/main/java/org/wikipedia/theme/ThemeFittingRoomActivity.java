226bec1b92ecbc7b399f1b8eac3eea7f96c6505d
sharvani <sharan@wikimedia.org>
2018-10-22 16:04:26 -0700

    Fix: Inflation crash
    https://rink.hockeyapp.net/manage/apps/226650/app_versions/258/crash_reasons/244323821
    
    Root cause looks like the attribute resolution issue while tinting drawables.
    I could not reproduce it, but the stacktrace we have and the explanation here made sense, so went with it:
    https://stackoverflow.com/questions/41727729/fatal-exception-java-lang-unsupportedoperationexception-failed-to-resolve-attr
    
    Bug: T207691
    Change-Id: I11f64145a9fffbdad625e3c855e8723103343ad1

--- app/src/main/java/org/wikipedia/theme/ThemeFittingRoomActivity.java
+++ app/src/main/java/org/wikipedia/theme/ThemeFittingRoomActivity.java
@@ -5,8 +5,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 
+import org.wikipedia.Constants;
 import org.wikipedia.activity.SingleFragmentActivity;
 import org.wikipedia.page.ExclusiveBottomSheetPresenter;
+import org.wikipedia.settings.SettingsActivity;
 
 public class ThemeFittingRoomActivity extends SingleFragmentActivity<ThemeFittingRoomFragment>
         implements ThemeChooserDialog.Callback {
@@ -39,6 +41,7 @@ public class ThemeFittingRoomActivity extends SingleFragmentActivity<ThemeFittin
 
     @Override
     public void onCancel() {
+        startActivityForResult(SettingsActivity.newIntent(this), Constants.ACTIVITY_REQUEST_SETTINGS);
         finish();
     }
 }
