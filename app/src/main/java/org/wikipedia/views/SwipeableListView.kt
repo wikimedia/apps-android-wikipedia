c6a47ce5e1f5f0cde25d2d4cdb6a5794caf6b5b6
cooltey <cfeng@wikimedia.org>
2018-11-14 17:19:05 -0800

    Swipe to close gesture for the ToC in PageActivity
     - Create a custom ListView with GestureDector
    
    Bug: T208060
    Change-Id: Ifc7c5e900fac3144a735b1ecb461082dd666b486

---
+++ app/src/main/java/org/wikipedia/views/SwipeableListView.kt
@@ -0,0 +1,59 @@
+package org.wikipedia.views
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.widget.ListView
+
+class SwipeableListView @JvmOverloads constructor(
+        context: Context, attrs: AttributeSet? = null, defStyle: Int = 0) : ListView(context, attrs, defStyle) {
+
+    private var listener: OnSwipeOutListener? = null
+    private var rtl: Boolean = false
+
+    init {
+        // use GestureDetector to take over the onTouchEvent
+        val gestureDetector = GestureDetector(context, ViewGestureListener())
+        setOnTouchListener { _, motionEvent -> gestureDetector.onTouchEvent(motionEvent) }
+    }
+
+    interface OnSwipeOutListener {
+        fun onSwipeOut()
+    }
+
+    fun setRtl(rtl: Boolean) {
+        this.rtl = rtl
+    }
+
+    fun setOnSwipeOutListener(listener: OnSwipeOutListener) {
+        this.listener = listener
+    }
+
+    private fun swipeDetected(event1: MotionEvent, event2: MotionEvent): Boolean {
+        return if (rtl) {
+            event1.y - event2.y <= SWIPE_MAX_DISTANCE && event1.x - event2.x > SWIPE_MIN_DISTANCE
+        } else {
+            event2.y - event1.y <= SWIPE_MAX_DISTANCE && event2.x - event1.x > SWIPE_MIN_DISTANCE
+        }
+    }
+
+    private inner class ViewGestureListener : GestureDetector.SimpleOnGestureListener() {
+
+        override fun onFling(event1: MotionEvent, event2: MotionEvent,
+                             velocityX: Float, velocityY: Float): Boolean {
+            if (swipeDetected(event1, event2) && Math.abs(velocityX) > SWIPE_MIN_X_VELOCITY
+                    && Math.abs(velocityY) < SWIPE_MAX_Y_VELOCITY) {
+                listener?.onSwipeOut()
+            }
+            return false
+        }
+    }
+
+    companion object {
+        const val SWIPE_MIN_DISTANCE = 200
+        const val SWIPE_MAX_DISTANCE = 300
+        const val SWIPE_MIN_X_VELOCITY = 100
+        const val SWIPE_MAX_Y_VELOCITY = 2000
+    }
+}
