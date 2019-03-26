4413d58157fe41a23b19ba86ab99ceb5595054cd
cooltey <cfeng@wikimedia.org>
2019-03-26 16:29:53 -0700

    Completed tweaks of T217173#5052973

--- app/src/main/java/org/wikipedia/descriptions/DescriptionEditReviewView.kt
+++ app/src/main/java/org/wikipedia/descriptions/DescriptionEditReviewView.kt
@@ -2,10 +2,10 @@ package org.wikipedia.descriptions
 
 import android.content.Context
 import android.net.Uri
+import android.support.constraint.ConstraintLayout
 import android.text.TextUtils
 import android.util.AttributeSet
 import android.view.View
-import android.widget.LinearLayout
 import kotlinx.android.synthetic.main.view_description_edit_review.view.*
 import org.apache.commons.lang3.StringUtils
 import org.wikipedia.R
@@ -14,11 +14,11 @@ import org.wikipedia.util.L10nUtil
 import org.wikipedia.util.StringUtil
 
 class DescriptionEditReviewView @JvmOverloads constructor(
-        context: Context, attrs: AttributeSet? = null, defStyle: Int = 0) : LinearLayout(context, attrs, defStyle) {
+        context: Context, attrs: AttributeSet? = null, defStyle: Int = 0) : ConstraintLayout(context, attrs, defStyle) {
 
     init {
         inflate(context, R.layout.view_description_edit_review, this)
-        orientation = LinearLayout.VERTICAL
+        licenseView.removeUnderlinesFromLinks()
     }
 
     val isShowing: Boolean
