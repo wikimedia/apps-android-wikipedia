c77995629454cfda2f04d8397cdcc5f242959ee4
sharvani <sharan@wikimedia.org>
2019-04-10 13:51:43 -0700

    Updated import

--- app/src/main/java/org/wikipedia/onboarding/SuggestedEditsOnboardingFragment.kt
+++ app/src/main/java/org/wikipedia/onboarding/SuggestedEditsOnboardingFragment.kt
@@ -3,10 +3,10 @@ package org.wikipedia.onboarding
 
 import android.app.Activity
 import android.os.Bundle
-import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_suggested_edits.*
 import org.wikipedia.Constants.INTENT_EXTRA_INVOKE_SOURCE
 import org.wikipedia.Constants.InvokeSource
