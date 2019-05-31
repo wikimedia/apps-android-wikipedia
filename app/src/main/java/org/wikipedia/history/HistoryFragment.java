236880c1083d9b76c12bcde7e8037998990efed3
sharvani <sharan@wikimedia.org>
2019-05-31 13:03:50 -0700

    History margin adjusted

--- app/src/main/java/org/wikipedia/history/HistoryFragment.java
+++ app/src/main/java/org/wikipedia/history/HistoryFragment.java
@@ -61,6 +61,7 @@ import io.reactivex.Completable;
 import io.reactivex.schedulers.Schedulers;
 
 import static org.wikipedia.Constants.HISTORY_FRAGMENT_LOADER_ID;
+import static org.wikipedia.views.PageItemView.IMAGE_CONTAINER_MARGIN;
 
 public class HistoryFragment extends Fragment implements BackPressedHandler {
     public interface Callback {
@@ -395,6 +396,7 @@ public class HistoryFragment extends Fragment implements BackPressedHandler {
             getView().setTitle(indexedEntry.getEntry().getTitle().getDisplayText());
             getView().setDescription(indexedEntry.getEntry().getTitle().getDescription());
             getView().setImageUrl(imageUrl);
+            getView().setImageContainerEndMargin(IMAGE_CONTAINER_MARGIN);
             getView().setSelected(selectedIndices.contains(indexedEntry.getIndex()), imageUrl);
             PageAvailableOfflineHandler.INSTANCE.check(indexedEntry.getEntry().getTitle(), available -> getView().setViewsGreyedOut(!available));
 
