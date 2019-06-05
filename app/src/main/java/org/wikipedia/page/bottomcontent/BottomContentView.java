a764864a1d5c03e35422feb480417e9b4e13c2d5
sharvani <sharan@wikimedia.org>
2019-06-05 12:54:29 -0700

    Checkstyle linting

--- app/src/main/java/org/wikipedia/page/bottomcontent/BottomContentView.java
+++ app/src/main/java/org/wikipedia/page/bottomcontent/BottomContentView.java
@@ -11,7 +11,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.widget.BaseAdapter;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
