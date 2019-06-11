dab5fe9748fd2e978e601b026aaffee9391eaf83
cooltey <cfeng@wikimedia.org>
2019-06-11 16:16:00 -0700

    Follow up: tweaking UI and logics

--- app/src/main/java/org/wikipedia/descriptions/DescriptionEditBottomBarView.kt
+++ app/src/main/java/org/wikipedia/descriptions/DescriptionEditBottomBarView.kt
@@ -2,14 +2,12 @@ package org.wikipedia.descriptions
 
 import android.content.Context
 import android.util.AttributeSet
-import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
 import kotlinx.android.synthetic.main.view_description_edit_read_article_bar.view.*
+import org.wikipedia.Constants.InvokeSource
 import org.wikipedia.R
 import org.wikipedia.suggestededits.SuggestedEditsSummary
-import org.wikipedia.util.DimenUtil
 import org.wikipedia.util.L10nUtil.setConditionalLayoutDirection
-import org.wikipedia.util.ResourceUtil
 import org.wikipedia.util.StringUtil
 import org.wikipedia.views.ViewUtil
 
@@ -30,55 +28,40 @@ class DescriptionEditBottomBarView @JvmOverloads constructor(
         visibility = GONE
     }
 
-    fun setSummary(summary: SuggestedEditsSummary) {
+    fun setSummary(summary: SuggestedEditsSummary, invokeSource: InvokeSource) {
         setConditionalLayoutDirection(this, summary.lang)
-        val horizontalMargin: Int = DimenUtil.dpToPx(16.0f).toInt()
-        val params: MarginLayoutParams = viewArticleTitle!!.layoutParams as MarginLayoutParams
-        params.setMargins(horizontalMargin, 0, 0, 0)
-        viewArticleTitle!!.layoutParams = params
-        viewArticleTitle!!.requestLayout()
         viewArticleTitle!!.text = StringUtil.fromHtml(summary.displayTitle)
+        if (invokeSource == InvokeSource.SUGGESTED_EDITS_ADD_CAPTION || invokeSource == InvokeSource.SUGGESTED_EDITS_TRANSLATE_CAPTION) {
+            setImageDetails(summary)
+        } else {
+            setArticleDetails(summary)
+        }
+        show()
+    }
 
+    private fun setArticleDetails(summary: SuggestedEditsSummary) {
         if (summary.thumbnailUrl.isNullOrEmpty()) {
             viewArticleImage!!.visibility = GONE
         } else {
-            val verticalMargin: Int = DimenUtil.dpToPx(8.0f).toInt()
-            val layoutparams: MarginLayoutParams = viewArticleImage.layoutParams as MarginLayoutParams
-            layoutparams.setMargins(horizontalMargin, verticalMargin, 0, verticalMargin)
-            viewArticleImage!!.layoutParams = layoutparams
-            viewArticleImage!!.requestLayout()
-            val padding: Int = DimenUtil.dpToPx(4.0f).toInt()
-            viewArticleImage!!.setPadding(padding, padding, padding, padding)
             viewArticleImage!!.visibility = VISIBLE
             ViewUtil.loadImageUrlInto(viewArticleImage!!, summary.thumbnailUrl)
         }
 
-        viewImageThumbnail.visibility = View.GONE
-        viewReadButton.visibility = View.VISIBLE
-        show()
+        viewImageThumbnail.visibility = GONE
+        viewDownChevron.visibility = GONE
+        viewReadButton.visibility = VISIBLE
     }
 
-    fun setImageDetails(suggestedEditsSummary: SuggestedEditsSummary) {
-        setConditionalLayoutDirection(this, suggestedEditsSummary.lang)
-        viewArticleTitle!!.text = suggestedEditsSummary.title
-        val titlePadding: Int = DimenUtil.dpToPx(62.0f).toInt()
-        viewArticleTitle!!.setPadding(0, 0, titlePadding, 0)
-
-        val padding: Int = DimenUtil.dpToPx(12.0f).toInt()
-        viewArticleImage!!.visibility = VISIBLE
-        viewArticleImage!!.setPadding(padding, padding, padding, padding)
-        viewArticleImage!!.setColorFilter(ResourceUtil.getThemedColor(context, R.attr.main_toolbar_icon_color))
-        viewArticleImage.setImageResource(R.drawable.ic_keyboard_arrow_up_black_24dp)
-
-        if (suggestedEditsSummary.thumbnailUrl!!.isEmpty()) {
+    private fun setImageDetails(summary: SuggestedEditsSummary) {
+        if (summary.thumbnailUrl!!.isEmpty()) {
             viewImageThumbnail!!.visibility = GONE
         } else {
-            viewImageThumbnail.visibility = View.VISIBLE
-            ViewUtil.loadImageUrlInto(viewImageThumbnail!!, suggestedEditsSummary.thumbnailUrl)
+            viewImageThumbnail.visibility = VISIBLE
+            ViewUtil.loadImageUrlInto(viewImageThumbnail!!, summary.thumbnailUrl)
         }
 
-        viewReadButton.visibility = View.GONE
-        show()
+        viewDownChevron.visibility = VISIBLE
+        viewArticleImage.visibility = GONE
+        viewReadButton.visibility = GONE
     }
-
 }
