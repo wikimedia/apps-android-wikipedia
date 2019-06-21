d50d78c1a4d2e06cd405a8971727bbaefaa0b348
cooltey <cfeng@wikimedia.org>
2019-06-21 08:35:38 -0700

    fix building failure

--- app/src/main/java/org/wikipedia/views/ImagePreviewDialog.kt
+++ app/src/main/java/org/wikipedia/views/ImagePreviewDialog.kt
@@ -49,7 +49,7 @@ class ImagePreviewDialog : ExtendedBottomSheetDialogFragment(), DialogInterface.
                     Uri.parse(String.format(getString(R.string.suggested_edits_image_file_page_commons_link), suggestedEditsSummary.title)))
         }
 
-        titleText!!.text = StringUtil.removeHTMLTags(suggestedEditsSummary.displayTitle)
+        titleText!!.text = StringUtil.removeHTMLTags(suggestedEditsSummary.displayTitle!!)
         loadImage(suggestedEditsSummary.thumbnailUrl)
         loadImageInfoIfNeeded()
     }
