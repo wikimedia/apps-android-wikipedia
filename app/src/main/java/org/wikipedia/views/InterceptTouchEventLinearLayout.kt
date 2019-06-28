a854e558da74bcf06f8426c9a0d391b95f9ba2bf
cooltey <cfeng@wikimedia.org>
2019-06-28 16:48:37 -0700

    Use a custom LinearLayout to intercept the touch event of child views

---
+++ app/src/main/java/org/wikipedia/views/InterceptTouchEventLinearLayout.kt
@@ -0,0 +1,12 @@
+package org.wikipedia.views
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.widget.LinearLayout
+
+class InterceptTouchEventLinearLayout @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyle: Int = 0) : LinearLayout(context, attrs, defStyle) {
+    override fun onInterceptTouchEvent(motionEvent: MotionEvent): Boolean {
+        return true
+    }
+}
