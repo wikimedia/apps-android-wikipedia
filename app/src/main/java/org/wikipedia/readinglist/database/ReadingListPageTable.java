3448e67eb24c943c902736fe0e515765ac85cbdb
cooltey <cfeng@wikimedia.org>
2019-07-10 14:34:16 -0700

    Prevent create default list everytime when upgrading db

--- app/src/main/java/org/wikipedia/readinglist/database/ReadingListPageTable.java
+++ app/src/main/java/org/wikipedia/readinglist/database/ReadingListPageTable.java
@@ -71,7 +71,7 @@ public class ReadingListPageTable extends DatabaseTable<ReadingListPage> {
     @Override
     public void onUpgradeSchema(@NonNull SQLiteDatabase db, int fromVersion, int toVersion) {
         if (toVersion == DB_VER_INTRODUCED) {
-            List<ReadingList> currentLists = new ArrayList<>();
+            List<ReadingList> currentLists = ReadingListDbHelper.instance().getAllLists();
             createDefaultList(db, currentLists);
             renameListsWithIdenticalNameAsDefault(db, currentLists);
             // TODO: add other one-time conversions here.
