73a7ea18118c619c1246d55941e2c6a68795abb6
sharvani <sharan@wikimedia.org>
2019-07-10 20:57:15 +0530

    Adjusting bottom sheet peekHeight

--- app/src/main/java/org/wikipedia/page/ExtendedBottomSheetDialogFragment.java
+++ app/src/main/java/org/wikipedia/page/ExtendedBottomSheetDialogFragment.java
@@ -1,9 +1,14 @@
 package org.wikipedia.page;
 
 import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 import org.wikipedia.R;
@@ -26,6 +31,23 @@ public class ExtendedBottomSheetDialogFragment extends BottomSheetDialogFragment
     public void onStart() {
         super.onStart();
         setWindowLayout();
+        setPeekHeightPerDevice();
+    }
+
+    private void setPeekHeightPerDevice() {
+        View view = getView();
+        if (view != null) {
+            view.post(() -> {
+                View parent = (View) view.getParent();
+                CoordinatorLayout.LayoutParams params = (CoordinatorLayout.LayoutParams) (parent).getLayoutParams();
+                CoordinatorLayout.Behavior behavior = params.getBehavior();
+                BottomSheetBehavior bottomSheetBehavior = (BottomSheetBehavior) behavior;
+                int screenHeight = Resources.getSystem().getDisplayMetrics().heightPixels;
+                if (bottomSheetBehavior != null) {
+                    bottomSheetBehavior.setPeekHeight(screenHeight / 2);
+                }
+            });
+        }
     }
 
     @Override
