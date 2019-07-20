f9d5fed885ec52d06d6440b2e8fe15f346bf9151
Dmitry Brant <dbrant@wikimedia.org>
2019-07-19 20:45:41 -0400

    Weave together the logic of loading page metadata with all the rest.
    Change-Id: Idd76d90bf21bdc260c46601b831bee993bec11d0

--- app/src/main/java/org/wikipedia/bridge/JavaScriptActionHandler.kt
+++ app/src/main/java/org/wikipedia/bridge/JavaScriptActionHandler.kt
@@ -3,7 +3,9 @@ package org.wikipedia.bridge
 import android.content.Context
 import org.wikipedia.R
 import org.wikipedia.dataclient.RestService
-import org.wikipedia.page.PageTitle
+import org.wikipedia.page.Namespace
+import org.wikipedia.page.PageViewModel
+import org.wikipedia.util.L10nUtil.formatDateRelative
 
 object JavaScriptActionHandler {
     @JvmStatic
@@ -27,26 +29,41 @@ object JavaScriptActionHandler {
     }
 
     @JvmStatic
-    fun setFooter(context: Context, title: PageTitle): String {
+    fun setFooter(context: Context, model: PageViewModel): String {
+        if (model.page == null) {
+            return "";
+        }
+
+        val showLanguagesLink = model.page!!.pageProperties.languageCount > 0
+        val showEditHistoryLink = !(model.page!!.isMainPage || model.page!!.isFilePage)
+        val lastEditText = (context.getString(R.string.last_updated_text, formatDateRelative(model.page!!.pageProperties.lastModified)))
+        val showTalkLink = !(model.page!!.title.namespace() === Namespace.TALK)
+        val showMapLink = model.page!!.pageProperties.geo != null
+
+        // TODO: page-library also supports showing disambiguation ("similar pages") links and
+        // "page issues". We should be mindful that they exist, even if we don't want them for now.
+
         return "pagelib.c1.Footer.add(document," +
-                "    '" + title.displayText + "'," +
-                "    [pagelib.c1.Footer.MenuItemType.languages, pagelib.c1.Footer.MenuItemType.lastEdited, pagelib.c1.Footer.MenuItemType.pageIssues, " +
-                "pagelib.c1.Footer.MenuItemType.disambiguation, pagelib.c1.Footer.MenuItemType.talkPage, pagelib.c1.Footer.MenuItemType.coordinate]," +
-                "    {" +
+                "    '${model.page?.displayTitle}'," +
+                "    [" +
+                (if (showLanguagesLink) "pagelib.c1.Footer.MenuItemType.languages, " else "") +
+                (if (showEditHistoryLink) "pagelib.c1.Footer.MenuItemType.lastEdited, " else "") +
+                (if (showTalkLink) "pagelib.c1.Footer.MenuItemType.talkPage, " else "") +
+                (if (showMapLink) "pagelib.c1.Footer.MenuItemType.coordinate " else "") +
+                "],   {" +
                 "        'readMoreHeading': '" + context.getString(R.string.read_more_section) + "'," +
                 "        'menuDisambiguationTitle': '" + context.getString(R.string.page_similar_titles) + "'," +
-                "        'menuLanguagesTitle': 'Available in FIXME other languages'," +
+                "        'menuLanguagesTitle': '" + context.getString(R.string.language_count_link_text, model.page?.pageProperties?.languageCount) + "'," +
                 "        'menuHeading': '" + context.getString(R.string.about_article_section) + "'," +
                 "        'menuLastEditedSubtitle': '" + context.getString(R.string.edit_history_link_text) + "'," +
-                "        'menuLastEditedTitle': 'Edited today'," +
+                "        'menuLastEditedTitle': '${lastEditText}'," +
                 "        'licenseString': '" + context.getString(R.string.content_license_html) + "'," +
                 "        'menuTalkPageTitle': '" + context.getString(R.string.talk_page_link_text) + "'," +
-                "        'menuPageIssuesTitle': 'Page issues'," +
                 "        'viewInBrowserString': '" + context.getString(R.string.page_view_in_browser) + "'," +
                 "        'licenseSubstitutionString': 'CC BY-SA 3.0'," +
                 "        'menuCoordinateTitle': '" + context.getString(R.string.map_view_link_text) + "'" +
                 "     }, 3," +
-                "     '" + title.wikiSite.url() + RestService.REST_API_PREFIX + "'" +
+                "     '${model.title?.wikiSite?.url()}${RestService.REST_API_PREFIX}'" +
                 ")"
     }
 }
