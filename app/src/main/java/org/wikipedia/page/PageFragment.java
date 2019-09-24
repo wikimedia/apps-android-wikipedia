c330135022f2ac17c04f077dc5aa81e7c321bf4b
Dmitry Brant <dbrant@wikimedia.org>
2019-09-24 11:00:20 -0400

    Fix internal crash in WebView on Lollipop.
    Change-Id: I40ff8a899be42aed1787a2dea326ddb3cd833e57

--- app/src/main/java/org/wikipedia/page/PageFragment.java
+++ app/src/main/java/org/wikipedia/page/PageFragment.java
@@ -343,7 +343,7 @@ public class PageFragment extends Fragment implements BackPressedHandler {
         // creating a seizure-inducing effect, or at the very least, a migraine with aura).
         webView.setBackgroundColor(getThemedColor(requireActivity(), R.attr.paper_color));
 
-        bridge = new CommunicationBridge(webView);
+        bridge = new CommunicationBridge(webView, requireActivity());
         setupMessageHandlers();
         sendDecorOffsetMessage();
 
