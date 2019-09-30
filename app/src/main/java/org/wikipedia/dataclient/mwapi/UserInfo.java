2ec88c133fd5cc0eaf270ec8daf0a07521e1decf
Dmitry Brant <dbrant@wikimedia.org>
2019-09-30 14:57:56 -0400

    Add API groundwork for detecting whether user is blocked.
    Change-Id: Ic0f5c2281f649173eb82f50ae7543acfa2438d5b

--- app/src/main/java/org/wikipedia/dataclient/mwapi/UserInfo.java
+++ app/src/main/java/org/wikipedia/dataclient/mwapi/UserInfo.java
@@ -1,21 +1,57 @@
 package org.wikipedia.dataclient.mwapi;
 
+import android.text.TextUtils;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.collection.ArraySet;
 
+import org.wikipedia.util.DateUtil;
+
+import java.text.ParseException;
+import java.util.Collections;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 @SuppressWarnings("unused")
 public class UserInfo {
     private String name;
     private int id;
+    @Nullable private List<String> groups;
+    private int blockid;
+    @Nullable private String blockreason;
+    @Nullable private String blockedby;
+    @Nullable private String blockedtimestamp;
+    @Nullable private String blockexpiry;
 
     // Object type is any JSON type.
     @Nullable private Map<String, ?> options;
 
     public int id() {
         return id;
+    }
+
+    @NonNull public Set<String> getGroups() {
+        return groups != null ? new ArraySet<>(groups) : Collections.emptySet();
+    }
+
+    public boolean isBlocked() {
+        if (TextUtils.isEmpty(blockexpiry)) {
+            return false;
+        }
+        try {
+            Date now = new Date();
+            Date expiry = DateUtil.iso8601DateParse(blockexpiry);
+            if (expiry.after(now)) {
+                return true;
+            }
+        } catch (ParseException e) {
+            // ignore
+        }
+        return false;
     }
 
     @NonNull public Map<String, String> userjsOptions() {
