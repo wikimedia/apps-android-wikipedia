db1f6da0a8ff708d97bb95dfda940a2fd5909d49
Dmitry Brant <dbrant@wikimedia.org>
2019-10-24 10:49:18 -0400

    Make NavTabs become able to span multiple lines.
    Change-Id: I0ac57bf855fc1b8aae05e0720e7e491798e03672

--- app/src/main/java/org/wikipedia/navtab/NavTabLayout.kt
+++ app/src/main/java/org/wikipedia/navtab/NavTabLayout.kt
@@ -1,12 +1,18 @@
 package org.wikipedia.navtab
 
 import android.content.Context
+import android.text.TextUtils
 import android.util.AttributeSet
 import android.view.Menu
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import org.wikipedia.R
 
 import org.wikipedia.auth.AccountUtil
+import org.wikipedia.util.DimenUtil
 
 class NavTabLayout constructor(context: Context, attrs: AttributeSet) : BottomNavigationView(context, attrs) {
     init {
@@ -21,6 +27,25 @@ class NavTabLayout constructor(context: Context, attrs: AttributeSet) : BottomNa
                 continue
             }
             menu.add(Menu.NONE, i, i, navTab.text()).setIcon(navTab.icon())
+        }
+        fixTextStyle()
+    }
+
+    private fun fixTextStyle() {
+        if (childCount > 0) {
+            val menuView = getChildAt(0)
+            if ((menuView as ViewGroup).childCount > 0) {
+                for (i in 0..menuView.childCount) {
+                    val menuItemView = menuView.getChildAt(i)
+                    if (menuItemView != null) {
+                        val labelView = menuItemView.findViewById<TextView>(R.id.largeLabel)
+                        labelView.setSingleLine(false)
+                        labelView.maxLines = 2
+                        labelView.ellipsize = TextUtils.TruncateAt.END
+                        labelView.textAlignment = View.TEXT_ALIGNMENT_CENTER
+                    }
+                }
+            }
         }
     }
 }
