b3bc7ebfdef6451e8dde7a4fe07db69dbfc25232
Dmitry Brant <dbrant@wikimedia.org>
2019-12-06 15:43:53 -0500

    Don't hide the WebView while loading the article.
    Change-Id: I5740127f9570a76dba7322f60bb318397a09fa1e

--- app/src/main/java/org/wikipedia/page/PageFragment.java
+++ app/src/main/java/org/wikipedia/page/PageFragment.java
@@ -626,7 +626,6 @@ public class PageFragment extends Fragment implements BackPressedHandler, Commun
      */
     public void loadPage(@NonNull PageTitle title, @NonNull HistoryEntry entry,
                          boolean pushBackStack, int stagedScrollY, boolean isRefresh) {
-        webView.setVisibility(View.GONE);
         // clear the title in case the previous page load had failed.
         clearActivityActionBarTitle();
 
