a758ba557fa9d90e68ed5041d0561aa78299e696
Dmitry Brant <dbrant@wikimedia.org>
2020-03-04 21:15:15 -0500

    Interface for adding arbitrary tags.
    Change-Id: Ib128267b7a39e6afab8481e498ba974da36e5829

---
+++ app/src/main/java/org/wikipedia/suggestededits/SuggestedEditsImageTagDialog.kt
@@ -0,0 +1,142 @@
+package org.wikipedia.suggestededits
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.DialogFragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.dialog_image_tag_select.*
+import org.wikipedia.R
+import org.wikipedia.WikipediaApp
+import org.wikipedia.activity.FragmentUtil
+import org.wikipedia.dataclient.Service
+import org.wikipedia.dataclient.ServiceFactory
+import org.wikipedia.dataclient.WikiSite
+import org.wikipedia.dataclient.mwapi.MwQueryPage
+import org.wikipedia.dataclient.wikidata.Search
+import org.wikipedia.util.DeviceUtil
+import org.wikipedia.util.log.L
+import org.wikipedia.views.DrawableItemDecoration
+import java.util.*
+import kotlin.collections.ArrayList
+
+class SuggestedEditsImageTagDialog : DialogFragment() {
+    interface Callback {
+        fun onSelect(item: MwQueryPage.ImageLabel)
+    }
+
+    private var currentSearchTerm: String = ""
+    private val textWatcher = SearchTextWatcher()
+    private val adapter = ResultListAdapter(Collections.emptyList())
+    private val disposables = CompositeDisposable()
+
+    private val searchRunnable = Runnable {
+        requestResults(currentSearchTerm)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return LayoutInflater.from(requireActivity()).inflate(R.layout.dialog_image_tag_select, container)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        imageTagsRecycler.layoutManager = LinearLayoutManager(activity)
+        imageTagsRecycler.addItemDecoration(DrawableItemDecoration(requireContext(), R.attr.list_separator_drawable, drawStart = false, drawEnd = false))
+        imageTagsRecycler.adapter = adapter
+        imageTagsSearchText.addTextChangedListener(textWatcher)
+
+        imageTagsSearchText.requestFocus()
+        //DeviceUtil.showSoftKeyboard(imageTagsSearchText)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        imageTagsSearchText.removeTextChangedListener(textWatcher)
+        disposables.clear()
+    }
+
+    private inner class SearchTextWatcher : TextWatcher {
+        override fun beforeTextChanged(text: CharSequence, i: Int, i1: Int, i2: Int) { }
+
+        override fun onTextChanged(text: CharSequence, i: Int, i1: Int, i2: Int) {
+            currentSearchTerm = text.toString()
+            imageTagsSearchText.removeCallbacks(searchRunnable)
+            imageTagsSearchText.postDelayed(searchRunnable, 500)
+        }
+
+        override fun afterTextChanged(editable: Editable) { }
+    }
+
+    private fun requestResults(searchTerm: String) {
+        if (searchTerm.isEmpty()) {
+            adapter.setResults(Collections.emptyList())
+            adapter.notifyDataSetChanged()
+            return
+        }
+        disposables.add(ServiceFactory.get(WikiSite(Service.WIKIDATA_URL)).searchEntities(searchTerm, WikipediaApp.getInstance().appOrSystemLanguageCode, WikipediaApp.getInstance().appOrSystemLanguageCode)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({ search: Search ->
+                    val labelList = ArrayList<MwQueryPage.ImageLabel>()
+                    for (result in search.results()) {
+                        val label = MwQueryPage.ImageLabel(result.id, result.label, result.description)
+                        labelList.add(label)
+                    }
+                    adapter.setResults(labelList)
+                    adapter.notifyDataSetChanged()
+                }) { t: Throwable? -> L.d(t) })
+    }
+
+    private inner class ResultItemHolder internal constructor(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+        fun bindItem(item: MwQueryPage.ImageLabel, position: Int) {
+            itemView.findViewById<TextView>(R.id.labelName).text = item.label
+            itemView.findViewById<TextView>(R.id.labelDescription).text = item.description
+            itemView.tag = item
+            itemView.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            val item = v!!.tag as MwQueryPage.ImageLabel
+            callback()!!.onSelect(item)
+            dismiss()
+        }
+    }
+
+    private inner class ResultListAdapter(private var results: List<MwQueryPage.ImageLabel>) : RecyclerView.Adapter<ResultItemHolder>() {
+        fun setResults(results: List<MwQueryPage.ImageLabel>) {
+            this.results = results
+        }
+
+        override fun getItemCount(): Int {
+            return results.size
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, pos: Int): ResultItemHolder {
+            val view = layoutInflater.inflate(R.layout.item_wikidata_label, null)
+            val params = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+            view.layoutParams = params
+            return ResultItemHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: ResultItemHolder, pos: Int) {
+            holder.bindItem(results[pos], pos)
+        }
+    }
+
+    private fun callback(): Callback? {
+        return FragmentUtil.getCallback(this, Callback::class.java)
+    }
+
+    companion object {
+        fun newInstance(): SuggestedEditsImageTagDialog {
+            return SuggestedEditsImageTagDialog()
+        }
+    }
+}
