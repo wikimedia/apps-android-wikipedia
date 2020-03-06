2bbc6109575e1e3f82cc1fc871f11762d29f5304
sharvani <sharan@wikimedia.org>
2020-03-06 13:53:11 -0800

    Final touches

--- app/src/main/java/org/wikipedia/suggestededits/SuggestedEditsFeedCardImageTagActivity.kt
+++ app/src/main/java/org/wikipedia/suggestededits/SuggestedEditsFeedCardImageTagActivity.kt
@@ -2,15 +2,14 @@ package org.wikipedia.suggestededits
 
 import android.content.Context
 import android.content.Intent
+import android.content.res.ColorStateList
 import android.content.res.Configuration
 import android.graphics.Color
 import android.os.Bundle
-import android.view.Gravity
 import android.view.MotionEvent
 import android.view.View.GONE
 import android.view.View.VISIBLE
-import android.widget.FrameLayout
-import android.widget.LinearLayout
+import androidx.core.widget.ImageViewCompat
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_suggested_edits_feed_card_image_tags.*
 import org.wikipedia.R
@@ -42,25 +41,30 @@ class SuggestedEditsFeedCardImageTagActivity : BaseActivity() {
         setContentView(R.layout.activity_suggested_edits_feed_card_image_tags)
         suggestedEditsImageTagsFragment = getSupportFragmentManager().findFragmentById(R.id.imageTagFragment) as SuggestedEditsImageTagsFragment?
         addContributionButton.setOnClickListener { suggestedEditsImageTagsFragment!!.publish() }
+        addContributionLandscapeImage.setOnClickListener { suggestedEditsImageTagsFragment!!.publish() }
     }
 
     fun updateActionButton() {
         if (suggestedEditsImageTagsFragment != null) {
+            addContributionLandscapeImage.setBackgroundColor(if (!suggestedEditsImageTagsFragment!!.publishOutlined()) ResourceUtil.getThemedColor(this, R.attr.colorAccent) else Color.WHITE)
+            if(suggestedEditsImageTagsFragment!!.publishOutlined()) addContributionLandscapeImage.setBackgroundResource(R.drawable.button_shape_border_light)
             addContributionButton.setBackgroundResource(if (suggestedEditsImageTagsFragment!!.publishOutlined()) R.drawable.button_shape_border_light else R.drawable.button_shape_add_reading_list)
             addContributionText?.setTextColor(if (suggestedEditsImageTagsFragment!!.publishOutlined()) ResourceUtil.getThemedColor(this, R.attr.colorAccent) else Color.WHITE)
             addContributionButton.isEnabled = suggestedEditsImageTagsFragment!!.publishEnabled()
+            addContributionLandscapeImage.isEnabled = suggestedEditsImageTagsFragment!!.publishEnabled()
             addContributionButton.alpha = if (suggestedEditsImageTagsFragment!!.publishEnabled()) 1f else 0.5f
         } else {
             addContributionButton.setBackgroundResource(R.drawable.button_shape_border_light)
         }
         if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
-           addContributionButton.foregroundGravity = Gravity.START
-            addContributionText.visibility = GONE
-            addContributionImage.visibility = VISIBLE
-            addContributionImage.setImageResource(R.drawable.ic_check_black_24dp)
+            addContributionButton.visibility = GONE
+            addContributionLandscapeImage.visibility = VISIBLE
+            val color = if (suggestedEditsImageTagsFragment!!.publishOutlined()) ResourceUtil.getThemedColor(this, R.attr.colorAccent) else Color.WHITE
+            val colorStateList = ColorStateList.valueOf(color)
+            ImageViewCompat.setImageTintList(addContributionLandscapeImage, colorStateList)
         } else {
-            addContributionText.visibility = VISIBLE
-
+            addContributionButton.visibility = VISIBLE
+            addContributionLandscapeImage.visibility = GONE
             addContributionText?.text = getString(R.string.description_edit_save)
             if (suggestedEditsImageTagsFragment != null) {
                 addContributionText?.setTextColor(if (suggestedEditsImageTagsFragment!!.publishOutlined()) ResourceUtil.getThemedColor(this, R.attr.colorAccent) else Color.WHITE)
