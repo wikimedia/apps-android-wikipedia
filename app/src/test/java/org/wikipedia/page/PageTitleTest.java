a57a00259fc2b3a5a1c8a30d76d483e6c1d7c747
cooltey <cfeng@wikimedia.org>
2020-04-08 09:48:37 -0700

    Add unit tests

--- app/src/test/java/org/wikipedia/page/PageTitleTest.java
+++ app/src/test/java/org/wikipedia/page/PageTitleTest.java
@@ -58,6 +58,12 @@ import static org.mockito.Mockito.when;
         assertThat(enwiki.titleForInternalLink("/wiki/India's Gate").getUri(), is("https://en.wikipedia.org/wiki/India%27s_Gate"));
     }
 
+    @Test public void testVariants() {
+        assertThat(new PageTitle("Taiwan", WikiSite.forLanguageCode("en")).getUri(), is("https://en.wikipedia.org/wiki/Taiwan"));
+        assertThat(new PageTitle("Taiwan", WikiSite.forLanguageCode("zh-hant")).getUri(), is("https://zh.wikipedia.org/zh-hant/Taiwan"));
+        assertThat(new PageTitle("Taiwan", WikiSite.forLanguageCode("zh-hans")).getUri(), is("https://zh.wikipedia.org/zh-hans/Taiwan"));
+    }
+
     @Test public void testWikiSite() {
         WikiSite enwiki = WikiSite.forLanguageCode("en");
 
