26c1e7f0921a22c7ffce07b9e6ea90dd54630474
sharvani <sharan@wikimedia.org>
2020-04-08 11:19:05 -0700

    checkstyle

--- app/src/main/java/org/wikipedia/feed/FeedFragment.java
+++ app/src/main/java/org/wikipedia/feed/FeedFragment.java
@@ -10,7 +10,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.annotation.IntRange;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
