ad9b3f155338bded00a24679831fe408b1acbcbc
cooltey <coolteygame@gmail.com>
2020-04-13 16:41:37 -0700

    Add notification strings

--- app/src/main/java/org/wikipedia/suggestededits/SuggestedEditsLocalNotificationTask.kt
+++ app/src/main/java/org/wikipedia/suggestededits/SuggestedEditsLocalNotificationTask.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.Intent
 import androidx.core.app.NotificationCompat
 import org.wikipedia.Constants.INTENT_EXTRA_GO_TO_SE_TAB
+import org.wikipedia.R
 import org.wikipedia.WikipediaApp
 import org.wikipedia.main.MainActivity
 import org.wikipedia.notifications.NotificationPresenter
@@ -26,12 +27,11 @@ class SuggestedEditsLocalNotificationTask(val context: Context) : RecurringTask(
         val days = System.currentTimeMillis() - Prefs.getLastDescriptionEditTime()
         if (days in FIRST_NOTIFICATION_SHOW_ON_DAY until SECOND_NOTIFICATION_SHOW_ON_DAY && !Prefs.isSuggestedEditsLocalNotificationShown()) {
             Prefs.setSuggestedEditsLocalNotificationShown(true)
-            // TODO: show local notification - 3 days after last SE edit
+            showSuggestedEditsLocalNotification(R.string.suggested_edits_local_notification_first, false)
             // showSuggestedEditsLocalNotification()
         } else if (days >= SECOND_NOTIFICATION_SHOW_ON_DAY && Prefs.isSuggestedEditsLocalNotificationShown()) {
             Prefs.setSuggestedEditsLocalNotificationShown(false)
-            // TODO: show local notification - 7 days after last SE edit
-            // showSuggestedEditsLocalNotification()
+            showSuggestedEditsLocalNotification(R.string.suggested_edits_local_notification_second, false)
         }
     }
 
@@ -39,17 +39,13 @@ class SuggestedEditsLocalNotificationTask(val context: Context) : RecurringTask(
         return "suggested-edits-local-notification-task"
     }
 
-    fun showSuggestedEditsLocalNotification(description: Int, forced: Boolean) {
+    private fun showSuggestedEditsLocalNotification(description: Int, forced: Boolean) {
         if (!WikipediaApp.getInstance().isAnyActivityResumed || forced) {
             val intent: Intent = MainActivity.newIntent(context).putExtra(INTENT_EXTRA_GO_TO_SE_TAB, true)
             val builder: NotificationCompat.Builder = NotificationPresenter.getDefaultBuilder(context)
             val pendingIntent = PendingIntent.getActivity(context, 1, intent, PendingIntent.FLAG_UPDATE_CURRENT)
-            // TODO: update this after received completed text.
-//            builder.addAction(0, context.getString(R.string.suggested_edits_unlock_notification_button), pendingIntent)
-//            NotificationPresenter.showNotification(context, builder, 0, context.getString(R.string.suggested_edits_unlock_add_descriptions_notification_title),
-//                    context.getString(R.string.suggested_edits_unlock_notification_text),
-//                    context.getString(R.string.suggested_edits_unlock_add_descriptions_notification_big_text),
-//                    R.drawable.ic_mode_edit_white_24dp, R.color.accent50, intent)
+            NotificationPresenter.showNotification(context, builder, 0, context.getString(R.string.suggested_edits_local_notification_title),
+                    context.getString(description), context.getString(description), R.drawable.ic_mode_edit_white_24dp, R.color.accent50, intent)
         }
     }
 
