e073904b43152459280cd89b2e8b15703a61aa5d
cooltey <coolteygame@gmail.com>
2020-04-30 13:44:48 -0700

    Use proper theme style

--- app/src/main/java/org/wikipedia/commons/FilePageActivity.kt
+++ app/src/main/java/org/wikipedia/commons/FilePageActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.MotionEvent
+import org.wikipedia.R
 import org.wikipedia.activity.SingleFragmentActivity
 import org.wikipedia.page.PageTitle
 import org.wikipedia.util.ResourceUtil
@@ -16,8 +17,8 @@ class FilePageActivity : SingleFragmentActivity<FilePageFragment>() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         imageZoomHelper = ImageZoomHelper(this)
-        supportActionBar!!.elevation = 0.0f
-        setNavigationBarColor(ResourceUtil.getThemedColor(this, android.R.attr.windowBackground))
+        setStatusBarColor(ResourceUtil.getThemedColor(this, R.attr.paper_color))
+        setNavigationBarColor(ResourceUtil.getThemedColor(this, R.attr.paper_color))
     }
 
     override fun createFragment(): FilePageFragment {
