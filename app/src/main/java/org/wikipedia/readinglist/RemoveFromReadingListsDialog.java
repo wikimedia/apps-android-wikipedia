00a30aa7b4574e112b5102d2eea7dfdb795ee93e
Isira Seneviratne <isirasen96@gmail.com>
2020-05-02 16:26:08 +0530

    Use CollectionUtils, IterableUtils and ListUtils in RemoveFromReadingListsDialog

--- app/src/main/java/org/wikipedia/readinglist/RemoveFromReadingListsDialog.java
+++ app/src/main/java/org/wikipedia/readinglist/RemoveFromReadingListsDialog.java
@@ -15,6 +15,12 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import static org.apache.commons.collections4.CollectionUtils.isEmpty;
+import static org.apache.commons.collections4.IterableUtils.first;
+import static org.apache.commons.collections4.IterableUtils.get;
+import static org.apache.commons.collections4.IterableUtils.size;
+import static org.apache.commons.collections4.ListUtils.emptyIfNull;
+
 public class RemoveFromReadingListsDialog {
     public interface Callback {
         void onDeleted(@NonNull List<ReadingList> lists, @NonNull ReadingListPage page);
@@ -28,14 +34,17 @@ public class RemoveFromReadingListsDialog {
     }
 
     public void deleteOrShowDialog(@NonNull Context context, @Nullable Callback callback) {
-        if (listsContainingPage == null || listsContainingPage.isEmpty()) {
+        if (isEmpty(listsContainingPage)) {
             return;
         }
-        if (listsContainingPage.size() == 1 && !listsContainingPage.get(0).pages().isEmpty()) {
-            ReadingListDbHelper.instance().markPagesForDeletion(listsContainingPage.get(0),
-                    Collections.singletonList(listsContainingPage.get(0).pages().get(0)));
-            if (callback != null) {
-                callback.onDeleted(listsContainingPage, listsContainingPage.get(0).pages().get(0));
+        if (size(listsContainingPage) == 1) {
+            final ReadingList first = first(listsContainingPage);
+            if (!first.pages().isEmpty()) {
+                ReadingListDbHelper.instance().markPagesForDeletion(first,
+                        Collections.singletonList(first.pages().get(0)));
+                if (callback != null) {
+                    callback.onDeleted(emptyIfNull(listsContainingPage), first.pages().get(0));
+                }
             }
             return;
         }
@@ -43,11 +52,11 @@ public class RemoveFromReadingListsDialog {
     }
 
     private void showDialog(@NonNull Context context, @Nullable final Callback callback) {
-        final String[] listNames = new String[listsContainingPage.size()];
+        final String[] listNames = new String[size(listsContainingPage)];
         final boolean[] selected = new boolean[listNames.length];
 
-        for (int i = 0; i < listsContainingPage.size(); i++) {
-            listNames[i] = listsContainingPage.get(i).title();
+        for (int i = 0; i < size(listsContainingPage); i++) {
+            listNames[i] = get(listsContainingPage, i).title();
         }
 
         new AlertDialog.Builder(context)
@@ -56,15 +65,16 @@ public class RemoveFromReadingListsDialog {
                     boolean atLeastOneSelected = false;
                     List<ReadingList> newLists = new ArrayList<>();
                     for (int i = 0; i < listNames.length; i++) {
+                        final ReadingList list = get(listsContainingPage, i);
                         if (selected[i]) {
                             atLeastOneSelected = true;
-                            ReadingListDbHelper.instance().markPagesForDeletion(listsContainingPage.get(i),
-                                    Collections.singletonList(listsContainingPage.get(i).pages().get(0)));
-                            newLists.add(listsContainingPage.get(i));
+                            ReadingListDbHelper.instance().markPagesForDeletion(list,
+                                    Collections.singletonList(list.pages().get(0)));
+                            newLists.add(list);
                         }
                     }
                     if (callback != null && atLeastOneSelected) {
-                        callback.onDeleted(newLists, listsContainingPage.get(0).pages().get(0));
+                        callback.onDeleted(newLists, first(listsContainingPage).pages().get(0));
                     }
                 })
                 .setNegativeButton(R.string.reading_list_remove_from_list_dialog_cancel_button_text, null)
