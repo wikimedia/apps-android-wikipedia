1b6d3748aa1d1662bdfac7840fd72bcbb3098fc0
sharvani <sharan@wikimedia.org>
2020-05-06 02:43:57 -0700

    minor

--- app/src/main/java/org/wikipedia/suggestededits/SuggestedEditsContributionsFragment.kt
+++ app/src/main/java/org/wikipedia/suggestededits/SuggestedEditsContributionsFragment.kt
@@ -215,7 +215,7 @@ class SuggestedEditsContributionsFragment : Fragment(), SuggestedEditsTypeItem.C
                                 editType = EDIT_TYPE_IMAGE_TAG
                             }
                         }
-                        continuedImageContributions.add(Contribution("", userContribution.title.replace("File:", ""), "", editType, "", DateUtil.iso8601DateParse(userContribution.timestamp), WikiSite.forLanguageCode(contributionLanguage)))
+                        continuedImageContributions.add(Contribution("", userContribution.title, "", editType, "", DateUtil.iso8601DateParse(userContribution.timestamp), WikiSite.forLanguageCode(contributionLanguage)))
                     }
                     for (contribution in continuedImageContributions) {
                         disposables.add(ServiceFactory.get(WikiSite(Service.COMMONS_URL)).getImageInfo(contribution.title, contribution.wikiSite.languageCode())
@@ -233,6 +233,7 @@ class SuggestedEditsContributionsFragment : Fragment(), SuggestedEditsTypeItem.C
                                         val imageInfo = page.imageInfo()!!
                                         contribution.description = imageInfo.metadata!!.imageDescription()
                                         contribution.imageUrl = imageInfo.originalUrl
+                                        contribution.title = contribution.title.replace("File:", "")
                                     }
                                 }, { caught ->
                                     L.e(caught)
