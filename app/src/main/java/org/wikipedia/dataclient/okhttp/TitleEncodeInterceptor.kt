6e17358ee04e4ff8f908ad28b49e844f2a1b1021
Dmitry Brant <dbrant@wikimedia.org>
2020-05-06 10:53:34 -0400

    Urlencode all path segments.
    Change-Id: Ie0467180a88cbc3cfa0f254f95ba88f39c194944

---
+++ app/src/main/java/org/wikipedia/dataclient/okhttp/TitleEncodeInterceptor.kt
@@ -0,0 +1,19 @@
+package org.wikipedia.dataclient.okhttp
+
+import okhttp3.Interceptor
+import okhttp3.Response
+import org.wikipedia.util.UriUtil
+import java.io.IOException
+
+internal class TitleEncodeInterceptor : Interceptor {
+    @Throws(IOException::class)
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val pathSegments = chain.request().url.pathSegments
+        val builder = chain.request().url.newBuilder()
+        for (i in pathSegments.indices) {
+            builder.setEncodedPathSegment(i, UriUtil.encodeURL(pathSegments[i]))
+        }
+        val request = chain.request() // chain.request().newBuilder().url(builder.build()).build()
+        return chain.proceed(request)
+    }
+}
