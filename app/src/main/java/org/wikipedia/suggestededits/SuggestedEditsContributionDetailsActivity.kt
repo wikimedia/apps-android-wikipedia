f1cb7ae9fe452228994e027fb909857bbb4475a6
sharvani <sharan@wikimedia.org>
2020-05-13 11:24:18 -0700

    Statusbar update

--- app/src/main/java/org/wikipedia/suggestededits/SuggestedEditsContributionDetailsActivity.kt
+++ app/src/main/java/org/wikipedia/suggestededits/SuggestedEditsContributionDetailsActivity.kt
@@ -2,12 +2,20 @@ package org.wikipedia.suggestededits
 
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
+import androidx.core.content.ContextCompat
+import org.wikipedia.R
 import org.wikipedia.activity.SingleFragmentActivity
 import org.wikipedia.json.GsonMarshaller
 
 class SuggestedEditsContributionDetailsActivity : SingleFragmentActivity<SuggestedEditsContributionDetailsFragment>() {
     override fun createFragment(): SuggestedEditsContributionDetailsFragment {
         return SuggestedEditsContributionDetailsFragment.newInstance()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStatusBarColor(ContextCompat.getColor(this, R.color.green90))
     }
 
     companion object {
