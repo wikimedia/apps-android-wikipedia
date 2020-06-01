4645ff05f4003122adc77e030ef35fa4ddaaed4e
sharvani <sharan@wikimedia.org>
2020-06-01 11:18:51 -0700

    CR comments addressed

--- app/src/main/java/org/wikipedia/suggestededits/SuggestedEditsContributionDetailsFragment.kt
+++ app/src/main/java/org/wikipedia/suggestededits/SuggestedEditsContributionDetailsFragment.kt
@@ -44,7 +44,7 @@ class SuggestedEditsContributionDetailsFragment : Fragment() {
         revisionText.text = contribution.revisionId.toString()
         contributionTitle.text = contribution.title
         if (contribution.imageUrl.isEmpty() || contribution.imageUrl == "null") contributionImage.visibility = GONE else ViewUtil.loadImageWithRoundedCorners(contributionImage, contribution.imageUrl)
-        dateTimeDetailView.setLabelAndDetail(getString(R.string.suggested_edits_date_time_label), DateUtil.getFeedCardDateString(contribution.date) + " / " + DateUtil.get24HrFormatTimeOnlyString(contribution.date), -1)
+        dateTimeDetailView.setLabelAndDetail(getString(R.string.suggested_edits_contribution_date_time_label), DateUtil.getFeedCardDateString(contribution.date) + " / " + DateUtil.get24HrFormatTimeOnlyString(contribution.date), -1)
         setTypSpecificData()
     }
 
@@ -60,25 +60,25 @@ class SuggestedEditsContributionDetailsFragment : Fragment() {
     private fun setTypSpecificData() {
         when (contribution.editType) {
             EDIT_TYPE_ARTICLE_DESCRIPTION -> {
-                contributionCategory.text = getString(R.string.suggested_edits_article_label)
+                contributionCategory.text = getString(R.string.suggested_edits_contribution_article_label)
                 contributionDiffText.text = getString(R.string.suggested_edits_contribution_label, contribution.description.length)
                 pageViewsDetailView.setLabelAndDetail(getString(R.string.suggested_edits_contribution_views,
-                        if (contribution.editType == EDIT_TYPE_ARTICLE_DESCRIPTION) getString(R.string.suggested_edits_article_label)
-                        else getString(R.string.suggested_edits_image_label)), contribution.pageViews.toString(), R.drawable.ic_trending_up_black_24dp)
-                typeDetailView.setLabelAndDetail(getString(R.string.suggested_edits_type_label), getString(R.string.description_edit_text_hint), R.drawable.ic_article_description)
-                languageDetailView.setLabelAndDetail(getString(R.string.suggested_edits_language_label), WikipediaApp.getInstance().language().getAppLanguageCanonicalName(contribution.wikiSite.languageCode()), -1)
+                        if (contribution.editType == EDIT_TYPE_ARTICLE_DESCRIPTION) getString(R.string.suggested_edits_contribution_article_label)
+                        else getString(R.string.suggested_edits_contribution_image_label)), contribution.pageViews.toString(), R.drawable.ic_trending_up_black_24dp)
+                typeDetailView.setLabelAndDetail(getString(R.string.suggested_edits_contribution_type_label), getString(R.string.description_edit_text_hint), R.drawable.ic_article_description)
+                languageDetailView.setLabelAndDetail(getString(R.string.suggested_edits_contribution_language_label), WikipediaApp.getInstance().language().getAppLanguageCanonicalName(contribution.wikiSite.languageCode()), -1)
             }
             EDIT_TYPE_IMAGE_CAPTION -> {
-                contributionCategory.text = getString(R.string.suggested_edits_image_label)
+                contributionCategory.text = getString(R.string.suggested_edits_contribution_image_label)
                 contributionDiffText.text = getString(R.string.suggested_edits_contribution_label, contribution.description.length)
                 pageViewsDetailView.setLabelAndDetail("", "", -1)
-                typeDetailView.setLabelAndDetail(getString(R.string.suggested_edits_type_label), getString(R.string.description_edit_add_caption_hint), R.drawable.ic_image_caption)
-                languageDetailView.setLabelAndDetail(getString(R.string.suggested_edits_language_label), WikipediaApp.getInstance().language().getAppLanguageCanonicalName(contribution.wikiSite.languageCode()), -1)
+                typeDetailView.setLabelAndDetail(getString(R.string.suggested_edits_contribution_type_label), getString(R.string.description_edit_add_caption_hint), R.drawable.ic_image_caption)
+                languageDetailView.setLabelAndDetail(getString(R.string.suggested_edits_contribution_language_label), WikipediaApp.getInstance().language().getAppLanguageCanonicalName(contribution.wikiSite.languageCode()), -1)
             }
             else -> {
-                contributionCategory.text = getString(R.string.suggested_edits_image_label)
+                contributionCategory.text = getString(R.string.suggested_edits_contribution_image_label)
                 contributionDiffText.text = resources.getQuantityString(R.plurals.suggested_edits_image_tag_contribution_label, contribution.tagCount, contribution.tagCount)
-                typeDetailView.setLabelAndDetail(getString(R.string.suggested_edits_type_label), getString(R.string.suggested_edits_type_image_tag), R.drawable.ic_image_tag)
+                typeDetailView.setLabelAndDetail(getString(R.string.suggested_edits_contribution_type_label), getString(R.string.suggested_edits_contribution_type_image_tag), R.drawable.ic_image_tag)
                 pageViewsDetailView.setLabelAndDetail("", "", -1)
                 languageDetailView.setLabelAndDetail("", "", -1)
             }
