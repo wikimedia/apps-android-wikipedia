fa3fc7abe0729351adeffe726bde4dc2001a2e0a
Dmitry Brant <dbrant@wikimedia.org>
2020-06-17 10:53:34 -0400

    Optimize a bit.
    Change-Id: Ibfcc679b46886907b917b726066036bd2c0d64fe

--- app/src/main/java/org/wikipedia/suggestededits/SuggestedEditsImageTagsFragment.kt
+++ app/src/main/java/org/wikipedia/suggestededits/SuggestedEditsImageTagsFragment.kt
@@ -13,8 +13,6 @@ import android.widget.CompoundButton
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.google.android.material.chip.Chip
-import io.reactivex.Observable
-import io.reactivex.ObservableSource
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.fragment_suggested_edits_image_tags_item.*
@@ -28,7 +26,6 @@ import org.wikipedia.csrf.CsrfTokenClient
 import org.wikipedia.dataclient.Service
 import org.wikipedia.dataclient.ServiceFactory
 import org.wikipedia.dataclient.WikiSite
-import org.wikipedia.dataclient.mwapi.MwPostResponse
 import org.wikipedia.dataclient.mwapi.MwQueryPage
 import org.wikipedia.dataclient.mwapi.media.MediaHelper
 import org.wikipedia.descriptions.DescriptionEditActivity.Action.ADD_IMAGE_TAGS
@@ -331,8 +328,6 @@ class SuggestedEditsImageTagsFragment : SuggestedEditsItemFragment(), CompoundBu
         csrfClient.request(false, object : CsrfTokenClient.Callback {
             override fun success(token: String) {
 
-                val claimObservables = ArrayList<ObservableSource<MwPostResponse>>()
-                val claimComments = ArrayList<String>()
                 val mId = "M" + page!!.pageId()
                 var claimStr = "{\"claims\":["
                 var commentStr = "/* add-depicts: "
@@ -356,26 +351,16 @@ class SuggestedEditsImageTagsFragment : SuggestedEditsItemFragment(), CompoundBu
                 claimStr += "]}"
                 commentStr += " */"
 
-                claimObservables.add(ServiceFactory.get(commonsSite).postEditEntity(mId, token, claimStr, commentStr, null))
-                claimComments.add(commentStr)
-
-                disposables.add(Observable.zip(claimObservables) { responses ->
-                        for (res in responses) {
-                            if (res is MwPostResponse) {
-                                if (res.pageInfo != null) {
-                                    funnel?.logSaved(res.pageInfo!!.lastRevId, if (claimComments.isEmpty()) "" else claimComments.removeAt(0))
-                                }
-                            }
-                        }
-                        responses[0]
-                    }
+                disposables.add(ServiceFactory.get(commonsSite).postEditEntity(mId, token, claimStr, commentStr, null)
                     .subscribeOn(Schedulers.io())
                     .observeOn(AndroidSchedulers.mainThread())
                     .doAfterTerminate {
                         publishing = false
                     }
                     .subscribe({
-                        // TODO: check anything else in the response?
+                        if (it.pageInfo != null) {
+                            funnel?.logSaved(it.pageInfo!!.lastRevId, commentStr)
+                        }
                         publishSuccess = true
                         onSuccess()
                     }, { caught ->
