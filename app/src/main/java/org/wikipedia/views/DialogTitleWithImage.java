744eecd232e87b3fe77b822ca558d2c2150b1d82
Isira Seneviratne <isirasen96@gmail.com>
2020-06-27 19:13:51 +0530

    Use view binding in DialogTitleWithImage.

--- app/src/main/java/org/wikipedia/views/DialogTitleWithImage.java
+++ app/src/main/java/org/wikipedia/views/DialogTitleWithImage.java
@@ -3,31 +3,28 @@ package org.wikipedia.views;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import androidx.annotation.DrawableRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.StringRes;
 import androidx.appcompat.widget.AppCompatImageView;
 
-import org.wikipedia.R;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
+import org.wikipedia.databinding.ViewDialogTitleWithImageBinding;
 
 @SuppressLint("ViewConstructor")
 public class DialogTitleWithImage extends LinearLayout {
-    @BindView(R.id.title) TextView titleView;
-    @BindView(R.id.image) AppCompatImageView image;
+    private AppCompatImageView image;
     private boolean preserveImageAspect;
 
     public DialogTitleWithImage(@NonNull Context context, @StringRes int titleRes, @DrawableRes int imageRes, boolean preserveImageAspect) {
         super(context);
         this.preserveImageAspect = preserveImageAspect;
         setOrientation(VERTICAL);
-        inflate(context, R.layout.view_dialog_title_with_image, this);
-        ButterKnife.bind(this);
-        titleView.setText(titleRes);
+
+        final ViewDialogTitleWithImageBinding binding = ViewDialogTitleWithImageBinding.bind(this);
+        image = binding.image;
+
+        binding.title.setText(titleRes);
         image.setImageResource(imageRes);
     }
 
