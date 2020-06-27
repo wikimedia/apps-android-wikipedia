6b79e91faa800d41550863daada9713b74afe977
Isira Seneviratne <isirasen96@gmail.com>
2020-06-27 16:23:00 +0530

    Use view binding in SearchCardView.

--- app/src/main/java/org/wikipedia/feed/searchbar/SearchCardView.java
+++ app/src/main/java/org/wikipedia/feed/searchbar/SearchCardView.java
@@ -1,14 +1,13 @@
 package org.wikipedia.feed.searchbar;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 
 import org.wikipedia.R;
+import org.wikipedia.databinding.ViewSearchBarBinding;
 import org.wikipedia.feed.view.DefaultFeedCardView;
 import org.wikipedia.util.FeedbackUtil;
 import org.wikipedia.util.ResourceUtil;
-
-import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 public class SearchCardView extends DefaultFeedCardView<SearchCard> {
     public interface Callback {
@@ -18,21 +17,21 @@ public class SearchCardView extends DefaultFeedCardView<SearchCard> {
 
     public SearchCardView(Context context) {
         super(context);
-        inflate(getContext(), R.layout.view_search_bar, this);
+
+        final ViewSearchBarBinding binding = ViewSearchBarBinding.inflate(LayoutInflater.from(context));
+
+        binding.searchContainer.setOnClickListener(v -> {
+            if (getCallback() != null) {
+                getCallback().onSearchRequested();
+            }
+        });
+        binding.voiceSearchButton.setOnClickListener(v -> {
+            if (getCallback() != null) {
+                getCallback().onVoiceSearchRequested();
+            }
+        });
+
         setCardBackgroundColor(ResourceUtil.getThemedColor(context, R.attr.searchItemBackground));
-        ButterKnife.bind(this);
         FeedbackUtil.setToolbarButtonLongPressToast(findViewById(R.id.voice_search_button));
-    }
-
-    @OnClick(R.id.search_container) void onSearchClick() {
-        if (getCallback() != null) {
-            getCallback().onSearchRequested();
-        }
-    }
-
-    @OnClick(R.id.voice_search_button) void onVoiceSearchClick() {
-        if (getCallback() != null) {
-            getCallback().onVoiceSearchRequested();
-        }
     }
 }
