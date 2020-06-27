1c3e3284949039d590deb2d0985f1cac58fc32a5
Isira Seneviratne <isirasen96@gmail.com>
2020-06-27 16:36:34 +0530

    Use view binding in AccessibilityCardView.

--- app/src/main/java/org/wikipedia/feed/accessibility/AccessibilityCardView.java
+++ app/src/main/java/org/wikipedia/feed/accessibility/AccessibilityCardView.java
@@ -1,32 +1,29 @@
 package org.wikipedia.feed.accessibility;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-import org.wikipedia.R;
+import org.wikipedia.databinding.ViewCardAccessibilityBinding;
 import org.wikipedia.feed.model.Card;
 import org.wikipedia.feed.view.FeedAdapter;
 import org.wikipedia.feed.view.FeedCardView;
-
-import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 public class AccessibilityCardView extends LinearLayout implements FeedCardView<Card> {
     @Nullable private FeedAdapter.Callback callback;
 
     public AccessibilityCardView(Context context) {
         super(context);
-        inflate(getContext(), R.layout.view_card_accessibility, this);
-        ButterKnife.bind(this);
-    }
-
-    @OnClick(R.id.view_card_accessibility_button_load_more) void onLoadMoreClick() {
-        if (callback != null) {
-            callback.onRequestMore();
-        }
+        final ViewCardAccessibilityBinding binding =
+                ViewCardAccessibilityBinding.inflate(LayoutInflater.from(context));
+        binding.viewCardAccessibilityButtonLoadMore.setOnClickListener(v -> {
+            if (callback != null) {
+                callback.onRequestMore();
+            }
+        });
     }
 
     @Override public void setCallback(@Nullable FeedAdapter.Callback callback) {
