7e14597d7da08a73732fc9a291cd3a2c4e581ace
sharvani <sharan@wikimedia.org>
2020-07-02 12:27:33 -0700

    Rename 'Suggested edits' in tabs and activity title to 'Edits' Rename related enum

--- app/src/main/java/org/wikipedia/main/MainActivity.java
+++ app/src/main/java/org/wikipedia/main/MainActivity.java
@@ -132,7 +132,7 @@ public class MainActivity extends SingleFragmentActivity<MainFragment> implement
                 ((HistoryFragment) getFragment().getCurrentFragment()).refresh();
             }
 
-            if (tab.equals(NavTab.SUGGESTED_EDITS)) {
+            if (tab.equals(NavTab.EDITS)) {
                 getFragment().hideNavTabOverlayLayout();
             }
 
