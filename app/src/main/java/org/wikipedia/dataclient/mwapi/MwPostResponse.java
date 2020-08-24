76b68397b25e10558a206e21c6b546dbddeab87a
Dmitry Brant <dbrant@wikimedia.org>
2020-08-24 13:25:26 -0400

    Use existing EntityPostResponse.
    Change-Id: Ic26e0a43e983c8f7dba7b3049632de5856c0fca8

--- app/src/main/java/org/wikipedia/dataclient/mwapi/MwPostResponse.java
+++ app/src/main/java/org/wikipedia/dataclient/mwapi/MwPostResponse.java
@@ -2,12 +2,9 @@ package org.wikipedia.dataclient.mwapi;
 
 import androidx.annotation.Nullable;
 
-import org.wikipedia.dataclient.wikidata.Entities;
-
 @SuppressWarnings("unused")
 public class MwPostResponse extends MwResponse {
     @Nullable private MwQueryPage pageinfo;
-    @Nullable private Entities.Entity entity;
     @Nullable private String options;
     private int success;
 
@@ -25,10 +22,6 @@ public class MwPostResponse extends MwResponse {
 
     @Nullable public MwQueryPage getPageInfo() {
         return pageinfo;
-    }
-
-    @Nullable public Entities.Entity getEntity() {
-        return entity;
     }
 }
 
