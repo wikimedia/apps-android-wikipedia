7f5e85a52a114523a868a19c06200a985949dd24
cooltey <coolteygame@gmail.com>
2020-08-26 17:12:52 -0700

    Remove unused logic

--- app/src/main/java/org/wikipedia/LongPressHandler.java
+++ app/src/main/java/org/wikipedia/LongPressHandler.java
@@ -65,8 +65,6 @@ public class LongPressHandler implements View.OnCreateContextMenuListener,
             AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
             title = ((ListViewOverflowMenuListener) overflowMenuListener).getTitleForListPosition(info.position);
             showPopupMenu(view, info);
-        } else {
-            showPopupMenu(view, null);
         }
     }
 
