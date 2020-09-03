fcedf45b904771a266e84dae2223c8b858c80b3a
sharvani <sharan@wikimedia.org>
2020-09-03 13:10:06 -0700

    unwanted binding removed

--- app/src/main/java/org/wikipedia/feed/view/CardFooterView.kt
+++ app/src/main/java/org/wikipedia/feed/view/CardFooterView.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
-import butterknife.ButterKnife
 import kotlinx.android.synthetic.main.view_card_footer.view.*
 import org.wikipedia.R
 
@@ -17,7 +16,6 @@ internal class CardFooterView constructor(context: Context, attrs: AttributeSet?
 
     init {
         View.inflate(context, R.layout.view_card_footer, this)
-        ButterKnife.bind(this)
         footerActionButton.setOnClickListener {
             callback?.onFooterClicked()
         }
