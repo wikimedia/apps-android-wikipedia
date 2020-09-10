a9ee0517c2a6449d69396101ba50216465ccc912
Dmitry Brant <me@dmitrybrant.com>
2020-09-10 13:34:31 -0400

    Revert "Revert "Feed Header and Footer Re-design changes" (#1608)"
    This reverts commit 46496d75fee2c09dc012259eba1e9461539704b4.

--- app/src/main/java/org/wikipedia/feed/becauseyouread/BecauseYouReadCardView.java
+++ app/src/main/java/org/wikipedia/feed/becauseyouread/BecauseYouReadCardView.java
@@ -6,7 +6,6 @@ import android.view.View;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-import org.wikipedia.R;
 import org.wikipedia.feed.model.Card;
 import org.wikipedia.feed.view.ListCardItemView;
 import org.wikipedia.feed.view.ListCardRecyclerAdapter;
@@ -36,8 +35,6 @@ public class BecauseYouReadCardView extends ListCardView<BecauseYouReadCard> {
     private void header(@NonNull final BecauseYouReadCard card) {
         int age = (int) card.daysOld();
         headerView().setTitle(card.title())
-                .setImage(R.drawable.ic_restore_black_24dp)
-                .setImageCircleColor(R.color.base30)
                 .setLangCode(null)
                 .setCard(card)
                 .setCallback(getCallback());
