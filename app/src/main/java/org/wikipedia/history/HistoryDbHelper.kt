f5f5c527c57e2281710b5ed88984aea8e78a055e
Dmitry Brant <dbrant@wikimedia.org>
2020-09-15 10:43:02 -0400

    Fix search order when searching History.
    Change-Id: I09c12f1452172cfe50f875ea0dc49e16c7724334

--- app/src/main/java/org/wikipedia/history/HistoryDbHelper.kt
+++ app/src/main/java/org/wikipedia/history/HistoryDbHelper.kt
@@ -14,7 +14,7 @@ object HistoryDbHelper {
 
     fun findHistoryItem(searchQuery: String): SearchResults {
         val db = WikipediaApp.getInstance().database.readableDatabase
-        val titleCol = PageHistoryContract.PageWithImage.API_TITLE.qualifiedName()
+        val titleCol = PageHistoryContract.PageWithImage.DISPLAY_TITLE.qualifiedName()
         var selection: String? = null
         var selectionArgs: Array<String>? = null
         var searchStr = searchQuery
@@ -26,7 +26,7 @@ object HistoryDbHelper {
         db.query(PageHistoryContract.PageWithImage.TABLES, null,
                 selection,
                 selectionArgs,
-                null, null, null).use { cursor ->
+                null, null, PageHistoryContract.PageWithImage.ORDER_MRU).use { cursor ->
             if (cursor.moveToFirst()) {
                 val indexedEntry = IndexedHistoryEntry(cursor)
                 val pageTitle: PageTitle = indexedEntry.entry.title
