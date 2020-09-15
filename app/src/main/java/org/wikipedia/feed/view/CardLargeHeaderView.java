2d43042232d45b5570791f89e56c18b7db91289d
cooltey <cfeng@wikimedia.org>
2020-09-14 19:37:47 -0700

    Apply the correct card background color to the border base

--- app/src/main/java/org/wikipedia/feed/view/CardLargeHeaderView.java
+++ app/src/main/java/org/wikipedia/feed/view/CardLargeHeaderView.java
@@ -96,7 +96,7 @@ public class CardLargeHeaderView extends ConstraintLayout {
                 new int[] { leftColor, rightColor });
 
         // card background
-        gradientDrawable.setAlpha(90);
+        gradientDrawable.setAlpha(70);
         gradientDrawable.setCornerRadius(borderBaseView.getRadius());
         container.setBackground(gradientDrawable);
 
