376508d009bb9edd51b8ff0cd806bfde8f17c0f5
cooltey <coolteygame@gmail.com>
2020-09-15 17:22:00 -0700

    Adjust layouts and add extra method of showing shadow on custom wikicard

--- app/src/main/java/org/wikipedia/views/WikiCardView.kt
+++ app/src/main/java/org/wikipedia/views/WikiCardView.kt
@@ -3,6 +3,7 @@ package org.wikipedia.views
 import android.content.Context
 import android.os.Build
 import android.util.AttributeSet
+import androidx.annotation.ColorInt
 import androidx.core.content.ContextCompat
 import com.google.android.material.card.MaterialCardView
 import org.wikipedia.R
@@ -15,43 +16,51 @@ open class WikiCardView @JvmOverloads constructor(context: Context, attrs: Attri
     : MaterialCardView(context, attrs, defStyleAttr) {
 
     init {
-        radius = context.resources.getDimension(R.dimen.wiki_card_radius)
-
+        var hasBorder = true
+        var shadowColor = if (WikipediaApp.getInstance().currentTheme == Theme.DARK)
+            ContextCompat.getColor(context, R.color.base0) else ContextCompat.getColor(context, R.color.base70)
         if (attrs != null) {
             val array = context.obtainStyledAttributes(attrs, R.styleable.WikiCardView)
-            val hasBorder = array.getBoolean(R.styleable.WikiCardView_hasBorder, true)
-            if (hasBorder) {
-                strokeWidth = when (WikipediaApp.getInstance().currentTheme) {
-                    Theme.DARK -> {
-                        DimenUtil.roundedDpToPx(0f)
-                    }
-                    Theme.BLACK -> {
-                        strokeColor = ContextCompat.getColor(context, R.color.base10)
-                        DimenUtil.roundedDpToPx(1f)
-                    }
-                    else -> {
-                        strokeColor = ContextCompat.getColor(context, R.color.base80)
-                        DimenUtil.roundedDpToPx(0.5f)
-                    }
+            hasBorder = array.getBoolean(R.styleable.WikiCardView_hasBorder, true)
+            shadowColor = array.getColor(R.styleable.WikiCardView_shadowColor, ContextCompat.getColor(context, R.color.base70))
+            array.recycle()
+        }
+
+        setup(hasBorder, shadowColor)
+    }
+
+    private fun setup(hasBorder: Boolean, @ColorInt shadowColor: Int) {
+        radius = context.resources.getDimension(R.dimen.wiki_card_radius)
+        if (hasBorder) {
+            strokeWidth = when (WikipediaApp.getInstance().currentTheme) {
+                Theme.DARK -> {
+                    DimenUtil.roundedDpToPx(0f)
+                }
+                Theme.BLACK -> {
+                    strokeColor = ContextCompat.getColor(context, R.color.base10)
+                    DimenUtil.roundedDpToPx(1f)
+                }
+                else -> {
+                    strokeColor = ContextCompat.getColor(context, R.color.base80)
+                    DimenUtil.roundedDpToPx(0.5f)
                 }
             }
-            array.recycle()
         }
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
             when (WikipediaApp.getInstance().currentTheme) {
                 Theme.DARK -> {
                     cardElevation = DimenUtil.dpToPx(8f)
-                    outlineAmbientShadowColor = ContextCompat.getColor(getContext(), R.color.base0)
-                    outlineSpotShadowColor = ContextCompat.getColor(getContext(), R.color.base0)
+                    outlineAmbientShadowColor = shadowColor
+                    outlineSpotShadowColor = shadowColor
                 }
                 Theme.BLACK -> {
                     cardElevation = 0f
                 }
                 else -> {
                     cardElevation = DimenUtil.dpToPx(8f)
-                    outlineAmbientShadowColor = ContextCompat.getColor(getContext(), R.color.base70)
-                    outlineSpotShadowColor = ContextCompat.getColor(getContext(), R.color.base70)
+                    outlineAmbientShadowColor = shadowColor
+                    outlineSpotShadowColor = shadowColor
                 }
             }
         } else {
