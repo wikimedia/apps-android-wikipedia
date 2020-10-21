4866e126ca237d787e1836c727789a81e806ed9d
sharvani <sharan@wikimedia.org>
2020-10-21 13:06:18 -0700

    Minor edit

--- app/src/main/java/org/wikipedia/feed/suggestededits/SuggestedEditsCardItemFragment.kt
+++ app/src/main/java/org/wikipedia/feed/suggestededits/SuggestedEditsCardItemFragment.kt
@@ -403,7 +403,7 @@ class SuggestedEditsCardItemFragment : Fragment() {
         showAddDescriptionUI()
         callToActionButton.text = context?.getString(R.string.suggested_edits_feed_card_add_translation_in_language_button,
                 app.language().getAppLanguageCanonicalName(targetLanguage))
-        sourceDescription = if (sourceSummaryForEdit != null) sourceSummaryForEdit!!.displayTitle!! else ""
+        sourceDescription = if (sourceSummaryForEdit != null) sourceSummaryForEdit!!.description!! else ""
         viewArticleSubtitle.visibility = VISIBLE
         viewArticleSubtitle.text = sourceDescription
     }
@@ -420,7 +420,7 @@ class SuggestedEditsCardItemFragment : Fragment() {
         showAddImageCaptionUI()
         callToActionButton.text = context?.getString(R.string.suggested_edits_feed_card_translate_image_caption,
                 app.language().getAppLanguageCanonicalName(targetLanguage))
-        sourceDescription = if (sourceSummaryForEdit != null) sourceSummaryForEdit!!.displayTitle!! else ""
+        sourceDescription = if (sourceSummaryForEdit != null) sourceSummaryForEdit!!.description!! else ""
         viewArticleSubtitle.visibility = VISIBLE
         viewArticleSubtitle.text = sourceDescription
     }
