202226ea42c74e882632b9797f2b557529c5dddf
sharvani <sharan@wikimedia.org>
2020-10-22 16:28:20 -0700

    Increase the confidence threshold to 65%

--- app/src/extra/java/org/wikipedia/mlkit/MlKitLanguageDetector.kt
+++ app/src/extra/java/org/wikipedia/mlkit/MlKitLanguageDetector.kt
@@ -1,6 +1,7 @@
 package org.wikipedia.mlkit
 
 import com.google.mlkit.nl.languageid.LanguageIdentification
+import com.google.mlkit.nl.languageid.LanguageIdentificationOptions
 
 class MlKitLanguageDetector {
     interface Callback {
@@ -9,7 +10,9 @@ class MlKitLanguageDetector {
 
     var callback: Callback? = null
     fun detectLanguageFromText(text: String) {
-        val languageIdentifier = LanguageIdentification.getClient()
+        val languageIdentifier = LanguageIdentification.getClient(LanguageIdentificationOptions.Builder()
+                .setConfidenceThreshold(0.65f)
+                .build())
         languageIdentifier.identifyLanguage(text)
                 .addOnSuccessListener { languageCode: String ->
                     if (languageCode != "und") {
