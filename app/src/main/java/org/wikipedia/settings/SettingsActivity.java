22cb10ed9ad3879508ee74b6a92826e82137903d
zhlcbri <zhlcbri@icloud.com>
2020-10-31 17:51:03 -0700

    Feed now won't refresh if language list hasn't changed from settings

--- app/src/main/java/org/wikipedia/settings/SettingsActivity.java
+++ app/src/main/java/org/wikipedia/settings/SettingsActivity.java
@@ -2,10 +2,13 @@ package org.wikipedia.settings;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 
+import org.wikipedia.WikipediaApp;
 import org.wikipedia.activity.SingleFragmentActivity;
+import org.wikipedia.util.StringUtil;
 
 import static org.wikipedia.Constants.ACTIVITY_REQUEST_ADD_A_LANGUAGE;
 import static org.wikipedia.Constants.ACTIVITY_REQUEST_FEED_CONFIGURE;
@@ -13,6 +16,8 @@ import static org.wikipedia.Constants.ACTIVITY_REQUEST_FEED_CONFIGURE;
 public class SettingsActivity extends SingleFragmentActivity<SettingsFragment> {
     public static final int ACTIVITY_RESULT_LANGUAGE_CHANGED = 1;
     public static final int ACTIVITY_RESULT_FEED_CONFIGURATION_CHANGED = 2;
+    private WikipediaApp app = WikipediaApp.getInstance();
+    private String initialLanguageList;
 
     public static Intent newIntent(@NonNull Context ctx) {
         return new Intent(ctx, SettingsActivity.class);
@@ -24,9 +29,18 @@ public class SettingsActivity extends SingleFragmentActivity<SettingsFragment> {
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initialLanguageList = StringUtil.listToJsonArrayString(app.language().getAppLanguageCodes());
+    }
+
+    @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == ACTIVITY_REQUEST_ADD_A_LANGUAGE) {
+        String finalLanguageList = StringUtil.listToJsonArrayString(app.language().getAppLanguageCodes());
+
+        if (requestCode == ACTIVITY_REQUEST_ADD_A_LANGUAGE
+                && (!finalLanguageList.equals(initialLanguageList))) {
             setResult(ACTIVITY_RESULT_LANGUAGE_CHANGED);
         } else if (requestCode == ACTIVITY_REQUEST_FEED_CONFIGURE) {
             setResult(ACTIVITY_RESULT_FEED_CONFIGURATION_CHANGED);
