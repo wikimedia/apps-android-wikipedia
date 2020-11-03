6fcc277ec088838741006adc663451eacf7e8805
cooltey <coolteygame@gmail.com>
2020-11-02 17:24:13 -0800

    remove unused imports

--- app/src/main/java/org/wikipedia/page/PageFragment.java
+++ app/src/main/java/org/wikipedia/page/PageFragment.java
@@ -6,8 +6,6 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Configuration;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
