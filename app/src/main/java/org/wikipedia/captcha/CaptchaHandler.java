f5d138fabfb623ef0530d8bb749a1b5af8ba5177
Cooltey Feng <cfeng@wikimedia.org>
2020-11-23 07:00:01 -0800

    Always apply white background when loading images (#1781)
    * Always apply white background when loading images
    
    * Fix white corners
    
    Co-authored-by: Sharvani Haran <sharvaniharan@users.noreply.github.com>
    Co-authored-by: Dmitry Brant <dbrant@wikimedia.org>

--- app/src/main/java/org/wikipedia/captcha/CaptchaHandler.java
+++ app/src/main/java/org/wikipedia/captcha/CaptchaHandler.java
@@ -139,7 +139,7 @@ public class CaptchaHandler {
         // In case there was a captcha attempt before
         captchaText.setText("");
 
-        ViewUtil.loadImage(captchaImage, captchaResult.getCaptchaUrl(wiki), false, false, true);
+        ViewUtil.loadImage(captchaImage, captchaResult.getCaptchaUrl(wiki), false, false, true, null);
     }
 
     public void hideCaptcha() {
