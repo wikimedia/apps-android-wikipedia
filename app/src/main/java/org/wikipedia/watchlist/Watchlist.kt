9f58eaedc4811d1e707656c4699ecd91bf30f112
cooltey <coolteygame@gmail.com>
2020-12-07 15:26:40 -0800

    Created database for saving time period of article

---
+++ app/src/main/java/org/wikipedia/watchlist/Watchlist.kt
@@ -0,0 +1,77 @@
+package org.wikipedia.watchlist
+
+import android.os.Parcel
+import android.os.Parcelable
+import java.util.*
+
+class Watchlist : Parcelable {
+    val apiTitle: String?
+    val displayTitle: String?
+    val timestamp: Date
+    val timePeriod: Long
+
+    constructor(apiTitle: String,
+                displayTitle: String,
+                timestamp: Date,
+                timePeriod: Long) {
+        this.apiTitle = apiTitle
+        this.displayTitle = displayTitle
+        this.timestamp = timestamp
+        this.timePeriod = timePeriod
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (other !is Watchlist) {
+            return false
+        }
+        return apiTitle == other.apiTitle && displayTitle == other.displayTitle
+    }
+
+    override fun hashCode(): Int {
+        var result = apiTitle.hashCode()
+        result = 31 * result + displayTitle.hashCode()
+        result = 31 * result + timestamp.hashCode()
+        result = 31 * result + timePeriod.toInt()
+        return result
+    }
+
+    override fun toString(): String {
+        return ("Watchlist{"
+                + "apiTitle=" + apiTitle
+                + ", displayTitle=" + displayTitle
+                + ", timestamp=" + timestamp.time
+                + ", timePeriod=" + timePeriod
+                + '}')
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.writeString(apiTitle)
+        dest.writeString(displayTitle)
+        dest.writeLong(timestamp.time)
+        dest.writeLong(timePeriod)
+    }
+
+    private constructor(`in`: Parcel) {
+        apiTitle = `in`.readString()
+        displayTitle = `in`.readString()
+        timestamp = Date(`in`.readLong())
+        timePeriod = `in`.readLong()
+    }
+
+    companion object {
+        val DATABASE_TABLE = WatchlistDatabaseTable()
+        val CREATOR: Parcelable.Creator<Watchlist?> = object : Parcelable.Creator<Watchlist?> {
+            override fun createFromParcel(`in`: Parcel): Watchlist {
+                return Watchlist(`in`)
+            }
+
+            override fun newArray(size: Int): Array<Watchlist?> {
+                return arrayOfNulls(size)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/org/wikipedia/watchlist/WatchlistDatabaseTable.kt
@@ -0,0 +1,64 @@
+package org.wikipedia.watchlist
+
+import android.content.ContentValues
+import android.database.Cursor
+import android.net.Uri
+import android.provider.BaseColumns
+import org.wikipedia.WikipediaApp
+import org.wikipedia.database.DatabaseTable
+import org.wikipedia.database.DbUtil
+import org.wikipedia.database.column.*
+import org.wikipedia.database.contract.AppContentProviderContract
+import org.wikipedia.database.contract.PageHistoryContract
+import org.wikipedia.dataclient.page.TalkPage.Topic
+
+private const val TABLE = "localwatchlist"
+private const val PATH = "localwatchlist"
+private val URI = Uri.withAppendedPath(AppContentProviderContract.AUTHORITY_BASE, PATH)
+
+class WatchlistDatabaseTable : DatabaseTable<Watchlist?>(TABLE, URI) {
+
+    override fun fromCursor(cursor: Cursor): Watchlist {
+        return Watchlist(API_TITLE.`val`(cursor), DISPLAY_TITLE.`val`(cursor), TIMESTAMP.`val`(cursor), TIME_PERIOD.`val`(cursor))
+    }
+
+    override fun toContentValues(obj: Watchlist?): ContentValues {
+        val contentValues = ContentValues()
+        contentValues.put(API_TITLE.name, obj!!.apiTitle)
+        contentValues.put(DISPLAY_TITLE.name, obj.displayTitle)
+        contentValues.put(TIMESTAMP.name, obj.timestamp.time)
+        contentValues.put(TIME_PERIOD.name, obj.timePeriod)
+        return contentValues
+    }
+
+    override fun getDBVersionIntroducedAt(): Int {
+        return Companion.DB_VER_INTRODUCED
+    }
+
+    override fun getColumnsAdded(version: Int): Array<Column<*>> {
+        return when (version) {
+            Companion.DB_VER_INTRODUCED -> arrayOf(ID, API_TITLE, DISPLAY_TITLE, TIMESTAMP, TIME_PERIOD)
+            else -> super.getColumnsAdded(version)
+        }
+    }
+
+    override fun getPrimaryKeySelection(obj: Watchlist, selectionArgs: Array<String>): String {
+        return super.getPrimaryKeySelection(obj, SELECTION)
+    }
+
+    override fun getUnfilteredPrimaryKeySelectionArgs(obj: Watchlist): Array<String> {
+        return arrayOf(obj.apiTitle, obj.displayTitle)
+    }
+
+    // TODO: add method of getting time period for specific article.
+
+    companion object {
+        private const val DB_VER_INTRODUCED = 22
+        private val ID = LongColumn(TABLE, BaseColumns._ID, "integer primary key")
+        private val API_TITLE = StrColumn(TABLE, "apiTitle", "string")
+        private val DISPLAY_TITLE = StrColumn(TABLE, "displayTitle", "string")
+        private val TIMESTAMP = DateColumn(TABLE, "timestamp", "integer")
+        private val TIME_PERIOD = LongColumn(TABLE, "timePeriod", "integer") // in days
+        private val SELECTION = DbUtil.qualifiedNames(API_TITLE)
+    }
+}
