d7b5a1b966da103f4a8dbfda0271902ecccf84b6
cooltey <coolteygame@gmail.com>
2020-12-07 13:59:13 -0800

    Create dialog

---
+++ app/src/main/java/org/wikipedia/watchlist/WatchlistTimePeriodDialog.kt
@@ -0,0 +1,39 @@
+package org.wikipedia.watchlist
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import butterknife.ButterKnife
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import org.wikipedia.R
+import org.wikipedia.page.ExtendedBottomSheetDialogFragment
+import org.wikipedia.util.DimenUtil
+
+class WatchlistTimePeriodDialog : ExtendedBottomSheetDialogFragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val rootView = inflater.inflate(R.layout.view_watchlist_time_period, container)
+        ButterKnife.bind(this, rootView)
+        return rootView
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        BottomSheetBehavior.from(requireView().parent as View).peekHeight = DimenUtil.roundedDpToPx(DimenUtil.getDimension(R.dimen.navTabDialogPeekHeight))
+    }
+
+    companion object {
+        fun newInstance(): WatchlistTimePeriodDialog {
+            return WatchlistTimePeriodDialog()
+        }
+    }
+}
