7437addede33d288ff2f71113dc02cfb084cdba7
cooltey <coolteygame@gmail.com>
2021-01-21 15:30:07 -0800

    Convert AutoFitRecyclerView.java to Kotlin

---
+++ app/src/main/java/org/wikipedia/feed/view/FeedView.kt
@@ -0,0 +1,55 @@
+package org.wikipedia.feed.view
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import org.wikipedia.R
+import org.wikipedia.feed.dayheader.DayHeaderMarginItemDecoration
+import org.wikipedia.util.DimenUtil
+import org.wikipedia.views.AutoFitRecyclerView
+import org.wikipedia.views.HeaderMarginItemDecoration
+import org.wikipedia.views.MarginItemDecoration
+
+class FeedView constructor(context: Context, attrs: AttributeSet? = null) : AutoFitRecyclerView(context, attrs) {
+    private var recyclerLayoutManager: StaggeredGridLayoutManager
+
+    val firstVisibleItemPosition: Int
+        get() {
+            val manager = layoutManager as StaggeredGridLayoutManager
+            val visibleItems = IntArray(manager.spanCount)
+            manager.findFirstVisibleItemPositions(visibleItems)
+            return visibleItems[0]
+        }
+
+    init {
+        isVerticalScrollBarEnabled = true
+        recyclerLayoutManager = StaggeredGridLayoutManager(columns,
+                StaggeredGridLayoutManager.VERTICAL)
+        itemAnimator = DefaultItemAnimator()
+        layoutManager = recyclerLayoutManager
+        addItemDecoration(MarginItemDecoration(context,
+                R.dimen.view_list_card_margin_horizontal, R.dimen.view_list_card_margin_vertical,
+                R.dimen.view_list_card_margin_horizontal, R.dimen.view_list_card_margin_vertical))
+        addItemDecoration(HeaderMarginItemDecoration(context,
+                R.dimen.view_feed_padding_top, R.dimen.view_feed_search_padding_bottom))
+        addItemDecoration(DayHeaderMarginItemDecoration(context, R.dimen.view_feed_day_header_margin_bottom))
+        callback = RecyclerViewColumnCallback()
+        clipChildren = false
+    }
+
+    private inner class RecyclerViewColumnCallback : Callback {
+        override fun onColumns(columns: Int) {
+            // todo: when there is only one element, should we setSpanCount to 1? e.g.:
+            //       adapter.getItemCount() <= 1 ? 1 : columns;
+            //       we would need to also notify the layout manager when the data set changes
+            //       though.
+            recyclerLayoutManager.spanCount = columns
+            val padding = DimenUtil.roundedDpToPx(DimenUtil.getDimension(R.dimen.view_list_card_margin_horizontal))
+            setPadding(padding, 0, padding, 0)
+
+            // Allow card children to overflow when there's only one column
+            clipChildren = columns > 1
+        }
+    }
+}
---
+++ app/src/main/java/org/wikipedia/views/AutoFitRecyclerView.kt
@@ -0,0 +1,62 @@
+package org.wikipedia.views
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.recyclerview.widget.RecyclerView
+import org.wikipedia.R
+import org.wikipedia.util.log.L.logRemoteErrorIfProd
+import androidx.annotation.IntRange as AndroidIntRange
+
+/** [RecyclerView] that invokes a callback when the number of columns should be updated.  */
+open class AutoFitRecyclerView constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) :
+        RecyclerView(context, attrs, defStyleAttr) {
+
+    interface Callback {
+        fun onColumns(columns: Int)
+    }
+
+    private var minColumnWidth = 0
+    @AndroidIntRange(from = MIN_COLUMNS.toLong())
+    var columns = MIN_COLUMNS
+    var callback: Callback = DefaultCallback()
+
+    init {
+        if (attrs != null) {
+            val array = context.obtainStyledAttributes(attrs,
+                    R.styleable.AutoFitRecyclerView, defStyleAttr, 0)
+            minColumnWidth = array.getDimensionPixelSize(R.styleable.AutoFitRecyclerView_minColumnWidth, 0)
+            array.recycle()
+        }
+    }
+
+    override fun onMeasure(widthSpec: Int, heightSpec: Int) {
+        super.onMeasure(widthSpec, heightSpec)
+        val cols = calculateColumns(minColumnWidth, measuredWidth)
+        if (columns != cols) {
+            columns = cols
+            callback.onColumns(columns)
+        }
+    }
+
+    override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
+        // https://issuetracker.google.com/issues/37034096
+        // TODO: check again in Sep 2021
+        try {
+            super.onLayout(changed, l, t, r, b)
+        } catch (e: Exception) {
+            logRemoteErrorIfProd(e)
+        }
+    }
+
+    private fun calculateColumns(columnWidth: Int, availableWidth: Int): Int {
+        return if (columnWidth > 0) MIN_COLUMNS.coerceAtLeast(availableWidth / columnWidth) else MIN_COLUMNS
+    }
+
+    private class DefaultCallback : Callback {
+        override fun onColumns(columns: Int) {}
+    }
+
+    companion object {
+        private const val MIN_COLUMNS = 1
+    }
+}
