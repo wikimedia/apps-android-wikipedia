1abe4f2a7f90218243cba414f11403c21ca69f7a
cooltey <coolteygame@gmail.com>
2021-01-21 16:15:35 -0800

    Fix: get fragment properly to prevent issue when changing device orientation

--- app/src/main/java/org/wikipedia/activity/SingleFragmentActivity.kt
+++ app/src/main/java/org/wikipedia/activity/SingleFragmentActivity.kt
@@ -1,24 +1,32 @@
 package org.wikipedia.activity
 
 import android.os.Bundle
+import androidx.annotation.IdRes
 import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
 import org.wikipedia.R
+
 
 /**
  * Boilerplate for a FragmentActivity containing a single stack of Fragments.
  */
 abstract class SingleFragmentActivity<T : Fragment> : BaseActivity() {
-    lateinit var fragment: T
-
-    public override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(layout)
-        fragment = createFragment()
-        supportFragmentManager.beginTransaction().add(R.id.fragment_container, fragment).commit()
+        if (getFragment() == null) {
+            supportFragmentManager.beginTransaction().add(containerId, createFragment()).commit()
+        }
     }
 
     protected abstract fun createFragment(): T
+
+    /** @return The Fragment added to the stack.
+     */
+    @Suppress("UNCHECKED_CAST")
+    protected open fun getFragment(): T? {
+        return supportFragmentManager.findFragmentById(containerId) as T?
+    }
 
     /** @return The resource layout to inflate which must contain a [android.view.ViewGroup]
      * whose ID is [.getContainerId].
@@ -26,4 +34,10 @@ abstract class SingleFragmentActivity<T : Fragment> : BaseActivity() {
     @get:LayoutRes
     protected open val layout: Int
         get() = R.layout.activity_single_fragment
+
+    /** @return The resource identifier for the Fragment container.
+     */
+    @get:IdRes
+    protected open val containerId: Int
+        get() = R.id.fragment_container
 }
