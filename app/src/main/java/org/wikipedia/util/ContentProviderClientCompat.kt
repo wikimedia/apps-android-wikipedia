b7666bb16917ec2436ed7deccc3e197ef5862edd
sharvani <sharan@wikimedia.org>
2021-01-22 16:23:24 -0800

    Remove suppression

--- app/src/main/java/org/wikipedia/util/ContentProviderClientCompat.kt
+++ app/src/main/java/org/wikipedia/util/ContentProviderClientCompat.kt
@@ -4,7 +4,6 @@ import android.content.ContentProviderClient
 import android.os.Build
 
 object ContentProviderClientCompat {
-    @Suppress("DEPRECATION")
     @JvmStatic
     fun close(client: ContentProviderClient) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
