71e7364585c8b06a9ad131523ef869b119e5f232
Dmitry Brant <dbrant@wikimedia.org>
2021-01-27 08:56:42 -0500

    Simplify and clean up a bit.

--- app/src/main/java/org/wikipedia/util/DeviceUtil.kt
+++ app/src/main/java/org/wikipedia/util/DeviceUtil.kt
@@ -2,7 +2,7 @@ package org.wikipedia.util
 
 import android.app.Activity
 import android.content.Context
-import android.graphics.PorterDuff
+import android.graphics.Color
 import android.net.ConnectivityManager
 import android.os.Build
 import android.view.KeyCharacterMap
@@ -12,6 +12,8 @@ import android.view.WindowManager
 import android.view.accessibility.AccessibilityManager
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.widget.Toolbar
+import androidx.core.graphics.BlendModeColorFilterCompat
+import androidx.core.graphics.BlendModeCompat
 import org.wikipedia.R
 import org.wikipedia.WikipediaApp
 
@@ -80,9 +82,9 @@ object DeviceUtil {
         } else {
             setLightSystemUiVisibility(activity)
         }
-        if (toolbar != null) {
-            toolbar.navigationIcon!!.setColorFilter(if (reset) activity.resources.getColor(android.R.color.white) else ResourceUtil.getThemedColor(activity, R.attr.toolbar_icon_color), PorterDuff.Mode.SRC_IN)
-        }
+        toolbar?.navigationIcon!!.colorFilter = BlendModeColorFilterCompat
+                .createBlendModeColorFilterCompat(if (reset) Color.WHITE
+                else ResourceUtil.getThemedColor(activity, R.attr.toolbar_icon_color), BlendModeCompat.SRC_IN)
     }
 
     @JvmStatic
@@ -94,7 +96,8 @@ object DeviceUtil {
 
     @JvmStatic
     fun isOnWiFi(): Boolean {
-        val info = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI)
+        val info = (WikipediaApp.getInstance().getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager)
+                .getNetworkInfo(ConnectivityManager.TYPE_WIFI)
         return info != null && info.isConnected
     }
 
@@ -102,8 +105,6 @@ object DeviceUtil {
     @JvmStatic
     val isNavigationBarShowing: Boolean
         get() = KeyCharacterMap.deviceHasKey(KeyEvent.KEYCODE_BACK) && KeyCharacterMap.deviceHasKey(KeyEvent.KEYCODE_HOME)
-    private val connectivityManager: ConnectivityManager
-        get() = WikipediaApp.getInstance().getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 
     @JvmStatic
     val isAccessibilityEnabled: Boolean
