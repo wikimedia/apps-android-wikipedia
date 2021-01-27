6829f98e270980eaa81e7bdfc77bb745b31be4ff
cooltey <coolteygame@gmail.com>
2021-01-27 15:54:00 -0800

    Convert NavMenu related classes to Kotlin

---
+++ app/src/main/java/org/wikipedia/views/NavMenuAwareRecyclerView.kt
@@ -0,0 +1,15 @@
+package org.wikipedia.views
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+import androidx.recyclerview.widget.RecyclerView
+import org.wikipedia.views.FrameLayoutNavMenuTriggerer.Companion.setChildViewScrolled
+
+class NavMenuAwareRecyclerView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) :
+        RecyclerView(context, attrs, defStyleAttr) {
+
+    override fun onScrolled(dx: Int, dy: Int) {
+        setChildViewScrolled()
+    }
+}
