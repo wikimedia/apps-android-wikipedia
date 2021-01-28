0dbf0a7472f5615b4c8f6da5c4155577b464ca6e
cooltey <coolteygame@gmail.com>
2021-01-27 16:45:59 -0800

    View binding on ImageDetailView

--- app/src/main/java/org/wikipedia/views/ImageDetailView.kt
+++ app/src/main/java/org/wikipedia/views/ImageDetailView.kt
@@ -2,14 +2,14 @@ package org.wikipedia.views
 
 import android.content.Context
 import android.util.AttributeSet
-import android.view.View
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.LinearLayout
-import org.wikipedia.R
+import org.wikipedia.databinding.ViewImageDetailBinding
 
 class ImageDetailView constructor(context: Context, attrs: AttributeSet? = null) : LinearLayout(context, attrs) {
     init {
-        View.inflate(context, R.layout.view_image_detail, this)
+        ViewImageDetailBinding.inflate(LayoutInflater.from(context), this, true)
         layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
     }
 }
