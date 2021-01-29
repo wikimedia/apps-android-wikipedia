973b4022ab5966620301e090ff6884507aea9367
sharvani <sharan@wikimedia.org>
2021-01-29 12:55:23 -0800

    removed unnecessary null check

--- app/src/main/java/org/wikipedia/util/DateUtil.kt
+++ app/src/main/java/org/wikipedia/util/DateUtil.kt
@@ -23,7 +23,7 @@ object DateUtil {
     @JvmStatic
     @Synchronized
     fun iso8601DateParse(date: String): Date {
-        return getCachedDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.ROOT, true)!!.parse(date!!)!!
+        return getCachedDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.ROOT, true)!!.parse(date)!!
     }
 
     @JvmStatic
