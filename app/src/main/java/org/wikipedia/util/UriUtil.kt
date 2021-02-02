2ef38fb081a32ba8c354f1e3a96cd272e474d029
sharvani <sharan@wikimedia.org>
2021-02-01 22:10:57 -0800

    Code clean-up

--- app/src/main/java/org/wikipedia/util/UriUtil.kt
+++ app/src/main/java/org/wikipedia/util/UriUtil.kt
@@ -4,11 +4,9 @@ import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
-import android.text.TextUtils
 import androidx.annotation.StringRes
 import androidx.annotation.VisibleForTesting
 import org.apache.commons.lang3.StringUtils
-import org.intellij.lang.annotations.RegExp
 import org.wikipedia.WikipediaApp
 import org.wikipedia.dataclient.WikiSite
 import org.wikipedia.page.PageTitle
@@ -22,15 +20,8 @@ object UriUtil {
     const val LOCAL_URL_LOGIN = "#login"
     const val LOCAL_URL_CUSTOMIZE_FEED = "#customizefeed"
     const val LOCAL_URL_LANGUAGES = "#languages"
-
     const val WIKI_REGEX = "/(wiki|[a-z]{2,3}|[a-z]{2,3}-.*)/"
 
-    /**
-     * Decodes a URL-encoded string into its UTF-8 equivalent. If the string cannot be decoded, the
-     * original string is returned.
-     * @param url The URL-encoded string that you wish to decode.
-     * @return The decoded string, or the input string if the decoding failed.
-     */
     @JvmStatic
     fun decodeURL(url: String): String {
         return try {
@@ -58,13 +49,6 @@ object UriUtil {
         }
     }
 
-    /**
-     * Open the specified URI in an external browser (even if our app's intent filter
-     * matches the given URI)
-     *
-     * @param context Context of the calling app
-     * @param uri URI to open in an external browser
-     */
     @JvmStatic
     fun visitInExternalBrowser(context: Context, uri: Uri) {
         val chooserIntent = ShareUtil.createChooserIntent(Intent(Intent.ACTION_VIEW, uri), context)
@@ -85,16 +69,13 @@ object UriUtil {
 
         // also handle images like /w/extensions/ImageMap/desc-20.png?15600 on Estados Unidos
         // or like /api/rest_v1/page/graph/png/API/0/019dd76b5f4887040716e65de53802c5033cb40c.png
-        return if (ret.startsWith("./") || ret.startsWith("/w/") || ret.startsWith("/wiki/")
-                || ret.startsWith("/api/")) wiki.uri().buildUpon().appendEncodedPath(ret.replaceFirst("/".toRegex(), "")).build().toString() else ret
+        return if (ret.startsWith("./") || ret.startsWith("/w/") ||
+                ret.startsWith("/wiki/") || ret.startsWith("/api/"))
+            wiki.uri().buildUpon().appendEncodedPath(ret.replaceFirst("/".toRegex(), ""))
+                    .build().toString()
+        else ret
     }
 
-    /**
-     * Resolves a potentially protocol relative URL to a 'full' URL
-     *
-     * @param url Url to check for (and fix) protocol relativeness
-     * @return A fully qualified, protocol specified URL
-     */
     @JvmStatic
     fun resolveProtocolRelativeUrl(url: String): String {
         return if (url.startsWith("//")) WikipediaApp.getInstance().wikiSite.scheme() + ":" + url else url
@@ -102,13 +83,12 @@ object UriUtil {
 
     @JvmStatic
     fun isValidPageLink(uri: Uri): Boolean {
-        return ((!TextUtils.isEmpty(uri.authority)
-                && uri.authority!!.endsWith("wikipedia.org")
-                && !TextUtils.isEmpty(uri.path)
-                && uri.path!!.matches(("^" + WIKI_REGEX + ".*").toRegex()))
-                && (uri.fragment == null
-                || (uri.fragment!!.isNotEmpty()
-                && !uri.fragment!!.startsWith("cite"))))
+        return ((!uri.authority.isNullOrEmpty() &&
+                uri.authority!!.endsWith("wikipedia.org") &&
+                !uri.path.isNullOrEmpty() &&
+                uri.path!!.matches(("^$WIKI_REGEX.*").toRegex())) &&
+                (uri.fragment == null || (uri.fragment!!.isNotEmpty() &&
+                        !uri.fragment!!.startsWith("cite"))))
     }
 
     @JvmStatic
@@ -131,9 +111,6 @@ object UriUtil {
         } else thumbnailName
     }
 
-    /**
-     * Note that while this method also replaces '_' with spaces it doesn't fully decode the string.
-     */
     @JvmStatic
     fun getTitleFromUrl(url: String): String {
         return removeFragment(removeLinkPrefix(url)).replace("_", " ")
@@ -142,30 +119,30 @@ object UriUtil {
     /** Get language variant code from a Uri, e.g. "zh.*", otherwise returns empty string.  */
     @JvmStatic
     fun getLanguageVariantFromUri(uri: Uri): String {
-        if (TextUtils.isEmpty(uri.path)) {
+        if (uri.path.isNullOrEmpty()) {
             return ""
         }
-        val parts = StringUtils.split(StringUtils.defaultString(uri.path), '/')
+        val parts = StringUtils.defaultString(uri.path).split('/')
         return if (parts.size > 1 && parts[0] != "wiki") parts[0] else ""
     }
 
     /** For internal links only  */
     @JvmStatic
     fun removeInternalLinkPrefix(link: String): String {
-        return link.replaceFirst(WIKI_REGEX, "")
+        return link.replaceFirst(WIKI_REGEX.toRegex(), "")
     }
 
     /** For links that could be internal or external links  */
     @JvmStatic
     fun removeLinkPrefix(link: String): String {
-        return link.replaceFirst("^.*?" + WIKI_REGEX, "")
+        return link.replaceFirst("^.*?$WIKI_REGEX".toRegex(), "")
     }
 
     /** Removes an optional fragment portion of a URL  */
     @JvmStatic
     @VisibleForTesting
     fun removeFragment(link: String): String {
-        return link.replaceFirst("#.*$", "")
+        return link.replaceFirst("#.*$".toRegex(), "")
     }
 
     fun getFragment(link: String?): String? {
