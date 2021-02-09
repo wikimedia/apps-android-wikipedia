485482d01d4275228d1d5aa954514d4754f0a59b
sharvani <sharan@wikimedia.org>
2021-02-08 17:48:47 -0800

    CR comment addressed

--- app/src/main/java/org/wikipedia/util/ResourceUtil.kt
+++ app/src/main/java/org/wikipedia/util/ResourceUtil.kt
@@ -14,6 +14,7 @@ import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.content.ContextCompat
 import androidx.core.graphics.drawable.DrawableCompat
 import androidx.core.view.MenuItemCompat
+import kotlin.jvm.Throws
 
 object ResourceUtil {
 
