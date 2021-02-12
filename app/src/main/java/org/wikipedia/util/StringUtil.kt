681eefd9d2c095127f0ed6c030d82263ae0ec660
cooltey <cfeng@wikimedia.org>
2021-02-12 10:27:56 -0800

    Fix checkstyle

--- app/src/main/java/org/wikipedia/util/StringUtil.kt
+++ app/src/main/java/org/wikipedia/util/StringUtil.kt
@@ -8,7 +8,6 @@ import androidx.annotation.IntRange
 import androidx.core.text.HtmlCompat
 import com.google.gson.Gson
 import okio.ByteString.Companion.encodeUtf8
-import org.apache.commons.lang3.StringUtils
 import org.json.JSONArray
 import java.text.Collator
 import java.text.Normalizer
