42b759e27b75cdae3f4e61f910c15d2015e31358
cooltey <coolteygame@gmail.com>
2021-02-16 17:33:03 -0800

    Remove comments

--- app/src/main/java/org/wikipedia/views/ViewAnimations.kt
+++ app/src/main/java/org/wikipedia/views/ViewAnimations.kt
@@ -1,51 +1,33 @@
 package org.wikipedia.views
 
-import android.R
 import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
 import android.content.res.Resources
 import android.view.View
 
-/**
- * Contains convenient methods for performing various animations on Views.
- */
 object ViewAnimations {
     private var SHORT_ANIMATION_DURATION: Long = 0
     private var MEDIUM_ANIMATION_DURATION: Long = 0
+
+    @JvmStatic
     fun init(resources: Resources) {
-        SHORT_ANIMATION_DURATION = resources.getInteger(R.integer.config_shortAnimTime).toLong()
-        MEDIUM_ANIMATION_DURATION = resources.getInteger(R.integer.config_mediumAnimTime).toLong()
+        SHORT_ANIMATION_DURATION = resources.getInteger(android.R.integer.config_shortAnimTime).toLong()
+        MEDIUM_ANIMATION_DURATION = resources.getInteger(android.R.integer.config_mediumAnimTime).toLong()
     }
 
-    /**
-     * Crossfades two views, one of which is assumed to be currently visible
-     * @param curView The view that is currently visible
-     * @param newView The new view that should be faded in
-     */
+    @JvmStatic
     fun crossFade(curView: View, newView: View) {
         fadeIn(newView)
         fadeOut(curView)
     }
 
-    /**
-     * Crossfades two views, one of which is assumed to be currently visible
-     * @param curView The view that is currently visible
-     * @param newView The new view that should be faded in
-     * @param runOnComplete Optional Runnable to be run when the animation is complete (may be null).
-     */
+    @JvmStatic
     fun crossFade(curView: View, newView: View, runOnComplete: Runnable?) {
         fadeIn(newView)
         fadeOut(curView, runOnComplete)
     }
-    /**
-     * Fades in a view.
-     * @param view The currently invisible view to be faded in
-     * @param runOnComplete Optional Runnable to be run when the animation is complete (may be null).
-     */
-    /**
-     * Fades in a view.
-     * @param view The currently invisible view to be faded in
-     */
+
+    @JvmStatic
     @JvmOverloads
     fun fadeIn(view: View, runOnComplete: Runnable? = null) {
         view.alpha = 0f
@@ -62,15 +44,8 @@ object ViewAnimations {
                 })
                 .start()
     }
-    /**
-     * Fades out a view.
-     * @param view The currently visible view to be faded out
-     * @param runOnComplete Optional Runnable to be run when the animation is complete (may be null).
-     */
-    /**
-     * Fades out a view.
-     * @param view The currently visible view to be faded out
-     */
+
+    @JvmStatic
     @JvmOverloads
     fun fadeOut(view: View, runOnComplete: Runnable? = null) {
         view.animate().cancel()
@@ -91,14 +66,7 @@ object ViewAnimations {
                 .start()
     }
 
-    /**
-     * Ensures that the translationY of a particular view is the given value.
-     *
-     * If it isn't the current value, then it performs a short animation to make it so.
-     *
-     * @param view The view to translate
-     * @param translation The value to ensure it is translated by
-     */
+    @JvmStatic
     fun ensureTranslationY(view: View, translation: Int) {
         if (view.translationY != translation.toFloat()) {
             view.animate().translationY(translation.toFloat()).setDuration(SHORT_ANIMATION_DURATION).start()
