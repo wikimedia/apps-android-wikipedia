16229304900c35ce18d8538991dce13dd04e0576
sharvani <sharan@wikimedia.org>
2021-02-19 17:34:38 -0800

    Converted PrefixSearchResponse

---
+++ app/src/main/java/org/wikipedia/search/PrefixSearchResponse.kt
@@ -0,0 +1,29 @@
+package org.wikipedia.search
+
+import com.google.gson.annotations.SerializedName
+import org.wikipedia.dataclient.mwapi.MwQueryResponse
+
+class PrefixSearchResponse : MwQueryResponse() {
+    @SerializedName("searchinfo")
+    private val searchInfo: SearchInfo? = null
+    private val search: Search? = null
+    fun suggestion(): String? {
+        return searchInfo?.suggestion()
+    }
+
+    internal class SearchInfo {
+        private val suggestion: String? = null
+
+        @SerializedName("suggestionsnippet")
+        private val snippet: String? = null
+        fun suggestion(): String? {
+            return suggestion
+        }
+    }
+
+    internal class Search {
+        @SerializedName("ns")
+        private val namespace = 0
+        private val title: String? = null
+    }
+}
