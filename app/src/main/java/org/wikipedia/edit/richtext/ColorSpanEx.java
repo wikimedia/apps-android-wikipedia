cef047573cfcf78b1556096805217bcef952dd0d
Dmitry Brant <dbrant@wikimedia.org>
2021-02-26 12:05:04 -0500

    Remove unnecessary stuff.

--- app/src/main/java/org/wikipedia/edit/richtext/ColorSpanEx.java
+++ app/src/main/java/org/wikipedia/edit/richtext/ColorSpanEx.java
@@ -1,30 +1,12 @@
 package org.wikipedia.edit.richtext;
 
-import android.os.Parcel;
-import android.os.Parcelable;
 import android.text.TextPaint;
 import android.text.style.ForegroundColorSpan;
 
 import androidx.annotation.ColorInt;
 import androidx.annotation.NonNull;
 
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
-import org.apache.commons.lang3.builder.ToStringBuilder;
-import org.wikipedia.json.GsonMarshaller;
-import org.wikipedia.json.GsonUnmarshaller;
-
 public class ColorSpanEx extends ForegroundColorSpan implements SpanExtents {
-    public static final Parcelable.Creator<ColorSpanEx> CREATOR = new Parcelable.Creator<ColorSpanEx>() {
-        @Override public ColorSpanEx createFromParcel(Parcel source) {
-            return new ColorSpanEx(source);
-        }
-
-        @Override public ColorSpanEx[] newArray(int size) {
-            return new ColorSpanEx[size];
-        }
-    };
-
     private int spanStart;
     private int spanEnd;
     private SyntaxRule syntaxRule;
@@ -35,14 +17,6 @@ public class ColorSpanEx extends ForegroundColorSpan implements SpanExtents {
         this.spanStart = spanStart;
         this.syntaxRule = syntaxRule;
         this.backColor = backColor;
-    }
-
-    public ColorSpanEx(@NonNull Parcel src) {
-        super(src.readInt());
-        this.spanStart = src.readInt();
-        this.spanEnd = src.readInt();
-        this.syntaxRule = GsonUnmarshaller.unmarshal(SyntaxRule.class, src.readString());
-        this.backColor = src.readInt();
     }
 
     @Override
@@ -79,26 +53,5 @@ public class ColorSpanEx extends ForegroundColorSpan implements SpanExtents {
     @Override
     public void setSyntaxRule(SyntaxRule item) {
         syntaxRule = item;
-    }
-
-    @Override public void writeToParcel(Parcel dest, int flags) {
-        super.writeToParcel(dest, flags);
-        dest.writeInt(spanStart);
-        dest.writeInt(spanEnd);
-        dest.writeString(GsonMarshaller.marshal(syntaxRule));
-        dest.writeInt(backColor);
-    }
-
-    @Override public String toString() {
-        return ToStringBuilder.reflectionToString(this);
-    }
-
-    @Override public int hashCode() {
-        return HashCodeBuilder.reflectionHashCode(this);
-    }
-
-    @SuppressWarnings("EqualsWhichDoesntCheckParameterClass")
-    @Override public boolean equals(Object other) {
-        return EqualsBuilder.reflectionEquals(this, other);
     }
 }
