2738f73b94e8e03720237b28542c6d8ea32aec0d
sharvani <sharan@wikimedia.org>
2021-03-01 13:53:23 -0800

    converting it to data class

--- app/src/main/java/org/wikipedia/search/SearchResults.kt
+++ app/src/main/java/org/wikipedia/search/SearchResults.kt
@@ -3,30 +3,24 @@ package org.wikipedia.search
 import org.wikipedia.dataclient.WikiSite
 import org.wikipedia.dataclient.mwapi.MwQueryPage
 
-class SearchResults {
-    var results: List<SearchResult>
-    var continuation: Map<String, String>?
-    var suggestion: String? = null
+data class SearchResults(
+        var results: List<SearchResult>,
+        var continuation: Map<String, String>?,
+        var suggestion: String? = null) {
 
-    constructor() {
-        results = ArrayList()
-        continuation = null
-    }
+    constructor() : this(ArrayList(), null)
 
-    constructor(results: List<SearchResult>) {
-        this.results = results
-        continuation = null
-    }
+    constructor(results: List<SearchResult>) : this(results, null)
 
     constructor(pages: List<MwQueryPage>, wiki: WikiSite,
-                continuation: Map<String, String>?, suggestion: String?) {
+                continuation: Map<String, String>?, suggestion: String?) : this() {
         val searchResults: MutableList<SearchResult> = ArrayList()
 
         // Sort the array based on the "index" property
         (pages as ArrayList).sortWith { o1, o2 -> (o1?.index()!!.compareTo(o2?.index()!!)) }
 
         for (page in pages) {
-            searchResults.add(SearchResult(page!!, wiki))
+            searchResults.add(SearchResult(page, wiki))
         }
 
         results = searchResults
