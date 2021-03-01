c5b017531545b02eadd097980111649c36055b6a
sharvani <sharan@wikimedia.org>
2021-03-01 14:09:03 -0800

    Convert to data class

--- app/src/main/java/org/wikipedia/search/SearchResult.kt
+++ app/src/main/java/org/wikipedia/search/SearchResult.kt
@@ -2,13 +2,11 @@ package org.wikipedia.search
 
 import org.wikipedia.dataclient.WikiSite
 import org.wikipedia.dataclient.mwapi.MwQueryPage
-import org.wikipedia.model.BaseModel
 import org.wikipedia.page.PageTitle
 
-class SearchResult : BaseModel {
-    val pageTitle: PageTitle
-    val redirectFrom: String?
-    val type: SearchResultType?
+data class SearchResult(val pageTitle: PageTitle,
+                        val redirectFrom: String?,
+                        val type: SearchResultType?) {
 
     enum class SearchResultType {
         SEARCH, HISTORY, READING_LIST, TAB_LIST
@@ -20,27 +18,4 @@ class SearchResult : BaseModel {
 
     constructor(pageTitle: PageTitle, searchResultType: SearchResultType) :
             this(pageTitle, null, searchResultType)
-
-    constructor(pageTitle: PageTitle, redirectFrom: String?, searchResultType: SearchResultType) {
-        this.pageTitle = pageTitle
-        this.redirectFrom = redirectFrom
-        type = searchResultType
-    }
-
-    override fun toString(): String {
-        return pageTitle.prefixedText
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (other !is SearchResult) {
-            return false
-        }
-        return other.pageTitle == pageTitle && other.redirectFrom.equals(redirectFrom)
-    }
-
-    override fun hashCode(): Int {
-        var result = pageTitle.hashCode()
-        result = 31 * result + redirectFrom.hashCode()
-        return result
-    }
 }
