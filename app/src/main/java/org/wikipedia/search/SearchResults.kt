55dfff458d256a4b3ab5e4c3dcb9ca41262608ef
sharvani <sharan@wikimedia.org>
2021-03-02 16:43:30 -0800

    CR comments addressed

--- app/src/main/java/org/wikipedia/search/SearchResults.kt
+++ app/src/main/java/org/wikipedia/search/SearchResults.kt
@@ -4,26 +4,23 @@ import org.wikipedia.dataclient.WikiSite
 import org.wikipedia.dataclient.mwapi.MwQueryPage
 
 data class SearchResults(
-        var results: List<SearchResult>,
+        var results: MutableList<SearchResult>,
         var continuation: Map<String, String>?,
         var suggestion: String? = null) {
 
     constructor() : this(ArrayList(), null)
 
-    constructor(results: List<SearchResult>) : this(results, null)
+    constructor(results: MutableList<SearchResult>) : this(results, null)
 
-    constructor(pages: List<MwQueryPage>, wiki: WikiSite,
+    constructor(pages: MutableList<MwQueryPage>, wiki: WikiSite,
                 continuation: Map<String, String>?, suggestion: String?) : this() {
-        val searchResults: MutableList<SearchResult> = ArrayList()
-
         // Sort the array based on the "index" property
-        (pages as ArrayList).sortWith { o1, o2 -> (o1?.index()!!.compareTo(o2?.index()!!)) }
+        (pages).sortWith { o1, o2 -> (o1?.index()!!.compareTo(o2?.index()!!)) }
 
         for (page in pages) {
-            searchResults.add(SearchResult(page, wiki))
+            results.add(SearchResult(page, wiki))
         }
 
-        results = searchResults
         this.continuation = continuation
         this.suggestion = suggestion
     }
