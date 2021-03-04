88e45c543f7053d5df42144636e97559c78c59fd
Sharvani Haran <sharvaniharan@users.noreply.github.com>
2021-03-04 08:19:34 -0800

    If a tab is found with the same title, we should still not navigate to that tab. (#2159)
    Co-authored-by: Cooltey Feng <cfeng@wikimedia.org>
    Co-authored-by: Dmitry Brant <dbrant@wikimedia.org>

--- app/src/main/java/org/wikipedia/page/PageFragment.java
+++ app/src/main/java/org/wikipedia/page/PageFragment.java
@@ -709,7 +709,6 @@ public class PageFragment extends Fragment implements BackPressedHandler, Commun
             }
         }
         if (selectedTabPosition >= 0) {
-            setCurrentTabAndReset(selectedTabPosition);
             return;
         }
 
