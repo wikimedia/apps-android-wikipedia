8fa43561af30737af01f63c8257f70ed6675b6e3
cooltey <coolteygame@gmail.com>
2021-03-05 16:48:12 -0800

    Convert ReadingList related classes to Kotlin

---
+++ app/src/main/java/org/wikipedia/readinglist/ReadingListHeaderView.kt
@@ -0,0 +1,78 @@
+package org.wikipedia.readinglist
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import org.wikipedia.R
+import org.wikipedia.databinding.ViewReadingListHeaderBinding
+import org.wikipedia.readinglist.database.ReadingList
+import org.wikipedia.util.GradientUtil.getPowerGradient
+import org.wikipedia.views.ViewUtil.loadImage
+
+class ReadingListHeaderView : FrameLayout {
+
+    private val binding = ViewReadingListHeaderBinding.inflate(LayoutInflater.from(context), this)
+    private var imageViews = listOf(binding.readingListHeaderImage0, binding.readingListHeaderImage1, binding.readingListHeaderImage2,
+            binding.readingListHeaderImage3, binding.readingListHeaderImage4, binding.readingListHeaderImage5)
+    private var readingList: ReadingList? = null
+
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    init {
+        layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+        binding.readingListHeaderImageGradient.background = getPowerGradient(R.color.black54, Gravity.TOP)
+        if (!isInEditMode) {
+            clearThumbnails()
+        }
+    }
+
+    fun setReadingList(readingList: ReadingList) {
+        this.readingList = readingList
+        if (readingList.pages().isEmpty()) {
+            binding.readingListHeaderImageContainer.visibility = GONE
+            binding.readingListHeaderEmptyImage.visibility = VISIBLE
+        } else {
+            binding.readingListHeaderImageContainer.visibility = VISIBLE
+            binding.readingListHeaderEmptyImage.visibility = GONE
+            updateThumbnails()
+        }
+    }
+
+    private fun clearThumbnails() {
+        imageViews.forEach {
+            loadImage(it, null)
+        }
+    }
+
+    private fun updateThumbnails() {
+        readingList?.let {
+            clearThumbnails()
+            val thumbUrls = mutableListOf<String>()
+            it.pages().forEach { page ->
+                if (!page.thumbUrl().isNullOrEmpty()) {
+                    thumbUrls.add(page.thumbUrl()!!)
+                }
+                if (thumbUrls.size > imageViews.size) {
+                    return
+                }
+            }
+            run {
+                var i = thumbUrls.size
+                while (i < imageViews.size && i < it.pages().size) {
+                    thumbUrls.add("")
+                    i++
+                }
+            }
+            var i = 0
+            while (i < thumbUrls.size && i < imageViews.size) {
+                loadImage(imageViews[i], thumbUrls[i])
+                ++i
+            }
+        }
+    }
+}
