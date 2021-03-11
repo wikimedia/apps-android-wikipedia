69dd751d6a4be797cdbea94bc31994956264ea97
Dmitry Brant <dbrant@wikimedia.org>
2021-03-11 13:53:05 -0500

    Convert Notification activity and dialog to kotlin.

---
+++ app/src/main/java/org/wikipedia/notifications/NotificationItemActionsDialog.kt
@@ -0,0 +1,152 @@
+package org.wikipedia.notifications
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.core.os.bundleOf
+import org.wikipedia.R
+import org.wikipedia.WikipediaApp
+import org.wikipedia.activity.FragmentUtil.getCallback
+import org.wikipedia.analytics.NotificationFunnel
+import org.wikipedia.databinding.ViewNotificationActionsBinding
+import org.wikipedia.dataclient.WikiSite
+import org.wikipedia.json.GsonUtil
+import org.wikipedia.page.ExtendedBottomSheetDialogFragment
+import org.wikipedia.page.LinkHandler
+import org.wikipedia.page.PageTitle
+import org.wikipedia.util.StringUtil.fromHtml
+import org.wikipedia.util.log.L
+
+class NotificationItemActionsDialog : ExtendedBottomSheetDialogFragment() {
+    interface Callback {
+        fun onArchive(notification: Notification)
+        fun onActionPageTitle(pageTitle: PageTitle)
+        val isShowingArchived: Boolean
+    }
+
+    private var _binding: ViewNotificationActionsBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var notification: Notification
+    private lateinit var linkHandler: NotificationLinkHandler
+
+    private var actionClickListener = View.OnClickListener {
+        val link = it.tag as Notification.Link
+        val linkIndex = if (it.id == R.id.notification_action_primary) 0 else if (it.id == R.id.notification_action_secondary) 1 else 2
+        val url = link.url
+        if (url.isNotEmpty()) {
+            NotificationFunnel(WikipediaApp.getInstance(), notification).logAction(linkIndex, link)
+            linkHandler.wikiSite = WikiSite(url)
+            linkHandler.onUrlClick(url, null, "")
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = ViewNotificationActionsBinding.inflate(inflater, container, false)
+
+        notification = GsonUtil.getDefaultGson().fromJson(requireArguments().getString(ARG_NOTIFICATION), Notification::class.java)
+        linkHandler = NotificationLinkHandler(requireContext())
+        if (notification.contents != null) {
+            binding.notificationItemText.text = fromHtml(notification.contents!!.header).toString()
+        }
+        if (notification.contents != null && notification.contents!!.links != null && notification.contents!!.links!!.primary != null) {
+            setUpViewForLink(binding.notificationActionPrimary, binding.notificationActionPrimaryIcon, binding.notificationActionPrimaryText, notification.contents!!.links!!.primary!!)
+        } else {
+            binding.notificationActionPrimary.visibility = View.GONE
+        }
+        if (notification.contents != null && notification.contents!!.links != null && notification.contents!!.links!!.secondary != null && notification.contents!!.links!!.secondary!!.size > 0) {
+            setUpViewForLink(binding.notificationActionSecondary, binding.notificationActionSecondaryIcon, binding.notificationActionSecondaryText, notification.contents!!.links!!.secondary!![0])
+        } else {
+            binding.notificationActionSecondary.visibility = View.GONE
+        }
+        if (notification.contents != null && notification.contents!!.links != null && notification.contents!!.links!!.secondary != null && notification.contents!!.links!!.secondary!!.size > 1) {
+            setUpViewForLink(binding.notificationActionTertiary, binding.notificationActionTertiaryIcon, binding.notificationActionTertiaryText, notification.contents!!.links!!.secondary!![1])
+        } else {
+            binding.notificationActionTertiary.visibility = View.GONE
+        }
+        binding.notificationItemArchiveIcon.setImageResource(if (callback()!!.isShowingArchived) R.drawable.ic_unarchive_themed_24dp else R.drawable.ic_archive_themed_24dp)
+        binding.notificationItemArchiveText.setText(if (callback()!!.isShowingArchived) R.string.notifications_mark_unread else R.string.notifications_archive)
+
+        binding.notificationItemArchive.setOnClickListener {
+            callback()?.onArchive(notification)
+        }
+
+        binding.notificationActionPrimary.setOnClickListener(actionClickListener)
+        binding.notificationActionSecondary.setOnClickListener(actionClickListener)
+        binding.notificationActionTertiary.setOnClickListener(actionClickListener)
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+
+    private fun setUpViewForLink(containerView: View?, iconView: AppCompatImageView?, labelView: TextView?, link: Notification.Link) {
+        if (link.tooltip.isNotEmpty()) {
+            labelView!!.text = fromHtml(link.tooltip)
+        } else {
+            labelView!!.text = fromHtml(link.label)
+        }
+        if ("userAvatar" == link.icon) {
+            iconView!!.setImageResource(R.drawable.ic_user_avatar)
+        } else {
+            iconView!!.setImageResource(R.drawable.ic_arrow_forward_black_24dp)
+        }
+        containerView!!.tag = link
+        containerView.visibility = View.VISIBLE
+    }
+
+    private inner class NotificationLinkHandler constructor(context: Context) : LinkHandler(context) {
+        private var wikiSite: WikiSite? = null
+        fun setWikiSite(wikiSite: WikiSite) {
+            this.wikiSite = wikiSite
+        }
+
+        override fun onPageLinkClicked(anchor: String, linkText: String) {
+            // ignore
+        }
+
+        override fun onMediaLinkClicked(title: PageTitle) {
+            // ignore
+        }
+
+        override fun onInternalLinkClicked(title: PageTitle) {
+            callback()!!.onActionPageTitle(title)
+        }
+
+        override fun onExternalLinkClicked(uri: Uri) {
+            try {
+                // TODO: handle "change password" since it will open a blank page in PageActivity
+                startActivity(Intent(Intent.ACTION_VIEW).setData(uri))
+            } catch (e: Exception) {
+                L.e(e)
+            }
+        }
+
+        override fun getWikiSite(): WikiSite {
+            return wikiSite!!
+        }
+    }
+
+    private fun callback(): Callback? {
+        return getCallback(this, Callback::class.java)
+    }
+
+    companion object {
+        private const val ARG_NOTIFICATION = "notification"
+
+        fun newInstance(notification: Notification): NotificationItemActionsDialog {
+            return NotificationItemActionsDialog().apply {
+                arguments = bundleOf(ARG_NOTIFICATION to GsonUtil.getDefaultGson().toJson(notification))
+            }
+        }
+    }
+}
