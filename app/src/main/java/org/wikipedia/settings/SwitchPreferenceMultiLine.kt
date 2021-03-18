7f8a89c21d5b24b2c19a7fcb821ffd7c6b378ae4
gouri-panda <gouripanda4@gmail.com>
2021-03-19 01:13:08 +0530

    Added more kotlin format

--- app/src/main/java/org/wikipedia/settings/SwitchPreferenceMultiLine.kt
+++ app/src/main/java/org/wikipedia/settings/SwitchPreferenceMultiLine.kt
@@ -8,14 +8,13 @@ import androidx.preference.PreferenceViewHolder
 import androidx.preference.SwitchPreferenceCompat
 
 open class SwitchPreferenceMultiLine : SwitchPreferenceCompat {
-    constructor(ctx: Context?, attrs: AttributeSet?, defStyle: Int) : super(ctx, attrs, defStyle) {}
-    constructor(ctx: Context?, attrs: AttributeSet?) : super(ctx, attrs) {}
-    constructor(ctx: Context?) : super(ctx) {}
+    constructor(ctx: Context?, attrs: AttributeSet?, defStyle: Int) : super(ctx, attrs, defStyle)
+    constructor(ctx: Context?, attrs: AttributeSet?) : super(ctx, attrs)
+    constructor(ctx: Context?) : super(ctx)
 
     override fun onBindViewHolder(holder: PreferenceViewHolder) {
         super.onBindViewHolder(holder)
-        val textView = holder.itemView.findViewById<TextView>(R.id.title)
-        if (textView != null) {
+        holder.itemView.findViewById<TextView>(R.id.title)?.also { textView ->
             textView.isSingleLine = false
         }
     }
