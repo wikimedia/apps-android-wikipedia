36f089740214893bc7ef1945c4e3cd9c560c972b
Dmitry Brant <dbrant@wikimedia.org>
2021-03-19 10:16:13 -0400

    Convert onboarding activity and fragment to kotlin.

---
+++ app/src/main/java/org/wikipedia/onboarding/InitialOnboardingActivity.kt
@@ -0,0 +1,32 @@
+package org.wikipedia.onboarding
+
+import android.content.Context
+import android.content.Intent
+import org.wikipedia.activity.SingleFragmentActivity
+import org.wikipedia.settings.Prefs
+
+class InitialOnboardingActivity : SingleFragmentActivity<InitialOnboardingFragment>(), OnboardingFragment.Callback {
+    override fun onComplete() {
+        setResult(RESULT_OK)
+        Prefs.setInitialOnboardingEnabled(false)
+        finish()
+    }
+
+    override fun onBackPressed() {
+        if (fragment.onBackPressed()) {
+            return
+        }
+        setResult(RESULT_OK)
+        finish()
+    }
+
+    override fun createFragment(): InitialOnboardingFragment {
+        return InitialOnboardingFragment.newInstance()
+    }
+
+    companion object {
+        fun newIntent(context: Context): Intent {
+            return Intent(context, InitialOnboardingActivity::class.java)
+        }
+    }
+}
