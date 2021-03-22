ddcf4aefc54b83a25dad7c35b83a72741b1ec142
gouri-panda <gouripanda4@gmail.com>
2021-03-23 03:42:06 +0530

    Converted ReadingListSyncService.java to kotlin

--- app/src/main/java/org/wikipedia/readinglist/sync/ReadingListSyncService.kt
+++ app/src/main/java/org/wikipedia/readinglist/sync/ReadingListSyncService.kt
@@ -1,30 +1,26 @@
-package org.wikipedia.readinglist.sync;
+package org.wikipedia.readinglist.sync
 
-import android.app.Service;
-import android.content.AbstractThreadedSyncAdapter;
-import android.content.Intent;
-import android.os.IBinder;
+import android.app.Service
+import android.content.AbstractThreadedSyncAdapter
+import android.content.Intent
+import android.os.IBinder
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-
-public class ReadingListSyncService extends Service {
-    @NonNull private static final Object SYNC_ADAPTER_LOCK = new Object();
-    @Nullable private static AbstractThreadedSyncAdapter SYNC_ADAPTER;
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        synchronized (SYNC_ADAPTER_LOCK) {
+class ReadingListSyncService : Service() {
+    override fun onCreate() {
+        super.onCreate()
+        synchronized(SYNC_ADAPTER_LOCK) {
             if (SYNC_ADAPTER == null) {
-                SYNC_ADAPTER = new ReadingListSyncAdapter(getApplicationContext(), true);
+                SYNC_ADAPTER = ReadingListSyncAdapter(applicationContext, true)
             }
         }
     }
 
-    @Nullable
-    @Override
-    public IBinder onBind(Intent intent) {
-        return SYNC_ADAPTER == null ? null : SYNC_ADAPTER.getSyncAdapterBinder();
+    override fun onBind(intent: Intent): IBinder? {
+        return if (SYNC_ADAPTER == null) null else SYNC_ADAPTER?.syncAdapterBinder
+    }
+
+    companion object {
+        private val SYNC_ADAPTER_LOCK = Any()
+        private var SYNC_ADAPTER: AbstractThreadedSyncAdapter? = null
     }
 }
