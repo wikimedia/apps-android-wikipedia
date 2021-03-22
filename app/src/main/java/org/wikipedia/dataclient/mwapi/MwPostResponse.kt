6ad574157087f500440327e9c985ddebe51986d8
gouri-panda <gouripanda4@gmail.com>
2021-03-23 03:26:44 +0530

    Converted MwPostResponse.java to kotlin

--- app/src/main/java/org/wikipedia/dataclient/mwapi/MwPostResponse.kt
+++ app/src/main/java/org/wikipedia/dataclient/mwapi/MwPostResponse.kt
@@ -1,27 +1,10 @@
-package org.wikipedia.dataclient.mwapi;
+package org.wikipedia.dataclient.mwapi
 
-import androidx.annotation.Nullable;
-
-@SuppressWarnings("unused")
-public class MwPostResponse extends MwResponse {
-    @Nullable private MwQueryPage pageinfo;
-    @Nullable private String options;
-    private int success;
-
-    public boolean success(@Nullable String result) {
-        return "success".equals(result);
-    }
-
-    @Nullable public String getOptions() {
-        return options;
-    }
-
-    public int getSuccessVal() {
-        return success;
-    }
-
-    @Nullable public MwQueryPage getPageInfo() {
-        return pageinfo;
+open class MwPostResponse : MwResponse() {
+    val pageInfo: MwQueryPage? = null
+    val options: String? = null
+    val successVal = 0
+    fun success(result: String?): Boolean {
+        return "success" == result
     }
 }
-
