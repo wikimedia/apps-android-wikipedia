b9b62918cd6da3a534589c14115725011f838aa6
gouri-panda <gouripanda4@gmail.com>
2021-03-23 03:38:39 +0530

    Converted ReadingListSyncNotification.java to kotlin

--- app/src/main/java/org/wikipedia/readinglist/sync/ReadingListSyncNotification.kt
+++ app/src/main/java/org/wikipedia/readinglist/sync/ReadingListSyncNotification.kt
@@ -1,39 +1,33 @@
-package org.wikipedia.readinglist.sync;
+package org.wikipedia.readinglist.sync
 
-import android.content.Context;
+import android.content.Context
+import org.wikipedia.R
+import org.wikipedia.views.NotificationWithProgressBar
 
-import androidx.annotation.NonNull;
+class ReadingListSyncNotification private constructor() {
+    private val notification: NotificationWithProgressBar = NotificationWithProgressBar()
 
-import org.wikipedia.R;
-import org.wikipedia.views.NotificationWithProgressBar;
-
-public final class ReadingListSyncNotification {
-
-    private static final ReadingListSyncNotification INSTANCE = new ReadingListSyncNotification();
-    private static final String CHANNEL_ID = "READING_LIST_SYNCING_CHANNEL";
-    private static final int NOTIFICATION_ID = 1002;
-    private NotificationWithProgressBar notification;
-
-    private ReadingListSyncNotification() {
-        notification = new NotificationWithProgressBar();
-        notification.setChannelId(CHANNEL_ID);
-        notification.setNotificationId(NOTIFICATION_ID);
-        notification.setChannelName(R.plurals.notification_syncing_reading_list_channel_name);
-        notification.setChannelDescription(R.string.notification_syncing_reading_list_channel_description);
-        notification.setNotificationIcon(android.R.drawable.ic_popup_sync);
-        notification.setNotificationTitle(R.plurals.notification_syncing_reading_list_title);
-        notification.setNotificationDescription(R.plurals.notification_syncing_reading_list_description);
+    init {
+        notification.channelId = CHANNEL_ID
+        notification.notificationId = NOTIFICATION_ID
+        notification.channelName = R.plurals.notification_syncing_reading_list_channel_name
+        notification.channelDescription = R.string.notification_syncing_reading_list_channel_description
+        notification.notificationIcon = android.R.drawable.ic_popup_sync
+        notification.notificationTitle = R.plurals.notification_syncing_reading_list_title
+        notification.notificationDescription = R.plurals.notification_syncing_reading_list_description
     }
 
-    public static ReadingListSyncNotification getInstance() {
-        return INSTANCE;
+    fun setNotificationProgress(context: Context, itemsTotal: Int, itemsProgress: Int) {
+        notification.setNotificationProgress(context, itemsTotal, itemsProgress)
     }
 
-    public void setNotificationProgress(@NonNull Context context, int itemsTotal, int itemsProgress) {
-        notification.setNotificationProgress(context, itemsTotal, itemsProgress);
+    fun cancelNotification(context: Context) {
+        notification.cancelNotification(context)
     }
 
-    public void cancelNotification(@NonNull Context context) {
-        notification.cancelNotification(context);
+    companion object {
+        val instance = ReadingListSyncNotification()
+        private const val CHANNEL_ID = "READING_LIST_SYNCING_CHANNEL"
+        private const val NOTIFICATION_ID = 1002
     }
 }
