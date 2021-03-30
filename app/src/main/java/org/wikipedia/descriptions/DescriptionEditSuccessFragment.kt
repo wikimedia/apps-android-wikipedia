11e11e211d59c700b8fbb97233c16ab1e1e24fc0
Dmitry Brant <dbrant@wikimedia.org>
2021-03-30 12:13:03 -0400

    Convert DescriptionEditSuccess to kotlin.

---
+++ app/src/main/java/org/wikipedia/descriptions/DescriptionEditSuccessFragment.kt
@@ -0,0 +1,45 @@
+package org.wikipedia.descriptions
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import org.wikipedia.activity.FragmentUtil.getCallback
+import org.wikipedia.databinding.FragmentDescriptionEditSuccessBinding
+
+class DescriptionEditSuccessFragment : Fragment(), DescriptionEditSuccessView.Callback {
+    private var _binding: FragmentDescriptionEditSuccessBinding? = null
+    private val binding get() = _binding!!
+
+    interface Callback {
+        fun onDismissClick()
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        super.onCreateView(inflater, container, savedInstanceState)
+        _binding = FragmentDescriptionEditSuccessBinding.inflate(layoutInflater, container, false)
+        binding.fragmentDescriptionEditSuccessView.callback = this
+        return binding.root
+    }
+
+    override fun onDismissClick() {
+        callback()?.onDismissClick()
+    }
+
+    override fun onDestroyView() {
+        binding.fragmentDescriptionEditSuccessView.callback = null
+        _binding = null
+        super.onDestroyView()
+    }
+
+    private fun callback(): Callback? {
+        return getCallback(this, Callback::class.java)
+    }
+
+    companion object {
+        fun newInstance(): DescriptionEditSuccessFragment {
+            return DescriptionEditSuccessFragment()
+        }
+    }
+}
