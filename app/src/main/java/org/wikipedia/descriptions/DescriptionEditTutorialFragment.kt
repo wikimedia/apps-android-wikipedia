ae1280cf60f6187ce3e32246856fb70edd04fda4
Dmitry Brant <dbrant@wikimedia.org>
2021-03-30 11:50:38 -0400

    Convert DescriptionEditTutorial to kotlin.

---
+++ app/src/main/java/org/wikipedia/descriptions/DescriptionEditTutorialFragment.kt
@@ -0,0 +1,49 @@
+package org.wikipedia.descriptions
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import org.wikipedia.R
+import org.wikipedia.onboarding.OnboardingFragment
+import org.wikipedia.onboarding.OnboardingPageView
+
+class DescriptionEditTutorialFragment : OnboardingFragment() {
+    override val doneButtonText: Int
+        get() = R.string.description_edit_tutorial_button_label_start_editing
+
+    override fun getAdapter(): FragmentStateAdapter {
+        return DescriptionEditTutorialPagerAdapter(this)
+    }
+
+    internal inner class DescriptionEditTutorialPagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
+        override fun getItemCount(): Int {
+            return pages.size
+        }
+
+        override fun createFragment(position: Int): Fragment {
+            return ItemFragment().apply { arguments = bundleOf("position" to position) }
+        }
+    }
+
+    class ItemFragment : Fragment() {
+        override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+            super.onCreateView(inflater, container, savedInstanceState)
+            val position = requireArguments().getInt("position", 0)
+            val view = inflater.inflate(pages[position], container, false) as OnboardingPageView
+            view.callback = OnboardingPageView.DefaultCallback()
+            return view
+        }
+    }
+
+    companion object {
+        val pages = arrayOf(R.layout.inflate_description_edit_tutorial_page_one, R.layout.inflate_description_edit_tutorial_page_two)
+
+        fun newInstance(): DescriptionEditTutorialFragment {
+            return DescriptionEditTutorialFragment()
+        }
+    }
+}
