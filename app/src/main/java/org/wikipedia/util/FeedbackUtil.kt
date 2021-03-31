d60204a1d6a0bedacdb317c3af2c7b6caeecd4fc
cooltey <coolteygame@gmail.com>
2021-03-31 16:14:57 -0700

    Wrap content and set minimun height for title view

--- app/src/main/java/org/wikipedia/util/FeedbackUtil.kt
+++ app/src/main/java/org/wikipedia/util/FeedbackUtil.kt
@@ -228,7 +228,8 @@ object FeedbackUtil {
             setBackgroundColorResource(ResourceUtil.getThemedAttributeId(context, R.attr.colorAccent))
             setDismissWhenTouchOutside(autoDismiss)
             setLayout(layoutRes)
-            setWidthRatio(if (isLandscape(context)) 0.4f else 0.8f)
+            setWidth(BalloonSizeSpec.WRAP)
+            setHeight(BalloonSizeSpec.WRAP)
             setArrowAlignAnchorPadding(arrowAnchorPadding)
         }
     }
