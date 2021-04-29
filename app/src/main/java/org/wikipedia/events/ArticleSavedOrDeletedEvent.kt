6e68d93e679c1918bff6746f2ab8150abae796b2
Arun sudharsan <sudarun@amazon.com>
2021-04-29 15:56:39 +0530

    Address PR comments

--- app/src/main/java/org/wikipedia/events/ArticleSavedOrDeletedEvent.kt
+++ app/src/main/java/org/wikipedia/events/ArticleSavedOrDeletedEvent.kt
@@ -3,5 +3,5 @@ package org.wikipedia.events
 import org.wikipedia.readinglist.database.ReadingListPage
 
 class ArticleSavedOrDeletedEvent(val isAdded: Boolean, vararg pages: ReadingListPage) {
-    val pages: Array<ReadingListPage> = arrayOf(*pages)
+    val pages = pages.toList()
 }
