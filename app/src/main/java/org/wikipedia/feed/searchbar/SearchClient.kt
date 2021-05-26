28e4d05b5c897e323207597fd846e18596dd33ba
cooltey <coolteygame@gmail.com>
2021-05-26 15:48:03 -0700

    Checkstyle

--- app/src/main/java/org/wikipedia/feed/searchbar/SearchClient.kt
+++ app/src/main/java/org/wikipedia/feed/searchbar/SearchClient.kt
@@ -3,7 +3,6 @@ package org.wikipedia.feed.searchbar
 import org.wikipedia.dataclient.WikiSite
 import org.wikipedia.feed.dataclient.DummyClient
 import org.wikipedia.feed.model.Card
-import org.wikipedia.feed.offline.OfflineCard
 
 class SearchClient : DummyClient() {
     override fun getNewCard(wiki: WikiSite?): Card {
