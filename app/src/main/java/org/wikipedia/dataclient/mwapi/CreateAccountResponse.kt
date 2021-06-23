e934b4e2f47c6a59f566be8bdb0b148933fd5bfb
sharvani <sharan@wikimedia.org>
2021-06-23 16:58:43 -0700

    Kotlin Conversion: CreateAccountResponse

---
+++ app/src/main/java/org/wikipedia/dataclient/mwapi/CreateAccountResponse.kt
@@ -0,0 +1,33 @@
+package org.wikipedia.dataclient.mwapi
+
+class CreateAccountResponse : MwResponse() {
+
+    private val createaccount: Result? = null
+
+    fun status(): String? {
+        return createaccount!!.status
+    }
+
+    fun user(): String? {
+        return createaccount!!.username
+    }
+
+    fun message(): String? {
+        return createaccount!!.message
+    }
+
+    fun hasResult(): Boolean {
+        return createaccount != null
+    }
+
+    class Result {
+
+        val status: String? = null
+            get() = field.orEmpty()
+        val message: String? = null
+            get() = field.orEmpty()
+
+        val username: String? = null
+            get() = field.orEmpty()
+    }
+}
