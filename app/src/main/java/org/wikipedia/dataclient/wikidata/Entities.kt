bce67cae3f9487583354e6ee3f77e4b17f37b02f
sharvani <sharan@wikimedia.org>
2021-06-28 16:40:45 -0700

    CR comments addressed

--- app/src/main/java/org/wikipedia/dataclient/wikidata/Entities.kt
+++ app/src/main/java/org/wikipedia/dataclient/wikidata/Entities.kt
@@ -1,47 +1,40 @@
 package org.wikipedia.dataclient.wikidata
 
+import com.google.gson.annotations.SerializedName
 import org.wikipedia.dataclient.mwapi.MwResponse
 import org.wikipedia.json.PostProcessingTypeAdapter.PostProcessable
 
 class Entities : MwResponse(), PostProcessable {
 
-    val entities: Map<String, Entity>? = null
-        get() = field ?: emptyMap()
+    val entities: Map<String, Entity> = emptyMap()
     val first: Entity?
-        get() = entities?.values?.iterator()?.next()
+        get() = if (entities.isEmpty()) null else entities.values.iterator().next()
 
     override fun postProcess() {
-        if (first != null && first?.isMissing!!) {
+        if (first?.isMissing == true) {
             throw RuntimeException("The requested entity was not found.")
         }
     }
 
     class Entity {
 
-        private val id: String? = null
-            get() = field.orEmpty()
-        val labels: Map<String, Label>? = null
-            get() = field ?: emptyMap()
-        val descriptions: Map<String, Label>? = null
-            get() = field ?: emptyMap()
-        val sitelinks: Map<String, SiteLink>? = null
-            get() = field ?: emptyMap()
+        private val id: String = ""
+        val labels: Map<String, Label> = emptyMap()
+        val descriptions: Map<String, Label> = emptyMap()
+        val sitelinks: Map<String, SiteLink> = emptyMap()
+        @SerializedName("missing")
         val isMissing: Boolean? = null
             get() = "-1" == id && field != null
         val lastRevId: Long = 0
     }
 
     class Label {
-        val language: String? = null
-            get() = field.orEmpty()
-        val value: String? = null
-            get() = field.orEmpty()
+        val language: String = ""
+        val value: String = ""
     }
 
     class SiteLink {
-        val site: String? = null
-            get() = field.orEmpty()
-        val title: String? = null
-            get() = field.orEmpty()
+        val site: String = ""
+        val title: String = ""
     }
 }
