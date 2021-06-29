b107892968dafc0d2c49ae60e81ada867b3849dc
Sharvani Haran <sharvaniharan@users.noreply.github.com>
2021-06-29 15:01:47 -0700

    Kotlin Conversion: GalleryItem (#2593)
    * Kotlin Conversion: GalleryItem
    
    * Code clean-up
    
    * Code clean-up
    
    * Code clean-up

---
+++ app/src/main/java/org/wikipedia/gallery/GalleryItem.kt
@@ -0,0 +1,84 @@
+package org.wikipedia.gallery
+
+import com.google.gson.annotations.SerializedName
+import org.wikipedia.Constants.PREFERRED_GALLERY_IMAGE_SIZE
+import org.wikipedia.dataclient.Service
+import org.wikipedia.util.ImageUrlUtil
+import org.wikipedia.util.StringUtil
+import java.io.Serializable
+import java.util.*
+
+open class GalleryItem : Serializable {
+
+    @SerializedName("section_id")
+    val sectionId = 0
+
+    @SerializedName("wb_entity_id")
+    val entityId: String? = null
+
+    @SerializedName("audio_type")
+    val audioType: String = ""
+
+    @SerializedName("structured")
+    var structuredData: StructuredData? = null
+
+    // return the base url of Wiki Commons for WikiSite() if the file_page is null.
+    @SerializedName("file_page")
+    var filePage: String = Service.COMMONS_URL
+
+    val duration = 0.0
+    val isShowInGallery = false
+    var type: String = ""
+    var thumbnail = ImageInfo()
+    var original = ImageInfo()
+    var description = TextInfo()
+
+    val caption: TextInfo? = null
+    val sources: List<ImageInfo>? = null
+    var titles: Titles? = null
+    var artist: ArtistInfo? = null
+    var license: ImageLicense? = null
+
+    val thumbnailUrl
+        get() = thumbnail.source
+    val preferredSizedImageUrl
+        get() = ImageUrlUtil.getUrlForPreferredSize(thumbnailUrl, PREFERRED_GALLERY_IMAGE_SIZE)
+
+    protected fun setTitle(title: String) {
+        titles = Titles(title, StringUtil.addUnderscores(title), title)
+    }
+
+    // The getSources has different levels of source,
+    // should have an option that allows user to chose which quality to play
+    val originalVideoSource
+        get() = // The getSources has different levels of source,
+            // should have an option that allows user to chose which quality to play
+            if (sources.isNullOrEmpty()) null else sources[sources.size - 1]
+
+    var structuredCaptions
+        get() = if (structuredData != null && structuredData!!.captions != null) structuredData!!.captions!! else emptyMap()
+        set(captions) {
+            if (structuredData == null) {
+                structuredData = StructuredData()
+            }
+            structuredData!!.captions = HashMap(captions)
+        }
+
+    class Titles internal constructor(display: String, canonical: String, normalized: String) : Serializable {
+
+        var canonical: String = ""
+        var normalized: String = ""
+        var display: String = ""
+
+        init {
+            this.display = display
+            this.canonical = canonical
+            this.normalized = normalized
+        }
+    }
+
+    class StructuredData : Serializable {
+
+        var captions: HashMap<String, String>? = null
+    }
+}
