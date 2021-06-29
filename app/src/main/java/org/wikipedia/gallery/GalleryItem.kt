c8432a2cf4653f3cc0d1d625994321439b19fc1d
sharvani <sharan@wikimedia.org>
2021-06-29 11:53:12 -0700

    Code clean-up

--- app/src/main/java/org/wikipedia/gallery/GalleryItem.kt
+++ app/src/main/java/org/wikipedia/gallery/GalleryItem.kt
@@ -1,99 +1,59 @@
 package org.wikipedia.gallery
 
 import com.google.gson.annotations.SerializedName
-import org.apache.commons.lang3.StringUtils
 import org.wikipedia.Constants.PREFERRED_GALLERY_IMAGE_SIZE
 import org.wikipedia.dataclient.Service
-import org.wikipedia.util.ImageUrlUtil.getUrlForPreferredSize
-import org.wikipedia.util.StringUtil.addUnderscores
+import org.wikipedia.util.ImageUrlUtil
+import org.wikipedia.util.StringUtil
 import java.io.Serializable
 import java.util.*
 
 open class GalleryItem : Serializable {
 
     @SerializedName("section_id")
-    private val sectionId = 0
-    private var type: String? = null
+    val sectionId = 0
+
+    @SerializedName("wb_entity_id")
+    val entityId: String? = null
+
+    @SerializedName("structured")
+    var structuredData: StructuredData? = null
 
     @SerializedName("audio_type")
-    val audioType: String? = null
-        get() = StringUtils.defaultString(field)
-    val caption: TextInfo? = null
-    val isShowInGallery = false
-    var titles: Titles? = null
-        private set
-    private var thumbnail: ImageInfo? = null
-    private var original: ImageInfo? = null
-    val sources: List<ImageInfo>? = null
+    val audioType: String = ""
 
     // return the base url of Wiki Commons for WikiSite() if the file_page is null.
     @SerializedName("file_page")
-    var filePage: String? = null
-        get() =// return the base url of Wiki Commons for WikiSite() if the file_page is null.
-            StringUtils.defaultString(field, Service.COMMONS_URL)
-    var artist: ArtistInfo? = null
+    var filePage: String = Service.COMMONS_URL
+
+    var type: String = ""
+    val isShowInGallery = false
+    var thumbnail = ImageInfo()
+    var original = ImageInfo()
+    var description = TextInfo()
     val duration = 0.0
+    var titles: Titles? = null
+    val caption: TextInfo? = null
+    val sources: List<ImageInfo>? = null
+    var artist: ArtistInfo? = null
     var license: ImageLicense? = null
-    private var description: TextInfo? = null
-
-    @SerializedName("wb_entity_id")
-    private val entityId: String? = null
-
-    @SerializedName("structured")
-    private var structuredData: StructuredData? = null
-
-    constructor() {}
-    constructor(title: String) {
-        type = "*/*"
-        titles = Titles(title, addUnderscores(title), title)
-        original = ImageInfo()
-        thumbnail = ImageInfo()
-        description = TextInfo()
-        license = ImageLicense()
-    }
-
-    fun getType(): String {
-        return StringUtils.defaultString(type)
-    }
+    val thumbnailUrl
+        get() = thumbnail.source
+    val preferredSizedImageUrl
+        get() = ImageUrlUtil.getUrlForPreferredSize(thumbnailUrl, PREFERRED_GALLERY_IMAGE_SIZE)
 
     protected fun setTitle(title: String) {
-        titles = Titles(title, addUnderscores(title), title)
-    }
-
-    fun getThumbnail(): ImageInfo {
-        if (thumbnail == null) {
-            thumbnail = ImageInfo()
-        }
-        return thumbnail!!
-    }
-
-    val thumbnailUrl: String
-        get() = getThumbnail().source
-    val preferredSizedImageUrl: String
-        get() = getUrlForPreferredSize(thumbnailUrl, PREFERRED_GALLERY_IMAGE_SIZE)
-
-    fun getOriginal(): ImageInfo {
-        if (original == null) {
-            original = ImageInfo()
-        }
-        return original!!
+        titles = Titles(title, StringUtil.addUnderscores(title), title)
     }
 
     // The getSources has different levels of source,
     // should have an option that allows user to chose which quality to play
-    val originalVideoSource: ImageInfo?
+    val originalVideoSource
         get() =// The getSources has different levels of source,
             // should have an option that allows user to chose which quality to play
             if (sources.isNullOrEmpty()) null else sources[sources.size - 1]
 
-    fun getDescription(): TextInfo {
-        if (description == null) {
-            description = TextInfo()
-        }
-        return description!!
-    }
-
-    var structuredCaptions: Map<String, String>
+    var structuredCaptions
         get() = if (structuredData != null && structuredData!!.captions != null) structuredData!!.captions!! else emptyMap()
         set(captions) {
             if (structuredData == null) {
@@ -102,8 +62,7 @@ open class GalleryItem : Serializable {
             structuredData!!.captions = HashMap(captions)
         }
 
-    class Titles internal constructor(display: String, canonical: String, normalized: String) :
-        Serializable {
+    class Titles internal constructor(display: String, canonical: String, normalized: String) : Serializable {
 
         var canonical: String = ""
         var normalized: String = ""
