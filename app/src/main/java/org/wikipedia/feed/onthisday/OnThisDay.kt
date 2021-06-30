f0ee71c4cdba8bc9dc20652b3893805ce9e866f3
sharvani <sharan@wikimedia.org>
2021-06-30 09:35:02 -0700

    Kotlin Conversion: OnThisDay

---
+++ app/src/main/java/org/wikipedia/feed/onthisday/OnThisDay.kt
@@ -0,0 +1,47 @@
+package org.wikipedia.feed.onthisday
+
+import org.wikipedia.dataclient.page.PageSummary
+import org.wikipedia.json.annotations.Required
+import java.util.*
+
+class OnThisDay {
+
+    private val events: List<Event>? = null
+    private val births: List<Event>? = null
+    private val deaths: List<Event>? = null
+    private val holidays: List<Event>? = null
+    var selected: List<Event> = emptyList()
+
+    fun events(): List<Event> {
+        val allEvents = ArrayList<Event>()
+        events?.let { allEvents.addAll(it) }
+        births?.let { allEvents.addAll(it) }
+        deaths?.let { allEvents.addAll(it) }
+        holidays?.let { allEvents.addAll(it) }
+
+        allEvents.sortWith { e1: Event, e2: Event ->
+            e2.year.compareTo(e1.year)
+        }
+        return allEvents
+    }
+
+    class Event {
+
+        @Required
+        val text: String? = null
+
+        @Required
+        private val pages: MutableList<PageSummary>? = null
+        val year = 0
+
+        fun pages(): List<PageSummary> {
+            val iterator: MutableIterator<*> = pages?.iterator()!!
+            while (iterator.hasNext()) {
+                if (iterator.next() == null) {
+                    iterator.remove()
+                }
+            }
+            return pages
+        }
+    }
+}
