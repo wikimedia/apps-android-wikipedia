a03ee8b958dff391a28c2667835209dc61133ac3
Cooltey Feng <cfeng@wikimedia.org>
2021-07-02 08:21:20 -0700

    Follow up: TopRead and FeaturedImage Kotlin conversion (#2609)
    Co-authored-by: Dmitry Brant <dbrant@wikimedia.org>

--- app/src/main/java/org/wikipedia/feed/image/FeaturedImage.kt
+++ app/src/main/java/org/wikipedia/feed/image/FeaturedImage.kt
@@ -3,19 +3,16 @@ package org.wikipedia.feed.image
 import org.wikipedia.gallery.GalleryItem
 import org.wikipedia.gallery.ImageInfo
 import org.wikipedia.json.PostProcessingTypeAdapter.PostProcessable
-import org.wikipedia.json.annotations.Required
-import org.wikipedia.util.StringUtil.addUnderscores
+import org.wikipedia.util.StringUtil
 
 class FeaturedImage : GalleryItem(), PostProcessable {
 
-    @Required
-    val title: String? = null
+    val title = ""
 
-    @Required
-    val image: ImageInfo? = null
+    val image = ImageInfo()
 
     override fun postProcess() {
-        titles = Titles(title!!, addUnderscores(title), title)
-        original.setSource(image!!.source)
+        titles = Titles(title, StringUtil.addUnderscores(title), title)
+        original.setSource(image.source)
     }
 }
