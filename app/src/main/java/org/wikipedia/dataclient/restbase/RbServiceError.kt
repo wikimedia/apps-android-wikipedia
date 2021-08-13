ae32bcad505e34317207f015d1b13e9d7157afd8
Cooltey Feng <cfeng@wikimedia.org>
2021-08-13 16:17:15 -0700

    Follow up: RbServiceError Kotlin conversion (#2732)

--- app/src/main/java/org/wikipedia/dataclient/restbase/RbServiceError.kt
+++ app/src/main/java/org/wikipedia/dataclient/restbase/RbServiceError.kt
@@ -1,20 +1,20 @@
 package org.wikipedia.dataclient.restbase
 
 import org.wikipedia.dataclient.ServiceError
-import org.wikipedia.json.GsonUnmarshaller
+import org.wikipedia.json.GsonUtil
 
 class RbServiceError : ServiceError {
 
-    private val type: String? = null
     override val title: String = ""
     override val details: String = ""
+    private val type: String? = null
     private val detail: String? = null
     private val method: String? = null
     private val uri: String? = null
 
     companion object {
         fun create(rspBody: String): RbServiceError {
-            return GsonUnmarshaller.unmarshal(RbServiceError::class.java, rspBody)
+            return GsonUtil.getDefaultGson().fromJson(rspBody, RbServiceError::class.java)
         }
     }
 }
