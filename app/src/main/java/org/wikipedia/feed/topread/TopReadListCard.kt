1f8c576c91a2072416840d49bd8ee82afebed725
Isira Seneviratne <isirasen96@gmail.com>
2021-08-13 10:27:17 +0530

    Remove JsonClass from TopReadListCard.

--- app/src/main/java/org/wikipedia/feed/topread/TopReadListCard.kt
+++ app/src/main/java/org/wikipedia/feed/topread/TopReadListCard.kt
@@ -2,7 +2,6 @@ package org.wikipedia.feed.topread
 
 import android.os.Parcelable
 import androidx.annotation.VisibleForTesting
-import com.squareup.moshi.JsonClass
 import kotlinx.parcelize.Parcelize
 import org.wikipedia.R
 import org.wikipedia.dataclient.WikiSite
@@ -13,9 +12,8 @@ import org.wikipedia.util.DateUtil
 import org.wikipedia.util.L10nUtil
 import java.util.concurrent.TimeUnit
 
-@JsonClass(generateAdapter = true)
 @Parcelize
-class TopReadListCard(internal val articles: TopRead, val site: WikiSite) :
+class TopReadListCard(private val articles: TopRead, val site: WikiSite) :
     ListCard<TopReadItemCard>(toItems(articles.articles, site), site), Parcelable {
 
     override fun title(): String {
