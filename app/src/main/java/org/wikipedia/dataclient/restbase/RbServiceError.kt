4009ab765f027603364c7e245af6fd3578de7dc4
sharvani <sharan@wikimedia.org>
2021-08-13 12:34:23 -0700

    Minor code-cleanup

--- app/src/main/java/org/wikipedia/dataclient/restbase/RbServiceError.kt
+++ app/src/main/java/org/wikipedia/dataclient/restbase/RbServiceError.kt
@@ -1,24 +1,18 @@
 package org.wikipedia.dataclient.restbase
 
-import org.apache.commons.lang3.StringUtils
+import kotlinx.serialization.Serializable
 import org.wikipedia.dataclient.ServiceError
 import org.wikipedia.json.GsonUnmarshaller
 
+@Serializable
 class RbServiceError : ServiceError {
 
     private val type: String? = null
-    private val title: String = ""
+    override val title: String = ""
+    override val details: String = ""
     private val detail: String? = null
     private val method: String? = null
     private val uri: String? = null
-
-    override fun getTitle(): String {
-        return StringUtils.defaultString(title)
-    }
-
-    override fun getDetails(): String {
-        return StringUtils.defaultString(detail)
-    }
 
     companion object {
         fun create(rspBody: String): RbServiceError {
