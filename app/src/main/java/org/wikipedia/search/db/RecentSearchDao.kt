4e7b8b889057f0dce054b9e40a1a5d3d03a65837
Isira Seneviratne <isirasen96@gmail.com>
2021-08-14 08:29:57 +0530

    Update RecentSearchDao method parameter.

--- app/src/main/java/org/wikipedia/search/db/RecentSearchDao.kt
+++ app/src/main/java/org/wikipedia/search/db/RecentSearchDao.kt
@@ -6,7 +6,7 @@ import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.core.Single
-import java.util.*
+import java.time.Instant
 
 @Dao
 interface RecentSearchDao {
@@ -20,7 +20,7 @@ interface RecentSearchDao {
     fun deleteAll(): Completable
 
     @Query("DELETE FROM RecentSearch WHERE text = :text AND timestamp = :timestamp")
-    fun deleteBy(text: String, timestamp: Date)
+    fun deleteBy(text: String, timestamp: Instant)
 
     fun delete(recentSearch: RecentSearch) {
         deleteBy(recentSearch.text, recentSearch.timestamp)
