d15104d20b736dd0b4d7d9698e6e093f6b136cfa
cooltey <coolteygame@gmail.com>
2021-08-19 12:32:28 -0700

    Integrate old notification channels

--- app/src/main/java/org/wikipedia/notifications/NotificationCategory.kt
+++ app/src/main/java/org/wikipedia/notifications/NotificationCategory.kt
@@ -9,11 +9,16 @@ import org.wikipedia.R
 import org.wikipedia.model.EnumCode
 import org.wikipedia.model.EnumCodeMap
 
+private const val GROUP_WIKIPEDIA_NOTIFICATIONS: String = "WIKIPEDIA_NOTIFICATIONS"
+private const val GROUP_OTHER = "OTHER"
+
 enum class NotificationCategory constructor(val id: String,
                                             val title: Int,
                                             val description: Int,
                                             val iconResId: Int,
-                                            val iconColor: Int = R.color.accent50) : EnumCode {
+                                            val iconColor: Int = R.color.accent50,
+                                            val importance: Int = NotificationManagerCompat.IMPORTANCE_HIGH,
+                                            val group: String = GROUP_WIKIPEDIA_NOTIFICATIONS) : EnumCode {
     SYSTEM("system", R.string.preference_title_notification_system, R.string.preference_summary_notification_system, R.drawable.ic_speech_bubbles),
     SYSTEM_NO_EMAIL("system-noemail", R.string.preference_title_notification_system, R.string.preference_summary_notification_system, R.drawable.ic_speech_bubbles), // default welcome
     MILESTONE_EDIT("thank-you-edit", R.string.preference_title_notification_milestone, R.string.preference_summary_notification_milestone, R.drawable.ic_edit_progressive), // milestone
@@ -21,7 +26,10 @@ enum class NotificationCategory constructor(val id: String,
     EDIT_THANK("edit-thank", R.string.preference_title_notification_thanks, R.string.preference_summary_notification_thanks, R.drawable.ic_user_talk, R.color.green50),
     REVERTED("reverted", R.string.preference_title_notification_revert, R.string.preference_summary_notification_revert, R.drawable.ic_revert, R.color.base20),
     LOGIN_FAIL("login-fail", R.string.preference_title_notification_login_fail, R.string.preference_summary_notification_login_fail, R.drawable.ic_user_avatar, R.color.base0),
-    MENTION("mention", R.string.preference_title_notification_mention, R.string.preference_summary_notification_mention, R.drawable.ic_mention); // combines "mention", "mention-failure" and "mention-success"
+    MENTION("mention", R.string.preference_title_notification_mention, R.string.preference_summary_notification_mention, R.drawable.ic_mention), // combines "mention", "mention-failure" and "mention-success"
+    ALPHA_BUILD_CHECKER("alpha-builder-checker", R.string.alpha_update_notification_title, R.string.alpha_update_notification_text, R.drawable.ic_w_transparent, importance = NotificationManagerCompat.IMPORTANCE_LOW, group = GROUP_OTHER),
+    READING_LIST_SYNCING("reading-list-syncing", R.string.notification_syncing_reading_list_channel_title, R.string.notification_syncing_reading_list_channel_description, android.R.drawable.ic_popup_sync, importance = NotificationManagerCompat.IMPORTANCE_LOW, group = GROUP_OTHER),
+    SYNCING("syncing", R.string.notification_channel_title, R.string.notification_channel_description, android.R.drawable.stat_sys_download, importance = NotificationManagerCompat.IMPORTANCE_LOW, group = GROUP_OTHER);
 
     override fun code(): Int {
         // This enumeration is not marshalled so tying declaration order to presentation order is
@@ -30,12 +38,9 @@ enum class NotificationCategory constructor(val id: String,
     }
 
     companion object {
-        private const val GROUP_WIKIPEDIA_NOTIFICATIONS = "WIKIPEDIA_NOTIFICATIONS"
-        private const val GROUP_OTHER = "OTHER"
 
         private val MAP = EnumCodeMap(NotificationCategory::class.java)
 
-        @JvmStatic
         fun find(id: String): NotificationCategory {
             for (i in 0 until MAP.size()) {
                 if (id == MAP[i].id) {
@@ -49,8 +54,13 @@ enum class NotificationCategory constructor(val id: String,
             // Notification channel ( >= API 26 )
             val notificationManagerCompat = NotificationManagerCompat.from(context)
 
-            // Remove old channel.
-            notificationManagerCompat.deleteNotificationChannel("MEDIAWIKI_ECHO_CHANNEL")
+            // Remove old channels
+            if (notificationManagerCompat.getNotificationChannelGroupCompat("MEDIAWIKI_ECHO_CHANNEL") != null) {
+                notificationManagerCompat.deleteNotificationChannel("MEDIAWIKI_ECHO_CHANNEL")
+                notificationManagerCompat.deleteNotificationChannel("ALPHA_UPDATE_CHECKER_CHANNEL")
+                notificationManagerCompat.deleteNotificationChannel("READING_LIST_SYNCING_CHANNEL")
+                notificationManagerCompat.deleteNotificationChannel("SYNCING_CHANNEL")
+            }
 
             var notificationChannelGroupWikipediaNotifications = notificationManagerCompat.getNotificationChannelGroupCompat(GROUP_WIKIPEDIA_NOTIFICATIONS)
             if (notificationChannelGroupWikipediaNotifications == null) {
@@ -72,18 +82,16 @@ enum class NotificationCategory constructor(val id: String,
                 val category = MAP[i]
                 var notificationChannelCompat = notificationManagerCompat.getNotificationChannelCompat(category.id)
                 if (notificationChannelCompat == null) {
-                    notificationChannelCompat = NotificationChannelCompat.Builder(category.id, NotificationManagerCompat.IMPORTANCE_HIGH)
+                    notificationChannelCompat = NotificationChannelCompat.Builder(category.id, category.importance)
                         .setName(context.getString(category.title))
                         .setDescription(context.getString(category.description))
-                        .setGroup(GROUP_WIKIPEDIA_NOTIFICATIONS)
+                        .setGroup(category.group)
                         .setLightColor(ContextCompat.getColor(context, R.color.accent50))
                         .setVibrationEnabled(true)
                         .build()
                     notificationManagerCompat.createNotificationChannel(notificationChannelCompat)
                 }
             }
-
-            // TODO: add sync reading list and downloading articles notifications here
         }
     }
 }
