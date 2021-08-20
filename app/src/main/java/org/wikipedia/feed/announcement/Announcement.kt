587ac42b8c0a6181b735ba113cac9dfb637d113e
cooltey <coolteygame@gmail.com>
2021-08-20 13:20:08 -0700

    Follow up: Announcement Kotlin conversion

--- app/src/main/java/org/wikipedia/feed/announcement/Announcement.kt
+++ app/src/main/java/org/wikipedia/feed/announcement/Announcement.kt
@@ -4,72 +4,33 @@ import com.google.gson.annotations.SerializedName
 import org.wikipedia.util.DateUtil
 import java.util.*
 
-class Announcement {
+class Announcement(val id: String = "",
+                   val text: String = "",
+                   val type: String = "",
+                   val platforms: List<String> = emptyList(),
+                   val countries: List<String> = emptyList(),
+                   @SerializedName("start_time") private val startTime: String = "",
+                   @SerializedName("end_time") private val endTime: String = "",
+                   @SerializedName("image_url") val imageUrl: String? = "",
+                   @SerializedName("negative_text") val negativeText: String? = "",
+                   @SerializedName("caption_HTML") val footerCaption: String? = "",
+                   @SerializedName("image_height") val imageHeight: String? = "",
+                   @SerializedName("logged_in") val loggedIn: Boolean? = null,
+                   @SerializedName("reading_list_sync_enabled") val readingListSyncEnabled: Boolean? = null,
+                   @SerializedName("min_version") val minVersion: String? = null,
+                   @SerializedName("max_version") val maxVersion: String? = null,
+                   val border: Boolean? = null,
+                   val beta: Boolean? = null,
+                   val placement: String = PLACEMENT_FEED,
+                   val action: Action?) {
 
-    private var action: Action? = null
-
-    @SerializedName("negative_text")
-    var negativeText: String? = null
-
-    var id: String = ""
-
-    @SerializedName("start_time")
-    private val startTime: String? = null
-
-    @SerializedName("end_time")
-    private val endTime: String? = null
-    private val border: Boolean? = null
-
-    var text: String? = null
-
-    val type: String = ""
-
-    @SerializedName("image_url")
-    var imageUrl: String? = null
-
-    @SerializedName("caption_HTML")
-    val footerCaption: String = ""
-
-    @SerializedName("image_height")
-    val imageHeight: String = ""
-
-    @SerializedName("logged_in")
-    val loggedIn: Boolean? = null
-
-    @SerializedName("reading_list_sync_enabled")
-    val readingListSyncEnabled: Boolean? = null
-
-    @SerializedName("min_version")
-    val minVersion: String? = null
-
-    @SerializedName("max_version")
-    val maxVersion: String? = null
-
-    val beta: Boolean? = null
-    val platforms = emptyList<String>()
-    val countries = emptyList<String>()
-    val placement: String = PLACEMENT_FEED
-
-    constructor()
-
-    constructor(id: String?,
-                text: String?,
-                imageUrl: String?,
-                action: Action?,
-                negativeText: String?) {
-        this.id = id.orEmpty()
-        this.text = text.orEmpty()
-        this.imageUrl = imageUrl.orEmpty()
-        this.action = action
-        this.negativeText = negativeText
-    }
 
     fun startTime(): Date {
-        return DateUtil.iso8601DateParse(startTime!!)
+        return DateUtil.iso8601DateParse(startTime)
     }
 
     fun endTime(): Date {
-        return DateUtil.iso8601DateParse(endTime!!)
+        return DateUtil.iso8601DateParse(endTime)
     }
 
     fun hasAction(): Boolean {
@@ -85,15 +46,11 @@ class Announcement {
     }
 
     fun hasFooterCaption(): Boolean {
-        return footerCaption.isNotEmpty()
+        return !footerCaption.isNullOrEmpty()
     }
 
     fun hasImageUrl(): Boolean {
         return !imageUrl.isNullOrEmpty()
-    }
-
-    fun hasBorder(): Boolean {
-        return border == true
     }
 
     class Action(val title: String, val url: String)
