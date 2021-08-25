c7531ca249794b3d0403f9a99fa8b81749f4f571
Cooltey Feng <cfeng@wikimedia.org>
2021-08-25 10:09:24 -0700

    Follow up: MwServiceError Kotlin conversion (#2751)

--- app/src/main/java/org/wikipedia/dataclient/mwapi/MwServiceError.kt
+++ app/src/main/java/org/wikipedia/dataclient/mwapi/MwServiceError.kt
@@ -7,22 +7,9 @@ import org.wikipedia.util.DateUtil
 import org.wikipedia.util.ThrowableUtil
 import java.util.*
 
-class MwServiceError : ServiceError, PostProcessable {
-
-    private var code: String? = null
-    private val text: String? = null
-    private var html: String? = null
-    private val data: Data? = null
-
-    constructor()
-    constructor(code: String?, html: String?) {
-        this.code = code
-        this.html = html
-    }
-
-    override val title: String get() = code.orEmpty()
-
-    override val details: String get() = html.orEmpty()
+class MwServiceError(val code: String?,
+                     var html: String?,
+                     val data: Data? = null) : ServiceError, PostProcessable {
 
     fun badToken(): Boolean {
         return "badtoken" == code
@@ -40,6 +27,10 @@ class MwServiceError : ServiceError, PostProcessable {
         return data?.messages?.first { it.name == messageName }?.html
     }
 
+    override val title: String get() = code.orEmpty()
+
+    override val details: String get() = html.orEmpty()
+
     override fun postProcess() {
         // Special case: if it's a Blocked error, parse the blockinfo structure ourselves.
         if (("blocked" == code || "autoblocked" == code) && data?.blockinfo != null) {
@@ -47,15 +38,9 @@ class MwServiceError : ServiceError, PostProcessable {
         }
     }
 
-    private class Data {
-        val messages: List<Message>? = null
-        val blockinfo: BlockInfo? = null
-    }
+    class Data(val messages: List<Message>?, val blockinfo: BlockInfo?)
 
-    private class Message {
-        val name: String? = null
-        val html: String = ""
-    }
+    class Message(val name: String?, val html: String = "")
 
     open class BlockInfo {
 
