051fd848d2761fd01fce717c95d93fd268f02367
cooltey <coolteygame@gmail.com>
2021-08-31 16:41:25 -0700

    Make the API levels surrending correctly

--- app/src/main/java/org/wikipedia/analytics/NotificationPreferencesFunnel.kt
+++ app/src/main/java/org/wikipedia/analytics/NotificationPreferencesFunnel.kt
@@ -1,7 +1,6 @@
 package org.wikipedia.analytics
 
 import android.os.Build
-import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationManagerCompat
 import org.json.JSONObject
 import org.wikipedia.R
@@ -14,24 +13,27 @@ class NotificationPreferencesFunnel(app: WikipediaApp) : Funnel(app, SCHEMA_NAME
 
     override fun preprocessSessionToken(eventData: JSONObject) {}
 
-    @RequiresApi(Build.VERSION_CODES.O)
     fun done() {
-        val toggleMap = HashMap<String, Boolean>()
-        val notificationManagerCompat = NotificationManagerCompat.from(app)
-        for (i in 0 until NotificationCategory.MAP.size()) {
-            val channelId = NotificationCategory.MAP[i].id
-            val importance = notificationManagerCompat.getNotificationChannel(channelId)?.importance
-            // TODO: figure out the "Show notifications" status
-            toggleMap[channelId] = importance != NotificationManagerCompat.IMPORTANCE_NONE &&
-                    importance != null && notificationManagerCompat.areNotificationsEnabled()
-        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val toggleMap = HashMap<String, Boolean>()
+            val notificationManagerCompat = NotificationManagerCompat.from(app)
+            for (i in 0 until NotificationCategory.MAP.size()) {
+                val channelId = NotificationCategory.MAP[i].id
+                val importance =
+                    notificationManagerCompat.getNotificationChannel(channelId)?.importance
+                // TODO: figure out the "Show notifications" status
+                toggleMap[channelId] = importance != NotificationManagerCompat.IMPORTANCE_NONE &&
+                        importance != null && notificationManagerCompat.areNotificationsEnabled()
+            }
 
-        log(
-            "type_toggles", GsonUtil.getDefaultGson().toJson(toggleMap),
-            "background_fetch",
-            if (Prefs.notificationPollEnabled()) app.resources.getInteger(R.integer.notification_poll_interval_minutes).toString()
-            else "disabled"
-        )
+            log(
+                "type_toggles", GsonUtil.getDefaultGson().toJson(toggleMap),
+                "background_fetch",
+                if (Prefs.notificationPollEnabled()) app.resources.getInteger(R.integer.notification_poll_interval_minutes)
+                    .toString()
+                else "disabled"
+            )
+        }
     }
 
     companion object {
