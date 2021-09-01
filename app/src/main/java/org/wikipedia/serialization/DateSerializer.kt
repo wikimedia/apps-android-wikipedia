268281e96605b61edc80e182ae56fa6cf9dd5a17
sharvani <sharan@wikimedia.org>
2021-09-01 01:40:58 -0700

    Minor update to ExtMetaData

--- app/src/main/java/org/wikipedia/serialization/DateSerializer.kt
+++ app/src/main/java/org/wikipedia/serialization/DateSerializer.kt
@@ -4,15 +4,12 @@ import kotlinx.serialization.KSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.Serializer
-import kotlinx.serialization.descriptors.PrimitiveKind
-import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
-import org.wikipedia.serialization.DynamicLookupSerializer.DateSurrogate
-import org.wikipedia.util.DateUtil
 import java.text.SimpleDateFormat
 import java.util.*
+
 @Serializer(forClass = DateSerializer::class)
 object DateSerializer : KSerializer<Date> {
     override val descriptor: SerialDescriptor = DateSurrogate.serializer().descriptor
