b7893976817da55a85b05628ca69cde4291ad13b
sharvani <sharan@wikimedia.org>
2021-08-31 19:43:11 -0700

    Fix for TopRead

--- app/src/main/java/org/wikipedia/feed/topread/TopRead.kt
+++ app/src/main/java/org/wikipedia/feed/topread/TopRead.kt
@@ -2,12 +2,12 @@ package org.wikipedia.feed.topread
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
-import kotlinx.serialization.Contextual
 import kotlinx.serialization.Serializable
 import org.wikipedia.dataclient.page.PageSummary
+import org.wikipedia.serialization.DynamicLookupSerializer
 import java.util.*
 
 @Parcelize
 @Serializable
-class TopRead(val date: @Contextual Date = Date(), val articles: List<PageSummary> = emptyList()) :
-    Parcelable
+class TopRead(@Serializable(with = DynamicLookupSerializer::class) val date: Date = Date(),
+              val articles: List<PageSummary> = emptyList()) : Parcelable
