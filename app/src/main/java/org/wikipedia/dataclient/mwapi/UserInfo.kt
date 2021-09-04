325eaec7c91ef3c1b3fb6ccf332761848e4e1cb1
Isira Seneviratne <isirasen96@gmail.com>
2021-09-04 09:48:35 +0530

    Pass values for BlockInfo in UserInfo.

--- app/src/main/java/org/wikipedia/dataclient/mwapi/UserInfo.kt
+++ app/src/main/java/org/wikipedia/dataclient/mwapi/UserInfo.kt
@@ -6,6 +6,16 @@ import org.wikipedia.dataclient.mwapi.MwServiceError.BlockInfo
 import java.util.*
 
 @JsonClass(generateAdapter = true)
-class UserInfo(val name: String = "", val id: Int = 0, val groups: Set<String> = emptySet(),
-               @Json(name = "editcount") val editCount: Int = 0,
-               @Json(name = "latestcontrib") val latestContribution: Date = Date(0)) : BlockInfo()
+class UserInfo(
+    val name: String = "",
+    val id: Int = 0,
+    val groups: Set<String> = emptySet(),
+    @Json(name = "editcount") val editCount: Int = 0,
+    @Json(name = "latestcontrib") val latestContribution: Date = Date(0),
+    @Json(name = "blockid") blockId: Int = 0,
+    @Json(name = "blockedbyid") blockedById: Int = 0,
+    @Json(name = "blockreason") blockReason: String = "",
+    @Json(name = "blockedby") blockedBy: String = "",
+    @Json(name = "blockedtimestamp") blockedTimeStamp: Date = Date(0),
+    @Json(name = "blockexpiry") blockExpiry: Date = Date(0)
+) : BlockInfo(blockId, blockedById, blockReason, blockedBy, blockedTimeStamp, blockExpiry)
