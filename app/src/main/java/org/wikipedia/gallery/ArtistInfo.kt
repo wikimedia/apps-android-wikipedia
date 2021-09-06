85e264bd191f471125fa15ccffcbf115de635590
sharvani <sharan@wikimedia.org>
2021-09-06 03:10:00 -0700

    Parcelize ArtistInfo

--- app/src/main/java/org/wikipedia/gallery/ArtistInfo.kt
+++ app/src/main/java/org/wikipedia/gallery/ArtistInfo.kt
@@ -1,9 +1,11 @@
 package org.wikipedia.gallery
 
 import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
 import kotlinx.serialization.Serializable
 
 @Serializable
+@Parcelize
 class ArtistInfo : TextInfo() {
 
     val name: String? = null
