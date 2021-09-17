ded4fbdcf5c8a6972096810953fdab03bbc75687
Dmitry Brant <dbrant@wikimedia.org>
2021-09-16 21:16:39 -0400

    Convert bridge message handling.

--- app/src/main/java/org/wikipedia/page/PageFragment.kt
+++ app/src/main/java/org/wikipedia/page/PageFragment.kt
@@ -28,6 +28,10 @@ import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.schedulers.Schedulers
+import kotlinx.serialization.json.float
+import kotlinx.serialization.json.jsonArray
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
 import org.wikipedia.*
 import org.wikipedia.Constants.InvokeSource
 import org.wikipedia.activity.FragmentUtil.getCallback
@@ -730,17 +734,17 @@ class PageFragment : Fragment(), BackPressedHandler, CommunicationBridge.Communi
         }
         bridge.addListener("back_link") { _, messagePayload ->
             messagePayload?.let { payload ->
-                val backLinks = payload.getAsJsonArray("backLinks")
-                if (backLinks != null && backLinks.size() > 0) {
-                    val backLinksList = backLinks.map { it.asJsonObject["id"].asString }
-                    showFindReferenceInPage(payload["referenceId"].asString.orEmpty(), backLinksList, payload["referenceText"].asString.orEmpty())
+                val backLinks = payload["backLinks"]?.jsonArray
+                if (backLinks != null && !backLinks.isEmpty()) {
+                    val backLinksList = backLinks.map { it.jsonObject["id"]?.jsonPrimitive?.content }
+                    showFindReferenceInPage(payload["referenceId"]?.jsonPrimitive?.content.orEmpty(), backLinksList, payload["referenceText"]?.jsonPrimitive?.content.orEmpty())
                 }
             }
         }
         bridge.addListener("scroll_to_anchor") { _, messagePayload ->
             messagePayload?.let { payload ->
-                payload.getAsJsonObject("rect")?.let {
-                    val diffY = if (it.has("y")) DimenUtil.roundedDpToPx(it["y"].asFloat) else DimenUtil.roundedDpToPx(it["top"].asFloat)
+                payload["rect"]?.jsonObject?.let {
+                    val diffY = if (it.containsKey("y")) DimenUtil.roundedDpToPx(it["y"]!!.jsonPrimitive.float) else DimenUtil.roundedDpToPx(it["top"]!!.jsonPrimitive.float)
                     val offsetFraction = 3
                     webView.scrollY = webView.scrollY + diffY - webView.height / offsetFraction
                 }
@@ -748,12 +752,12 @@ class PageFragment : Fragment(), BackPressedHandler, CommunicationBridge.Communi
         }
         bridge.addListener("image") { _, messagePayload ->
             messagePayload?.let { payload ->
-                linkHandler.onUrlClick(UriUtil.decodeURL(payload["href"].asString), payload["title"]?.asString, "")
+                linkHandler.onUrlClick(UriUtil.decodeURL(payload["href"]?.jsonPrimitive?.content.orEmpty()), payload["title"]?.jsonPrimitive?.content, "")
             }
         }
         bridge.addListener("media") { _, messagePayload ->
             messagePayload?.let { payload ->
-                linkHandler.onUrlClick(UriUtil.decodeURL(payload["href"].asString), payload["title"]?.asString, "")
+                linkHandler.onUrlClick(UriUtil.decodeURL(payload["href"]?.jsonPrimitive?.content.orEmpty()), payload["title"]?.jsonPrimitive?.content, "")
             }
         }
         bridge.addListener("pronunciation") { _, messagePayload ->
@@ -764,9 +768,9 @@ class PageFragment : Fragment(), BackPressedHandler, CommunicationBridge.Communi
                 if (avCallback == null) {
                     avCallback = AvCallback()
                 }
-                if (!avPlayer!!.isPlaying && payload.has("url")) {
+                if (!avPlayer!!.isPlaying && payload.containsKey("url")) {
                     updateProgressBar(true)
-                    avPlayer!!.play(UriUtil.resolveProtocolRelativeUrl(payload["url"].asString), avCallback!!)
+                    avPlayer!!.play(UriUtil.resolveProtocolRelativeUrl(payload["url"]?.jsonPrimitive?.content.orEmpty()), avCallback!!)
                 } else {
                     updateProgressBar(false)
                     avPlayer!!.stop()
@@ -775,7 +779,7 @@ class PageFragment : Fragment(), BackPressedHandler, CommunicationBridge.Communi
         }
         bridge.addListener("footer_item") { _, messagePayload ->
             messagePayload?.let { payload ->
-                when (payload["itemType"].asString) {
+                when (payload["itemType"]?.jsonPrimitive?.content) {
                     "talkPage" -> model.title?.run { startTalkTopicActivity(this) }
                     "languages" -> startLangLinksActivity()
                     "lastEdited" -> {
