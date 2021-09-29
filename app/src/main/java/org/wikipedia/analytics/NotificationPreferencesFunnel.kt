7900da1521153831bdd4ac0fea475ab906d37dc5
Sharvani Haran <sharvaniharan@users.noreply.github.com>
2021-09-29 16:05:00 -0700

    Wire-up analytics for notifications search and filter (#2854)
    * Wire-up analytics for notifications search and filter
    
    * Update revision Id
    
    * Code review comments addressed

--- app/src/main/java/org/wikipedia/analytics/NotificationPreferencesFunnel.kt
+++ app/src/main/java/org/wikipedia/analytics/NotificationPreferencesFunnel.kt
@@ -7,6 +7,9 @@ import org.wikipedia.R
 import org.wikipedia.WikipediaApp
 import org.wikipedia.json.GsonUtil
 import org.wikipedia.notifications.NotificationCategory
+import org.wikipedia.notifications.NotificationsFilterActivity
+import org.wikipedia.settings.Prefs
+import org.wikipedia.util.StringUtil
 
 class NotificationPreferencesFunnel(app: WikipediaApp) : Funnel(app, SCHEMA_NAME, REV_ID) {
 
@@ -32,8 +35,26 @@ class NotificationPreferencesFunnel(app: WikipediaApp) : Funnel(app, SCHEMA_NAME
         }
     }
 
+    fun logNotificationFilterPrefs() {
+        val fullFiltersList = mutableListOf<String>()
+        val toggleMap = HashMap<String, Boolean>()
+        val filteredList = StringUtil.csvToList(Prefs.notificationsFilterLanguageCodes.orEmpty())
+        fullFiltersList.addAll(NotificationsFilterActivity.allWikisList())
+        fullFiltersList.addAll(NotificationsFilterActivity.allTypesIdList())
+        fullFiltersList.forEach { toggleMap[it] = filteredList.contains(it) }
+        log("type_toggles", GsonUtil.getDefaultGson().toJson(toggleMap))
+    }
+
+    fun logSearchClick() {
+        log("type_toggles", "search_clicked")
+    }
+
+    fun logFilterClick() {
+        log("type_toggles", "filter_clicked")
+    }
+
     companion object {
         private const val SCHEMA_NAME = "MobileWikiAppNotificationPreferences"
-        private const val REV_ID = 18325724
+        private const val REV_ID = 22083261
     }
 }
