7548d04fed6cc9af8ec1135bc0918eea498ba219
sharvani <sharan@wikimedia.org>
2021-09-29 18:12:16 -0700

    Lint fix

--- app/src/main/java/org/wikipedia/notifications/NotificationFilterItemView.kt
+++ app/src/main/java/org/wikipedia/notifications/NotificationFilterItemView.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.content.res.ColorStateList
 import android.os.Build
 import android.util.AttributeSet
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
--- app/src/main/java/org/wikipedia/notifications/NotificationsFilterActivity.kt
+++ app/src/main/java/org/wikipedia/notifications/NotificationsFilterActivity.kt
@@ -3,7 +3,6 @@ package org.wikipedia.notifications
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
