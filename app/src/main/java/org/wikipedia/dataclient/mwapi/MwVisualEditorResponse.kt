7eae1ea094dca90eb1e2a46c70cc7b44de09fc97
Dmitry Brant <dbrant@wikimedia.org>
2021-10-05 08:41:19 -0400

    Make serializable.

--- app/src/main/java/org/wikipedia/dataclient/mwapi/MwVisualEditorResponse.kt
+++ app/src/main/java/org/wikipedia/dataclient/mwapi/MwVisualEditorResponse.kt
@@ -1,8 +1,12 @@
 package org.wikipedia.dataclient.mwapi
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 class MwVisualEditorResponse : MwResponse() {
     var visualeditor: VisualEditorData? = null
 
+    @Serializable
     class VisualEditorData {
         val notices: Map<String, String>? = null
     }
