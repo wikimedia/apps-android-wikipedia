3a199b820823f74afbe991697d240dc111a39721
Dmitry Brant <me@dmitrybrant.com>
2021-11-22 10:01:20 -0500

    Cleanup.

--- app/src/main/java/org/wikipedia/language/LanguagesListViewModel.kt
+++ app/src/main/java/org/wikipedia/language/LanguagesListViewModel.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -13,6 +14,7 @@ import org.wikipedia.WikipediaApp
 import org.wikipedia.dataclient.ServiceFactory
 import org.wikipedia.dataclient.mwapi.SiteMatrix
 import org.wikipedia.util.Resource
+import org.wikipedia.util.log.L
 import java.util.*
 
 class LanguagesListViewModel : ViewModel() {
@@ -20,14 +22,21 @@ class LanguagesListViewModel : ViewModel() {
     val siteListData = MutableLiveData<Resource<List<SiteMatrix.SiteInfo>>>()
     private val suggestedLanguageCodes = WikipediaApp.getInstance().language().remainingAvailableLanguageCodes
     private val nonSuggestedLanguageCodes = WikipediaApp.getInstance().language()
-        .appMruLanguageCodes.toMutableList().also { it.removeAll(suggestedLanguageCodes) }
+        .appMruLanguageCodes.toMutableList().also {
+                it.removeAll(suggestedLanguageCodes)
+                it.removeAll(WikipediaApp.getInstance().language().appLanguageCodes)
+            }
+
+    private val handler = CoroutineExceptionHandler { _, throwable ->
+        L.e(throwable)
+    }
 
     init {
         fetchData()
     }
 
     private fun fetchData() {
-        viewModelScope.launch {
+        viewModelScope.launch(handler) {
             withContext(Dispatchers.IO) {
                 val siteMatrix = ServiceFactory.get(WikipediaApp.getInstance().wikiSite).getSiteMatrix()
                 val sites = SiteMatrix.getSites(siteMatrix)
@@ -39,35 +48,32 @@ class LanguagesListViewModel : ViewModel() {
     fun getListBySearchTerm(context: Context, searchTerm: String?): List<LanguageListItem> {
         val results = mutableListOf<LanguageListItem>()
         val filter = StringUtils.stripAccents(searchTerm.orEmpty()).lowercase(Locale.getDefault())
-        var first = true
-        for (code in suggestedLanguageCodes) {
-            val localizedName = StringUtils.stripAccents(WikipediaApp.getInstance().language().getAppLanguageLocalizedName(code).orEmpty())
-            val canonicalName = StringUtils.stripAccents(getCanonicalName(code).orEmpty())
-            if (filter.isEmpty() || code.contains(filter) ||
-                localizedName.lowercase(Locale.getDefault()).contains(filter) ||
-                canonicalName.lowercase(Locale.getDefault()).contains(filter)) {
-                if (first) {
-                    results.add(LanguageListItem(context.getString(R.string.languages_list_suggested_text), true))
-                    first = false
-                }
-                results.add(LanguageListItem(code))
-            }
-        }
-        first = true
-        for (code in nonSuggestedLanguageCodes) {
-            val localizedName = StringUtils.stripAccents(WikipediaApp.getInstance().language().getAppLanguageLocalizedName(code).orEmpty())
-            val canonicalName = StringUtils.stripAccents(getCanonicalName(code).orEmpty())
-            if (filter.isEmpty() || code.contains(filter) ||
-                localizedName.lowercase(Locale.getDefault()).contains(filter) ||
-                canonicalName.lowercase(Locale.getDefault()).contains(filter)) {
-                if (first) {
-                    results.add(LanguageListItem(context.getString(R.string.languages_list_all_text), true))
-                    first = false
-                }
-                results.add(LanguageListItem(code))
-            }
-        }
+
+        addFilteredLanguageListItems(filter, suggestedLanguageCodes,
+                context.getString(R.string.languages_list_suggested_text), results)
+
+        addFilteredLanguageListItems(filter, nonSuggestedLanguageCodes,
+                context.getString(R.string.languages_list_all_text), results)
+
         return results
+    }
+
+    private fun addFilteredLanguageListItems(filter: String, codes: List<String>, headerText: String,
+                                             results: MutableList<LanguageListItem>) {
+        var first = true
+        for (code in codes) {
+            val localizedName = StringUtils.stripAccents(WikipediaApp.getInstance().language().getAppLanguageLocalizedName(code).orEmpty())
+            val canonicalName = StringUtils.stripAccents(getCanonicalName(code).orEmpty())
+            if (filter.isEmpty() || code.contains(filter) ||
+                    localizedName.lowercase(Locale.getDefault()).contains(filter) ||
+                    canonicalName.lowercase(Locale.getDefault()).contains(filter)) {
+                if (first) {
+                    results.add(LanguageListItem(headerText, true))
+                    first = false
+                }
+                results.add(LanguageListItem(code))
+            }
+        }
     }
 
     fun getCanonicalName(code: String): String? {
