7eb6bba751c2e0ed9b9c6ca653ca0f4a3aff8bf8
Sharvani Haran <sharvaniharan@users.noreply.github.com>
2022-01-14 11:02:53 -0800

    Wired-up analytics for customizing toolbar (#3062)
    * Wired-up analytics for customizing toolbar
    
    * Updated rfm value sent to analytics
    
    * Base class should be sealed
    
    * Minor update to send correct data

--- app/src/main/java/org/wikipedia/analytics/eventplatform/CustomizeToolbarEvent.kt
+++ app/src/main/java/org/wikipedia/analytics/eventplatform/CustomizeToolbarEvent.kt
@@ -2,22 +2,27 @@ package org.wikipedia.analytics.eventplatform
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import org.wikipedia.Constants.InvokeSource
 import org.wikipedia.auth.AccountUtil
+import org.wikipedia.page.action.PageActionItem
+import org.wikipedia.settings.Prefs
 
 @Suppress("unused")
 @Serializable
 @SerialName("/analytics/mobile_apps/android_customize_toolbar_interaction/1.0.0")
-class CustomizeToolbarEvent(private var is_anon: Boolean,
-                            private var is_rfm_enabled: Boolean,
-                            private var source: String,
-                            private var favorites_order: List<Int>,
-                            private var menu_order: List<Int>,
-                            private var time_spent_ms: Int) : TimedEvent(STREAM_NAME) {
+class CustomizeToolbarEvent : TimedEvent(STREAM_NAME) {
+    private var is_anon: Boolean? = null
+    private var is_rfm_enabled: Boolean? = null
+    private var source: String = ""
+    private var favorites_order: List<Int> = emptyList()
+    private var menu_order: List<Int> = emptyList()
+    private var time_spent_ms: Int = 0
 
-    fun logCustomization(source: String, favoritesOrder: List<Int>, menuOrder: List<Int>, isRfmEnabled: Boolean) {
-        this.is_anon = AccountUtil.isLoggedIn
-        this.is_rfm_enabled = isRfmEnabled
-        this.source = source
+    fun logCustomization(favoritesOrder: List<Int>, menuOrder: List<Int>) {
+        is_anon = !AccountUtil.isLoggedIn
+        is_rfm_enabled = Prefs.readingFocusModeEnabled
+        this.source = if (Prefs.customizeFavoritesMenuOrder.contains(PageActionItem.THEME.id))
+            InvokeSource.PAGE_OVERFLOW_MENU.value else InvokeSource.PAGE_ACTION_TAB.value
         this.favorites_order = favoritesOrder
         this.menu_order = menuOrder
         time_spent_ms = duration.toInt()
