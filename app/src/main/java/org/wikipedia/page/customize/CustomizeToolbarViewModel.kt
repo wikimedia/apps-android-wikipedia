3e4f56ceba380fe346e51011049e494437846dc9
Cooltey Feng <cfeng@wikimedia.org>
2022-01-31 11:05:47 -0800

    Create Customizable Toolbar (#3022)
    * Initial commit of customize toolbar
    
    * Rename
    
    * Rename #2
    
    * Create header and fix build error
    
    * Create PageMenuItem class
    
    * Fix lint
    
    * Added two item views
    
    * Add Prefs
    
    * Add initial setup of recycler view
    
    * Use viewmodel
    
    * Set up view items
    
    * Adding test button
    
    * Fix error
    
    * Fix build error
    
    * Set up default order
    
    * Make swipeable items correctly
    
    * Comment out
    
    * Make the top category header un rearrangeable
    
    * Show placeholder correctly
    
    * Make placeholder not movable
    
    * Update padding of empty placeholder
    
    * Use notifyItemRangeChanged
    
    * Set up default button and its behavior
    
    * Fix lint
    
    * Handle fixed item in quick actions
    
    * Handle category limitation correctly
    
    * Add comment
    
    * Add preferences to developer_preference.xml
    
    * revert change
    
    * Rename from favorites to quick actions
    
    * Fix build error
    
    * Make "Quick actions" has a fixed size
    
    * Update comment
    
    * Binding preference to page fragment
    
    * Update inflate
    
    * Show customize items in overflow menu
    
    * Fix lint
    
    * Make customize items show correctly
    
    * Fix lint
    
    * Update icon stats
    
    * Update icon/text status correctly
    
    * Update quick action bar
    
    * Update icons status correctly
    
    * Update icons status correctly
    
    * Update icon tint
    
    * Remove unwanted logging
    
    * Fix lint
    
    * Update description of the page
    
    * Get correct anchor for the PopupMenu
    
    * Wired-up analytics for customizing toolbar (#3062)
    
    * Wired-up analytics for customizing toolbar
    
    * Updated rfm value sent to analytics
    
    * Base class should be sealed
    
    * Minor update to send correct data
    
    * Follow up: use @SerialName on CuusutomizeToolbarEvent (#3072)
    
    * Rename quick actions to toolbar
    
    * Make entire item draggable
    
    * Disable buttons conditionally
    
    * Set up disable button status correctlly
    
    * Adjust tab text position to align with Explore tab
    
    * Add updateQuickActionsAndMenuOptions() to onResume
    
    * Show disabled button correctly
    
    * Show text color correctly
    
    * Make sure disable buttons are not clickable
    
    * Make theme button clickable status correctly
    
    * Only make draggable when holding drag icon
    
    * Using view items instead of nestedscrollview
    
    * RFM: Updates to theme bottom sheet (#3060)
    
    * Adjust height of the theme dialog bottom sheet
    
    * Testing code removed
    
    * Added tooltip
    
    * Minor adjustments to tooltip
    
    * Copy changes
    
    * Added quick actions option to theme sheet
    
    * Minor update
    
    * Added qq value
    
    * Added qq value
    
    * Design tweaks
    
    * design tweak
    
    * Minor change
    
    * Code review comments addressed
    
    * cr comments
    
    Co-authored-by: Cooltey Feng <cfeng@wikimedia.org>
    
    * Fix lint error
    
    Co-authored-by: Sharvani Haran <sharvaniharan@users.noreply.github.com>
    Co-authored-by: Dmitry Brant <dbrant@wikimedia.org>

---
+++ app/src/main/java/org/wikipedia/page/customize/CustomizeToolbarViewModel.kt
@@ -0,0 +1,144 @@
+package org.wikipedia.page.customize
+
+import androidx.lifecycle.ViewModel
+import org.wikipedia.R
+import org.wikipedia.page.action.PageActionItem
+import org.wikipedia.settings.Prefs
+import java.util.*
+
+class CustomizeToolbarViewModel : ViewModel() {
+
+    private var toolbarOrder = mutableListOf<Int>()
+    private var menuOrder = mutableListOf<Int>()
+
+    // List that contains header, empty placeholder and actual items.
+    var fullList = mutableListOf<Pair<Int, Any>>()
+        private set
+
+    init {
+        setupDefaultOrder(Prefs.customizeToolbarMenuOrder.isEmpty() && Prefs.customizeToolbarOrder.isEmpty())
+        preProcessList()
+    }
+
+    private fun setupDefaultOrder(default: Boolean) {
+        if (default) {
+            Prefs.customizeToolbarOrder = listOf(0, 1, 2, 3, 4)
+            Prefs.customizeToolbarMenuOrder = listOf(5, 6, 7, 8, 9, 10)
+        }
+        toolbarOrder = Prefs.customizeToolbarOrder.toMutableList()
+        menuOrder = Prefs.customizeToolbarMenuOrder.toMutableList()
+    }
+
+    private fun preProcessList() {
+        fullList.clear()
+        // Description
+        fullList.add(CustomizeToolbarFragment.VIEW_TYPE_DESCRIPTION to "")
+        // Toolbar
+        fullList.add(headerPair(true))
+        fullList.addAll(addItemsOrEmptyPlaceholder(toolbarOrder, true))
+        // Menu
+        fullList.add(headerPair(false))
+        fullList.addAll(addItemsOrEmptyPlaceholder(menuOrder, false))
+        // Set to default
+        fullList.add(CustomizeToolbarFragment.VIEW_TYPE_SET_TO_DEFAULT to "")
+    }
+
+    private fun addItemsOrEmptyPlaceholder(list: List<Int>, toolbar: Boolean): List<Pair<Int, Any>> {
+        return if (list.isEmpty()) {
+            listOf(emptyPlaceholderPair(toolbar))
+        } else {
+            list.map { CustomizeToolbarFragment.VIEW_TYPE_ITEM to PageActionItem.find(it) }
+        }
+    }
+
+    private fun headerPair(isToolbar: Boolean): Pair<Int, Any> {
+        return CustomizeToolbarFragment.VIEW_TYPE_HEADER to
+                if (isToolbar) R.string.customize_toolbar_category_toolbar else R.string.customize_toolbar_category_menu
+    }
+
+    private fun emptyPlaceholderPair(isToolbar: Boolean): Pair<Int, Any> {
+        return CustomizeToolbarFragment.VIEW_TYPE_EMPTY_PLACEHOLDER to isToolbar
+    }
+
+    private fun collectCategoriesItems(): Pair<MutableList<Int>, MutableList<Int>> {
+        var saveIntoToolbar = true
+        val toolbarItems = mutableListOf<Int>()
+        val menuItems = mutableListOf<Int>()
+        fullList.filterNot { it.first == CustomizeToolbarFragment.VIEW_TYPE_EMPTY_PLACEHOLDER }.forEach {
+            if (it == headerPair(false)) {
+                saveIntoToolbar = false
+            }
+            if (it.first == CustomizeToolbarFragment.VIEW_TYPE_ITEM) {
+                if (saveIntoToolbar) {
+                    toolbarItems.add((it.second as PageActionItem).id)
+                } else {
+                    menuItems.add((it.second as PageActionItem).id)
+                }
+            }
+        }
+        return toolbarItems to menuItems
+    }
+
+    // First: toolbar; Second: Menu
+    private fun handleCategoryLimitation(pair: Pair<MutableList<Int>, MutableList<Int>>): List<Int> {
+        val list = mutableListOf<Int>()
+        // To avoid seeing the bug from the library, we have to use a while loop to manually swap items.
+        while (pair.first.size > CustomizeToolbarFragment.TOOLBAR_ITEMS_LIMIT) {
+            // Last item swap with "Menu" header
+            swapList(pair.first.size, pair.first.size + 1)
+            // Add swapped item to list
+            list.add(pair.first.size)
+            // Add to "Menu" order list and remove the last item of toolbar
+            pair.second.add(0, pair.first.removeLast())
+        }
+        return list
+    }
+
+    fun resetToDefault() {
+        setupDefaultOrder(true)
+        preProcessList()
+        saveChanges()
+    }
+
+    fun saveChanges(): List<Int> {
+        val pair = collectCategoriesItems()
+        val rearrangedItems = handleCategoryLimitation(pair)
+
+        toolbarOrder = pair.first
+        menuOrder = pair.second
+        Prefs.customizeToolbarOrder = toolbarOrder
+        Prefs.customizeToolbarMenuOrder = menuOrder
+
+        return rearrangedItems
+    }
+
+    fun swapList(oldPosition: Int, newPosition: Int) {
+        Collections.swap(fullList, oldPosition, newPosition)
+    }
+
+    fun addEmptyPlaceholder(): Int {
+        if (toolbarOrder.isEmpty() && !fullList.contains(emptyPlaceholderPair(true))) {
+            fullList.add(1, emptyPlaceholderPair(true))
+            return 1
+        }
+        if (menuOrder.isEmpty() && !fullList.contains(emptyPlaceholderPair(false))) {
+            fullList.add(emptyPlaceholderPair(false))
+            return fullList.size - 1
+        }
+        return -1
+    }
+
+    fun removeEmptyPlaceholder(): Int {
+        if (toolbarOrder.isNotEmpty() && fullList.contains(emptyPlaceholderPair(true))) {
+            val index = fullList.indexOf(emptyPlaceholderPair(true))
+            fullList.removeAt(index)
+            return index
+        }
+        if (menuOrder.isNotEmpty() && fullList.contains(emptyPlaceholderPair(false))) {
+            val index = fullList.indexOf(emptyPlaceholderPair(false))
+            fullList.removeAt(index)
+            return index
+        }
+        return -1
+    }
+}
