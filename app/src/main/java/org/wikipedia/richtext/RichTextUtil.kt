0b75247a5469ec529272591a4f2f033bd2232942
Isira Seneviratne <isirasen96@gmail.com>
2022-02-03 09:27:06 +0530

    Remove RichTextUtil.getSpans().

--- app/src/main/java/org/wikipedia/richtext/RichTextUtil.kt
+++ app/src/main/java/org/wikipedia/richtext/RichTextUtil.kt
@@ -18,7 +18,7 @@ object RichTextUtil {
      *
      * @see {@link android.text.TextUtils.copySpansFrom}
      */
-    private fun copySpans(src: Spanned, dst: Spannable, spans: Collection<Any?>) {
+    private fun copySpans(src: Spanned, dst: Spannable, spans: Collection<Any>) {
         for (span in spans) {
             val start = src.getSpanStart(span)
             val end = src.getSpanEnd(span)
@@ -43,12 +43,8 @@ object RichTextUtil {
      *
      * @see {@link android.view.inputmethod.BaseInputConnection.removeComposingSpans}
      */
-    private fun getComposingSpans(spanned: Spanned, start: Int, end: Int): List<Any?> {
-        return getSpans(spanned, start, end).filter { isComposingSpan(spanned, it) }
-    }
-
-    fun getSpans(spanned: Spanned, start: Int, end: Int): Array<out Any> {
-        return spanned.getSpans(start, end)
+    private fun getComposingSpans(spanned: Spanned, start: Int, end: Int): List<Any> {
+        return spanned.getSpans<Any>(start, end).filter { isComposingSpan(spanned, it) }
     }
 
     private fun isComposingSpan(spanned: Spanned, span: Any?): Boolean {
