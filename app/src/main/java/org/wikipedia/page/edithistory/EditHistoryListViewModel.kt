7104cde3d4aaedcd52645fee7cbbc12e5ac355fe
Dmitry Brant <dbrant@wikimedia.org>
2022-03-01 13:39:41 -0500

    Towards proper ViewModel and PagingSource usage: Part 1. (#3144)
    * Edit history follow-up 1.
    
    * Towards proper ViewModel and PagingSource usage: Part 1.
    
    Co-authored-by: Cooltey Feng <cfeng@wikimedia.org>

--- app/src/main/java/org/wikipedia/page/edithistory/EditHistoryListViewModel.kt
+++ app/src/main/java/org/wikipedia/page/edithistory/EditHistoryListViewModel.kt
@@ -1,7 +1,9 @@
 package org.wikipedia.page.edithistory
 
+import android.os.Bundle
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.Dispatchers
@@ -15,22 +17,33 @@ import org.wikipedia.util.Resource
 import org.wikipedia.util.Resource.Success
 import org.wikipedia.util.log.L
 
-class EditHistoryListViewModel : ViewModel() {
+class EditHistoryListViewModel(bundle: Bundle) : ViewModel() {
+
+    var pageTitle: PageTitle = bundle.getParcelable(EditHistoryListActivity.INTENT_EXTRA_PAGE_TITLE)!!
 
     val editHistoryListData = MutableLiveData<Resource<List<Revision>>>()
 
-    private val handler = CoroutineExceptionHandler { _, throwable ->
-        L.e(throwable)
+    init {
+        fetchData(pageTitle)
     }
 
     fun fetchData(pageTitle: PageTitle) {
-        viewModelScope.launch(handler) {
+        viewModelScope.launch(CoroutineExceptionHandler { _, throwable ->
+            L.e(throwable)
+        }) {
             withContext(Dispatchers.IO) {
                 val response = ServiceFactory.get(WikiSite.forLanguageCode(pageTitle.wikiSite.languageCode))
                     .getEditHistoryDetails(pageTitle.prefixedText)
                 val revisions = response.query!!.pages?.get(0)?.revisions
                 editHistoryListData.postValue(Success(revisions!!))
             }
+        }
+    }
+
+    class Factory(private val bundle: Bundle) : ViewModelProvider.Factory {
+        @Suppress("unchecked_cast")
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            return EditHistoryListViewModel(bundle) as T
         }
     }
 }
