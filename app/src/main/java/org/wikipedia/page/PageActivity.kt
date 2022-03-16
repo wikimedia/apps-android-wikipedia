391f2a99385c45eefcf6feca30e6e82b263d79de
sharvani <sharan@wikimedia.org>
2022-03-16 10:22:21 -0700

    Minor clean-up

--- app/src/main/java/org/wikipedia/page/PageActivity.kt
+++ app/src/main/java/org/wikipedia/page/PageActivity.kt
@@ -635,8 +635,8 @@ class PageActivity : BaseActivity(), PageFragment.Callback, LinkPreviewDialog.Ca
     private fun maybeShowWatchlistTooltip() {
         pageFragment.historyEntry?.let {
             if (!Prefs.isWatchlistPageOnboardingTooltipShown && AccountUtil.isLoggedIn &&
-                it.source != HistoryEntry.SOURCE_SUGGESTED_EDITS &&
-                Prefs.loggedInPageActivityVisitCount >= 3) {
+                    it.source != HistoryEntry.SOURCE_SUGGESTED_EDITS &&
+                    Prefs.loggedInPageActivityVisitCount >= 3) {
                 enqueueTooltip {
                     watchlistFunnel.logShowTooltip()
                     Prefs.isWatchlistPageOnboardingTooltipShown = true
@@ -666,7 +666,7 @@ class PageActivity : BaseActivity(), PageFragment.Callback, LinkPreviewDialog.Ca
     // TODO: remove on March 2022.
     private fun maybeShowNotificationTooltip() {
         if (!Prefs.isPageNotificationTooltipShown && AccountUtil.isLoggedIn &&
-            Prefs.loggedInPageActivityVisitCount >= 1) {
+                Prefs.loggedInPageActivityVisitCount >= 1) {
             enqueueTooltip {
                 FeedbackUtil.showTooltip(this, binding.pageToolbarButtonNotifications, getString(R.string.page_notification_tooltip),
                     aboveOrBelow = false, autoDismiss = false, -32, -8).setOnBalloonDismissListener {
