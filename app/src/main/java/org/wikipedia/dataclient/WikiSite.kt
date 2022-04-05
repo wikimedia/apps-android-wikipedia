6187b31f7926e8346d17063995cd42d7e04506e6
Casper Hildebrand <42659828+CasHil@users.noreply.github.com>
2022-04-05 21:34:16 +0200

    Refactor title functions (#3232)
    * Update tests for page title internal link refactor
    
    * Move functions from Wikipage to PageTitle
    
    * Update references to title functions
    
    * Use constants wikisite
    
    * Fix linting issues
    
    * Fix failing test
    
    * Remove seperate wiki variable assignment
    
    Co-authored-by: Cooltey Feng <cfeng@wikimedia.org>

--- app/src/main/java/org/wikipedia/dataclient/WikiSite.kt
+++ app/src/main/java/org/wikipedia/dataclient/WikiSite.kt
@@ -9,7 +9,6 @@ import org.wikipedia.WikipediaApp
 import org.wikipedia.json.UriSerializer
 import org.wikipedia.language.AppLanguageLookUpTable
 import org.wikipedia.language.LanguageUtil
-import org.wikipedia.page.PageTitle
 import org.wikipedia.util.UriUtil
 
 /**
@@ -103,21 +102,6 @@ data class WikiSite(
 
     fun url(segment: String): String {
         return url() + path(segment)
-    }
-
-    // TODO: this method doesn't have much to do with WikiSite. Move to PageTitle?
-    fun titleForInternalLink(internalLink: String?): PageTitle {
-        // Strip the /wiki/ from the href
-        return PageTitle(UriUtil.removeInternalLinkPrefix(internalLink.orEmpty()), this)
-    }
-
-    // TODO: this method doesn't have much to do with WikiSite. Move to PageTitle?
-    fun titleForUri(uri: Uri): PageTitle {
-        var path = uri.path
-        if (!uri.fragment.isNullOrEmpty()) {
-            path += "#" + uri.fragment
-        }
-        return titleForInternalLink(path)
     }
 
     fun dbName(): String {
