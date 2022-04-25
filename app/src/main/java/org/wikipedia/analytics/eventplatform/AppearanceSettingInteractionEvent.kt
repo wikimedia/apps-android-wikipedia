037ad4f8ee94d57c7b24c09aa72baa3fb5f61515
sharvani <sharan@wikimedia.org>
2022-04-25 10:53:38 -0700

    Add app wiring to appearance schema

---
+++ app/src/main/java/org/wikipedia/analytics/eventplatform/AppearanceSettingInteractionEvent.kt
@@ -0,0 +1,38 @@
+package org.wikipedia.analytics.eventplatform
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import org.wikipedia.Constants.InvokeSource
+import org.wikipedia.theme.Theme
+
+class AppearanceSettingInteractionEvent(private var source: InvokeSource) {
+
+    fun logFontSizeChange(currentFontSize: Float, newFontSize: Float) {
+        submitEvent("fontSizeChange", currentFontSize.toString(), newFontSize.toString())
+    }
+
+    fun logThemeChange(currentTheme: Theme, newTheme: Theme) {
+        submitEvent("themeChange", currentTheme.funnelName, newTheme.funnelName)
+    }
+
+    fun logFontThemeChange(currentFontFamily: String?, newFontFamily: String?) {
+        submitEvent("fontThemeChange", currentFontFamily.orEmpty(), newFontFamily.orEmpty())
+    }
+
+    fun logReadingFocusMode(newValue: Boolean) {
+        submitEvent("readingFocusMode", (!newValue).toString(), newValue.toString())
+    }
+
+    private fun submitEvent(action: String, currentValue: String, newValue: String) {
+        EventPlatformClient.submit(AppearanceSettingInteractionEventImpl(action, currentValue, newValue, source.value))
+    }
+
+    @Suppress("unused")
+    @Serializable
+    @SerialName("/analytics/mobile_apps/android_app_appearance_settings_interaction/1.0.0")
+    class AppearanceSettingInteractionEventImpl(private var action: String,
+                                                @SerialName("current_value") private var currentValue: String,
+                                                @SerialName("new_value") private var newValue: String,
+                                                private var source: String) :
+        MobileAppsEvent("android.app_appearance_settings_interaction")
+}
