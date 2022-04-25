27aa29cfda3c8010628fe7ff756e9eac71c2cbf3
sharvani <sharan@wikimedia.org>
2022-04-25 11:36:20 -0700

    Add app wiring to link preview interaction schema

--- app/src/main/java/org/wikipedia/analytics/eventplatform/AppearanceSettingInteractionEvent.kt
+++ app/src/main/java/org/wikipedia/analytics/eventplatform/AppearanceSettingInteractionEvent.kt
@@ -3,6 +3,7 @@ package org.wikipedia.analytics.eventplatform
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import org.wikipedia.Constants.InvokeSource
+import org.wikipedia.auth.AccountUtil
 import org.wikipedia.theme.Theme
 
 class AppearanceSettingInteractionEvent(private var source: InvokeSource) {
@@ -24,13 +25,14 @@ class AppearanceSettingInteractionEvent(private var source: InvokeSource) {
     }
 
     private fun submitEvent(action: String, currentValue: String, newValue: String) {
-        EventPlatformClient.submit(AppearanceSettingInteractionEventImpl(action, currentValue, newValue, source.value))
+        EventPlatformClient.submit(AppearanceSettingInteractionEventImpl(action, !AccountUtil.isLoggedIn, currentValue, newValue, source.value))
     }
 
     @Suppress("unused")
     @Serializable
     @SerialName("/analytics/mobile_apps/android_app_appearance_settings_interaction/1.0.0")
     class AppearanceSettingInteractionEventImpl(private var action: String,
+                                                @SerialName("is_anon") private val isAnon: Boolean,
                                                 @SerialName("current_value") private var currentValue: String,
                                                 @SerialName("new_value") private var newValue: String,
                                                 private var source: String) :
