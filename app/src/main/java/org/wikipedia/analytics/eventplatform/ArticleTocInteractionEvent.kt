a8fb59f890dcb8536e0d3f27f246d795f24d9747
Sharvani Haran <sharvaniharan@users.noreply.github.com>
2022-05-04 13:59:30 -0700

    Wiring for analytics schemas for Mar 4 -20 cycle (#3280)
    * Deprecate legacy schema
    
    * Add app wiring to appearance schema
    
    * Add app wiring to link preview interaction schema
    
    * Add app wiring to article toc interaction schema
    
    * Add app wiring to article find in page interaction schema
    
    * Peeking logic is no more used
    
    * Code review comments addressed
    
    Co-authored-by: Dmitry Brant <dbrant@wikimedia.org>

---
+++ app/src/main/java/org/wikipedia/analytics/eventplatform/ArticleTocInteractionEvent.kt
@@ -0,0 +1,53 @@
+package org.wikipedia.analytics.eventplatform
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import org.wikipedia.auth.AccountUtil
+import java.util.concurrent.TimeUnit
+
+class ArticleTocInteractionEvent(private val pageId: Int,
+                                 private val wikiDb: String,
+                                 private val numSections: Int) {
+
+    private var numOpens = 0
+    private var numSectionClicks = 0
+    private var lastScrollStartMillis = 0L
+    private var totalOpenedSec = 0
+
+    fun scrollStart() {
+        numOpens++
+        lastScrollStartMillis = System.currentTimeMillis()
+    }
+
+    fun scrollStop() {
+        if (lastScrollStartMillis == 0L) {
+            return
+        }
+        totalOpenedSec += ((System.currentTimeMillis() - lastScrollStartMillis) / TimeUnit.SECONDS.toMillis(1)).toInt()
+        lastScrollStartMillis = 0
+    }
+
+    fun logClick() {
+        numSectionClicks++
+    }
+
+    fun logEvent() {
+        scrollStop()
+        if (numSections == 0 || numOpens == 0) {
+            return
+        }
+        EventPlatformClient.submit(ArticleTocInteractionEventImpl(!AccountUtil.isLoggedIn, wikiDb, pageId, numOpens, numSectionClicks, totalOpenedSec, numSections))
+    }
+
+    @Suppress("unused")
+    @Serializable
+    @SerialName("/analytics/mobile_apps/android_article_toc_interaction/1.0.0")
+    class ArticleTocInteractionEventImpl(@SerialName("is_anon") private val isAnon: Boolean,
+                                         @SerialName("wiki_db") private val wikiDb: String,
+                                         @SerialName("page_id") private val pageId: Int,
+                                         @SerialName("num_opens") private val numberOfOpens: Int,
+                                         @SerialName("num_section_clicks") private val numberOfSectionClicks: Int,
+                                         @SerialName("total_open_sec") private val totalOpenSec: Int,
+                                         @SerialName("num_sections") private val numSections: Int) :
+        MobileAppsEvent("android.article_toc_interaction")
+}
