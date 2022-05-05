dfac32fea7672420dd6dec34b2af2f0806146ac7
Dmitry Brant <me@dmitrybrant.com>
2022-05-05 08:31:30 -0400

    Fix tests.

--- app/src/main/java/org/wikipedia/analytics/eventplatform/TestAppsEvent.kt
+++ app/src/main/java/org/wikipedia/analytics/eventplatform/TestAppsEvent.kt
@@ -3,6 +3,6 @@ package org.wikipedia.analytics.eventplatform
 import kotlinx.serialization.Serializable
 
 @Serializable
-class TestAppsEvent : MobileAppsEvent {
+class TestAppsEvent : MobileAppsEventBase {
     constructor(_stream: String) : super(_stream)
 }
