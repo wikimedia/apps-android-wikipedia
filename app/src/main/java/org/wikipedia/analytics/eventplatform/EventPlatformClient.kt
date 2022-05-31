7ede8e43d16db9dfcc5b1a4f3d4f379473de7869
sharvani <sharan@wikimedia.org>
2022-05-31 20:54:03 +0530

    Lint fix

--- app/src/main/java/org/wikipedia/analytics/eventplatform/EventPlatformClient.kt
+++ app/src/main/java/org/wikipedia/analytics/eventplatform/EventPlatformClient.kt
@@ -104,7 +104,7 @@ object EventPlatformClient {
          * If another item is added to QUEUE during this time, reset the countdown.
          */
         private const val WAIT_MS = 30000
-        private const val MAX_QUEUE_SIZE = 64
+        private const val MAX_QUEUE_SIZE = 128
         private val SEND_RUNNABLE = Runnable { sendAllScheduled() }
 
         @Synchronized
