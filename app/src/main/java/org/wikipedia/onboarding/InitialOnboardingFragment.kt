cfd3916a95d2b1894e4611491c61549619c5a2e4
sharvani <sharan@wikimedia.org>
2022-06-03 17:06:53 +0530

    lint fix

--- app/src/main/java/org/wikipedia/onboarding/InitialOnboardingFragment.kt
+++ app/src/main/java/org/wikipedia/onboarding/InitialOnboardingFragment.kt
@@ -3,7 +3,6 @@ package org.wikipedia.onboarding
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
