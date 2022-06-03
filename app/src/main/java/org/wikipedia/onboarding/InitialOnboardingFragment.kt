2975bbf79405104162ab5e82dbf8a33c73a01947
sharvani <sharan@wikimedia.org>
2022-06-03 17:05:59 +0530

    Non-GestureDetector solution for Onboarding breadcrumbs

--- app/src/main/java/org/wikipedia/onboarding/InitialOnboardingFragment.kt
+++ app/src/main/java/org/wikipedia/onboarding/InitialOnboardingFragment.kt
@@ -3,6 +3,7 @@ package org.wikipedia.onboarding
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
