475d44ab219518dcbf9b47f36a930f13e7272f90
cooltey <coolteygame@gmail.com>
2022-07-07 17:35:43 -0700

    Add padding and update background color

--- app/src/main/java/org/wikipedia/talk/TalkTopicsActivity.kt
+++ app/src/main/java/org/wikipedia/talk/TalkTopicsActivity.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.text.format.DateUtils
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
