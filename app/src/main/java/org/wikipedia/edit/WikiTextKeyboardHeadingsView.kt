fc23253fe02777c5540a7d36fc3c70d6462e433c
Dmitry Brant <me@dmitrybrant.com>
2022-09-09 09:34:45 -0400

    Update style of headings buttons.

--- app/src/main/java/org/wikipedia/edit/WikiTextKeyboardHeadingsView.kt
+++ app/src/main/java/org/wikipedia/edit/WikiTextKeyboardHeadingsView.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.FrameLayout
+import org.wikipedia.R
 import org.wikipedia.databinding.ViewWikitextKeyboardHeadingsBinding
 import org.wikipedia.util.FeedbackUtil
 
@@ -20,26 +21,33 @@ class WikiTextKeyboardHeadingsView : FrameLayout {
         binding.closeButton.setOnClickListener {
             callback?.onSyntaxOverlayCollapse()
         }
-        FeedbackUtil.setButtonLongPressToast(binding.closeButton)
+
+        binding.wikitextButtonH2.contentDescription = context.getString(R.string.wikitext_heading_n, 2)
         binding.wikitextButtonH2.setOnClickListener {
             editText?.inputConnection?.let {
                 WikiTextKeyboardView.toggleSyntaxAroundCurrentSelection(editText, it, "==", "==")
             }
         }
+        binding.wikitextButtonH3.contentDescription = context.getString(R.string.wikitext_heading_n, 3)
         binding.wikitextButtonH3.setOnClickListener {
             editText?.inputConnection?.let {
                 WikiTextKeyboardView.toggleSyntaxAroundCurrentSelection(editText, it, "===", "===")
             }
         }
+        binding.wikitextButtonH4.contentDescription = context.getString(R.string.wikitext_heading_n, 4)
         binding.wikitextButtonH4.setOnClickListener {
             editText?.inputConnection?.let {
                 WikiTextKeyboardView.toggleSyntaxAroundCurrentSelection(editText, it, "====", "====")
             }
         }
+        binding.wikitextButtonH5.contentDescription = context.getString(R.string.wikitext_heading_n, 5)
         binding.wikitextButtonH5.setOnClickListener {
             editText?.inputConnection?.let {
                 WikiTextKeyboardView.toggleSyntaxAroundCurrentSelection(editText, it, "=====", "=====")
             }
         }
+
+        FeedbackUtil.setButtonLongPressToast(binding.closeButton, binding.wikitextButtonH2, binding.wikitextButtonH3,
+                binding.wikitextButtonH4, binding.wikitextButtonH5)
     }
 }
