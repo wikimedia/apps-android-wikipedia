ec5cf591848dc5e20f05c91bfd4fd794e6e12143
sharvani <sharan@wikimedia.org>
2022-09-16 13:37:12 -0700

    Lint fix

--- app/src/main/java/org/wikipedia/util/FileUtil.kt
+++ app/src/main/java/org/wikipedia/util/FileUtil.kt
@@ -44,13 +44,13 @@ object FileUtil {
                 val exportsFolder = File(downloadsFolder, appExportsFolderName)
                 val csvFile = File(exportsFolder, filename)
                 exportsFolder.mkdir()
-                csvFile.delete() //To overwrite when file exists
+                csvFile.delete() // To overwrite when file exists
                 FileOutputStream(csvFile, true).bufferedWriter().use { writer ->
                     writer.write(stringBuilder.toString())
                 }
             }
         } catch (e: Exception) {
-            //ignore
+            // ignore
         }
     }
 
