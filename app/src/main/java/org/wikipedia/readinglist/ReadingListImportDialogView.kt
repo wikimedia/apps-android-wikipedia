e5ec585e44e16682f433254d0526ecaabf90be72
cooltey <coolteygame@gmail.com>
2022-09-23 12:50:42 -0700

    Create custom view for import dialog

---
+++ app/src/main/java/org/wikipedia/readinglist/ReadingListImportDialogView.kt
@@ -0,0 +1,34 @@
+package org.wikipedia.readinglist
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.FrameLayout
+import org.wikipedia.R
+import org.wikipedia.databinding.DialogImportReadingListBinding
+import org.wikipedia.readinglist.database.ReadingList
+import org.wikipedia.views.ViewUtil
+
+class ReadingListImportDialogView : FrameLayout {
+
+    private val binding = DialogImportReadingListBinding.inflate(LayoutInflater.from(context), this, false)
+    private val imageViews = listOf(binding.itemImage1, binding.itemImage2, binding.itemImage3, binding.itemImage4)
+
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    fun setReadingList(readingList: ReadingList) {
+
+        binding.readingListTitle.text = readingList.listTitle
+        binding.readingListNumberOfPages.text = resources.getQuantityString(R.plurals.shareable_reading_lists_import_dialog_content_articles, readingList.pages.size, readingList.pages.size)
+
+        readingList.let {
+            val thumbUrls = it.pages.mapNotNull { page -> page.thumbUrl }
+                .filterNot { url -> url.isEmpty() }
+            (imageViews zip thumbUrls).forEach { (imageView, url) ->
+                ViewUtil.loadImage(imageView, url)
+            }
+        }
+    }
+}
