e766bbb777272b27722a85aff28d750531348fe3
sharvani <sharan@wikimedia.org>
2022-10-21 11:54:46 -0700

    Minor fix

--- app/src/main/java/org/wikipedia/dataclient/Service.kt
+++ app/src/main/java/org/wikipedia/dataclient/Service.kt
@@ -44,6 +44,14 @@ interface Service {
         @Query("srsearch") repeat: String?
     ): Observable<PrefixSearchResponse>
 
+    @GET(
+        MW_API_PREFIX + "action=query&converttitles=" +
+                "&prop=description|pageimages|pageprops|info&ppprop=mainpage|disambiguation" +
+                "&generator=search&gsrnamespace=0&gsrwhat=text" +
+                "&inprop=varianttitles" +
+                "&gsrinfo=&gsrprop=redirecttitle&piprop=thumbnail&pilicense=any&pithumbsize=" +
+                PREFERRED_THUMB_SIZE
+        )
     fun fullTextSearch(
         @Query("gsrsearch") searchTerm: String?,
         @Query("gsrlimit") gsrLimit: Int,
