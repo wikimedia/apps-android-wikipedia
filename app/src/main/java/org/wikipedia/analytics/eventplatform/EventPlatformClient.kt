b0542971b690c226b65b90063ab1004769ef7f3a
Dmitry Brant <me@dmitrybrant.com>
2022-12-12 14:19:27 -0500

    Show loud warning messages upon MEP errors.

--- app/src/main/java/org/wikipedia/analytics/eventplatform/EventPlatformClient.kt
+++ app/src/main/java/org/wikipedia/analytics/eventplatform/EventPlatformClient.kt
@@ -1,5 +1,6 @@
 package org.wikipedia.analytics.eventplatform
 
+import android.widget.Toast
 import androidx.core.os.postDelayed
 import io.reactivex.rxjava3.schedulers.Schedulers
 import org.wikipedia.BuildConfig
@@ -171,22 +172,20 @@ object EventPlatformClient {
                             }
                         }
                     }) {
+                        L.e(it)
                         if (it is HttpStatusException) {
-                            when (it.code) {
-                                HttpURLConnection.HTTP_BAD_REQUEST,
-                                HttpURLConnection.HTTP_INTERNAL_ERROR,
-                                HttpURLConnection.HTTP_UNAVAILABLE,
-                                HttpURLConnection.HTTP_GATEWAY_TIMEOUT -> {
-                                    L.e(it)
-                                    // TODO: queue up to retry?
-                                }
-                                else -> {
-                                    // Something unexpected happened. Crash if this is a pre-production build.
-                                    L.logRemoteErrorIfProd(it)
+                            if (it.code >= HttpURLConnection.HTTP_INTERNAL_ERROR) {
+                                // TODO: For errors >= 500, queue up to retry?
+                            } else {
+                                // Something unexpected happened.
+                                if (ReleaseUtil.isDevRelease) {
+                                    // If it's a pre-beta release, show a loud toast to signal that
+                                    // a potential issue should be investigated.
+                                    WikipediaApp.instance.mainThreadHandler.post {
+                                        Toast.makeText(WikipediaApp.instance, it.message, Toast.LENGTH_LONG).show()
+                                    }
                                 }
                             }
-                        } else {
-                            L.w(it)
                         }
                     }
         }
