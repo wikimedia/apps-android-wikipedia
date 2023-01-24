040d440eb094d83f1fff5ca967ae7fd8b6e49fec
cooltey <coolteygame@gmail.com>
2023-01-24 14:06:40 -0800

    Minor update

--- app/src/main/java/org/wikipedia/talk/TalkTopicsActivity.kt
+++ app/src/main/java/org/wikipedia/talk/TalkTopicsActivity.kt
@@ -381,7 +381,7 @@ class TalkTopicsActivity : BaseActivity(), WatchlistExpiryDialog.Callback {
         FeedbackUtil.setButtonLongPressToast(binding.toolbarTitle)
     }
 
-    fun updateNotificationDot(animate: Boolean) {
+    private fun updateNotificationDot(animate: Boolean) {
         if (AccountUtil.isLoggedIn && Prefs.notificationUnreadCount > 0) {
             notificationButtonView.setUnreadCount(Prefs.notificationUnreadCount)
             if (animate) {
