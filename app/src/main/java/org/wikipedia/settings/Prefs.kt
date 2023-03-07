01ddf20aa3a5e50660099974bd24a3faddc94fec
Cooltey Feng <cfeng@wikimedia.org>
2023-03-07 13:32:14 -0800

    Fix lint

--- app/src/main/java/org/wikipedia/settings/Prefs.kt
+++ app/src/main/java/org/wikipedia/settings/Prefs.kt
@@ -666,7 +666,7 @@ object Prefs {
     var suggestedEditsMachineGeneratedDescriptionTooltipShown
         get() = PrefsIoUtil.getBoolean(R.string.preference_key_se_machine_generated_descriptions_tooltip_shown, false)
         set(value) = PrefsIoUtil.setBoolean(R.string.preference_key_se_machine_generated_descriptions_tooltip_shown, value)
-    
+
     var watchlistExcludedWikiCodes
         get() = JsonUtil.decodeFromString<Set<String>>(PrefsIoUtil.getString(R.string.preference_key_excluded_wiki_codes_watchlist, null))
             ?: emptySet()
