848a91e4b6abde5991b926bc9c1948f3204257ea
Sharvani Haran <sharvaniharan83@gmail.com>
2023-03-28 12:29:14 -0700

    Move experienced user logic to helper class

--- app/src/main/java/org/wikipedia/analytics/eventplatform/ABTest.kt
+++ app/src/main/java/org/wikipedia/analytics/eventplatform/ABTest.kt
@@ -1,13 +1,7 @@
 package org.wikipedia.analytics.eventplatform
 
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
-import org.wikipedia.Constants
-import org.wikipedia.WikipediaApp
 import org.wikipedia.auth.AccountUtil
-import org.wikipedia.dataclient.ServiceFactory
 import org.wikipedia.settings.PrefsIoUtil
 import kotlin.random.Random
 
@@ -22,7 +16,7 @@ class ABTest(private val abTestName: String, private val abTestGroupCount: Int) 
                 group = Random(System.currentTimeMillis()).nextInt(Int.MAX_VALUE).mod(abTestGroupCount)
                 if (group == GROUP_2 && AccountUtil.isLoggedIn) {
                     runBlocking {
-                        isUserExperienced()
+                        MachineGeneratedArticleDescriptionsAnalyticsHelper.isUserExperienced()
                     }.also {
                         if (it) {
                             group = GROUP_3
@@ -34,24 +28,6 @@ class ABTest(private val abTestName: String, private val abTestGroupCount: Int) 
                 PrefsIoUtil.setInt(AB_TEST_KEY_PREFIX + abTestName, group)
             }
             return group
-        }
-
-    private suspend fun isUserExperienced(): Boolean =
-        withContext(Dispatchers.Default) {
-            var totalContributions = 0
-
-            val homeSiteResponse = async { ServiceFactory.get(WikipediaApp.instance.wikiSite)
-                .getUserContrib(AccountUtil.userName!!, 10) }
-            val commonsResponse = async { ServiceFactory.get(Constants.commonsWikiSite)
-                .getUserContrib(AccountUtil.userName!!, 10) }
-            val wikidataResponse = async { ServiceFactory.get(Constants.wikidataWikiSite)
-                .getUserContrib(AccountUtil.userName!!, 10) }
-
-            totalContributions += homeSiteResponse.await().query?.userInfo?.editCount ?: 0
-            totalContributions += commonsResponse.await().query?.userInfo?.editCount ?: 0
-            totalContributions += wikidataResponse.await().query?.userInfo?.editCount ?: 0
-
-            return@withContext totalContributions > EXP_CONTRIBUTOR_REQ
         }
 
     companion object {
--- app/src/main/java/org/wikipedia/analytics/eventplatform/MachineGeneratedArticleDescriptionsAnalyticsHelper.kt
+++ app/src/main/java/org/wikipedia/analytics/eventplatform/MachineGeneratedArticleDescriptionsAnalyticsHelper.kt
@@ -1,6 +1,13 @@
 package org.wikipedia.analytics.eventplatform
 
 import android.content.Context
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.withContext
+import org.wikipedia.Constants
+import org.wikipedia.WikipediaApp
+import org.wikipedia.auth.AccountUtil
+import org.wikipedia.dataclient.ServiceFactory
 
 object MachineGeneratedArticleDescriptionsAnalyticsHelper {
 
@@ -81,4 +88,25 @@ object MachineGeneratedArticleDescriptionsAnalyticsHelper {
             )
         )
     }
+
+    suspend fun isUserExperienced(): Boolean =
+        withContext(Dispatchers.Default) {
+            var totalContributions = 0
+
+            val homeSiteResponse = async {
+                ServiceFactory.get(WikipediaApp.instance.wikiSite).getUserContrib(AccountUtil.userName!!, 10)
+            }
+            val commonsResponse = async {
+                ServiceFactory.get(Constants.commonsWikiSite).getUserContrib(AccountUtil.userName!!, 10)
+            }
+            val wikidataResponse = async {
+                ServiceFactory.get(Constants.wikidataWikiSite).getUserContrib(AccountUtil.userName!!, 10)
+            }
+
+            totalContributions += homeSiteResponse.await().query?.userInfo?.editCount ?: 0
+            totalContributions += commonsResponse.await().query?.userInfo?.editCount ?: 0
+            totalContributions += wikidataResponse.await().query?.userInfo?.editCount ?: 0
+
+            return@withContext totalContributions > ABTest.EXP_CONTRIBUTOR_REQ
+        }
 }
