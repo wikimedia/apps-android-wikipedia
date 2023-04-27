ab1bd2ceeb4c9a9b62319f6de801e3b37d01c789
Clare Ming <cming@wikimedia.org>
2023-04-27 11:15:46 -0600

    Integrate Metrics Platform client library
    Bug: T330355

---
+++ app/src/main/java/org/wikipedia/analytics/metricsplatform/ClientMetadata.kt
@@ -0,0 +1,151 @@
+package org.wikipedia.analytics.metricsplatform
+
+import org.wikimedia.metrics_platform.ClientMetadata
+import org.wikipedia.analytics.eventplatform.EventPlatformClient
+import org.wikipedia.auth.AccountUtil
+import org.wikipedia.page.PageFragmentLoadState
+import org.wikipedia.WikipediaApp
+import org.wikipedia.util.ReleaseUtil
+import java.time.Instant
+
+
+class ClientMetadata : ClientMetadata {
+
+    private lateinit var pageFragmentLoadState: PageFragmentLoadState
+
+    override fun getAgentAppInstallId(): String {
+        return WikipediaApp.instance.appInstallID
+    }
+
+    override fun getAgentClientPlatform(): String {
+        return "mobile app"
+    }
+
+    override fun getAgentClientPlatformFamily(): String {
+        return "android"
+    }
+
+    override fun getMediawikiSkin(): String {
+        return ""
+    }
+
+    override fun getMediawikiVersion(): String {
+        return WikipediaApp.instance.versionCode.toString()
+    }
+
+    override fun getMediawikiIsProduction(): Boolean {
+        return ReleaseUtil.isProdRelease
+    }
+
+    override fun getMediawikiIsDebugMode(): Boolean {
+        return ReleaseUtil.isDevRelease
+    }
+
+    override fun getMediawikiDatabase(): String {
+        return ""// we need to get from page fragment WikipediaApp.instance.wikiSite.dbName()
+    }
+
+    override fun getMediawikiSiteContentLanguage(): String {
+        return ""//WikipediaApp.instance.languageState.appLanguageCode
+    }
+
+    override fun getMediawikiSiteContentLanguageVariant(): String {
+        return ""//WikipediaApp.instance.languageState.appLanguageCodes.component2()
+    }
+
+    override fun getPageId(): Int {
+        return EventPlatformClient.AssociationController.pageViewId.toInt()
+    }
+
+    override fun getPageNamespace(): Int? {
+        return null
+    }
+
+    override fun getPageNamespaceName(): String {
+        return ""
+    }
+
+    override fun getPageTitle(): String {
+        return ""
+    }
+
+    override fun getPageIsRedirect(): Boolean? {
+        return null
+    }
+
+    override fun getPageRevisionId(): Int? {
+        return null
+    }
+
+    override fun getPageWikidataItemQid(): String {
+        return ""
+    }
+
+    override fun getPageContentLanguage(): String {
+        return ""
+    }
+
+    override fun getPageUserGroupsAllowedToEdit(): MutableCollection<String>? {
+        return null
+    }
+
+    override fun getPageUserGroupsAllowedToMove(): MutableCollection<String>? {
+        return null
+    }
+
+    override fun getPerformerId(): Int? {
+        return null
+    }
+
+    override fun getPerformerName(): String? {
+        return AccountUtil.userName
+    }
+
+    override fun getPerformerSessionId(): String {
+        return EventPlatformClient.AssociationController.sessionId
+    }
+
+    override fun getPerformerPageviewId(): String {
+        return ""
+    }
+
+    override fun getPerformerIsLoggedIn(): Boolean {
+        return AccountUtil.isLoggedIn
+    }
+
+    override fun getPerformerGroups(): Set<String> {
+        return AccountUtil.groups
+    }
+
+    override fun getPerformerIsBot(): Boolean? {
+        return null
+    }
+
+    override fun getPerformerCanProbablyEditPage(): Boolean? {
+        return null
+    }
+
+    override fun getPerformerEditCount(): Int? {
+        return null
+    }
+
+    override fun getPerformerEditCountBucket(): String {
+        return ""
+    }
+
+    override fun getPerformerRegistrationDt(): Instant? {
+        return null
+    }
+
+    override fun getPerformerLanguage(): String {
+        return WikipediaApp.instance.appOrSystemLanguageCode
+    }
+
+    override fun getPerformerLanguageVariant(): String {
+        return ""
+    }
+
+    override fun getDomain(): String {
+        return ""
+    }
+}
---
+++ app/src/main/java/org/wikipedia/analytics/metricsplatform/MetricsPlatformClient.kt
@@ -0,0 +1,4 @@
+package org.wikipedia.analytics.metricsplatform
+
+object MetricsPlatformClient {
+}
