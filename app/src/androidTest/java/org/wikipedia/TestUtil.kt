17603539f78d5513be13ceb324a3e951c92552ea
Sharvani Haran <sharvaniharan83@gmail.com>
2023-05-17 14:54:27 -0700

    Clean-up

--- app/src/androidTest/java/org/wikipedia/TestUtil.kt
+++ app/src/androidTest/java/org/wikipedia/TestUtil.kt
@@ -7,12 +7,7 @@ import androidx.annotation.ColorInt
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.UiController
 import androidx.test.espresso.ViewAction
-import androidx.test.espresso.action.CoordinatesProvider
-import androidx.test.espresso.action.GeneralLocation
-import androidx.test.espresso.action.GeneralSwipeAction
-import androidx.test.espresso.action.Press
-import androidx.test.espresso.action.Swipe
-import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.*
 import androidx.test.espresso.matcher.ViewMatchers.isRoot
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.uiautomator.By
