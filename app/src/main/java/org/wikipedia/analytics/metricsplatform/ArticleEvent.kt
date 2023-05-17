9fdfe7422ecb07a90e5b7f4afa70c06570378e8c
Clare Ming <cming@wikimedia.org>
2023-05-17 14:31:15 -0600

    Update MetricsClient object
    - Refrain from passing in performer, mediawiki data in client.
    - Add more dynamic properties to ApplicationData.
    - Remove unneeded methods.

--- app/src/main/java/org/wikipedia/analytics/metricsplatform/ArticleEvent.kt
+++ app/src/main/java/org/wikipedia/analytics/metricsplatform/ArticleEvent.kt
@@ -300,17 +300,7 @@ class ArticleEvent {
     /**
      * Merges custom data with additional client metadata.
      */
-    private fun mergeData(data: Map<String, Any>): Map<String, Any> {
-        return data + ApplicationData.getApplicationData()
+    private fun mergeData(data: Map<String, Any?>): Map<String, Any?> {
+        return data + ApplicationData.data
     }
-
-    /**
-     * Creates a new read-only map by replacing or adding entries to this map from another map.
-     *
-     * The returned map preserves the entry iteration order of the original map.
-     * Those entries of another map that are missing in this map are iterated
-     * in the end in the order of that map.
-     */
-    public operator fun <K, V> Map<out K, V>.plus(map: Map<out K, V>): Map<K, V> =
-        LinkedHashMap(this).apply { putAll(map) }
 }
