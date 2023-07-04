816a0eb0f4c6364d66eb2c00b8ec79f19b49155e
Isira Seneviratne <isirasen96@gmail.com>
2023-07-04 08:59:48 +0530

    Use SpannableStringBuilder extensions

--- app/src/main/java/org/wikipedia/feed/view/CardFooterView.kt
+++ app/src/main/java/org/wikipedia/feed/view/CardFooterView.kt
@@ -1,14 +1,14 @@
 package org.wikipedia.feed.view
 
 import android.content.Context
-import android.text.SpannableStringBuilder
-import android.text.Spanned
 import android.text.style.ImageSpan
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.graphics.drawable.toBitmap
+import androidx.core.text.buildSpannedString
+import androidx.core.text.inSpans
 import org.wikipedia.R
 import org.wikipedia.WikipediaApp
 import org.wikipedia.databinding.ViewCardFooterBinding
@@ -30,19 +30,20 @@ class CardFooterView constructor(context: Context, attrs: AttributeSet? = null) 
     }
 
     fun setFooterActionText(actionText: String, langCode: String?) {
-        val actionTextWithSpace = "$actionText  "
-        val spannableStringBuilder = SpannableStringBuilder(actionTextWithSpace)
         val isRTL = L10nUtil.isLangRTL(langCode ?: WikipediaApp.instance.languageState.systemLanguageCode)
         val iconColor = ResourceUtil.getThemedColor(context, R.attr.progressive_color)
         // TODO: revisit this after the ImageSpan can render drawable instead of converting it to bitmap.
-        val arrowLeftDrawable = AppCompatResources.getDrawable(context, R.drawable.ic_baseline_arrow_left_alt_24px)?.apply {
-            setTint(iconColor)
-        }?.toBitmap()!!
-        val arrowRightDrawable = AppCompatResources.getDrawable(context, R.drawable.ic_baseline_arrow_right_alt_24px)?.apply {
-            setTint(iconColor)
-        }?.toBitmap()!!
-        val arrowImageSpan = ImageSpan(context, if (isRTL) arrowLeftDrawable else arrowRightDrawable)
-        spannableStringBuilder.setSpan(arrowImageSpan, actionTextWithSpace.length - 1, actionTextWithSpace.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
-        binding.footerActionButton.text = spannableStringBuilder
+        val arrowLeftDrawable = AppCompatResources.getDrawable(context, R.drawable.ic_baseline_arrow_left_alt_24px)!!
+            .apply { setTint(iconColor) }
+            .toBitmap()
+        val arrowRightDrawable = AppCompatResources.getDrawable(context, R.drawable.ic_baseline_arrow_right_alt_24px)!!
+            .apply { setTint(iconColor) }
+            .toBitmap()
+        binding.footerActionButton.text = buildSpannedString {
+            append("$actionText ")
+            inSpans(ImageSpan(context, if (isRTL) arrowLeftDrawable else arrowRightDrawable)) {
+                append(" ")
+            }
+        }
     }
 }
