17850ae95b8f5d14f8d61e44348c94c9d7fb9efb
Dmitry Brant <me@dmitrybrant.com>
2023-07-13 08:52:50 -0400

    Update Rest API paths and error handling.

--- app/src/main/java/org/wikipedia/dataclient/restbase/RbServiceError.kt
+++ app/src/main/java/org/wikipedia/dataclient/restbase/RbServiceError.kt
@@ -12,9 +12,18 @@ class RbServiceError : ServiceError {
     private val method: String? = null
     private val uri: String? = null
 
+    private val errorKey: String? = null
+    private val messageTranslations: Map<String, String>? = null
+
     override val title: String = ""
 
-    override val details: String get() = detail.orEmpty()
+    override val details: String get() {
+        if (messageTranslations != null) {
+            return messageTranslations.values.firstOrNull() ?: ""
+        } else {
+            return detail.orEmpty()
+        }
+    }
 
     companion object {
         fun create(rspBody: String): RbServiceError {
