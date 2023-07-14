e0c2866e9b33c6fd4c7ceb72c92d1d0785091c51
Dmitry Brant <me@dmitrybrant.com>
2023-07-14 16:16:29 -0400

    Keep refining algorithm for inserting into infobox, and add tests.

--- app/src/main/java/org/wikipedia/edit/insertmedia/InsertMediaViewModel.kt
+++ app/src/main/java/org/wikipedia/edit/insertmedia/InsertMediaViewModel.kt
@@ -76,14 +76,7 @@ class InsertMediaViewModel(bundle: Bundle) : ViewModel() {
             return
         }
 
-        magicWords[IMAGE_POSITION_NONE] = "none"
-        magicWords[IMAGE_POSITION_CENTER] = "center"
-        magicWords[IMAGE_POSITION_LEFT] = "left"
-        magicWords[IMAGE_POSITION_RIGHT] = "right"
-        magicWords[IMAGE_TYPE_THUMBNAIL] = "thumb"
-        magicWords[IMAGE_TYPE_FRAMELESS] = "frameless"
-        magicWords[IMAGE_TYPE_FRAME] = "frame"
-        magicWords[IMAGE_ALT_TEXT] = "alt=$1"
+        initMagicWords()
 
         viewModelScope.launch(CoroutineExceptionHandler { _, throwable ->
             L.e(throwable)
@@ -126,6 +119,17 @@ class InsertMediaViewModel(bundle: Bundle) : ViewModel() {
         private var magicWordsLang = ""
         private val magicWords = mutableMapOf<String, String>()
 
+        fun initMagicWords() {
+            magicWords[IMAGE_POSITION_NONE] = "none"
+            magicWords[IMAGE_POSITION_CENTER] = "center"
+            magicWords[IMAGE_POSITION_LEFT] = "left"
+            magicWords[IMAGE_POSITION_RIGHT] = "right"
+            magicWords[IMAGE_TYPE_THUMBNAIL] = "thumb"
+            magicWords[IMAGE_TYPE_FRAMELESS] = "frameless"
+            magicWords[IMAGE_TYPE_FRAME] = "frame"
+            magicWords[IMAGE_ALT_TEXT] = "alt=$1"
+        }
+
         fun insertImageIntoWikiText(langCode: String, oldWikiText: String, imageTitle: String, imageCaption: String,
                                     imageAltText: String, imageSize: String, imageType: String, imagePos: String,
                                     cursorPos: Int = 0, attemptInfobox: Boolean = false): String {
@@ -153,7 +157,12 @@ class InsertMediaViewModel(bundle: Bundle) : ViewModel() {
             // But before we resort to inserting the image at the top of the wikitext, let's see if
             // the article has an infobox, and if so, see if we can inject the image right in there.
 
-            var insertAtTop = true
+            var insertedIntoInfobox = false
+
+            // initialize template parameters to the defaults for Infobox
+            var imageParamName = "image"
+            var imageCaptionParamName = "caption"
+            var imageAltParamName = "alt"
 
             var infoboxStartIndex = -1
             var infoboxEndIndex = -1
@@ -172,55 +181,128 @@ class InsertMediaViewModel(bundle: Bundle) : ViewModel() {
                         continue
                     }
 
-                    if (templateName.endsWith("box") || templateName.contains("box ")) {
+                    val firstWord = templateName.split(" ").first().lowercase()
+                    if (firstWord == "infobox" || firstWord == "taxobox" || firstWord == "chembox" ||
+                        firstWord == "drugbox" || firstWord == "speciesbox") {
                         infoboxStartIndex = i
                         infoboxEndIndex = wikiText.indexOf("}}", infoboxStartIndex)
+
+                        when (firstWord) {
+                            "taxobox" -> {
+                                imageCaptionParamName = "image_caption"
+                                imageAltParamName = "image_alt"
+                            }
+                            "speciesbox" -> {
+                                imageCaptionParamName = "image_caption"
+                                imageAltParamName = "image_alt"
+                            }
+                            "chembox" -> {
+                                imageParamName = "ImageFile"
+                                imageCaptionParamName = "ImageCaption"
+                                imageAltParamName = "ImageAlt"
+                            }
+                        }
                     }
                 }
             }
 
             if (attemptInfobox && (infoboxStartIndex in 0 until infoboxEndIndex)) {
-                val regexImage = """\|\s*image(\d+)?\s*(=)\s*(\|)""".toRegex()
-                var match = regexImage.find(wikiText, infoboxStartIndex)
+                var haveNameParam = false
+                var paramInsertPos = -1
+                var paramNameSpaceConvention = ""
 
-                if (match != null && match.groups[3] != null &&
-                    match.groups[3]!!.range.first < infoboxEndIndex) {
-                    insertAtTop = false
-
-                    var insertPos = match.groups[3]!!.range.first
-                    val curImageStr = wikiText.substring(match.groups[2]!!.range.first + 1, match.groups[3]!!.range.first)
-                    if (curImageStr.contains("\n")) {
-                        insertPos = wikiText.indexOf("\n", match.groups[2]!!.range.first)
+                listOf("name", "species", "taxon", "drug_name").forEach { nameParam ->
+                    if (haveNameParam) {
+                        return@forEach
                     }
-
-                    wikiText = wikiText.substring(0, insertPos) + namespaceName + ":" + imageTitle + wikiText.substring(insertPos)
+                    val regexName = """\|(\s*)$nameParam(\d+)?\s*(=)\s*(\|)""".toRegex()
+                    val nameMatch = regexName.find(wikiText, infoboxStartIndex)
+                    if (nameMatch != null && nameMatch.groups[4] != null && nameMatch.groups[4]!!.range.first < infoboxEndIndex) {
+                        haveNameParam = true
+                        paramInsertPos = nameMatch.groups[4]!!.range.first
+                        paramNameSpaceConvention = nameMatch.groups[1]!!.value
+                    }
                 }
 
-                val regexCaption = """\|\s*caption(\d+)?\s*(=)\s*(\|)""".toRegex()
-                match = regexCaption.find(wikiText, infoboxStartIndex)
-                if (match != null && match.groups[3] != null &&
-                    match.groups[3]!!.range.first < infoboxEndIndex) {
+                if (haveNameParam) {
+                    val regexImage = """\|\s*$imageParamName(\d+)?\s*(=)\s*(\|)""".toRegex()
+                    var match = regexImage.find(wikiText, infoboxStartIndex)
 
-                    var insertPos = match.groups[3]!!.range.first
-                    val curImageStr = wikiText.substring(match.groups[2]!!.range.first + 1, match.groups[3]!!.range.first)
-                    if (curImageStr.contains("\n")) {
-                        insertPos = wikiText.indexOf("\n", match.groups[2]!!.range.first)
+                    if (match != null && match.groups[3] != null && match.groups[3]!!.range.first < infoboxEndIndex) {
+                        // an 'image' parameter already exists, so try to insert the image into it,
+                        // provided that it's empty.
+
+                        var insertPos = match.groups[3]!!.range.first
+                        val curImageStr = wikiText.substring(match.groups[2]!!.range.first + 1, match.groups[3]!!.range.first)
+
+                        if (curImageStr.trim().isEmpty()) {
+                            if (curImageStr.contains("\n")) {
+                                insertPos = wikiText.indexOf("\n", match.groups[2]!!.range.first)
+                            }
+                            insertedIntoInfobox = true
+                            wikiText = wikiText.substring(0, insertPos) + namespaceName + ":" + imageTitle + wikiText.substring(insertPos)
+                        } else {
+                            // image parameter already has a value, so do nothing.
+                        }
+                    } else {
+                        // no 'image' parameter exists, so insert one at the end of the 'name' parameter.
+                        insertedIntoInfobox = true
+                        val insertText = "|$paramNameSpaceConvention$imageParamName = $namespaceName:$imageTitle\n"
+                        wikiText = wikiText.substring(0, paramInsertPos) + insertText + wikiText.substring(paramInsertPos)
+                        paramInsertPos += insertText.length
                     }
 
-                    wikiText = wikiText.substring(0, insertPos) + imageCaption + wikiText.substring(insertPos)
-                }
+                    if (insertedIntoInfobox && imageCaption.isNotEmpty()) {
+                        // now try to insert the caption
+                        val regexCaption = """\|\s*$imageCaptionParamName(\d+)?\s*(=)\s*(\|)""".toRegex()
+                        match = regexCaption.find(wikiText, infoboxStartIndex)
+                        if (match != null && match.groups[3] != null && match.groups[3]!!.range.first < infoboxEndIndex) {
+                            // Caption field already exists, so insert into it.
+                            var insertPos = match.groups[3]!!.range.first
+                            val curStr = wikiText.substring(match.groups[2]!!.range.first + 1, match.groups[3]!!.range.first)
+                            if (curStr.trim().isEmpty()) {
+                                if (curStr.contains("\n")) {
+                                    insertPos = wikiText.indexOf("\n", match.groups[2]!!.range.first)
+                                }
+                                wikiText = wikiText.substring(0, insertPos) + imageCaption + wikiText.substring(insertPos)
+                            }
+                        } else {
+                            // insert a new caption field
+                            val insertText = "|$paramNameSpaceConvention$imageCaptionParamName = $imageCaption\n"
+                            wikiText = wikiText.substring(0, paramInsertPos) + insertText + wikiText.substring(paramInsertPos)
+                            paramInsertPos += insertText.length
+                        }
+                    }
 
-                // TODO: insert image alt text.
+                    if (insertedIntoInfobox && imageAltText.isNotEmpty() && imageAltParamName.isNotEmpty()) {
+                        // now try to insert the caption
+                        val regexCaption = """\|\s*$imageAltParamName(\d+)?\s*(=)\s*(\|)""".toRegex()
+                        match = regexCaption.find(wikiText, infoboxStartIndex)
+                        if (match != null && match.groups[3] != null && match.groups[3]!!.range.first < infoboxEndIndex) {
+                            // Caption field already exists, so insert into it.
+                            var insertPos = match.groups[3]!!.range.first
+                            val curStr = wikiText.substring(match.groups[2]!!.range.first + 1, match.groups[3]!!.range.first)
+                            if (curStr.trim().isEmpty()) {
+                                if (curStr.contains("\n")) {
+                                    insertPos = wikiText.indexOf("\n", match.groups[2]!!.range.first)
+                                }
+                                wikiText = wikiText.substring(0, insertPos) + imageCaption + wikiText.substring(insertPos)
+                            }
+                        } else {
+                            // insert a new alt-text field
+                            val insertText = "|$paramNameSpaceConvention$imageAltParamName = $imageAltText\n"
+                            wikiText = wikiText.substring(0, paramInsertPos) + insertText + wikiText.substring(paramInsertPos)
+                            paramInsertPos += insertText.length
+                        }
+                    }
+                }
             }
 
-            if (insertAtTop) {
+            if (!insertedIntoInfobox) {
                 val pos = cursorPos.coerceIn(0, wikiText.length)
                 wikiText = wikiText.substring(0, pos) + template + "\n" + wikiText.substring(pos)
             }
 
-            // Save the new wikitext to the article.
-
-            L.d(">>> $wikiText")
             return wikiText
         }
     }
---
+++ app/src/test/java/org/wikipedia/edit/insertmedia/InsertMediaTest.kt
@@ -0,0 +1,97 @@
+package org.wikipedia.edit.insertmedia
+
+import org.hamcrest.MatcherAssert
+import org.hamcrest.Matchers
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class InsertMediaTest {
+    @Before
+    fun setUp() {
+        InsertMediaViewModel.initMagicWords()
+    }
+
+    @Test
+    fun testInsertImageIntoArticleWithoutInfobox() {
+        val wikitext = "'''Gabrielle de Bourbon''' or '''Gabrielle de Bourbon-Montpensier''' " +
+                "(c.[[1447]]–30 November [[1516]]), princess of [[Talmont-Saint-Hilaire|Talmont]], " +
+                "was a French [[author]] and daughter of the [[House of Bourbon]].\n" +
+                "\n== Biography ==\nShe was the oldest daughter of [[Louis I, Count of Montpensier]] " +
+                "and [[Gabrielle de La Tour d'Auvergne]].<ref name=\":0\">{{Cite web |title=Gabrielle " +
+                "de Bourbon-Montpensier — SiefarWikiFr |url=http://siefar.org/dictionnaire/fr/Gabrielle_de_Bourbon-Montpensier" +
+                " |access-date=2021-07-21 |website=siefar.org}}</ref>\n"
+
+        val expected = "[[File:Test_image.jpg|thumb|right|alt=Bar|Foo]]\n'''Gabrielle de Bourbon''' or '''Gabrielle de Bourbon-Montpensier''' " +
+                "(c.[[1447]]–30 November [[1516]]), princess of [[Talmont-Saint-Hilaire|Talmont]], " +
+                "was a French [[author]] and daughter of the [[House of Bourbon]].\n" +
+                "\n== Biography ==\nShe was the oldest daughter of [[Louis I, Count of Montpensier]] " +
+                "and [[Gabrielle de La Tour d'Auvergne]].<ref name=\":0\">{{Cite web |title=Gabrielle " +
+                "de Bourbon-Montpensier — SiefarWikiFr |url=http://siefar.org/dictionnaire/fr/Gabrielle_de_Bourbon-Montpensier" +
+                " |access-date=2021-07-21 |website=siefar.org}}</ref>\n"
+
+        MatcherAssert.assertThat(InsertMediaViewModel.insertImageIntoWikiText("en", wikitext, "Test_image.jpg", "Foo",
+            "Bar", InsertMediaViewModel.IMAGE_SIZE_DEFAULT, InsertMediaViewModel.IMAGE_TYPE_THUMBNAIL, InsertMediaViewModel.IMAGE_POSITION_RIGHT,
+            0, true), Matchers.`is`(expected))
+    }
+
+    @Test
+    fun testInsertImageIntoArticleWithInfoboxEmptyImageParam() {
+        val wikitext = "{{Short description|Genus of plants}}\n" +
+                "{{Italic title}}\n" +
+                "{{taxobox\n" +
+                "|name = \n" +
+                "|image = \n" +
+                "|image_caption = \n" +
+                "|regnum = [[Plantae]]\n" +
+                "|unranked_divisio = [[Angiosperms]]\n" +
+                "|unranked_classis = [[Eudicots]]\n" +
+                "|unranked_ordo = [[Asterids]]\n" +
+                "|ordo = [[Gentianales]]\n" +
+                "|familia = [[Apocynaceae]]\n" +
+                "|subfamilia = [[Asclepiadoideae]]\n" +
+                "|genus = '''''Calostigma'''''\n" +
+                "|genus_authority = [[Decne.]] 1838 not Schott 1832, the latter name published " +
+                "without description<ref name=r>[http://www.ipni.org/ipni/idPlantNameSearch.do?id=86526-1 " +
+                "International Plant Names Index, Calostigma imbe Schott]</ref>\n" +
+                "|synonyms_ref=\n" +
+                "|synonyms=\n" +
+                "}}\n\n" +
+                "'''''Calostigma''''' is a genus of flowering plants in the family [[Apocynaceae]], " +
+                "first described as a genus in 1838.<ref>Decaisne, Joseph. 1838.  Annales des Sciences " +
+                "Naturelles; Botanique, sér. 2 9: 343–4, t. 12H</ref><ref>[http://www.tropicos.org/Name/40035627 " +
+                "Tropicos, genus ''Calostigma'' Decne.]</ref> The genus is native to [[South America]]."
+
+        val expected = "{{Short description|Genus of plants}}\n" +
+                "{{Italic title}}\n" +
+                "{{taxobox\n" +
+                "|name = \n" +
+                "|image_alt = Bar\n" +
+                "|image = File:Test_image.jpg\n" +
+                "|image_caption = Foo\n" +
+                "|regnum = [[Plantae]]\n" +
+                "|unranked_divisio = [[Angiosperms]]\n" +
+                "|unranked_classis = [[Eudicots]]\n" +
+                "|unranked_ordo = [[Asterids]]\n" +
+                "|ordo = [[Gentianales]]\n" +
+                "|familia = [[Apocynaceae]]\n" +
+                "|subfamilia = [[Asclepiadoideae]]\n" +
+                "|genus = '''''Calostigma'''''\n" +
+                "|genus_authority = [[Decne.]] 1838 not Schott 1832, the latter name published " +
+                "without description<ref name=r>[http://www.ipni.org/ipni/idPlantNameSearch.do?id=86526-1 " +
+                "International Plant Names Index, Calostigma imbe Schott]</ref>\n" +
+                "|synonyms_ref=\n" +
+                "|synonyms=\n" +
+                "}}\n\n" +
+                "'''''Calostigma''''' is a genus of flowering plants in the family [[Apocynaceae]], " +
+                "first described as a genus in 1838.<ref>Decaisne, Joseph. 1838.  Annales des Sciences " +
+                "Naturelles; Botanique, sér. 2 9: 343–4, t. 12H</ref><ref>[http://www.tropicos.org/Name/40035627 " +
+                "Tropicos, genus ''Calostigma'' Decne.]</ref> The genus is native to [[South America]]."
+
+        MatcherAssert.assertThat(InsertMediaViewModel.insertImageIntoWikiText("en", wikitext, "Test_image.jpg", "Foo",
+            "Bar", InsertMediaViewModel.IMAGE_SIZE_DEFAULT, InsertMediaViewModel.IMAGE_TYPE_THUMBNAIL, InsertMediaViewModel.IMAGE_POSITION_RIGHT,
+            0, true), Matchers.`is`(expected))
+    }
+}
