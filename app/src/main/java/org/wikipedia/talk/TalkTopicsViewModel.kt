3edfe1d2118113a1312912c206772e0a69ca6e78
Cooltey Feng <cfeng@wikimedia.org>
2023-08-03 08:57:04 -0700

    Change to regular equals

--- app/src/main/java/org/wikipedia/talk/TalkTopicsViewModel.kt
+++ app/src/main/java/org/wikipedia/talk/TalkTopicsViewModel.kt
@@ -68,7 +68,7 @@ class TalkTopicsViewModel(var pageTitle: PageTitle, private val sidePanel: Boole
             pageTitle.namespace = TalkAliasData.valueFor(pageTitle.wikiSite.languageCode)
         } else if (pageTitle.isUserPage) {
             // Make sure to remove HTML tags and duplicated namespace if it is a user page.
-            if (pageTitle.namespace() === Namespace.USER) {
+            if (pageTitle.namespace() == Namespace.USER) {
                 pageTitle.displayText = StringUtil.removeNamespace(StringUtil.removeHTMLTags(pageTitle.displayText))
             }
             pageTitle.namespace = UserTalkAliasData.valueFor(pageTitle.wikiSite.languageCode)
