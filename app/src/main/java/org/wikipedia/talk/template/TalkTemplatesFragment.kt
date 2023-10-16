ee4c6118eeb71397c4f7d3a597cd64da158ff97b
cooltey <coolteygame@gmail.com>
2023-10-16 12:14:23 -0700

    Update dialog text

--- app/src/main/java/org/wikipedia/talk/template/TalkTemplatesFragment.kt
+++ app/src/main/java/org/wikipedia/talk/template/TalkTemplatesFragment.kt
@@ -195,7 +195,7 @@ class TalkTemplatesFragment : Fragment(), MenuProvider {
 
                 override fun onCancel() {
                     MaterialAlertDialogBuilder(requireContext(), R.style.AlertDialogTheme_Delete)
-                        .setMessage(getString(R.string.talk_templates_edit_message_confirm_description, talkTemplate.title))
+                        .setMessage(getString(R.string.talk_templates_edit_message_delete_description, talkTemplate.title))
                         .setPositiveButton(R.string.talk_templates_edit_message_dialog_delete) { _, _ -> viewModel.deleteTemplate(talkTemplate) }
                         .setNegativeButton(R.string.talk_templates_new_message_dialog_cancel, null)
                         .show()
