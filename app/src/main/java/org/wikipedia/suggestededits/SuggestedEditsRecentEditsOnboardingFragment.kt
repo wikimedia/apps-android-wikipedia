6defe974213c0258eed5810bed8ac8736a52278b
cooltey <coolteygame@gmail.com>
2023-10-16 14:09:24 -0700

    Add data preference screen

--- app/src/main/java/org/wikipedia/suggestededits/SuggestedEditsRecentEditsOnboardingFragment.kt
+++ app/src/main/java/org/wikipedia/suggestededits/SuggestedEditsRecentEditsOnboardingFragment.kt
@@ -10,6 +10,7 @@ import androidx.viewpager2.adapter.FragmentStateAdapter
 import org.wikipedia.R
 import org.wikipedia.onboarding.OnboardingFragment
 import org.wikipedia.onboarding.OnboardingPageView
+import org.wikipedia.settings.Prefs
 
 class SuggestedEditsRecentEditsOnboardingFragment : OnboardingFragment() {
     override val doneButtonText = R.string.onboarding_get_started
@@ -21,7 +22,7 @@ class SuggestedEditsRecentEditsOnboardingFragment : OnboardingFragment() {
 
     internal inner class DescriptionEditTutorialPagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
         override fun getItemCount(): Int {
-            return pages.size
+            return if (!Prefs.isEventLoggingEnabled) pages.size else pages.size - 1
         }
 
         override fun createFragment(position: Int): Fragment {
@@ -41,7 +42,11 @@ class SuggestedEditsRecentEditsOnboardingFragment : OnboardingFragment() {
 
     companion object {
         const val ARG_POSITION = "position"
-        val pages = arrayOf(R.layout.inflate_patroller_tasks_onboarding_page_one, R.layout.inflate_patroller_tasks_onboarding_page_two)
+        val pages = arrayOf(
+            R.layout.inflate_patroller_tasks_onboarding_page_one,
+            R.layout.inflate_patroller_tasks_onboarding_page_two,
+            R.layout.inflate_initial_onboarding_page_three
+        )
         fun newInstance() = SuggestedEditsRecentEditsOnboardingFragment()
     }
 }
