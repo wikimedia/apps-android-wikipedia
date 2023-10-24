e9957090214caf75c5a3ac9793be3a38269a2e40
cooltey <coolteygame@gmail.com>
2023-10-24 15:14:15 -0700

    lint

--- app/src/main/java/org/wikipedia/suggestededits/SuggestedEditsTasksFragment.kt
+++ app/src/main/java/org/wikipedia/suggestededits/SuggestedEditsTasksFragment.kt
@@ -38,7 +38,6 @@ import org.wikipedia.login.LoginActivity
 import org.wikipedia.main.MainActivity
 import org.wikipedia.settings.Prefs
 import org.wikipedia.settings.languages.WikipediaLanguagesActivity
-import org.wikipedia.talk.template.TalkTemplatesActivity
 import org.wikipedia.usercontrib.UserContribListActivity
 import org.wikipedia.usercontrib.UserContribStats
 import org.wikipedia.util.DateUtil
