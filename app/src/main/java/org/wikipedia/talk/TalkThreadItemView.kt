1e818293514a1771960f422519bc2c60675143a7
cooltey <coolteygame@gmail.com>
2023-11-07 19:54:38 -0800

    Add parameter back

--- app/src/main/java/org/wikipedia/talk/TalkThreadItemView.kt
+++ app/src/main/java/org/wikipedia/talk/TalkThreadItemView.kt
@@ -74,7 +74,7 @@ class TalkThreadItemView constructor(context: Context, attrs: AttributeSet? = nu
             val timestamp = DateUtil.getTimeAndDateString(context, it)
             StringUtil.setHighlightedAndBoldenedText(binding.timeStampText, timestamp, searchQuery)
         }
-        val body = CustomHtmlParser.fromHtml(StringUtil.removeStyleTags(item.html), binding.bodyText).trim()
+        val body = CustomHtmlParser.fromHtml(StringUtil.removeStyleTags(item.html), binding.bodyText, hasMinImageSize = false).trim()
         StringUtil.setHighlightedAndBoldenedText(binding.bodyText, body, searchQuery)
         binding.bodyText.movementMethod = movementMethod
 
