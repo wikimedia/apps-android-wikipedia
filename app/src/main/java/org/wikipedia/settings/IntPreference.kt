43dcddd400c178e92a8a88b2cb88fda60c5f1aae
cooltey <coolteygame@gmail.com>
2024-01-04 17:49:17 -0800

    Not update preference related classes

--- app/src/main/java/org/wikipedia/settings/IntPreference.kt
+++ app/src/main/java/org/wikipedia/settings/IntPreference.kt
@@ -4,12 +4,11 @@ import android.content.Context
 import android.util.AttributeSet
 import org.wikipedia.R
 
-open class IntPreference(
-    context: Context,
-    attrs: AttributeSet? = null,
-    defStyleAttr: Int = R.attr.intPreferenceStyle,
-    defStyleRes: Int = R.style.IntPreference
-) : LongPreference(context, attrs, defStyleAttr, defStyleRes) {
+open class IntPreference @JvmOverloads constructor(context: Context,
+                                                   attrs: AttributeSet? = null,
+                                                   defStyleAttr: Int = R.attr.intPreferenceStyle,
+                                                   defStyleRes: Int = R.style.IntPreference) :
+        LongPreference(context, attrs, defStyleAttr, defStyleRes) {
 
     override fun getPersistedString(defaultRadixValue: String?): String {
         return intToSummary(getPersistedInt(radixStringToInt(defaultRadixValue)))
