49b8aa005897f04338de07a780246cac307b7dfd
cooltey <coolteygame@gmail.com>
2024-01-11 15:20:34 -0800

    Add adapter and viewholder

--- app/src/main/java/org/wikipedia/places/PlacesFragment.kt
+++ app/src/main/java/org/wikipedia/places/PlacesFragment.kt
@@ -30,6 +30,8 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.CustomTarget
 import com.bumptech.glide.request.transition.Transition
@@ -59,8 +61,12 @@ import org.wikipedia.Constants
 import org.wikipedia.R
 import org.wikipedia.WikipediaApp
 import org.wikipedia.databinding.FragmentPlacesBinding
+import org.wikipedia.databinding.ItemNotificationBinding
+import org.wikipedia.databinding.ItemPlacesListBinding
 import org.wikipedia.dataclient.okhttp.OkHttpConnectionFactory
 import org.wikipedia.history.HistoryEntry
+import org.wikipedia.notifications.NotificationActivity
+import org.wikipedia.notifications.NotificationListItemContainer
 import org.wikipedia.page.ExclusiveBottomSheetPresenter
 import org.wikipedia.page.PageActivity
 import org.wikipedia.page.PageTitle
@@ -68,12 +74,14 @@ import org.wikipedia.page.linkpreview.LinkPreviewDialog
 import org.wikipedia.page.tabs.TabActivity
 import org.wikipedia.search.SearchFragment
 import org.wikipedia.settings.Prefs
+import org.wikipedia.util.DeviceUtil
 import org.wikipedia.util.DimenUtil
 import org.wikipedia.util.FeedbackUtil
 import org.wikipedia.util.L10nUtil
 import org.wikipedia.util.Resource
 import org.wikipedia.util.ResourceUtil
 import org.wikipedia.util.log.L
+import org.wikipedia.views.DrawableItemDecoration
 import org.wikipedia.views.ViewUtil
 import kotlin.math.abs
 
@@ -186,6 +194,9 @@ class PlacesFragment : Fragment(), MapboxMap.OnMapClickListener {
             buttonView.setTextColor(ResourceUtil.getThemedColorStateList(requireContext(), textColor))
             buttonView.backgroundTintList = ResourceUtil.getThemedColorStateList(requireContext(), backgroundColor)
         }
+
+        binding.listRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        binding.listRecyclerView.addItemDecoration(DrawableItemDecoration(requireContext(), R.attr.list_divider, skipSearchBar = true))
 
         return binding.root
     }
@@ -411,6 +422,8 @@ class PlacesFragment : Fragment(), MapboxMap.OnMapClickListener {
                 }
             }
         }
+
+        binding.listRecyclerView.adapter = RecyclerViewAdapter(pages)
     }
 
     private fun haveLocationPermissions(): Boolean {
@@ -516,6 +529,44 @@ class PlacesFragment : Fragment(), MapboxMap.OnMapClickListener {
             }
         }
         return false
+    }
+
+    private inner class RecyclerViewAdapter(val list: List<PlacesFragmentViewModel.NearbyPage>) : RecyclerView.Adapter<RecyclerViewItemHolder>() {
+        override fun getItemCount(): Int {
+            return list.size
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, type: Int): RecyclerViewItemHolder {
+            return RecyclerViewItemHolder(ItemPlacesListBinding.inflate(layoutInflater, parent, false))
+        }
+
+        override fun onBindViewHolder(holder: RecyclerViewItemHolder, position: Int) {
+            holder.bindItem(list[position], position)
+        }
+    }
+
+    private inner class RecyclerViewItemHolder constructor(val binding: ItemPlacesListBinding) :
+        RecyclerView.ViewHolder(binding.root), View.OnClickListener, View.OnLongClickListener {
+
+        init {
+            itemView.setOnClickListener(this)
+            itemView.setOnLongClickListener(this)
+            DeviceUtil.setContextClickAsLongClick(itemView)
+        }
+
+        fun bindItem(item: PlacesFragmentViewModel.NearbyPage, position: Int) {
+            // TODO: implement this
+        }
+
+
+        override fun onClick(v: View) {
+            // TODO: implement this
+        }
+
+        override fun onLongClick(v: View): Boolean {
+            // TODO: implement this)
+            return true
+        }
     }
 
     companion object {
