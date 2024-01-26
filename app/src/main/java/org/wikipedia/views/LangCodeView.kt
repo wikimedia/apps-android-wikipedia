209948ff18b6e82c664aad2d90546ef423d90822
cooltey <coolteygame@gmail.com>
2024-01-25 17:17:51 -0800

    Update language scroll view

--- app/src/main/java/org/wikipedia/views/LangCodeView.kt
+++ app/src/main/java/org/wikipedia/views/LangCodeView.kt
@@ -1,12 +1,17 @@
 package org.wikipedia.views
 
 import android.content.Context
+import android.content.res.ColorStateList
 import android.util.AttributeSet
 import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.FrameLayout
+import androidx.annotation.ColorInt
+import androidx.core.view.ViewCompat
+import org.wikipedia.R
 import org.wikipedia.databinding.ViewLangCodeBinding
+import org.wikipedia.language.LanguageUtil
 import org.wikipedia.util.DimenUtil
 import org.wikipedia.util.ResourceUtil
 
@@ -21,12 +26,20 @@ class LangCodeView(context: Context, attrs: AttributeSet? = null) : FrameLayout(
     }
 
     fun setLangCode(langCode: String) {
-        binding.langCodeText.text = langCode.uppercase()
+        binding.langCodeText.text = LanguageUtil.formatLangCodeForButton(langCode.uppercase())
         val textSize = if (langCode.length > 2) 5f else 10f
-        if (langCode.contains("-") && langCode.length > 3) {
-            val newLangCode = langCode.uppercase().replace("-", "-\n")
-            binding.langCodeText.text = newLangCode
-        }
         binding.langCodeText.setTextSize(TypedValue.COMPLEX_UNIT_SP, textSize)
+    }
+
+    fun setTextColor(@ColorInt textColor: Int) {
+        binding.langCodeText.setTextColor(textColor)
+    }
+
+    fun fillBackground(fillBackground: Boolean) {
+        binding.langCodeText.setBackgroundResource(if (fillBackground) R.drawable.tab_counts_shape_border_filled else R.drawable.tab_counts_shape_border)
+    }
+
+    fun setBackgroundTint(@ColorInt tintColor: Int) {
+        ViewCompat.setBackgroundTintList(binding.langCodeText, ColorStateList.valueOf(tintColor))
     }
 }
