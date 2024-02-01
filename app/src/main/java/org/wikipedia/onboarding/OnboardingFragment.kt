72f9263c2c7694a8f324afca7beeffad4cab9e3d
Sharvani Haran <sharvaniharan83@gmail.com>
2024-01-31 18:05:27 -0800

    Lint fix

--- app/src/main/java/org/wikipedia/onboarding/OnboardingFragment.kt
+++ app/src/main/java/org/wikipedia/onboarding/OnboardingFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.StringRes
-import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
