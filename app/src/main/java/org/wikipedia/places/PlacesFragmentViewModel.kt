ef8de7c8d60cc54e482a15c42729b3db095c3c29
Cooltey Feng <cfeng@wikimedia.org>
2024-02-07 22:54:08 +0800

    Fix lint

--- app/src/main/java/org/wikipedia/places/PlacesFragmentViewModel.kt
+++ app/src/main/java/org/wikipedia/places/PlacesFragmentViewModel.kt
@@ -40,9 +40,11 @@ class PlacesFragmentViewModel(bundle: Bundle) : ViewModel() {
                         if (it.thumbUrl().isNullOrEmpty()) null else ImageUrlUtil.getUrlForPreferredSize(it.thumbUrl()!!, PlacesFragment.THUMB_SIZE),
                         it.description, it.displayTitle(wikiSite.languageCode)), it.coordinates!![0].lat, it.coordinates[0].lon)
                 }
-                .sortedBy { lastKnownLocation?.run {
-                    it.location.distanceTo(this)
-                }}
+                .sortedBy {
+                    lastKnownLocation?.run {
+                        it.location.distanceTo(this)
+                    }
+                }
             nearbyPagesLiveData.postValue(Resource.Success(pages))
         }
     }
