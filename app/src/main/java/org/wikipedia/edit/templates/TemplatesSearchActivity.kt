89760d908e62cd6557f575d82e0f9d827d9c769b
cooltey <coolteygame@gmail.com>
2024-03-11 16:39:19 -0700

    Fix lint

--- app/src/main/java/org/wikipedia/edit/templates/TemplatesSearchActivity.kt
+++ app/src/main/java/org/wikipedia/edit/templates/TemplatesSearchActivity.kt
@@ -29,7 +29,6 @@ import org.wikipedia.page.PageTitle
 import org.wikipedia.util.DeviceUtil
 import org.wikipedia.util.ResourceUtil
 import org.wikipedia.util.StringUtil
-import org.wikipedia.util.log.L
 
 class TemplatesSearchActivity : BaseActivity() {
     private lateinit var binding: ActivityTemplatesSearchBinding
