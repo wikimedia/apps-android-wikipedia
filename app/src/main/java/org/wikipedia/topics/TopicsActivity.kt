78fcf7a05dbb3b763090d6996dde94fe9cdae705
Dmitry Brant <dbrant@wikimedia.org>
2024-04-09 18:33:35 -0400

    Use chips.

--- app/src/main/java/org/wikipedia/topics/TopicsActivity.kt
+++ app/src/main/java/org/wikipedia/topics/TopicsActivity.kt
@@ -1,12 +1,7 @@
 package org.wikipedia.topics
 
-import android.content.Context
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import android.util.TypedValue
 import com.google.android.material.chip.Chip
 import org.wikipedia.R
 import org.wikipedia.activity.BaseActivity
@@ -16,36 +11,25 @@ import org.wikipedia.settings.Prefs
 class TopicsActivity : BaseActivity() {
     private lateinit var binding: ActivityTopicsBinding
 
+    private lateinit var allTopics: Array<String>
+    private var topics = Prefs.selectedTopics.toMutableSet()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityTopicsBinding.inflate(layoutInflater)
-        binding.topicsListRecycler.layoutManager = GridLayoutManager(this, 2)
-        binding.topicsListRecycler.adapter = CustomAdapter(this)
         setContentView(binding.root)
-    }
 
-    class CustomAdapter(val context: Context) : RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
-        val dataSet: Array<String> = context.resources.getStringArray(R.array.topics)
-        var topics = Prefs.selectedTopics.toMutableSet()
+        allTopics = resources.getStringArray(R.array.topics)
 
-        class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-            val chip: Chip = view.findViewById(R.id.topicName)
-        }
-
-        override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
-            val view =
-                LayoutInflater.from(viewGroup.context).inflate(R.layout.view_topic_item, viewGroup, false)
-            return ViewHolder(view)
-        }
-
-        override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-            viewHolder.chip.text = dataSet[position]
-            if (topics.contains(dataSet[position])) {
-                viewHolder.chip.isChecked = true
-            } else {
-                viewHolder.chip.isChecked = false
-            }
-            viewHolder.chip.setOnClickListener {
+        allTopics.forEach {
+            val chip = Chip(this)
+            chip.text = it
+            chip.isCheckable = true
+            chip.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20f)
+            chip.setCheckedIconResource(R.drawable.checked)
+            chip.isCheckedIconVisible = true
+            binding.chipGroup.addView(chip)
+            chip.setOnClickListener {
                 val topic = (it as Chip).text.toString()
                 if (topics.contains(topic)) {
                     topics.remove(topic)
@@ -53,10 +37,7 @@ class TopicsActivity : BaseActivity() {
                     topics.add(topic)
                 }
                 Prefs.selectedTopics = topics
-                notifyDataSetChanged()
             }
         }
-
-        override fun getItemCount() = dataSet.size
     }
 }
