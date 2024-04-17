22eb71c6d7f52334854bce8ab0e4a2be08cf028b
cooltey <coolteygame@gmail.com>
2024-04-17 15:18:47 -0700

    Remove withContext

--- app/src/main/java/org/wikipedia/feed/aggregated/AggregatedFeedContentClient.kt
+++ app/src/main/java/org/wikipedia/feed/aggregated/AggregatedFeedContentClient.kt
@@ -146,36 +146,34 @@ class AggregatedFeedContentClient {
                     cb.error(caught)
                 }
             ) {
-                withContext(Dispatchers.IO) {
-                    val cards = mutableListOf<Card>()
-                    FeedContentType.aggregatedLanguages.forEach { langCode ->
-                        val wikiSite = WikiSite.forLanguageCode(langCode)
-                        val hasParentLanguageCode = !WikipediaApp.instance.languageState.getDefaultLanguageCode(langCode).isNullOrEmpty()
-                        var feedContentResponse = ServiceFactory.getRest(wikiSite).getFeedFeatured(date.year, date.month, date.day)
+                val cards = mutableListOf<Card>()
+                FeedContentType.aggregatedLanguages.forEach { langCode ->
+                    val wikiSite = WikiSite.forLanguageCode(langCode)
+                    val hasParentLanguageCode = !WikipediaApp.instance.languageState.getDefaultLanguageCode(langCode).isNullOrEmpty()
+                    var feedContentResponse = ServiceFactory.getRest(wikiSite).getFeedFeatured(date.year, date.month, date.day)
 
-                        // TODO: This is a temporary fix for T355192
-                        if (hasParentLanguageCode) {
-                            // TODO: Needs to update tfa and most read
-                            feedContentResponse.tfa?.let {
-                                val tfaResponse = getPageSummaryForLanguageVariant(it, wikiSite)
-                                feedContentResponse = AggregatedFeedContent(
-                                    tfa = tfaResponse,
-                                    news = feedContentResponse.news,
-                                    topRead = feedContentResponse.topRead,
-                                    potd = feedContentResponse.potd,
-                                    onthisday = feedContentResponse.onthisday
-                                )
-                            }
+                    // TODO: This is a temporary fix for T355192
+                    if (hasParentLanguageCode) {
+                        // TODO: Needs to update tfa and most read
+                        feedContentResponse.tfa?.let {
+                            val tfaResponse = getPageSummaryForLanguageVariant(it, wikiSite)
+                            feedContentResponse = AggregatedFeedContent(
+                                tfa = tfaResponse,
+                                news = feedContentResponse.news,
+                                topRead = feedContentResponse.topRead,
+                                potd = feedContentResponse.potd,
+                                onthisday = feedContentResponse.onthisday
+                            )
                         }
+                    }
 
-                        aggregatedClient.aggregatedResponses[langCode] = feedContentResponse
-                        aggregatedClient.aggregatedResponseAge = age
-                    }
-                    if (aggregatedClient.aggregatedResponses.containsKey(wiki.languageCode)) {
-                        getCardFromResponse(aggregatedClient.aggregatedResponses, wiki, age, cards)
-                    }
-                    FeedCoordinator.postCardsToCallback(cb, cards)
+                    aggregatedClient.aggregatedResponses[langCode] = feedContentResponse
+                    aggregatedClient.aggregatedResponseAge = age
                 }
+                if (aggregatedClient.aggregatedResponses.containsKey(wiki.languageCode)) {
+                    getCardFromResponse(aggregatedClient.aggregatedResponses, wiki, age, cards)
+                }
+                FeedCoordinator.postCardsToCallback(cb, cards)
             }
         }
 
