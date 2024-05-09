c4c09c22c875996c91eff67723d575b40b526a5c
cooltey <coolteygame@gmail.com>
2024-05-08 17:08:22 -0700

    Convert SearchActionModeCallback to Kotlin

---
+++ app/src/main/java/org/wikipedia/history/SearchActionModeCallback.kt
@@ -0,0 +1,45 @@
+package org.wikipedia.history
+
+import android.content.Context
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.view.ActionMode
+import androidx.core.view.MenuItemCompat
+import org.wikipedia.views.SearchActionProvider
+
+abstract class SearchActionModeCallback : ActionMode.Callback {
+    override fun onCreateActionMode(mode: ActionMode, menu: Menu): Boolean {
+        mode.tag = ACTION_MODE_TAG
+        val menuItem = menu.add(getSearchHintString())
+        // Manually setup a action provider to be able to adjust the left margin of the search field.
+        MenuItemCompat.setActionProvider(
+            menuItem,
+            SearchActionProvider(getParentContext(), getSearchHintString()) { onQueryChange(it) }
+        )
+
+        return true
+    }
+
+    protected abstract fun getSearchHintString(): String
+    protected abstract fun onQueryChange(s: String)
+    protected abstract fun getParentContext(): Context
+
+    override fun onPrepareActionMode(mode: ActionMode, menu: Menu): Boolean {
+        return true
+    }
+
+    override fun onActionItemClicked(mode: ActionMode, menuItem: MenuItem): Boolean {
+        return false
+    }
+
+    override fun onDestroyActionMode(mode: ActionMode) {
+    }
+
+    companion object {
+        const val ACTION_MODE_TAG: String = "searchActionMode"
+
+        fun matches(mode: ActionMode?): Boolean {
+            return mode != null && ACTION_MODE_TAG == mode.tag
+        }
+    }
+}
--- app/src/main/java/org/wikipedia/language/LangLinksActivity.kt
+++ app/src/main/java/org/wikipedia/language/LangLinksActivity.kt
@@ -2,7 +2,11 @@ package org.wikipedia.language
 
 import android.content.Context
 import android.os.Bundle
-import android.view.*
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.view.ActionMode
