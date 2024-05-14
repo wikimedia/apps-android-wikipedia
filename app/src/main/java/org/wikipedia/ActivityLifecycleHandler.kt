3fd077e527a07b655e92db52d0fbab514f07d0ac
Dmitry Brant <me@dmitrybrant.com>
2024-05-14 10:55:31 -0400

    Improve integration with Metrics Platform library lifecycle.

--- app/src/main/java/org/wikipedia/ActivityLifecycleHandler.kt
+++ app/src/main/java/org/wikipedia/ActivityLifecycleHandler.kt
@@ -7,6 +7,7 @@ import android.content.res.Configuration
 import android.os.Build
 import android.os.Bundle
 import org.wikipedia.analytics.eventplatform.EventPlatformClient
+import org.wikipedia.analytics.metricsplatform.MetricsPlatform
 import org.wikipedia.main.MainActivity
 import org.wikipedia.settings.Prefs
 import org.wikipedia.theme.Theme
@@ -46,10 +47,13 @@ class ActivityLifecycleHandler : ActivityLifecycleCallbacks, ComponentCallbacks2
 
     override fun onActivityResumed(activity: Activity) {
         isAnyActivityResumed = true
+        MetricsPlatform.client.onAppResume()
     }
 
     override fun onActivityPaused(activity: Activity) {
         isAnyActivityResumed = false
+        MetricsPlatform.client.onAppPause()
+        EventPlatformClient.flushCachedEvents()
     }
 
     override fun onActivityStopped(activity: Activity) {}
@@ -60,15 +64,15 @@ class ActivityLifecycleHandler : ActivityLifecycleCallbacks, ComponentCallbacks2
         if (activity is MainActivity) {
             haveMainActivity = false
         }
+        if (activity.isTaskRoot) {
+            MetricsPlatform.client.onAppClose()
+            EventPlatformClient.flushCachedEvents()
+        }
     }
 
     override fun onConfigurationChanged(configuration: Configuration) {}
 
     override fun onLowMemory() {}
 
-    override fun onTrimMemory(trimMemoryType: Int) {
-        if (trimMemoryType == ComponentCallbacks2.TRIM_MEMORY_UI_HIDDEN) {
-            EventPlatformClient.flushCachedEvents()
-        }
-    }
+    override fun onTrimMemory(trimMemoryType: Int) {}
 }
