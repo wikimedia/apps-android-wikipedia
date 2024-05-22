f362715f924c5c757b0f4a0b04ea9206551310ae
cooltey <coolteygame@gmail.com>
2024-05-22 10:17:36 -0700

    Add `withContext()` back

--- app/src/main/java/org/wikipedia/page/PageAvailableOfflineHandler.kt
+++ app/src/main/java/org/wikipedia/page/PageAvailableOfflineHandler.kt
@@ -2,7 +2,9 @@ package org.wikipedia.page
 
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.wikipedia.WikipediaApp
 import org.wikipedia.database.AppDatabase
 import org.wikipedia.readinglist.database.ReadingListPage
@@ -26,8 +28,10 @@ object PageAvailableOfflineHandler {
             callback.onFinish(false)
             L.w(exception)
         }) {
-            val readingListPage = AppDatabase.instance.readingListPageDao().findPageInAnyList(pageTitle)
-            callback.onFinish(readingListPage != null && readingListPage.offline && !readingListPage.saving)
+            withContext(Dispatchers.IO) {
+                val readingListPage = AppDatabase.instance.readingListPageDao().findPageInAnyList(pageTitle)
+                callback.onFinish(readingListPage != null && readingListPage.offline && !readingListPage.saving)
+            }
         }
     }
 }
