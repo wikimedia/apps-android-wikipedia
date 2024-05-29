f18881ebe0404958bacc7fa470c8eb091ba14372
Dmitry Brant <me@dmitrybrant.com>
2024-05-29 09:31:42 -0400

    Add optional parameter.

--- app/src/main/java/org/wikipedia/extensions/View.kt
+++ app/src/main/java/org/wikipedia/extensions/View.kt
@@ -5,7 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlin.coroutines.CoroutineContext
 
-fun View.coroutineScope(): CoroutineScope {
-    return (context as? AppCompatActivity)?.lifecycleScope ?: CoroutineScope(Dispatchers.Main)
+fun View.coroutineScope(coroutineContext: CoroutineContext = Dispatchers.Main): CoroutineScope {
+    return (context as? AppCompatActivity)?.lifecycleScope ?: CoroutineScope(coroutineContext)
 }
