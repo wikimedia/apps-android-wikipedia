3b403a4c8782f90895d91d026bd5b690f82ae972
Dmitry Brant <dbrant@wikimedia.org>
2024-07-11 15:04:44 -0400

    Batch 2 of edit tags. (#4796)
    * Batch 2 of edit tags.
    
    * Convert to single tag.
    
    ---------
    
    Co-authored-by: Cooltey Feng <cfeng@wikimedia.org>

--- app/src/main/java/org/wikipedia/edit/EditTags.kt
+++ app/src/main/java/org/wikipedia/edit/EditTags.kt
@@ -7,4 +7,10 @@ object EditTags {
     const val APP_DESCRIPTION_TRANSLATE = "app-description-translate"
     const val APP_ROLLBACK = "app-rollback"
     const val APP_UNDO = "app-undo"
+    const val APP_SECTION_SOURCE = "app-section-source"
+    const val APP_FULL_SOURCE = "app-full-source"
+    const val APP_SELECT_SOURCE = "app-select-source"
+    const val APP_TALK_SOURCE = "app-talk-source"
+    const val APP_TALK_REPLY = "app-talk-reply"
+    const val APP_TALK_TOPIC = "app-talk-topic"
 }
