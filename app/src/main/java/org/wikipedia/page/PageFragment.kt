857a013877acb873db891441b6910f4f980b1c90
cooltey <coolteygame@gmail.com>
2024-07-12 17:03:04 -0700

    Remove FLAG_ACTIVITY_CLEAR_TOP when going back to Main tab on Explore feed

--- app/src/main/java/org/wikipedia/page/PageFragment.kt
+++ app/src/main/java/org/wikipedia/page/PageFragment.kt
@@ -3,7 +3,6 @@ package org.wikipedia.page
 import android.animation.ObjectAnimator
 import android.app.Activity
 import android.content.Context
-import android.content.Intent
 import android.content.res.Configuration
 import android.net.Uri
 import android.os.Bundle
@@ -1260,7 +1259,6 @@ class PageFragment : Fragment(), BackPressedHandler, CommunicationBridge.Communi
 
     fun goToMainTab() {
         startActivity(MainActivity.newIntent(requireContext())
-            .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
             .putExtra(Constants.INTENT_RETURN_TO_MAIN, true)
             .putExtra(Constants.INTENT_EXTRA_GO_TO_MAIN_TAB, NavTab.EXPLORE.code()))
         requireActivity().finish()
