c253912b2c25f135eab878b3f4147b9512cefaa6
Cooltey Feng <cfeng@wikimedia.org>
2024-08-14 14:01:02 -0700

    Adapter for the list

--- app/src/main/java/org/wikipedia/games/onthisday/OnThisDayGameShareFragment.kt
+++ app/src/main/java/org/wikipedia/games/onthisday/OnThisDayGameShareFragment.kt
@@ -12,12 +12,21 @@ import androidx.core.view.isVisible
 import androidx.core.view.setPadding
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import org.wikipedia.Constants
 import org.wikipedia.R
+import org.wikipedia.WikipediaApp
 import org.wikipedia.databinding.FragmentOnThisDayGameShareBinding
+import org.wikipedia.databinding.ItemOnThisDayGameTopicBinding
+import org.wikipedia.dataclient.page.PageSummary
+import org.wikipedia.history.HistoryEntry
+import org.wikipedia.page.PageActivity
 import org.wikipedia.util.DimenUtil
 import org.wikipedia.util.Resource
 import org.wikipedia.util.ResourceUtil
+import org.wikipedia.util.StringUtil
+import org.wikipedia.views.ViewUtil
 
 class OnThisDayGameShareFragment : Fragment() {
     private var _binding: FragmentOnThisDayGameShareBinding? = null
@@ -39,6 +48,8 @@ class OnThisDayGameShareFragment : Fragment() {
                 is Resource.Success -> onSuccess(it.data)
             }
         }
+
+        // TODO: initialize the PNG image and start the share intent?
     }
 
     override fun onDestroyView() {
@@ -49,6 +60,8 @@ class OnThisDayGameShareFragment : Fragment() {
     private fun onSuccess(gameState: OnThisDayGameViewModel.GameState) {
         binding.shareContainer.visibility = View.VISIBLE
         createDots(gameState)
+        binding.shareArticlesList.layoutManager = LinearLayoutManager(requireContext())
+        binding.shareArticlesList.adapter = RecyclerViewAdapter(viewModel.topicsList)
     }
 
     private fun createDots(gameState: OnThisDayGameViewModel.GameState) {
@@ -68,6 +81,45 @@ class OnThisDayGameShareFragment : Fragment() {
             binding.shareContainer.addView(dotView)
         }
         binding.questionDotsFlow.referencedIds = dotViews.map { it.id }.toIntArray()
+    }
+
+    private inner class RecyclerViewAdapter(val pages: List<PageSummary>) : RecyclerView.Adapter<RecyclerViewItemHolder>() {
+        override fun getItemCount(): Int {
+            return pages.size
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, type: Int): RecyclerViewItemHolder {
+            return RecyclerViewItemHolder(ItemOnThisDayGameTopicBinding.inflate(layoutInflater, parent, false))
+        }
+
+        override fun onBindViewHolder(holder: RecyclerViewItemHolder, position: Int) {
+            holder.bindItem(pages[position])
+        }
+    }
+
+    private inner class RecyclerViewItemHolder(val binding: ItemOnThisDayGameTopicBinding) :
+        RecyclerView.ViewHolder(binding.root), View.OnClickListener {
+
+        private lateinit var page: PageSummary
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        fun bindItem(page: PageSummary) {
+            this.page = page
+            binding.listItemTitle.text = StringUtil.fromHtml(page.displayTitle)
+            binding.listItemDescription.text = StringUtil.fromHtml(page.description)
+            binding.listItemDescription.isVisible = !page.description.isNullOrEmpty()
+            page.thumbnailUrl?.let {
+                ViewUtil.loadImage(binding.listItemThumbnail, it, roundedCorners = true)
+            }
+        }
+
+        override fun onClick(v: View) {
+            val entry = HistoryEntry(page.getPageTitle(WikipediaApp.instance.wikiSite), HistoryEntry.SOURCE_PLACES)
+            startActivity(PageActivity.newIntentForNewTab(requireActivity(), entry, entry.title))
+        }
     }
 
     companion object {
