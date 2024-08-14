b6159b9847c1bd4624d82531b4115249b3b38c07
cooltey <coolteygame@gmail.com>
2024-08-14 16:23:23 -0700

    Complete the share function

--- app/src/main/java/org/wikipedia/games/onthisday/OnThisDayGameShareFragment.kt
+++ app/src/main/java/org/wikipedia/games/onthisday/OnThisDayGameShareFragment.kt
@@ -1,6 +1,7 @@
 package org.wikipedia.games.onthisday
 
 import android.content.res.ColorStateList
+import android.graphics.Bitmap
 import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,24 +9,25 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.core.os.bundleOf
+import androidx.core.view.drawToBitmap
 import androidx.core.view.isVisible
 import androidx.core.view.setPadding
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import org.wikipedia.Constants
 import org.wikipedia.R
-import org.wikipedia.WikipediaApp
 import org.wikipedia.databinding.FragmentOnThisDayGameShareBinding
 import org.wikipedia.databinding.ItemOnThisDayGameTopicBinding
 import org.wikipedia.dataclient.page.PageSummary
-import org.wikipedia.history.HistoryEntry
-import org.wikipedia.page.PageActivity
 import org.wikipedia.util.DimenUtil
 import org.wikipedia.util.ResourceUtil
+import org.wikipedia.util.ShareUtil
 import org.wikipedia.util.StringUtil
 import org.wikipedia.views.ViewUtil
+import java.time.LocalDate
 
 class OnThisDayGameShareFragment : Fragment() {
     private var _binding: FragmentOnThisDayGameShareBinding? = null
@@ -44,6 +46,17 @@ class OnThisDayGameShareFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         buildSharableContent(viewModel.getCurrentGameState())
+
+        binding.backButton.setOnClickListener {
+            requireActivity().supportFragmentManager.popBackStack()
+        }
+
+        binding.shareContainer.post {
+            binding.shareContainer.drawToBitmap(Bitmap.Config.RGB_565).run {
+                ShareUtil.shareImage(lifecycleScope, requireContext(), this, "on_this_day_game_" + LocalDate.now(),
+                    binding.resultText.text.toString(), binding.resultText.text.toString())
+            }
+        }
     }
 
     override fun onDestroyView() {
@@ -101,27 +114,15 @@ class OnThisDayGameShareFragment : Fragment() {
     }
 
     private inner class RecyclerViewItemHolder(val binding: ItemOnThisDayGameTopicBinding) :
-        RecyclerView.ViewHolder(binding.root), View.OnClickListener {
-
-        private lateinit var page: PageSummary
-
-        init {
-            itemView.setOnClickListener(this)
-        }
+        RecyclerView.ViewHolder(binding.root) {
 
         fun bindItem(page: PageSummary) {
-            this.page = page
             binding.listItemTitle.text = StringUtil.fromHtml(page.displayTitle)
             binding.listItemDescription.text = StringUtil.fromHtml(page.description)
             binding.listItemDescription.isVisible = !page.description.isNullOrEmpty()
             page.thumbnailUrl?.let {
                 ViewUtil.loadImage(binding.listItemThumbnail, it, roundedCorners = true)
             }
-        }
-
-        override fun onClick(v: View) {
-            val entry = HistoryEntry(page.getPageTitle(WikipediaApp.instance.wikiSite), HistoryEntry.SOURCE_PLACES)
-            startActivity(PageActivity.newIntentForNewTab(requireActivity(), entry, entry.title))
         }
     }
 
