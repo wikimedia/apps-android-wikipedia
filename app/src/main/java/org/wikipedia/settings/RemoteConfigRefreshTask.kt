271d948b0f9a6fdd96bcdc636946a75b3da5fc9b
Dmitry Brant <me@dmitrybrant.com>
2024-08-21 18:25:34 -0400

    Harden check.

--- app/src/main/java/org/wikipedia/settings/RemoteConfigRefreshTask.kt
+++ app/src/main/java/org/wikipedia/settings/RemoteConfigRefreshTask.kt
@@ -36,8 +36,10 @@ class RemoteConfigRefreshTask : RecurringTask() {
             }
 
             val userInfo = ServiceFactory.get(WikipediaApp.instance.wikiSite).getUserInfo()
-            // This clumsy comparison is necessary because the field is an integer value when enabled, but a string when disabled.
-            Prefs.donationBannerOptIn = userInfo.query?.userInfo?.options?.fundraisingOptIn?.toString().orEmpty().replace("\"", "").isNotEmpty()
+            // This clumsy comparison is necessary because the field is an integer value when enabled, but an empty string when disabled.
+            // Since we want the default to lean towards opt-in, we check very specifically for an empty string, to make sure the user has opted out.
+            val fundraisingOptOut = userInfo.query?.userInfo?.options?.fundraisingOptIn?.toString()?.replace("\"", "")?.isEmpty()
+            Prefs.donationBannerOptIn = fundraisingOptOut != true
         }
     }
 
