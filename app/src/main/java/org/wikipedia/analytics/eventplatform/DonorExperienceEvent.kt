f086945ea466bbda116bb922850d894fd20e6cd4
Dmitry Brant <dbrant@wikimedia.org>
2024-08-21 18:36:24 -0400

    Send fundraising banner opt-in state in app_donor_experience stream. (#4885)
    * Fix blocked message in Suggested Edits, and use coroutines.
    
    * Fix tests.
    
    * Check for banner opt-in.
    
    * Plug it in.
    
    * Plug it in.
    
    * Invert check.
    
    * Harden check.
    
    ---------
    
    Co-authored-by: Cooltey Feng <cfeng@wikimedia.org>

--- app/src/main/java/org/wikipedia/analytics/eventplatform/DonorExperienceEvent.kt
+++ app/src/main/java/org/wikipedia/analytics/eventplatform/DonorExperienceEvent.kt
@@ -1,6 +1,7 @@
 package org.wikipedia.analytics.eventplatform
 
 import org.wikipedia.WikipediaApp
+import org.wikipedia.settings.Prefs
 
 class DonorExperienceEvent {
 
@@ -15,7 +16,7 @@ class DonorExperienceEvent {
             submit(
                 action,
                 activeInterface,
-                campaignId?.let { "campaign_id: $it, " }.orEmpty(),
+                campaignId?.let { "campaign_id: $it, " }.orEmpty() + "banner_opt_in: ${Prefs.donationBannerOptIn}",
                 wikiId
             )
         }
