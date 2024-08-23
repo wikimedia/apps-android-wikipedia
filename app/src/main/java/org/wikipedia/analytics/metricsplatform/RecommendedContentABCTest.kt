a2a6cfb6304b346a8971d62a585b7bce5b16f4f3
cooltey <coolteygame@gmail.com>
2024-08-23 15:02:41 -0700

    Add preference

--- app/src/main/java/org/wikipedia/analytics/metricsplatform/RecommendedContentABCTest.kt
+++ app/src/main/java/org/wikipedia/analytics/metricsplatform/RecommendedContentABCTest.kt
@@ -3,7 +3,7 @@ package org.wikipedia.analytics.metricsplatform
 import org.wikipedia.analytics.ABTest
 
 // TODO: update abTestName
-class RecommendedContentABCTest : ABTest("mBART25", GROUP_SIZE_3) {
+class RecommendedContentABCTest : ABTest("recommendedContent", GROUP_SIZE_3) {
 
     override fun assignGroup() {
         super.assignGroup()
