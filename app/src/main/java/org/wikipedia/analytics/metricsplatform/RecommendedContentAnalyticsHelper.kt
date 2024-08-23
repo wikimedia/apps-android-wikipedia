6e65e04af1668410aed8134b69581fdab8316aec
cooltey <coolteygame@gmail.com>
2024-08-23 11:18:07 -0700

    Add ABC test logic for Recommended Content

---
+++ app/src/main/java/org/wikipedia/analytics/metricsplatform/RecommendedContentAnalyticsHelper.kt
@@ -0,0 +1,10 @@
+package org.wikipedia.analytics.metricsplatform
+
+// TODO: integrate this with ArticleLinkPreviewInteraction
+class RecommendedContentAnalyticsHelper {
+
+    companion object {
+        private const val RECOMMENDED_CONTENT = "recommendedContent"
+        val abcTest = RecommendedContentABCTest()
+    }
+}
