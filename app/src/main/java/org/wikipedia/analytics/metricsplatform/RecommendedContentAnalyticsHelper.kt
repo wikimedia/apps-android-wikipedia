0f6fac74b9e5164d6b450e857c93b92755ac75d2
cooltey <coolteygame@gmail.com>
2024-08-27 08:50:27 -0700

    Remove MU from the list

--- app/src/main/java/org/wikipedia/analytics/metricsplatform/RecommendedContentAnalyticsHelper.kt
+++ app/src/main/java/org/wikipedia/analytics/metricsplatform/RecommendedContentAnalyticsHelper.kt
@@ -13,8 +13,8 @@ class RecommendedContentAnalyticsHelper {
         private val enableCountries = listOf(
             // sub-saharan africa
             "AO", "BJ", "BW", "IO", "BF", "BI", "CV", "CM", "CF", "TD", "KM", "CG", "IC", "CD", "DJ", "GQ", "ER",
-            "SZ", "ET", "GA", "GM", "GH", "GN", "GW", "KE", "LS", "LR", "MG", "MW", "ML", "MR", "MU", "YT", "MZ",
-            "NA", "NE", "NG", "RE", "RW", "SH", "ST", "SN", "SC", "SL", "SO", "ZA", "SS", "TG", "UG", "TZ", "ZM", "ZW",
+            "SZ", "ET", "GA", "GM", "GH", "GN", "GW", "KE", "LS", "LR", "MG", "MW", "ML", "MR", "YT", "MZ", "NA",
+            "NE", "NG", "RE", "RW", "SH", "ST", "SN", "SC", "SL", "SO", "ZA", "SS", "TG", "UG", "TZ", "ZM", "ZW",
             // south asia
             "IN", "PK", "BD", "LK", "MU", "MV", "NP", "BT", "AF"
         )
