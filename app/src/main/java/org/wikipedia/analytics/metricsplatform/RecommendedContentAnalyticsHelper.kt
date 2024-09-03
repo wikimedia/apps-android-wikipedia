bf38ac1c4183b44276c39c1c5951024c311def30
Dmitry Brant <me@dmitrybrant.com>
2024-09-03 13:40:00 -0400

    Enforce end-date for Recommended Content experiment.

--- app/src/main/java/org/wikipedia/analytics/metricsplatform/RecommendedContentAnalyticsHelper.kt
+++ app/src/main/java/org/wikipedia/analytics/metricsplatform/RecommendedContentAnalyticsHelper.kt
@@ -2,13 +2,14 @@ package org.wikipedia.analytics.metricsplatform
 
 import org.wikipedia.util.GeoUtil
 import org.wikipedia.util.ReleaseUtil
+import java.time.LocalDate
 
 class RecommendedContentAnalyticsHelper {
 
     companion object {
         val abcTest = RecommendedContentABCTest()
 
-        private val enableCountries = listOf(
+        private val enabledCountries = listOf(
             // sub-saharan africa
             "AO", "BJ", "BW", "IO", "BF", "BI", "CV", "CM", "CF", "TD", "KM", "CG", "IC", "CD", "DJ", "GQ", "ER",
             "SZ", "ET", "GA", "GM", "GH", "GN", "GW", "KE", "LS", "LR", "MG", "MW", "ML", "MR", "YT", "MZ", "NA",
@@ -17,8 +18,8 @@ class RecommendedContentAnalyticsHelper {
             "IN", "PK", "BD", "LK", "MU", "MV", "NP", "BT", "AF"
         )
 
-        fun recommendedContentEnabled(): Boolean {
-            return ReleaseUtil.isPreProdRelease || enableCountries.contains(GeoUtil.geoIPCountry.orEmpty())
-        }
+        val recommendedContentEnabled get() = ReleaseUtil.isPreProdRelease &&
+                enabledCountries.contains(GeoUtil.geoIPCountry.orEmpty()) &&
+                LocalDate.now() <= LocalDate.of(2024, 10, 1)
     }
 }
