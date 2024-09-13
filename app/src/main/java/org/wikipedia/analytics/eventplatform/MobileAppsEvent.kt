931a0ced5cd7e79db0775b333a22347acf43d42c
Dmitry Brant <me@dmitrybrant.com>
2024-09-13 10:14:19 -0400

    Not.

--- app/src/main/java/org/wikipedia/analytics/eventplatform/MobileAppsEvent.kt
+++ app/src/main/java/org/wikipedia/analytics/eventplatform/MobileAppsEvent.kt
@@ -12,7 +12,7 @@ import org.wikipedia.auth.AccountUtil
 sealed class MobileAppsEvent(@Transient private val _streamName: String = "") : Event(_streamName) {
 
     @SerialName("is_anon") @Required private val anon = !AccountUtil.isLoggedIn
-    @SerialName("is_temp") @Required private val temp = !AccountUtil.isTemporaryAccount
+    @SerialName("is_temp") @Required private val temp = AccountUtil.isTemporaryAccount
     @SerialName("app_session_id") @Required private val sessionId = EventPlatformClient.AssociationController.sessionId
     @SerialName("app_install_id") @Required private val appInstallId = WikipediaApp.instance.appInstallID
 }
