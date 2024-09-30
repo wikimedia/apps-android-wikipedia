28d7754061a60c57a3e713f0f3d76a7203a27e43
Dmitry Brant <dbrant@wikimedia.org>
2024-09-30 15:38:12 -0400

    Correctly format Campaign ID for native payments and analytics. (#4998)

--- app/src/main/java/org/wikipedia/analytics/eventplatform/DonorExperienceEvent.kt
+++ app/src/main/java/org/wikipedia/analytics/eventplatform/DonorExperienceEvent.kt
@@ -1,6 +1,7 @@
 package org.wikipedia.analytics.eventplatform
 
 import org.wikipedia.WikipediaApp
+import org.wikipedia.dataclient.donate.CampaignCollection
 import org.wikipedia.settings.Prefs
 
 class DonorExperienceEvent {
@@ -16,7 +17,7 @@ class DonorExperienceEvent {
             submit(
                 action,
                 activeInterface,
-                campaignId?.let { "campaign_id: $it, " }.orEmpty() + "banner_opt_in: ${Prefs.donationBannerOptIn}",
+                campaignId?.let { "campaign_id: ${CampaignCollection.getFormattedCampaignId(it)}, " }.orEmpty() + "banner_opt_in: ${Prefs.donationBannerOptIn}",
                 wikiId
             )
         }
