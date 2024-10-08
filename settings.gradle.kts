9ce7dec8368700e8c60fdace9f39f265279652eb
Dmitry Brant <dbrant@wikimedia.org>
2024-10-08 15:51:24 -0400

    Migrate top-level gradle files to KTS, and update syntax. (#5018)
    Co-authored-by: Cooltey Feng <cfeng@wikimedia.org>

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        mavenLocal()
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+
+include(":app")
