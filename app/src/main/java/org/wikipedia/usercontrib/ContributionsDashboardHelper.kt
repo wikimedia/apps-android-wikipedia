3e37bdb7043af1e8cb3d545d531ee5bca41d574a
William Rai <48931640+Williamrai@users.noreply.github.com>
2024-11-21 16:40:59 -0500

    - updates survey form (#5147)

--- app/src/main/java/org/wikipedia/usercontrib/ContributionsDashboardHelper.kt
+++ app/src/main/java/org/wikipedia/usercontrib/ContributionsDashboardHelper.kt
@@ -30,9 +30,9 @@ class ContributionsDashboardHelper {
 
         private fun getSurveyDialogUrl(): String {
             val surveyUrls = mapOf(
-                "fr" to "https://docs.google.com/forms/d/1EfPNslpWWQd1WQoA3IkFRKhWy02BTaBgTer1uCKiIHU/edit",
-                "nl" to "https://docs.google.com/forms/d/15GXIEfQTujFtXNU5NqfDyr9lxxET6fP0hk_p_Xz-NOk/edit",
-                "en" to "https://docs.google.com/forms/d/1wIJWp75MMyp2e51kSaPH9ctByUzbyhazEOaJTxQhKqs/edit"
+                "fr" to "https://docs.google.com/forms/d/1EfPNslpWWQd1WQoA3IkFRKhWy02BTaBgTer1uCKiIHU/viewform",
+                "nl" to "https://docs.google.com/forms/d/15GXIEfQTujFtXNU5NqfDyr9lxxET6fP0hk_p_Xz-NOk/viewform",
+                "en" to "https://docs.google.com/forms/d/1wIJWp75MMyp2e51kSaPH9ctByUzbyhazEOaJTxQhKqs/viewform"
             )
             return surveyUrls[WikipediaApp.instance.languageState.appLanguageCode].orEmpty()
         }
