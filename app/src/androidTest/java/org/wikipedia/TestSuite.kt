4d76049dd4ad8a9975bc68bc1ea0e8dfe8d3f752
williamrai <williamrai13@gmail.com>
2024-12-06 12:23:54 -0500

    - ci fix

--- app/src/androidTest/java/org/wikipedia/TestSuite.kt
+++ app/src/androidTest/java/org/wikipedia/TestSuite.kt
@@ -4,8 +4,8 @@ import org.junit.runner.RunWith
 import org.junit.runners.Suite
 import org.junit.runners.Suite.SuiteClasses
 import org.wikipedia.tests.OnboardingTest
-import org.wikipedia.tests.SuggestedEditScreenTest
 import org.wikipedia.tests.SearchTest
+import org.wikipedia.tests.SuggestedEditScreenTest
 
 @RunWith(Suite::class)
 @SuiteClasses(
--- app/src/androidTest/java/org/wikipedia/robots/feature/SearchRobot.kt
+++ app/src/androidTest/java/org/wikipedia/robots/feature/SearchRobot.kt
@@ -1,15 +1,13 @@
 package org.wikipedia.robots.feature
 
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import org.hamcrest.Matchers.allOf
