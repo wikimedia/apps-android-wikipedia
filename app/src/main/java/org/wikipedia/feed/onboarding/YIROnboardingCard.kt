7daf9ac4fbe1928ba35bb99392bbbb963c755e19
Dmitry Brant <me@dmitrybrant.com>
2025-01-06 09:47:40 -0500

    Add list of excluded countries.

--- app/src/main/java/org/wikipedia/feed/onboarding/YIROnboardingCard.kt
+++ app/src/main/java/org/wikipedia/feed/onboarding/YIROnboardingCard.kt
@@ -3,6 +3,7 @@ package org.wikipedia.feed.onboarding
 import org.wikipedia.R
 import org.wikipedia.feed.announcement.Announcement
 import org.wikipedia.feed.model.CardType
+import org.wikipedia.util.GeoUtil
 import java.time.LocalDate
 
 class YIROnboardingCard(announcement: Announcement) : OnboardingCard(announcement) {
@@ -12,10 +13,14 @@ class YIROnboardingCard(announcement: Announcement) : OnboardingCard(announcemen
     }
 
     override fun shouldShow(): Boolean {
-        return super.shouldShow() && LocalDate.now() <= LocalDate.of(2025, 1, 31)
+        return super.shouldShow() &&
+                LocalDate.now() <= LocalDate.of(2025, 1, 31) &&
+                !excludedCountries.contains(GeoUtil.geoIPCountry)
     }
 
     override fun prefKey(): Int {
         return R.string.preference_key_feed_yir_onboarding_card_enabled
     }
+
+    private val excludedCountries = setOf("RU", "IR", "CN", "HK", "MO", "SA", "CU", "MM", "BY", "EG", "PS", "GN", "PK", "KH", "VN", "SD", "AE", "SY", "JO", "VE", "AF")
 }
