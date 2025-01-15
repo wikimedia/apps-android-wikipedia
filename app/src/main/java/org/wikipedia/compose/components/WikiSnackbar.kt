a993d795f63cdbccb9a6bc6dec231aa233c97030
cooltey <coolteygame@gmail.com>
2025-01-15 11:23:20 -0800

    Code review comments

--- app/src/main/java/org/wikipedia/compose/components/WikiSnackbar.kt
+++ app/src/main/java/org/wikipedia/compose/components/WikiSnackbar.kt
@@ -11,44 +11,41 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import org.wikipedia.compose.theme.WikipediaTheme
 
-class WikiSnackbar {
-
-    @Composable
-    fun Snackbar(
-        message: String,
-        actionLabel: String? = null,
-        onActionClick: (() -> Unit)? = null,
-        modifier: Modifier = Modifier
-    ) {
-        Snackbar(
-            action = {
-                if (actionLabel != null && onActionClick != null) {
-                    TextButton(
-                        onClick = onActionClick,
-                        modifier = Modifier.padding(end = 8.dp)
-                    ) {
-                        Text(
-                            text = actionLabel,
-                            style = WikipediaTheme.typography.h3.copy(
-                                color = WikipediaTheme.colors.progressiveColor
-                            )
+@Composable
+fun Snackbar(
+    message: String,
+    modifier: Modifier = Modifier,
+    actionLabel: String? = null,
+    onActionClick: (() -> Unit)? = null,
+) {
+    Snackbar(
+        action = {
+            if (actionLabel != null && onActionClick != null) {
+                TextButton(
+                    onClick = onActionClick,
+                    modifier = Modifier.padding(end = 8.dp)
+                ) {
+                    Text(
+                        text = actionLabel,
+                        style = WikipediaTheme.typography.h3.copy(
+                            color = WikipediaTheme.colors.progressiveColor
                         )
-                    }
+                    )
                 }
-            },
-            modifier = modifier.padding(16.dp)
-        ) {
-            Text(
-                text = message,
-                style = WikipediaTheme.typography.h3.copy(
-                    color = WikipediaTheme.colors.primaryColor,
-                    letterSpacing = 0.sp
-                ),
-                maxLines = 10,
-                overflow = TextOverflow.Ellipsis,
-                modifier = Modifier
-                    .padding(top = 0.dp, bottom = 0.dp, start = 0.dp, end = 8.dp)
-            )
-        }
+            }
+        },
+        modifier = modifier.padding(16.dp)
+    ) {
+        Text(
+            text = message,
+            style = WikipediaTheme.typography.h3.copy(
+                color = WikipediaTheme.colors.primaryColor,
+                letterSpacing = 0.sp
+            ),
+            maxLines = 10,
+            overflow = TextOverflow.Ellipsis,
+            modifier = Modifier
+                .padding(top = 0.dp, bottom = 0.dp, start = 0.dp, end = 8.dp)
+        )
     }
 }
