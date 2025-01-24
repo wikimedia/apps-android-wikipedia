d8bee03851c1a153b5eb9dddf9022a0b8108a15f
Cooltey Feng <cfeng@wikimedia.org>
2025-01-24 11:11:31 -0800

    Make Developer preferences searchable (#5249)
    * Make Developer preferences searchable
    
    * Remove dev setting menu
    
    ---------
    
    Co-authored-by: Dmitry Brant <dbrant@wikimedia.org>

--- app/src/main/java/org/wikipedia/settings/DeveloperSettingsFragment.kt
+++ app/src/main/java/org/wikipedia/settings/DeveloperSettingsFragment.kt
@@ -1,8 +1,53 @@
 package org.wikipedia.settings
 
+import android.content.Context
+import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.view.ActionMode
+import org.wikipedia.R
+import org.wikipedia.history.SearchActionModeCallback
+
 class DeveloperSettingsFragment : PreferenceLoaderFragment() {
+
+    private val searchActionModeCallback = DevPreferencesSearchCallback()
+    private var actionMode: ActionMode? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
     override fun loadPreferences() {
         DeveloperSettingsPreferenceLoader(this).loadPreferences()
+    }
+
+    fun startSearchActionMode() {
+        (requireActivity() as AppCompatActivity).startSupportActionMode(searchActionModeCallback)
+    }
+
+    private inner class DevPreferencesSearchCallback : SearchActionModeCallback() {
+        override fun onCreateActionMode(mode: ActionMode, menu: Menu): Boolean {
+            actionMode = mode
+            return super.onCreateActionMode(mode, menu)
+        }
+
+        override fun onQueryChange(s: String) {
+            DeveloperSettingsPreferenceLoader(this@DeveloperSettingsFragment).filterPreferences(s.trim())
+        }
+
+        override fun onDestroyActionMode(mode: ActionMode) {
+            super.onDestroyActionMode(mode)
+            actionMode = null
+            DeveloperSettingsPreferenceLoader(this@DeveloperSettingsFragment).filterPreferences()
+        }
+
+        override fun getSearchHintString(): String {
+            return requireContext().resources.getString(R.string.preferences_developer_search_hint)
+        }
+
+        override fun getParentContext(): Context {
+            return requireContext()
+        }
     }
 
     companion object {
