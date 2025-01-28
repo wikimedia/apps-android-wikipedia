4a6a2a5c19710f16d4139d3595d20059f204fbe3
Dmitry Brant <me@dmitrybrant.com>
2025-01-28 13:18:42 -0500

    Doesn't need to be a coroutine.

--- app/src/main/java/org/wikipedia/feed/wikigames/WikiGamesCardClient.kt
+++ app/src/main/java/org/wikipedia/feed/wikigames/WikiGamesCardClient.kt
@@ -1,18 +1,12 @@
 package org.wikipedia.feed.wikigames
 
 import android.content.Context
-import kotlinx.coroutines.CoroutineExceptionHandler
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
 import org.wikipedia.WikipediaApp
-import org.wikipedia.dataclient.ServiceFactory
 import org.wikipedia.dataclient.WikiSite
+import org.wikipedia.feed.FeedContentType
 import org.wikipedia.feed.dataclient.FeedClient
-import org.wikipedia.util.log.L
 
-class WikiGamesCardClient(private val coroutineScope: CoroutineScope) : FeedClient {
-    private var clientJob: Job? = null
+class WikiGamesCardClient() : FeedClient {
 
     override fun request(context: Context, wiki: WikiSite, age: Int, cb: FeedClient.Callback) {
         cancel()
@@ -20,25 +14,16 @@ class WikiGamesCardClient(private val coroutineScope: CoroutineScope) : FeedClie
             cb.success(emptyList())
             return
         }
-
-        clientJob = coroutineScope.launch(
-            CoroutineExceptionHandler { _, caught ->
-                L.v(caught)
-                cb.error(caught)
-            }) {
-
-            val feedAvailability = ServiceFactory.getRest(wiki).feedAvailability()
-            val availableLanguages = feedAvailability.games.filter { langCode ->
+        val availableLanguages = FeedContentType.WIKI_GAMES.langCodesSupported
+            .filter { !FeedContentType.WIKI_GAMES.langCodesDisabled.contains(it) }
+            .filter { langCode ->
                 WikipediaApp.instance.languageState.appLanguageCodes.contains(langCode)
             }
-            val cards = availableLanguages.map { langCode ->
-                WikiGamesCard(WikiSite.forLanguageCode(langCode))
-            }
-            cb.success(cards)
+        val cards = availableLanguages.map { langCode ->
+            WikiGamesCard(WikiSite.forLanguageCode(langCode))
         }
+        cb.success(cards)
     }
 
-    override fun cancel() {
-        clientJob?.cancel()
-    }
+    override fun cancel() { }
 }
