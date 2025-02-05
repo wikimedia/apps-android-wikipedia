433534c5f5f50ec2505e1a20c84d36bc8791a1d0
horsicq <horsicq@gmail.com>
2025-02-05 22:22:09 +0100

    HW3

---
+++ app/src/androidTest/java/org/wikipedia/homeworks/homework03.kt
@@ -0,0 +1,30 @@
+package org.wikipedia.homeworks
+
+import android.widget.Button
+import android.widget.ImageView
+
+val logoImageView = listOf(
+    ImageView::class.java,
+    "imageViewCentered",
+    ""
+)
+
+val addLanguageButton = listOf(
+    Button::class.java,
+    "addLanguageButton",
+    "onboarding_multilingual_add_language_text"
+)
+
+val continueButton = listOf(
+    Button::class.java,
+    "fragment_onboarding_forward_button",
+    "onboarding_continue"
+)
+
+val skipButton = listOf(
+    Button::class.java,
+    "fragment_onboarding_skip_button",
+    "onboarding_skip"
+)
+
+
