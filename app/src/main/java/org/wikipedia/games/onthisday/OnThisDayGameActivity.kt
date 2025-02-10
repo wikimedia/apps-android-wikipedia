b17ab68a08db7e362d11ca4d73e3af8852770ec1
cooltey <coolteygame@gmail.com>
2025-02-10 15:06:19 -0800

    Follow up: handle close button properly for the wikigame

--- app/src/main/java/org/wikipedia/games/onthisday/OnThisDayGameActivity.kt
+++ app/src/main/java/org/wikipedia/games/onthisday/OnThisDayGameActivity.kt
@@ -42,6 +42,8 @@ import org.wikipedia.dataclient.WikiSite
 import org.wikipedia.dataclient.page.PageSummary
 import org.wikipedia.feed.onthisday.OnThisDay
 import org.wikipedia.history.HistoryEntry
+import org.wikipedia.main.MainActivity
+import org.wikipedia.navtab.NavTab
 import org.wikipedia.page.PageActivity
 import org.wikipedia.readinglist.LongPressMenu
 import org.wikipedia.readinglist.ReadingListBehaviorsUtil
@@ -184,13 +186,14 @@ class OnThisDayGameActivity : BaseActivity(), BaseActivity.Callback {
                         .setTitle(R.string.on_this_day_game_pause_title)
                         .setMessage(R.string.on_this_day_game_pause_body)
                         .setPositiveButton(R.string.on_this_day_game_pause_positive) { _, _ ->
-                            finish()
+                            onFinish()
                         }
                         .setNegativeButton(R.string.on_this_day_game_pause_negative, null)
                         .show()
                     true
                 } else {
-                    super.onOptionsItemSelected(item)
+                    onFinish()
+                    true
                 }
             }
             R.id.menu_learn_more -> {
@@ -217,6 +220,22 @@ class OnThisDayGameActivity : BaseActivity(), BaseActivity.Callback {
             return
         }
         super.onBackPressed()
+        onFinish()
+    }
+
+    private fun onFinish() {
+        if (WikipediaApp.instance.haveMainActivity) {
+            finish()
+        } else {
+            goToMainTab()
+        }
+    }
+
+    private fun goToMainTab() {
+        startActivity(MainActivity.newIntent(this)
+            .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            .putExtra(Constants.INTENT_RETURN_TO_MAIN, true)
+            .putExtra(Constants.INTENT_EXTRA_GO_TO_MAIN_TAB, NavTab.EXPLORE.code()))
         finish()
     }
 
