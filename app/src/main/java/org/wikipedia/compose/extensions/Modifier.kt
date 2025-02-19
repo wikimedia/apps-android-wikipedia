bd004bf7c18572753d0ec311cf20bf4272743052
Cooltey Feng <cfeng@wikimedia.org>
2025-02-19 11:00:05 -0800

    Add repeatCount back

--- app/src/main/java/org/wikipedia/compose/extensions/Modifier.kt
+++ app/src/main/java/org/wikipedia/compose/extensions/Modifier.kt
@@ -28,26 +28,29 @@ fun Modifier.pulse(
     toScale: Float = 2.5f,
     durationMillis: Int = 1000,
     pivot: Float = 0.5f,
+    repeatCount: Int = 1,
     easing: Easing = FastOutSlowInEasing
 ): Modifier = composed {
     val scale = remember { mutableFloatStateOf(fromScale) }
     val targetScale = rememberUpdatedState(toScale)
 
     LaunchedEffect(Unit) {
-        animate(
-            initialValue = fromScale,
-            targetValue = targetScale.value,
-            animationSpec = tween(durationMillis, easing = easing)
-        ) { value, _ ->
-            scale.floatValue = value
-        }
+        repeat(repeatCount) {
+            animate(
+                initialValue = fromScale,
+                targetValue = targetScale.value,
+                animationSpec = tween(durationMillis, easing = easing)
+            ) { value, _ ->
+                scale.floatValue = value
+            }
 
-        animate(
-            initialValue = targetScale.value,
-            targetValue = fromScale,
-            animationSpec = tween(durationMillis, easing = easing)
-        ) { value, _ ->
-            scale.floatValue = value
+            animate(
+                initialValue = targetScale.value,
+                targetValue = fromScale,
+                animationSpec = tween(durationMillis, easing = easing)
+            ) { value, _ ->
+                scale.floatValue = value
+            }
         }
     }
 
@@ -78,6 +81,7 @@ private fun PreviewPulse() {
                     text = "Pulse",
                     color = WikipediaTheme.colors.primaryColor,
                     modifier = Modifier.pulse(
+                        repeatCount = 5,
                         easing = FastOutSlowInEasing
                     )
                 )
