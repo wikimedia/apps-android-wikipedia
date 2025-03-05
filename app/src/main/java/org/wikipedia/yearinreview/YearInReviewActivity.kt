8a55808f4005cae0b96c582aa9fb2711faea2f87
Lamar Petty <sv709@yahoo.com>
2025-03-05 15:11:32 -0800

    update dependencies, manifest. Add yearinreview package with navigation and test screen

---
+++ app/src/main/java/org/wikipedia/yearinreview/YearInReviewActivity.kt
@@ -0,0 +1,27 @@
+package org.wikipedia.yearinreview
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+class YearInReviewActivity: ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent{
+
+            val navigationController = rememberNavController()
+
+            NavHost(
+                navController = navigationController, startDestination = "testscreen"
+            ){
+                composable("testscreen"){
+                    TestScreen()
+                }
+            }
+        }
+    }
+}
