ecb5f510cbddfbf34f03bfad90b182cae0bd0f2c
Lamar Petty <sv709@yahoo.com>
2025-03-25 15:41:07 -0700

    removed fetchedData property from YearInReviewScreenData

--- app/src/main/java/org/wikipedia/yearinreview/YearInReviewScreenData.kt
+++ app/src/main/java/org/wikipedia/yearinreview/YearInReviewScreenData.kt
@@ -5,27 +5,23 @@ import org.wikipedia.R
 data class YearInReviewScreenData(
     val imageResource: Int,
     val headLineText: Int,
-    val bodyText: Int,
-    val fetchedData: Any? = null
+    val bodyText: Int
 )
 
 val getStartedData = YearInReviewScreenData(
     imageResource = R.drawable.year_in_review_block_10_resize,
     headLineText = R.string.year_in_review_get_started_headline,
-    bodyText = R.string.year_in_review_get_started_bodytext,
-    fetchedData = null
+    bodyText = R.string.year_in_review_get_started_bodytext
 )
 
 val readCountData = YearInReviewScreenData(
     imageResource = R.drawable.wyir_block_5_resize,
     headLineText = R.string.year_in_review_read_count_headline,
-    bodyText = R.string.year_in_review_read_count_bodytext,
-    fetchedData = null
+    bodyText = R.string.year_in_review_read_count_bodytext
 )
 
 val editCountData = YearInReviewScreenData(
     imageResource = R.drawable.wyir_bytes,
     headLineText = R.string.year_in_review_edit_count_headline,
-    bodyText = R.string.year_in_review_edit_count_bodytext,
-    fetchedData = null
+    bodyText = R.string.year_in_review_edit_count_bodytext
 )
