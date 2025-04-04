209f4058cd8bfdff9d697f479562d280f5e53032
cooltey <coolteygame@gmail.com>
2025-04-04 14:58:13 -0700

    Lint

--- app/src/main/java/org/wikipedia/compose/components/WikiCard.kt
+++ app/src/main/java/org/wikipedia/compose/components/WikiCard.kt
@@ -24,14 +24,10 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import org.wikipedia.WikipediaApp
-import org.wikipedia.compose.theme.WikipediaTheme
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
 import org.wikipedia.WikipediaApp
-import org.wikipedia.compose.theme.BaseTheme
 import org.wikipedia.compose.theme.WikipediaTheme
+import org.wikipedia.compose.theme.BaseTheme
 import org.wikipedia.theme.Theme
 
 @Composable
