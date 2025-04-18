0d938676db5717a988a8d47354db87c53bc23c6a
lamarpetty <sv709@yahoo.com>
2025-04-18 15:22:40 -0700

    added nonEnglish collective data and fetchedargs property

--- app/src/main/java/org/wikipedia/yearinreview/YearInReviewScreenData.kt
+++ app/src/main/java/org/wikipedia/yearinreview/YearInReviewScreenData.kt
@@ -12,7 +12,7 @@ data class YearInReviewScreenData(
     val imageResource: Int,
     val headLineText: Int,
     val bodyText: Int,
-    val jobId: String = ""
+    var fetchedArgs: MutableList<String>? = null
 )
 
 data class YearInReviewTextData(
@@ -24,19 +24,33 @@ val getStartedData = YearInReviewScreenData(
     imageResource = R.drawable.year_in_review_block_10_resize,
     headLineText = R.string.year_in_review_get_started_headline,
     bodyText = R.string.year_in_review_get_started_bodytext,
-    jobId = PersonalizedJobID.ONBOARDING.name
+    fetchedArgs = null
 )
 
 val readCountData = YearInReviewScreenData(
     imageResource = R.drawable.wyir_block_5_resize,
     headLineText = R.string.year_in_review_read_count_headline,
     bodyText = R.string.year_in_review_read_count_bodytext,
-    jobId = PersonalizedJobID.READ_COUNT.name
+    fetchedArgs = null
 )
 
 val editCountData = YearInReviewScreenData(
     imageResource = R.drawable.wyir_bytes,
     headLineText = R.string.year_in_review_edit_count_headline,
     bodyText = R.string.year_in_review_edit_count_bodytext,
-    jobId = PersonalizedJobID.EDIT_COUNT.name
+    fetchedArgs = null
 )
+
+val nonEnglishCollectiveReadCountData = YearInReviewScreenData(
+    imageResource = R.drawable.wyir_puzzle_3,
+    headLineText = R.string.year_in_review_non_english_collective_readcount_headline,
+    bodyText = R.string.year_in_review_non_english_collective_readcount_bodytext,
+)
+
+val nonEnglishCollectiveEditCountData = YearInReviewScreenData(
+    imageResource = R.drawable.wyir_puzzle_2_v5,
+    headLineText = R.string.year_in_review_non_english_collective_editcount_headline,
+    bodyText = R.string.year_in_review_non_english_collective_editcount_bodytext,
+)
+
+
