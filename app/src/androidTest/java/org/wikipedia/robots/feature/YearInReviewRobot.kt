b8ec29b2858ddc2f84f6d7c88204fb21c613ad47
lamarpetty <sv709@yahoo.com>
2025-04-29 12:25:13 -0700

    UI testing changes

---
+++ app/src/androidTest/java/org/wikipedia/robots/feature/YearInReviewRobot.kt
@@ -0,0 +1,60 @@
+package org.wikipedia.robots.feature
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.test.DeviceConfigurationOverride
+import androidx.compose.ui.test.LayoutDirection
+import androidx.compose.ui.test.junit4.ComposeContentTestRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
+import org.junit.Test
+import org.wikipedia.yearinreview.YearInReviewScreenContent
+import org.wikipedia.yearinreview.readCountData
+
+class YearInReviewRobot {
+
+    private lateinit var composeTestRule: ComposeContentTestRule
+
+    fun setComposeTestRule(rule: ComposeContentTestRule) = apply {
+        this.composeTestRule = rule
+        return@apply
+    }
+
+    fun assertHeadlineIsRtL() {
+        composeTestRule.setContent {
+            DeviceConfigurationOverride(
+                DeviceConfigurationOverride.LayoutDirection(LayoutDirection.Rtl)
+            ) {
+            YearInReviewScreenContent(
+                innerPadding = PaddingValues(48.dp),
+                screenData = readCountData)
+            }
+        }
+
+        composeTestRule.onNodeWithTag("hello")
+            .
+
+        }
+
+    }
+
+/*
+fun assertHeadlineIsLTR() {
+        var currentTextDirection: LayoutDirection? = null
+        composeTestRule.setContent {
+            YearInReviewScreenContent(
+                innerPadding = PaddingValues(48.dp),
+                screenData = readCountData
+            ).run{ currentTextDirection = LocalLayoutDirection.current}
+        }
+        composeTestRule.onNodeWithTag("hello").also {
+            assert(LocalLayoutDirection.current == LayoutDirection.Ltr)
+        }
+        /*
+        composeTestRule.runOnIdle {
+            currentTextDirection = LocalLayoutDirection.current
+        }
+        assert(currentTextDirection == LayoutDirection.Ltr) */
+    }
+ */
---
+++ app/src/androidTest/java/org/wikipedia/tests/YearInReviewScreenTest.kt
@@ -0,0 +1,27 @@
+package org.wikipedia.tests
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.wikipedia.base.BaseTest
+import org.wikipedia.robots.feature.YearInReviewRobot
+import org.wikipedia.yearinreview.YearInReviewActivity
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class YearInReviewScreenTest : BaseTest<YearInReviewActivity>(
+    activityClass = YearInReviewActivity::class.java
+) {
+
+    private val yearInReviewRobot = YearInReviewRobot()
+
+    @Test
+    fun runTest() {
+        yearInReviewRobot
+            .setComposeTestRule(composeTestRule)
+            .assertHeadlineIsLTR()
+
+
+    }
+}
