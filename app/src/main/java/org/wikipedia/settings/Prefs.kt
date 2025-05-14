579a5b8506ffc37d140a54f3299595d21b89d9b1
William Rai <48931640+Williamrai@users.noreply.github.com>
2025-05-14 14:56:21 -0400

    Wikigames archive calendar (#5544)
    * - initial calendar work
    
    * - code fixes
    
    * - updates title text of calendar and selects today's date when calendar is opened
    
    * - adds logic to show toast for archive calendar
    - adds date util function to get date in format MMMM d, yyyy
    
    * - code fixes
    - replaces calendar parameter with Date
    - updates getDateKey logic (return long combination of year, month and day)
    
    * Allow receiving callbacks for days clicked in MaterialDatePicker. (#5528)
    
    * - starts OnThisDayGameOnboardingFragment all the time, disable state update when OnThisDayGameOnboardingFragment is visible
    - adds archive calendar logic
    - updates OnThisDayGameOnboardingFragment to observer state and change based on the state
    - adds Prefs to track archive game playing
    - adds a new outline button style for xml
    
    * - updates colors
    
    * - adds logic for playing archive games
    
    * - logical fixes for archive calendar states
    
    * - aligning score view with the current archive calendar
    
    * - logical fixes after merge
    
    * - lint fixes
    
    * Wikigames sound menu option (#5546)
    
    * - adds menu option to fragments
    
    * - adds sound to game menu
    - adds menu on the main game screen
    
    * - adds strings resource
    - code fixes
    
    * - logic fixes to support different language for archive games
    - adds loading/error state on OnboardingFragment
    
    * - reset all states next day from last active date
    - code fixes
    
    * - code fixes
    
    * - separates the game logic to functions
    - remove redundant date update
    
    ---------
    
    Co-authored-by: Dmitry Brant <dbrant@wikimedia.org>
    Co-authored-by: Cooltey Feng <cfeng@wikimedia.org>

--- app/src/main/java/org/wikipedia/settings/Prefs.kt
+++ app/src/main/java/org/wikipedia/settings/Prefs.kt
@@ -773,4 +773,12 @@ object Prefs {
             OnThisDayGameNotificationState.valueOf(it)
         } ?: OnThisDayGameNotificationState.NO_INTERACTED
         set(value) = PrefsIoUtil.setString(R.string.preference_key_otd_notification_state, value.name)
+
+    var isOtdSoundOn: Boolean
+        get() = PrefsIoUtil.getBoolean(R.string.pref_key_otd_sound_on, true)
+        set(value) = PrefsIoUtil.setBoolean(R.string.pref_key_otd_sound_on, value)
+
+    var otdGameLastActiveDate: String
+        get() = PrefsIoUtil.getString(R.string.pref_key_otd_last_active_date, "").orEmpty()
+        set(value) = PrefsIoUtil.setString(R.string.pref_key_otd_last_active_date, value)
 }
