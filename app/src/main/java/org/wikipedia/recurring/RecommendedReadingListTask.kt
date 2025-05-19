a0c8f69063d25d39030da87295ac4080905a2c39
cooltey <coolteygame@gmail.com>
2025-05-19 13:24:28 -0700

    Add recurring task

---
+++ app/src/main/java/org/wikipedia/recurring/RecommendedReadingListTask.kt
@@ -0,0 +1,28 @@
+package org.wikipedia.recurring
+
+import org.wikipedia.WikipediaApp
+import org.wikipedia.readinglist.recommended.RecommendedReadingListUpdateFrequency
+import org.wikipedia.readinglist.recommended.RecommendedReadingListViewModel
+import org.wikipedia.settings.Prefs
+import java.util.Date
+import java.util.concurrent.TimeUnit
+
+class RecommendedReadingListTask(app: WikipediaApp) : RecurringTask() {
+    override val name = "generateRecommendedReadingListTask"
+
+    override fun shouldRun(lastRun: Date): Boolean {
+        return millisSinceLastRun(lastRun) >= when (Prefs.recommendedReadingListUpdateFrequency) {
+            RecommendedReadingListUpdateFrequency.DAILY -> TimeUnit.DAYS.toMillis(1)
+            RecommendedReadingListUpdateFrequency.WEEKLY -> TimeUnit.DAYS.toMillis(7)
+            RecommendedReadingListUpdateFrequency.MONTHLY -> TimeUnit.DAYS.toMillis(31)
+        }
+    }
+
+    override suspend fun run(lastRun: Date) {
+        RecommendedReadingListViewModel.generateRecommendedReadingList()
+    }
+
+    companion object {
+        private const val CLEANUP_TIME_IN_YEARS = 2
+    }
+}
