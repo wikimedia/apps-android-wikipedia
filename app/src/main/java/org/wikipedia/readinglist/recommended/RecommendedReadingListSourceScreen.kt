7d2a61d4a93a1c66400bace394afa5efb010f7a8
Cooltey Feng <cfeng@wikimedia.org>
2025-05-27 13:41:45 -0700

    Unused imports

--- app/src/main/java/org/wikipedia/readinglist/recommended/RecommendedReadingListSourceScreen.kt
+++ app/src/main/java/org/wikipedia/readinglist/recommended/RecommendedReadingListSourceScreen.kt
@@ -31,9 +31,6 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
