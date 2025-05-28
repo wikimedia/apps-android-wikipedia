59bcb03866553d6e7e935f221a2646feb76a5209
lamarpetty <sv709@yahoo.com>
2025-05-28 08:38:53 -0700

    Add preview for screenshot

--- app/src/main/java/org/wikipedia/yearinreview/YearInReviewScaffold.kt
+++ app/src/main/java/org/wikipedia/yearinreview/YearInReviewScaffold.kt
@@ -69,6 +69,7 @@ import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.compose.ui.zIndex
@@ -80,9 +81,11 @@ import coil3.compose.AsyncImage
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.launch
 import org.wikipedia.R
+import org.wikipedia.compose.theme.BaseTheme
 import org.wikipedia.compose.theme.WikipediaTheme
 import org.wikipedia.util.ShareUtil
 import org.wikipedia.util.UriUtil
+import org.wikipedia.yearinreview.YearInReviewViewModel.Companion.nonEnglishCollectiveEditCountData
 import kotlin.math.absoluteValue
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -565,12 +568,16 @@ fun CaptureComposableToBitmap(
         }
     }
 }
-/*
+
 @Preview
 @Composable
 fun PreviewScreenShot() {
-    ScreenShotScaffold(
-        screenContent = nonEnglishCollectiveEditCountData
-    )
+    val context = LocalContext.current
+    BaseTheme {
+        ScreenShotScaffold(
+            screenContent = nonEnglishCollectiveEditCountData,
+            context = context
+        )
+    }
 }
-*/
+
