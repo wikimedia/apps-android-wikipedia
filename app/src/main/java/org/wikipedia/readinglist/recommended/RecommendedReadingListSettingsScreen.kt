1c0a946e3e80a686ecd9f319823a1e1cb5b3f03e
williamrai <williamrai13@gmail.com>
2025-06-04 10:04:29 -0400

    - reverting strings name

--- app/src/main/java/org/wikipedia/readinglist/recommended/RecommendedReadingListSettingsScreen.kt
+++ app/src/main/java/org/wikipedia/readinglist/recommended/RecommendedReadingListSettingsScreen.kt
@@ -121,8 +121,8 @@ fun RecommendedReadingListSettingsScreen(
             WikipediaAlertDialog(
                 title = stringResource(R.string.recommended_reading_list_settings_turn_off_dialog_title),
                 message = stringResource(R.string.recommended_reading_list_settings_turn_off_dialog_message, uiState.updateFrequency.name.lowercase()),
-                confirmButtonText = stringResource(R.string.recommended_reading_list_settings_notifications_dialog_positive_button),
-                dismissButtonText = stringResource(R.string.recommended_reading_list_settings_notifications_dialog_negative_button),
+                confirmButtonText = stringResource(R.string.recommended_reading_list_settings_notifications_dialog_negative_button),
+                dismissButtonText = stringResource(R.string.recommended_reading_list_settings_notifications_dialog_positive_button),
                 onDismissRequest = {
                     showAlertDialog = false
                 },
@@ -536,8 +536,8 @@ private fun NotificationView(
         WikipediaAlertDialog(
             title = stringResource(R.string.recommended_reading_list_settings_notifications_dialog_title),
             message = stringResource(R.string.recommended_reading_list_settings_notifications_dialog_message),
-            confirmButtonText = stringResource(R.string.recommended_reading_list_settings_notifications_dialog_positive_button),
-            dismissButtonText = stringResource(R.string.recommended_reading_list_settings_notifications_dialog_negative_button),
+            confirmButtonText = stringResource(R.string.recommended_reading_list_settings_notifications_dialog_negative_button),
+            dismissButtonText = stringResource(R.string.recommended_reading_list_settings_notifications_dialog_positive_button),
             onDismissRequest = {
                 showAlertDialog = false
             },
