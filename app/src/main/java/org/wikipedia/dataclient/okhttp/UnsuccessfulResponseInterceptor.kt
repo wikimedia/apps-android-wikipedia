4e678ab0e0f5ac6df1cf52f4533552c5a744d58a
Dmitry Brant <me@dmitrybrant.com>
2025-06-16 14:47:00 -0400

    Don't treat redirects (HTTP 3xx) as errors.

--- app/src/main/java/org/wikipedia/dataclient/okhttp/UnsuccessfulResponseInterceptor.kt
+++ app/src/main/java/org/wikipedia/dataclient/okhttp/UnsuccessfulResponseInterceptor.kt
@@ -3,16 +3,18 @@ package org.wikipedia.dataclient.okhttp
 import okhttp3.Interceptor
 import okhttp3.Response
 import okhttp3.internal.closeQuietly
-import java.io.IOException
 
 class UnsuccessfulResponseInterceptor : Interceptor {
-    @Throws(IOException::class)
+    @Throws(HttpStatusException::class)
     override fun intercept(chain: Interceptor.Chain): Response {
         val rsp = chain.proceed(chain.request())
-        if (rsp.isSuccessful) {
+
+        // If the response is successful (2xx) or a redirect (3xx), then proceed.
+        if (rsp.code < 400) {
             return rsp
         }
 
+        // Otherwise, treat it as an exception and throw it.
         val e = HttpStatusException(rsp)
         rsp.closeQuietly()
         throw e
