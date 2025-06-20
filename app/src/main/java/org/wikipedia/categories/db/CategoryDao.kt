7e0c1a266695b1acea7158ee93661a8564d69ab7
Cooltey Feng <cfeng@wikimedia.org>
2025-06-20 11:37:06 -0700

    add upsert

--- app/src/main/java/org/wikipedia/categories/db/CategoryDao.kt
+++ app/src/main/java/org/wikipedia/categories/db/CategoryDao.kt
@@ -13,6 +13,14 @@ interface CategoryDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertAll(categories: List<Category>): List<Long>
 
+    // find categories by primary keys
+    @Query("SELECT * FROM Category WHERE year = :year AND month = :month AND title = :title AND lang = :lang")
+    suspend fun findByPrimaryKey(year: Int, month: Int, title: String, lang: String): Category?
+
+    // Update count by primary keys
+    @Query("UPDATE Category SET count = count + 1 WHERE year = :year AND month = :month AND title = :title AND lang = :lang")
+    suspend fun updateCountByPrimaryKeys(year: Int, month: Int, title: String, lang: String)
+
     @Query("SELECT year, month, title, lang, SUM (count) AS count FROM Category WHERE year BETWEEN :startYear AND :endYear GROUP BY title, lang ORDER BY count DESC")
     suspend fun getCategoriesByTimeRange(startYear: Int, endYear: Int): List<Category>
 
@@ -24,4 +32,12 @@ interface CategoryDao {
 
     @Query("DELETE FROM Category WHERE rowid IN (SELECT rowid FROM Category WHERE year <:year LIMIT :batchSize)")
     suspend fun deleteOlderThanInBatch(year: Int, batchSize: Int): Int
+
+    suspend fun upsertAll(list: List<Category>) {
+        list.forEach { category ->
+            findByPrimaryKey(category.year, category.month, category.title, category.lang)?.let {
+                updateCountByPrimaryKeys(category.year, category.month, category.title, category.lang)
+            } ?: insert(category)
+        }
+    }
 }
