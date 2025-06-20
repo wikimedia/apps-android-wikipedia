56365fe17d3b5c2a8f54c851f7d1b653e3d17666
williamrai <williamrai13@gmail.com>
2025-06-20 16:02:49 -0400

    - fixes suggested edit screen test after adding logic to disable article description when edits are less than 50

--- app/src/androidTest/java/org/wikipedia/robots/screen/SuggestedEditsScreenRobot.kt
+++ app/src/androidTest/java/org/wikipedia/robots/screen/SuggestedEditsScreenRobot.kt
@@ -1,9 +1,11 @@
 package org.wikipedia.robots.screen
 
 import BaseRobot
+import android.content.Context
 import androidx.test.espresso.action.ViewActions.click
 import org.wikipedia.R
 import org.wikipedia.base.TestConfig
+import org.wikipedia.settings.Prefs
 
 class SuggestedEditsScreenRobot : BaseRobot() {
 
@@ -44,6 +46,7 @@ class SuggestedEditsScreenRobot : BaseRobot() {
 
     fun clickImageCaptions() = apply {
         list.scrollToRecyclerViewInsideNestedScrollView(recyclerViewId = R.id.tasksRecyclerView, position = 1, viewAction = click())
+        pressBack()
         delay(TestConfig.DELAY_MEDIUM)
     }
 
@@ -60,5 +63,21 @@ class SuggestedEditsScreenRobot : BaseRobot() {
     fun pressBack() = apply {
         goBack()
         delay(TestConfig.DELAY_MEDIUM)
+    }
+
+    fun verifyArticleDescriptionDoesNotExist(context: Context) = apply {
+        list.verifyItemDoesNotExistWithText(
+            recyclerViewId = R.id.tasksRecyclerView,
+            text = context.getString(R.string.description_edit_tutorial_title_descriptions)
+        )
+    }
+
+    fun increaseContribution() = apply {
+        Prefs.overrideSuggestedEditContribution = 100
+    }
+
+    fun disableImageCaptionOnboarding() = apply {
+        Prefs.showImageTagsOnboarding = false
+        Prefs.suggestedEditsImageRecsOnboardingShown = true
     }
 }
