74f9a1415e0db4bca972b268526addb58a00d80d
Cooltey Feng <cfeng@wikimedia.org>
2025-07-21 11:10:55 -0700

    Add donation reminder helper class for T399605

---
+++ app/src/main/java/org/wikipedia/donate/DonationReminderHelper.kt
@@ -0,0 +1,26 @@
+package org.wikipedia.donate
+
+import org.wikipedia.WikipediaApp
+import org.wikipedia.auth.AccountUtil
+import org.wikipedia.util.GeoUtil
+import org.wikipedia.util.ReleaseUtil
+import java.time.LocalDate
+
+object DonationReminderHelper {
+
+    // Users with a device location in Germany, France, Poland, or Philippines
+    private val enabledCountries = listOf(
+        "DE", "FR", "PL", "PH"
+    )
+
+    // Users with a primary language of German, English, French, or Polish
+    private val enabledLanguages = listOf(
+        "de", "fr", "pl", "en"
+    )
+
+    // TODO: update the end date when before release to production for 30-day experiment
+    val isEnabled get() = ReleaseUtil.isDevRelease ||
+            (enabledCountries.contains(GeoUtil.geoIPCountry.orEmpty()) &&
+                    enabledLanguages.contains(WikipediaApp.instance.languageState.appLanguageCode) &&
+                    LocalDate.now() <= LocalDate.of(2025, 12, 1) && !AccountUtil.isLoggedIn)
+}
