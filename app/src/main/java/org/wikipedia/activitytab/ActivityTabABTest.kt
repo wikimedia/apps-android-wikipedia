9c9720edc98b36df1cbe6fe3bf4496558b9a782d
Dmitry Brant <me@dmitrybrant.com>
2025-07-25 08:58:03 -0400

    Feature-flag for pre-beta only.

--- app/src/main/java/org/wikipedia/activitytab/ActivityTabABTest.kt
+++ app/src/main/java/org/wikipedia/activitytab/ActivityTabABTest.kt
@@ -1,9 +1,11 @@
 package org.wikipedia.activitytab
 
 import org.wikipedia.analytics.ABTest
+import org.wikipedia.util.ReleaseUtil
 
 class ActivityTabABTest : ABTest("activityTab", GROUP_SIZE_2) {
     fun isInTestGroup(): Boolean {
         return group == GROUP_2
+                && ReleaseUtil.isPreBetaRelease // TODO: remove before releasing
     }
 }
