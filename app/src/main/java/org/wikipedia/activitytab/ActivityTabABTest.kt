69330fc49f7e4864a4e4f342dacda7ab5de6d95b
Dmitry Brant <me@dmitrybrant.com>
2025-07-25 09:07:14 -0400

    Lint.

--- app/src/main/java/org/wikipedia/activitytab/ActivityTabABTest.kt
+++ app/src/main/java/org/wikipedia/activitytab/ActivityTabABTest.kt
@@ -5,7 +5,7 @@ import org.wikipedia.util.ReleaseUtil
 
 class ActivityTabABTest : ABTest("activityTab", GROUP_SIZE_2) {
     fun isInTestGroup(): Boolean {
-        return group == GROUP_2
-                && ReleaseUtil.isPreBetaRelease // TODO: remove before releasing
+        return group == GROUP_2 &&
+                ReleaseUtil.isPreBetaRelease // TODO: remove before releasing
     }
 }
