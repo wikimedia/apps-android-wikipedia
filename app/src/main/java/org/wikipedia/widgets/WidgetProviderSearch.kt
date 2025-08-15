e7a18c790b2bf2662b33d01df56f417342c5be93
Isira Seneviratne <isirasen96@gmail.com>
2025-08-16 04:36:42 +0530

    Backport dynamic layout for search widget

--- app/src/main/java/org/wikipedia/widgets/WidgetProviderSearch.kt
+++ app/src/main/java/org/wikipedia/widgets/WidgetProviderSearch.kt
@@ -7,9 +7,11 @@ import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.os.Build
-import android.util.SizeF
+import android.os.Bundle
 import android.widget.RemoteViews
 import androidx.core.app.PendingIntentCompat
+import androidx.core.util.SizeFCompat
+import androidx.core.widget.updateAppWidget
 import org.wikipedia.Constants.InvokeSource
 import org.wikipedia.R
 import org.wikipedia.search.SearchActivity
@@ -17,34 +19,40 @@ import org.wikipedia.search.SearchActivity
 class WidgetProviderSearch : AppWidgetProvider() {
     override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
         val thisWidget = ComponentName(context, WidgetProviderSearch::class.java)
-        val allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget)
-        for (widgetId in allWidgetIds) {
+        for (widgetId in appWidgetManager.getAppWidgetIds(thisWidget)) {
+            updateWidget(context, appWidgetManager, widgetId)
+        }
+    }
 
-            val pendingIntent = PendingIntentCompat.getActivity(context, 0,
-                SearchActivity.newIntent(context, InvokeSource.WIDGET, null)
-                    .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK),
-                PendingIntent.FLAG_UPDATE_CURRENT, false)
+    override fun onAppWidgetOptionsChanged(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetId: Int,
+        newOptions: Bundle,
+    ) {
+        // appWidgetManager.updateAppWidget must be called here on API levels < 31
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S) {
+            updateWidget(context, appWidgetManager, appWidgetId)
+        }
+    }
 
-            val largeView = RemoteViews(context.packageName, R.layout.widget_search_large)
-            largeView.setOnClickPendingIntent(R.id.widget_container, pendingIntent)
+    private fun updateWidget(context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int) {
+        val small = SizeFCompat(32f, 32f)
+        val medium = SizeFCompat(64f, 32f)
+        val large = SizeFCompat(160f, 32f)
+        val pendingIntent = PendingIntentCompat.getActivity(context, 0,
+            SearchActivity.newIntent(context, InvokeSource.WIDGET, null)
+                .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK),
+            PendingIntent.FLAG_UPDATE_CURRENT, false)
 
-            val remoteViews = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-                val smallView = RemoteViews(context.packageName, R.layout.widget_search_small)
-                val mediumView = RemoteViews(context.packageName, R.layout.widget_search_medium)
-                smallView.setOnClickPendingIntent(R.id.widget_container, pendingIntent)
-                mediumView.setOnClickPendingIntent(R.id.widget_container, pendingIntent)
-
-                val viewMapping: Map<SizeF, RemoteViews> = mapOf(
-                    SizeF(32f, 32f) to smallView,
-                    SizeF(64f, 32f) to mediumView,
-                    SizeF(160f, 32f) to largeView
-                )
-                RemoteViews(viewMapping)
-            } else {
-                largeView
+        appWidgetManager.updateAppWidget(appWidgetId, listOf(small, medium, large)) {
+            val remoteViewId = when (it) {
+                small -> R.layout.widget_search_small
+                medium -> R.layout.widget_search_medium
+                else -> R.layout.widget_search_large
             }
-
-            appWidgetManager.updateAppWidget(widgetId, remoteViews)
+            RemoteViews(context.packageName, remoteViewId)
+                .apply { setOnClickPendingIntent(R.id.widget_container, pendingIntent) }
         }
     }
 }
