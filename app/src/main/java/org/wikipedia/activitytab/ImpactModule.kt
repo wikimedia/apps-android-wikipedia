87d9d5f02da44dc6f67a813b0a7b6896ce09d001
Cooltey Feng <cfeng@wikimedia.org>
2025-08-25 17:25:30 -0700

    Remove maxLength

--- app/src/main/java/org/wikipedia/activitytab/ImpactModule.kt
+++ app/src/main/java/org/wikipedia/activitytab/ImpactModule.kt
@@ -39,7 +39,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -228,9 +227,7 @@ fun MostViewedCard(
                                 Text(
                                     text = description,
                                     style = MaterialTheme.typography.bodyMedium,
-                                    color = WikipediaTheme.colors.secondaryColor,
-                                    maxLines = 1,
-                                    overflow = TextOverflow.Ellipsis
+                                    color = WikipediaTheme.colors.secondaryColor
                                 )
                             }
                             Row(
