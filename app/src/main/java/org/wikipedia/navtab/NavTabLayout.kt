a0694e9f95645f1199748a853f43511fc0749df7
Cooltey Feng <cfeng@wikimedia.org>
2025-09-05 16:28:53 -0700

    Fix lint

--- app/src/main/java/org/wikipedia/navtab/NavTabLayout.kt
+++ app/src/main/java/org/wikipedia/navtab/NavTabLayout.kt
@@ -8,7 +8,6 @@ import android.view.Menu
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.LinearLayout
 import androidx.core.view.isVisible
 import androidx.core.view.updateMarginsRelative
 import com.google.android.material.bottomnavigation.BottomNavigationView
