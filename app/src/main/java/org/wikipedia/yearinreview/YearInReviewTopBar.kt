57ff01b8f196731a77dd7087f4d669ac10a86b16
williamrai <williamrai13@gmail.com>
2025-09-17 15:28:42 -0400

    - replace CenterAlignedTopAppBar with TopAppBar

--- app/src/main/java/org/wikipedia/yearinreview/YearInReviewTopBar.kt
+++ app/src/main/java/org/wikipedia/yearinreview/YearInReviewTopBar.kt
@@ -1,10 +1,10 @@
 package org.wikipedia.yearinreview
 
 import androidx.compose.foundation.layout.RowScope
-import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.painterResource
@@ -18,7 +18,7 @@ fun YearInReviewTopBar(
     onNavigationBackButtonClick: () -> Unit,
     actions: @Composable RowScope.() -> Unit = {},
 ) {
-    CenterAlignedTopAppBar(
+    TopAppBar(
         colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
             containerColor = WikipediaTheme.colors.paperColor),
         title = {
