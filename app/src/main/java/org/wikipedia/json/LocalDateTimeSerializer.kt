34953163f80d0c6a542db932495a7ea0d82407ee
Dmitry Brant <me@dmitrybrant.com>
2025-10-10 13:53:59 -0400

    Add files.

---
+++ app/src/main/java/org/wikipedia/json/LocalDateTimeSerializer.kt
@@ -0,0 +1,25 @@
+package org.wikipedia.json
+
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+object LocalDateTimeSerializer : KSerializer<LocalDateTime> {
+    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("LocalDateTime", PrimitiveKind.STRING)
+
+    override fun serialize(encoder: Encoder, value: LocalDateTime) {
+        encoder.encodeString(value.format(DateTimeFormatter.ISO_DATE_TIME))
+    }
+
+    override fun deserialize(decoder: Decoder): LocalDateTime {
+
+        val str = decoder.decodeString()
+
+        return LocalDateTime.parse(str, DateTimeFormatter.ISO_DATE_TIME)
+    }
+}
