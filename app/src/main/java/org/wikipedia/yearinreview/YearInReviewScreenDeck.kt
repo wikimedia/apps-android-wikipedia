7116643a21fb5fd4d24b47e4873a65f0e48d9607
Cooltey Feng <cfeng@wikimedia.org>
2025-10-16 11:19:29 -0700

    Lint

--- app/src/main/java/org/wikipedia/yearinreview/YearInReviewScreenDeck.kt
+++ app/src/main/java/org/wikipedia/yearinreview/YearInReviewScreenDeck.kt
@@ -27,8 +27,6 @@ import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
