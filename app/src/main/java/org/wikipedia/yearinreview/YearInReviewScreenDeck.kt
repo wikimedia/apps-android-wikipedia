24b8c6acbac20b51bf154d6dce4d6d05fe73f0d4
Cooltey Feng <cfeng@wikimedia.org>
2025-11-03 11:57:10 -0800

    YiR tweak: update image resources and function parameters for slides (#6058)
    * YiR tweak: update image resources and function parameters for slides
    
    * Change to modifier instead of size dp
    
    * Update asset for slide
    
    * Rename
    
    * Update image resource
    
    * - update proper gif resource
    
    ---------
    
    Co-authored-by: williamrai <williamrai13@gmail.com>

--- app/src/main/java/org/wikipedia/yearinreview/YearInReviewScreenDeck.kt
+++ app/src/main/java/org/wikipedia/yearinreview/YearInReviewScreenDeck.kt
@@ -555,7 +555,7 @@ fun PreviewScreenShot() {
         CreateScreenShotBitmap(
             screenContent = YearInReviewScreenData.StandardScreen(
                 allowDonate = true,
-                animatedImageResource = R.drawable.year_in_review_puzzle_pieces,
+                imageResource = R.drawable.yir_puzzle_browser,
                 headlineText = "Over 3 billion bytes added",
                 bodyText = "TBD"
             ),
@@ -572,7 +572,7 @@ fun PreviewStandardContent() {
             state = UiState.Success(listOf(
                 YearInReviewScreenData.StandardScreen(
                     allowDonate = true,
-                    animatedImageResource = R.drawable.year_in_review_puzzle_pieces,
+                    imageResource = R.drawable.yir_puzzle_bytes,
                     headlineText = "Over 3 billion bytes added",
                     bodyText = "TBD"
                 )
@@ -590,7 +590,7 @@ fun PreviewReadingPatternsContent() {
             state = UiState.Success(listOf(
                 YearInReviewScreenData.ReadingPatterns(
                     allowDonate = false,
-                    animatedImageResource = R.drawable.year_in_review_puzzle_pieces,
+                    imageResource = R.drawable.yir_puzzle_browser,
                     headlineText = "You have clear reading patterns",
                     bodyText = "",
                     favoriteTimeText = "Afternoon",
