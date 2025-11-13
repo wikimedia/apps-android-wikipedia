bd900fb09f7e0761fe564661d722785ca2d87abd
Cooltey Feng <cfeng@wikimedia.org>
2025-11-13 11:06:08 -0800

    Fix error

--- app/src/main/java/org/wikipedia/util/FeedbackUtil.kt
+++ app/src/main/java/org/wikipedia/util/FeedbackUtil.kt
@@ -14,6 +14,7 @@ import androidx.annotation.LayoutRes
 import androidx.annotation.StringRes
 import androidx.appcompat.widget.TooltipCompat
 import androidx.core.app.ActivityCompat
+import androidx.core.net.toUri
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
