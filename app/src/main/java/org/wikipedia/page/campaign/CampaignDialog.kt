8bacee71c75f485cd2eea1a565abcb80c1e4554a
Cooltey Feng <cfeng@wikimedia.org>
2025-11-19 13:02:07 -0800

    Append after constructed ID for campaign dialog

--- app/src/main/java/org/wikipedia/page/campaign/CampaignDialog.kt
+++ app/src/main/java/org/wikipedia/page/campaign/CampaignDialog.kt
@@ -8,6 +8,7 @@ import org.wikipedia.WikipediaApp
 import org.wikipedia.activity.BaseActivity
 import org.wikipedia.analytics.eventplatform.DonorExperienceEvent
 import org.wikipedia.dataclient.donate.Campaign
+import org.wikipedia.donate.donationreminder.DonationReminderAbTest
 import org.wikipedia.donate.donationreminder.DonationReminderHelper
 import org.wikipedia.settings.Prefs
 import org.wikipedia.util.CustomTabsUtil
@@ -19,11 +20,10 @@ import java.util.Date
 
 class CampaignDialog internal constructor(private val context: Context, val campaign: Campaign, val onNeutralBtnClick: ((campaignId: String) -> Unit)? = null) : AlertDialog.Builder(context), CampaignDialogView.Callback {
     private var dialog: AlertDialog? = null
-    private val campaignId = if (DonationReminderHelper.isInEligibleCountry) {
-        DonationReminderHelper.campaignId
-    } else {
-        campaign.getIdForLang(WikipediaApp.instance.appOrSystemLanguageCode)
-    }
+    private val campaignId = campaign.getIdForLang(WikipediaApp.instance.appOrSystemLanguageCode) +
+            if (DonationReminderHelper.isInEligibleCountry) {
+                if (DonationReminderAbTest().isTestGroupUser()) "_reminderB" else "_reminderA"
+            } else ""
 
     init {
         val campaignView = CampaignDialogView(context)
