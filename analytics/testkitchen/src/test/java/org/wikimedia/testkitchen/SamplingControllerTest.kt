8d84a077793984dfd792e69040194d7d8560a310
Dmitry Brant <me@dmitrybrant.com>
2025-12-12 11:50:15 -0500

    Delete java files, so that tests start passing.

--- analytics/testkitchen/src/test/java/org/wikimedia/testkitchen/SamplingControllerTest.kt
+++ analytics/testkitchen/src/test/java/org/wikimedia/testkitchen/SamplingControllerTest.kt
@@ -1,6 +1,12 @@
 package org.wikimedia.testkitchen
 
-import org.assertj.core.api.Assertions.assertThat
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertTrue
+import org.junit.Test
+import org.wikimedia.testkitchen.config.StreamConfig
+import org.wikimedia.testkitchen.config.sampling.SampleConfig
+import org.wikimedia.testkitchen.context.DataFixtures
 
 internal class SamplingControllerTest {
     private val samplingController = SamplingController(
@@ -10,51 +16,48 @@ internal class SamplingControllerTest {
 
     @Test
     fun testGetSamplingValue() {
-        val deviceVal = samplingController.getSamplingValue(DEVICE)
-        assertThat(deviceVal).isBetween(0.0, 1.0)
+        val deviceVal = samplingController.getSamplingValue(SampleConfig.UNIT_DEVICE)
+        assertTrue(deviceVal >= 0.0)
+        assertTrue(deviceVal <= 1.0)
     }
 
     @Test
     fun testGetSamplingId() {
-        assertThat(samplingController.getSamplingId(DEVICE)).isNotNull()
-        assertThat(samplingController.getSamplingId(SESSION)).isNotNull()
+        assertNotNull(samplingController.getSamplingId(SampleConfig.UNIT_DEVICE))
+        assertNotNull(samplingController.getSamplingId(SampleConfig.UNIT_SESSION))
     }
 
     @Test
     fun testNoSamplingConfig() {
-        val noSamplingConfig: StreamConfig = StreamConfig("foo", "bar", null, null, null)
-        assertThat(samplingController.isInSample(noSamplingConfig)).isTrue()
+        val noSamplingConfig = StreamConfig().also {
+            it.streamName = "foo"
+            it.schemaTitle = "bar"
+        }
+        assertTrue(samplingController.isInSample(noSamplingConfig))
     }
 
     @Test
     fun testAlwaysInSample() {
-        val alwaysInSample: StreamConfig = StreamConfig(
-            "foo", "bar", null,
-            ProducerConfig(
-                MetricsPlatformClientConfig(
-                    null,
-                    null,
-                    null
-                )
-            ),
-            null
-        )
-        assertThat(samplingController.isInSample(alwaysInSample)).isTrue()
+        val alwaysInSample = StreamConfig().also {
+            it.streamName = "foo"
+            it.schemaTitle = "bar"
+            it.producerConfig = StreamConfig.ProducerConfig().also { pc ->
+                pc.metricsPlatformClientConfig = StreamConfig.MetricsPlatformClientConfig()
+            }
+        }
+        assertTrue(samplingController.isInSample(alwaysInSample))
     }
 
     @Test
     fun testNeverInSample() {
-        val neverInSample: StreamConfig = StreamConfig(
-            "foo", "bar", null,
-            ProducerConfig(
-                MetricsPlatformClientConfig(
-                    null,
-                    null,
-                    null
-                )
-            ),
-            SampleConfig(0.0, SESSION)
-        )
-        assertThat(samplingController.isInSample(neverInSample)).isFalse()
+        val neverInSample = StreamConfig().also {
+            it.streamName = "foo"
+            it.schemaTitle = "bar"
+            it.producerConfig = StreamConfig.ProducerConfig().also { pc ->
+                pc.metricsPlatformClientConfig = StreamConfig.MetricsPlatformClientConfig()
+            }
+            it.sampleConfig = SampleConfig(0.0, SampleConfig.UNIT_SESSION)
+        }
+        assertFalse(samplingController.isInSample(neverInSample))
     }
 }
