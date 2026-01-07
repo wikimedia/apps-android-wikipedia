3bc41e6d9d3052e900955ebfcfe644bccc6680db
Cooltey Feng <cfeng@wikimedia.org>
2026-01-07 15:12:16 -0800

    Semantic Search: A/B test

---
+++ app/src/main/java/org/wikipedia/search/SemanticSearchAbTest.kt
@@ -0,0 +1,17 @@
+package org.wikipedia.search
+
+import org.wikipedia.analytics.ABTest
+
+class SemanticSearchAbTest : ABTest("semanticSearch", GROUP_SIZE_2) {
+
+    fun getGroupName(): String {
+        return when (group) {
+            GROUP_2 -> "b" // test group
+            else -> "a" // control
+        }
+    }
+
+    fun isTestGroupUser(): Boolean {
+        return group == GROUP_2
+    }
+}
