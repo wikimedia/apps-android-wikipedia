43c2417e0ba7385877e11c4b9bd765960105d734
Dmitry Brant <dbrant@wikimedia.org>
2026-01-07 13:42:23 -0500

    Initial conversion of Test Kitchen client library to Kotlin. (#6179)
    * First commit of MP module copied from client library.
    
    * Right...
    
    * Rename.
    
    * Rename.
    
    * Proceed...
    
    * Make it build.
    
    * Make it run.
    
    * Stay on target.
    
    * Start deduplicating.
    
    * Stay on target...
    
    * Update package name.
    
    * Clean up gradle a bit.
    
    * Further update naming.
    
    * Wire up logger.
    
    * A bit more cleanup.
    
    * First contact with actual consumption by app.base schema.
    
    * Correctly wire up interactionData.
    
    * Display errors very loudly when using Dev flavor.
    
    * Merge Event with EventProcessed.
    
    * Use constants.
    
    * Rename general ArticleEvent.
    
    * Introduce ktlint task.
    
    * Lint.
    
    * Better naming for default logging adapter.
    
    * Use string constants for serialization names.
    
    * Clarify sample integration.
    
    * Begin to tackle tests.
    
    * Simplify sessionId generation.
    
    * Touch up.
    
    * Add function to explicitly flush event queue.
    
    * Add json util class for testing.
    
    * First passing test.
    
    * Another test.
    
    * More tests and lint.
    
    * Delete java files, so that tests start passing.
    
    * More tests.
    
    * Correctly deserialize DestinationEventService.
    
    * More tests.
    
    * Flesh out more tests.
    
    * Fix app tests.
    
    * Wire up hasty parameter.
    
    * Tidy up.
    
    ---------
    
    Co-authored-by: Cooltey Feng <cfeng@wikimedia.org>

--- analytics/testkitchen/src/main/java/org/wikimedia/testkitchen/config/sampling/SampleConfig.kt
+++ analytics/testkitchen/src/main/java/org/wikimedia/testkitchen/config/sampling/SampleConfig.kt
@@ -1,14 +1,11 @@
-package org.wikipedia.analytics.eventplatform
+package org.wikimedia.testkitchen.config.sampling
 
 import kotlinx.serialization.Serializable
 
-/**
- * Represents the sampling config component of a stream configuration.
- */
 @Serializable
-class SamplingConfig(
-        val rate: Double = 1.0,
-        val unit: String = UNIT_SESSION
+class SampleConfig(
+    val rate: Double = 1.0,
+    val unit: String = UNIT_SESSION
 ) {
     companion object {
         const val UNIT_PAGEVIEW = "pageview"
---
+++ analytics/testkitchen/src/test/java/org/wikimedia/testkitchen/event/EventFixtures.kt
@@ -0,0 +1,39 @@
+package org.wikimedia.testkitchen.event
+
+import org.wikimedia.testkitchen.context.ClientData
+import org.wikimedia.testkitchen.context.InteractionData
+import org.wikimedia.testkitchen.context.PageData
+import org.wikimedia.testkitchen.context.PerformerData
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+
+object EventFixtures {
+    private val dt get() = DateTimeFormatter.ISO_DATE_TIME.format(ZonedDateTime.now(ZoneId.of("Z")))
+
+    fun minimalEvent(clientData: ClientData = ClientData()): Event {
+        return Event("test_schema", "test_stream", dt, clientData, InteractionData())
+    }
+
+    fun getEvent(
+        id: Int = 0,
+        namespaceName: String? = null,
+        groups: List<String> = emptyList(),
+        isLoggedIn: Boolean = false,
+        editCount: String? = null
+    ): Event {
+        return Event("test/event", "test.event", dt,
+            ClientData(
+                pageData = PageData().also {
+                    it.id = id
+                    it.namespaceName = namespaceName
+                },
+                performerData = PerformerData().also {
+                    it.groups = groups
+                    it.isLoggedIn = isLoggedIn
+                }
+            ),
+            InteractionData()
+        )
+    }
+}
