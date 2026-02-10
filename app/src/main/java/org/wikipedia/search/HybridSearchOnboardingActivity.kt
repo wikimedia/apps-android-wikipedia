e5eee38dd1ec60f4a3444333153bd1734f779ceb
williamrai <williamrai13@gmail.com>
2026-02-10 09:31:17 -0500

    - consolidate onboarding screens into single screen

--- app/src/main/java/org/wikipedia/search/HybridSearchOnboardingActivity.kt
+++ app/src/main/java/org/wikipedia/search/HybridSearchOnboardingActivity.kt
@@ -26,16 +26,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.foundation.pager.PagerState
-import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SuggestionChip
@@ -152,15 +149,9 @@ class HybridSearchOnboardingActivity : BaseActivity() {
 @Composable
 fun HybridSearchOnboardingScreen(
     modifier: Modifier = Modifier,
-    isHybridSearchEnabled: Boolean = Prefs.isHybridSearchEnabled,
     onGetStarted: (String?) -> Unit,
     onLearnMoreClick: () -> Unit
 ) {
-    val totalPageCount = 2
-    val pagerState = rememberPagerState(pageCount = { totalPageCount })
-    val coroutineScope = rememberCoroutineScope()
-    var isHybridSearchEnabled by remember { mutableStateOf(isHybridSearchEnabled) }
-
     Scaffold(
         modifier = modifier
             .safeDrawingPadding(),
@@ -202,117 +193,39 @@ fun HybridSearchOnboardingScreen(
             )
         },
         bottomBar = {
+            HorizontalDivider(
+                thickness = 0.5.dp,
+                color = WikipediaTheme.colors.borderColor
+            )
             MainBottomBar(
-                isHybridSearchEnabled = isHybridSearchEnabled,
-                pagerState = pagerState,
                 onNextButtonClick = {
-                    if (pagerState.currentPage == 0 && isHybridSearchEnabled) {
-                        coroutineScope.launch {
-                            pagerState.animateScrollToPage(
-                                page = 1,
-                                animationSpec = tween(durationMillis = 400)
-                            )
-                        }
-                    } else {
-                        onGetStarted(null)
-                    }
+                    onGetStarted(null)
                 },
                 onLearnMoreClick = onLearnMoreClick
             )
         }
     ) { paddingValues ->
-        HorizontalPager(
-            modifier = Modifier
+        Column(
+            Modifier
                 .padding(paddingValues)
-                .fillMaxSize(),
-            state = pagerState,
-            verticalAlignment = Alignment.Top,
-            userScrollEnabled = isHybridSearchEnabled
-        ) { page ->
-            Column(
-                modifier = Modifier
-                    .padding(horizontal = 24.dp)
-                    .verticalScroll(rememberScrollState())
-            ) {
-                when (page) {
-                    0 -> {
-                        val isVisible = remember { MutableTransitionState(false) }
-                        LaunchedEffect(Unit) {
-                            isVisible.targetState = true
-                        }
-
-                        AnimatedVisibility(
-                            visibleState = isVisible,
-                            enter = slideInVertically(
-                                animationSpec = tween(durationMillis = 300),
-                                initialOffsetY = { it / 2 }
-                            ) + fadeIn(
-                                animationSpec = tween(durationMillis = 300)
-                            )
-                        ) {
-                            Column {
-                                onboardingItems.forEachIndexed { index, onboardingItem ->
-                                    OnboardingListItem(item = onboardingItem)
-                                }
-                                ExperimentalFeatureToggleView(
-                                    isChecked = isHybridSearchEnabled,
-                                    onCheckedChange = {
-                                        isHybridSearchEnabled = it
-                                        Prefs.isHybridSearchEnabled = it
-                                    }
-                                )
-                            }
-                        }
-                    }
-
-                    1 -> {
-                        SearchExamplesView(
-                            modifier = Modifier
-                                .background(WikipediaTheme.colors.paperColor),
-                            onClick = { exampleQuery ->
-                                onGetStarted(exampleQuery)
-                            }
-                        )
-                    }
-                }
+                .padding(horizontal = 24.dp)
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+        ) {
+            onboardingItems.forEachIndexed { index, onboardingItem ->
+                OnboardingListItem(item = onboardingItem)
             }
+            SearchExamplesView(
+                modifier = Modifier
+                    .background(WikipediaTheme.colors.paperColor),
+                onClick = { exampleQuery ->
+                    onGetStarted(exampleQuery)
+                }
+            )
         }
     }
 }
 
-@Composable
-fun ExperimentalFeatureToggleView(
-    isChecked: Boolean,
-    onCheckedChange: (Boolean) -> Unit
-) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth(),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Spacer(modifier = Modifier.width(36.dp)) // (Icon size + spacer width) which aligns with the OnboardingList item
-        Text(
-            modifier = Modifier
-                .weight(1f),
-            text = stringResource(R.string.hybrid_search_experimental_feature_switch_label),
-            style = MaterialTheme.typography.titleMedium,
-            fontWeight = FontWeight.SemiBold,
-            color = WikipediaTheme.colors.primaryColor
-        )
-
-        Switch(
-            checked = isChecked,
-            onCheckedChange = onCheckedChange,
-            colors = SwitchDefaults.colors(
-                uncheckedTrackColor = WikipediaTheme.colors.paperColor,
-                uncheckedThumbColor = MaterialTheme.colorScheme.outline,
-                uncheckedBorderColor = MaterialTheme.colorScheme.outline,
-                checkedTrackColor = WikipediaTheme.colors.progressiveColor,
-                checkedThumbColor = WikipediaTheme.colors.paperColor
-            )
-        )
-    }
-}
 
 @Composable
 fun SearchExamplesView(
@@ -320,30 +233,16 @@ fun SearchExamplesView(
     searchExamples: List<Int> = defaultSearchQueries,
     onClick: (String) -> Unit
 ) {
-    val isVisible = remember { MutableTransitionState(false) }
-    LaunchedEffect(Unit) {
-        isVisible.targetState = true
-    }
     Column(
         modifier = modifier
     ) {
-        AnimatedVisibility(
-            visibleState = isVisible,
-            enter = slideInVertically(
-                animationSpec = tween(durationMillis = 300),
-                initialOffsetY = { it / 2 }
-            ) + fadeIn(
-                animationSpec = tween(durationMillis = 300)
+        OnboardingListItem(
+            item = OnboardingItem(
+                icon = R.drawable.ic_light_bulb,
+                title = R.string.hybrid_search_onboarding_search_example_title,
+                subTitle = R.string.hybrid_search_onboarding_search_example_description
             )
-        ) {
-            OnboardingListItem(
-                item = OnboardingItem(
-                    icon = R.drawable.ic_light_bulb,
-                    title = R.string.hybrid_search_onboarding_search_example_title,
-                    subTitle = R.string.hybrid_search_onboarding_search_example_description
-                )
-            )
-        }
+        )
 
         FlowRow(
             modifier = Modifier
@@ -352,41 +251,23 @@ fun SearchExamplesView(
         ) {
             searchExamples.forEachIndexed { index, exampleQuery ->
                 val exampleQueryString = stringResource(exampleQuery)
-                AnimatedVisibility(
-                    visibleState = isVisible,
-                    enter = slideInVertically(
-                        animationSpec = tween(
-                            durationMillis = 300,
-                            delayMillis = (index * 50) + 150,
-                            easing = LinearOutSlowInEasing
-                        ),
-                        initialOffsetY = { it / 2 }
-                    ) + fadeIn(
-                        animationSpec = tween(
-                            durationMillis = 300,
-                            delayMillis = (index * 50) + 150,
-                            easing = LinearOutSlowInEasing
+                SuggestionChip(
+                    onClick = { onClick(exampleQueryString) },
+                    label = {
+                        Text(
+                            text = exampleQueryString,
+                            style = MaterialTheme.typography.bodyMedium
                         )
+                    },
+                    colors = SuggestionChipDefaults.suggestionChipColors(
+                        containerColor = WikipediaTheme.colors.paperColor,
+                        labelColor = WikipediaTheme.colors.secondaryColor,
+                    ),
+                    border = SuggestionChipDefaults.suggestionChipBorder(
+                        enabled = true,
+                        borderColor = WikipediaTheme.colors.borderColor
                     )
-                ) {
-                    SuggestionChip(
-                        onClick = { onClick(exampleQueryString) },
-                        label = {
-                            Text(
-                                text = exampleQueryString,
-                                style = MaterialTheme.typography.bodyMedium
-                            )
-                        },
-                        colors = SuggestionChipDefaults.suggestionChipColors(
-                            containerColor = WikipediaTheme.colors.paperColor,
-                            labelColor = WikipediaTheme.colors.secondaryColor,
-                        ),
-                        border = SuggestionChipDefaults.suggestionChipBorder(
-                            enabled = true,
-                            borderColor = WikipediaTheme.colors.borderColor
-                        )
-                    )
-                }
+                )
             }
         }
     }
@@ -394,24 +275,23 @@ fun SearchExamplesView(
 
 @Composable
 fun MainBottomBar(
-    isHybridSearchEnabled: Boolean,
-    pagerState: PagerState,
     onNextButtonClick: () -> Unit,
     onLearnMoreClick: () -> Unit
 ) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(horizontal = 16.dp, vertical = 16.dp),
+            .padding(horizontal = 24.dp, vertical = 12.dp),
         horizontalArrangement = Arrangement.spacedBy(
             space = 24.dp,
         ),
         verticalAlignment = Alignment.CenterVertically
     ) {
         TextButton(
-            modifier = Modifier,
+            modifier = Modifier
+                .weight(1f),
             colors = ButtonDefaults.buttonColors(
-                containerColor = WikipediaTheme.colors.paperColor
+                containerColor = WikipediaTheme.colors.backgroundColor
             ),
             onClick = onLearnMoreClick
         ) {
@@ -422,41 +302,19 @@ fun MainBottomBar(
             )
         }
 
-        Row(
+        Button(
             modifier = Modifier
                 .weight(1f),
-            horizontalArrangement = Arrangement.Center
-        ) {
-            AnimatedVisibility(
-                visible = isHybridSearchEnabled,
-                enter = fadeIn() + expandHorizontally(),
-                exit = fadeOut() + shrinkHorizontally()
-            ) {
-                PageIndicator(
-                    modifier = Modifier,
-                    pagerState = pagerState
-                )
-            }
-        }
-
-        Button(
-            modifier = Modifier,
             colors = ButtonDefaults.buttonColors(
                 containerColor = WikipediaTheme.colors.progressiveColor
             ),
             onClick = onNextButtonClick
         ) {
-            AnimatedContent(
-                targetState = if (pagerState.currentPage == 0) stringResource(
-                    R.string.onboarding_next
-                ) else stringResource(R.string.onboarding_get_started),
-            ) { targetText ->
-                Text(
-                    text = targetText,
-                    style = MaterialTheme.typography.labelLarge,
-                    color = WikipediaTheme.colors.paperColor
-                )
-            }
+            Text(
+                text = stringResource(R.string.onboarding_get_started),
+                style = MaterialTheme.typography.labelLarge,
+                color = WikipediaTheme.colors.paperColor
+            )
         }
     }
 }
@@ -468,7 +326,6 @@ private fun HybridSearchOnboardingScreenPreview() {
         currentTheme = Theme.LIGHT
     ) {
         HybridSearchOnboardingScreen(
-            isHybridSearchEnabled = true,
             onGetStarted = {},
             onLearnMoreClick = {}
         )
