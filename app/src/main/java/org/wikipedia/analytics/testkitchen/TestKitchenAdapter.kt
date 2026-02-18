183a6a63464ebfffade7f457569cefb0822e4f10
Dmitry Brant <me@dmitrybrant.com>
2026-02-18 08:57:15 -0500

    Consolidate a bit.

--- app/src/main/java/org/wikipedia/analytics/testkitchen/TestKitchenAdapter.kt
+++ app/src/main/java/org/wikipedia/analytics/testkitchen/TestKitchenAdapter.kt
@@ -12,7 +12,11 @@ import org.wikipedia.BuildConfig
 import org.wikipedia.WikipediaApp
 import org.wikipedia.auth.AccountUtil
 import org.wikipedia.dataclient.okhttp.OkHttpConnectionFactory
+import org.wikipedia.dataclient.page.PageSummary
 import org.wikipedia.json.JsonUtil
+import org.wikipedia.page.Namespace
+import org.wikipedia.page.PageFragment
+import org.wikipedia.page.PageTitle
 import org.wikipedia.settings.Prefs
 import org.wikipedia.util.ReleaseUtil
 
@@ -65,5 +69,44 @@ object TestKitchenAdapter : ClientDataCallback {
 
     override fun getDomain(): String {
         return WikipediaApp.instance.wikiSite.authority()
+    }
+
+    fun getPageData(fragment: PageFragment?): PageData? {
+        val pageProperties = fragment?.page?.pageProperties ?: return null
+        return PageData(
+            pageProperties.pageId,
+            fragment.model.title?.prefixedText.orEmpty(),
+            pageProperties.namespace.code(),
+            Namespace.of(pageProperties.namespace.code()).toString(),
+            pageProperties.revisionId,
+            pageProperties.wikiBaseItem.orEmpty(),
+            fragment.model.title?.wikiSite?.languageCode.orEmpty()
+        )
+    }
+
+    fun getPageData(pageTitle: PageTitle?, pageId: Int = 0, revisionId: Long = 0): PageData? {
+        if (pageTitle == null) return null
+        return PageData(
+            pageId,
+            pageTitle.prefixedText,
+            pageTitle.namespace().code(),
+            Namespace.of(pageTitle.namespace().code()).toString(),
+            revisionId,
+            null,
+            pageTitle.wikiSite.languageCode
+        )
+    }
+
+    fun getPageData(pageTitle: PageTitle?, pageSummary: PageSummary?): PageData? {
+        if (pageTitle == null) return null
+        return PageData(
+            pageSummary?.pageId,
+            pageTitle.prefixedText,
+            pageTitle.namespace().code(),
+            Namespace.of(pageTitle.namespace().code()).toString(),
+            pageSummary?.revision,
+            pageSummary?.wikiBaseItem,
+            pageTitle.wikiSite.languageCode
+        )
     }
 }
