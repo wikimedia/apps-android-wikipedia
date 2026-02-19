496fb20f4e491c7a00e31a9c3ed666a87c617c92
Dmitry Brant <me@dmitrybrant.com>
2026-02-18 19:22:32 -0500

    Add timing.

--- analytics/testkitchen/src/main/java/org/wikimedia/testkitchen/instrument/Funnel.kt
+++ analytics/testkitchen/src/main/java/org/wikimedia/testkitchen/instrument/Funnel.kt
@@ -7,10 +7,32 @@ class Funnel(
 ) {
     val token = SessionController.generateSessionId()
     val sequence get() = _sequence
-
+    private var startTime = System.currentTimeMillis()
+    private var pauseTime = 0L
     private var _sequence: Int = 1
 
     fun touch() {
         _sequence++
+    }
+
+    val duration get() = (System.currentTimeMillis() - startTime).toInt()
+
+    fun pause() {
+        pauseTime = System.currentTimeMillis()
+    }
+
+    fun resume() {
+        if (pauseTime > 0) {
+            startTime += System.currentTimeMillis() - pauseTime
+        }
+        pauseTime = 0
+    }
+
+    fun reset() {
+        startTime = System.currentTimeMillis()
+    }
+
+    fun addActionContext(actionContext: MutableMap<String, String>) {
+        actionContext["time_spent_ms"] = duration.toString()
     }
 }
