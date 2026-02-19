5c40f3fe3e195f5612d8294b17940f2ec88ce79c
Dmitry Brant <me@dmitrybrant.com>
2026-02-19 12:53:51 -0500

    Hybrid search: wire up with remote config.

--- app/src/main/java/org/wikipedia/search/HybridSearchAbCTest.kt
+++ app/src/main/java/org/wikipedia/search/HybridSearchAbCTest.kt
@@ -2,31 +2,40 @@ package org.wikipedia.search
 
 import org.wikipedia.analytics.ABTest
 import org.wikipedia.settings.Prefs
+import org.wikipedia.settings.RemoteConfig
 
 class HybridSearchAbCTest : ABTest("hybridSearch", GROUP_SIZE_3) {
 
     fun getGroupName(): String {
         return when (group) {
-            GROUP_2 -> GROUP_LEXICAL_SEMANTIC // test group B
-            GROUP_3 -> GROUP_SEMANTIC_LEXICAL // test group C
+            GROUP_2 -> GROUP_LEXICAL_SEMANTIC
+            GROUP_3 -> GROUP_SEMANTIC_LEXICAL
             else -> GROUP_CONTROL
         }
+    }
+
+    fun isTestActive(): Boolean {
+        return RemoteConfig.config.androidv1?.hybridSearchEnabled ?: true
     }
 
     fun isTestGroupUser(): Boolean {
         return group != GROUP_1
     }
 
-    fun shouldShowOnboarding(languageCode: String?): Boolean {
-        return isTestGroupUser() && supportedLanguages.any { it.equals(languageCode, true) } && !Prefs.isHybridSearchOnboardingShown
+    private fun isLanguageSupported(languageCode: String?): Boolean {
+        return (RemoteConfig.config.androidv1?.hybridSearchLanguages ?: supportedLanguages).any { it.equals(languageCode, true) }
     }
 
-    val supportedLanguages = listOf(
+    fun shouldShowOnboarding(languageCode: String?): Boolean {
+        return isTestGroupUser() && isLanguageSupported(languageCode) && !Prefs.isHybridSearchOnboardingShown
+    }
+
+    private val supportedLanguages = listOf(
         "el"
     )
 
     fun isHybridSearchEnabled(languageCode: String?): Boolean {
-        return Prefs.isHybridSearchEnabled && isTestGroupUser() && supportedLanguages.any { it.equals(languageCode, true) }
+        return isTestActive() && Prefs.isHybridSearchEnabled && isTestGroupUser() && isLanguageSupported(languageCode)
     }
 
     companion object {
