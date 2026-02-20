9b5f5126d075924e84b2b95da78558349e83b8f7
Cooltey Feng <cfeng@wikimedia.org>
2026-02-19 16:55:23 -0800

    WikiGames: Games Hub

---
+++ app/src/main/java/org/wikipedia/games/GamesHubFragment.kt
@@ -0,0 +1,108 @@
+package org.wikipedia.games
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import androidx.core.net.toUri
+import androidx.core.view.MenuProvider
+import androidx.fragment.app.Fragment
+import org.wikipedia.R
+import org.wikipedia.auth.AccountUtil
+import org.wikipedia.compose.theme.BaseTheme
+import org.wikipedia.notifications.NotificationActivity
+import org.wikipedia.settings.Prefs
+import org.wikipedia.util.FeedbackUtil
+import org.wikipedia.util.UriUtil
+import org.wikipedia.views.NotificationButtonView
+
+class GamesHubFragment : Fragment() {
+
+    private lateinit var notificationButtonView: NotificationButtonView
+    private val menuProvider = object : MenuProvider {
+
+        override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+            menuInflater.inflate(R.menu.menu_games_hub_overflow, menu)
+        }
+
+        override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+            return when (menuItem.itemId) {
+                R.id.menu_game_stats -> {
+                    // TODO: open the game stats screen
+                    true
+                }
+                R.id.menu_learn_more -> {
+                    UriUtil.visitInExternalBrowser(requireActivity(), getString(R.string.on_this_day_game_wiki_url).toUri())
+                    true
+                }
+                else -> false
+            }
+        }
+
+        override fun onPrepareMenu(menu: Menu) {
+            val notificationMenuItem = menu.findItem(R.id.menu_notifications)
+            if (AccountUtil.isLoggedIn) {
+                notificationMenuItem.isVisible = true
+                notificationButtonView.setUnreadCount(Prefs.notificationUnreadCount)
+                notificationButtonView.setOnClickListener {
+                    if (AccountUtil.isLoggedIn) {
+                        startActivity(NotificationActivity.newIntent(requireActivity()))
+                    }
+                }
+                notificationButtonView.contentDescription = getString(R.string.notifications_activity_title)
+                notificationMenuItem.actionView = notificationButtonView
+                notificationMenuItem.expandActionView()
+                FeedbackUtil.setButtonTooltip(notificationButtonView)
+            } else {
+                notificationMenuItem.isVisible = false
+            }
+            updateNotificationDot(false)
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        super.onCreateView(inflater, container, savedInstanceState)
+
+        notificationButtonView = NotificationButtonView(requireActivity())
+
+        return ComposeView(requireContext()).apply {
+            setContent {
+                BaseTheme {
+                }
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        requireActivity().addMenuProvider(menuProvider, viewLifecycleOwner)
+        requireActivity().invalidateOptionsMenu()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        requireActivity().removeMenuProvider(menuProvider)
+    }
+
+
+    fun updateNotificationDot(animate: Boolean) {
+        if (AccountUtil.isLoggedIn && Prefs.notificationUnreadCount > 0) {
+            notificationButtonView.setUnreadCount(Prefs.notificationUnreadCount)
+            if (animate) {
+                notificationButtonView.runAnimation()
+            }
+        } else {
+            notificationButtonView.setUnreadCount(0)
+        }
+    }
+
+    companion object {
+        fun newInstance(): GamesHubFragment {
+            return GamesHubFragment()
+        }
+    }
+}
