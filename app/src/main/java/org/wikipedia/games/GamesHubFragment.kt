25c2cd7e44ca1826e6fc16c84a36484a66b5e66f
Cooltey Feng <cfeng@wikimedia.org>
2026-02-19 19:28:32 -0800

    language selector

--- app/src/main/java/org/wikipedia/games/GamesHubFragment.kt
+++ app/src/main/java/org/wikipedia/games/GamesHubFragment.kt
@@ -7,13 +7,36 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.core.net.toUri
 import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import org.wikipedia.R
+import org.wikipedia.WikipediaApp
 import org.wikipedia.auth.AccountUtil
 import org.wikipedia.compose.theme.BaseTheme
+import org.wikipedia.compose.theme.WikipediaTheme
 import org.wikipedia.notifications.NotificationActivity
 import org.wikipedia.settings.Prefs
 import org.wikipedia.util.FeedbackUtil
@@ -72,6 +95,7 @@ class GamesHubFragment : Fragment() {
         return ComposeView(requireContext()).apply {
             setContent {
                 BaseTheme {
+                    GamesHubScreen()
                 }
             }
         }
@@ -96,6 +120,57 @@ class GamesHubFragment : Fragment() {
             }
         } else {
             notificationButtonView.setUnreadCount(0)
+        }
+    }
+
+    @Composable
+    fun GamesHubScreen() {
+        val languageList = WikipediaApp.instance.languageState.appLanguageCodes
+        var selectedLanguage by remember { mutableStateOf(WikipediaApp.instance.languageState.appLanguageCode) }
+        Scaffold(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(WikipediaTheme.colors.paperColor),
+            containerColor = WikipediaTheme.colors.paperColor
+        ) { paddingValues ->
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues)
+            ) {
+                LazyRow(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    items(languageList.size) { index ->
+                        val langText =
+                            WikipediaApp.instance.languageState.getAppLanguageLocalizedName(
+                                languageList[index]
+                            )
+                        val isSelected = languageList[index] == selectedLanguage
+                        val textColor =
+                            if (isSelected) WikipediaTheme.colors.primaryColor else WikipediaTheme.colors.secondaryColor
+                        FilterChip(
+                            selected = isSelected,
+                            onClick = {
+                                selectedLanguage = languageList[index]
+                            },
+                            label = {
+                                Text(
+                                    text = langText.orEmpty(),
+                                    style = MaterialTheme.typography.labelLarge,
+                                    textAlign = TextAlign.Center,
+                                    fontWeight = FontWeight.Medium,
+                                    color = textColor
+                                )
+                            }
+                        )
+                    }
+                }
+            }
         }
     }
 
