6bb67a889297d4b49b7d716e1d2f75a498a8c598
Dmitry Brant <me@dmitrybrant.com>
2026-02-20 09:18:00 -0500

    Adapt other a/b tests.

--- app/src/main/java/org/wikipedia/yearinreview/YearInReviewReadingListAbTest.kt
+++ app/src/main/java/org/wikipedia/yearinreview/YearInReviewReadingListAbTest.kt
@@ -6,4 +6,8 @@ class YearInReviewReadingListAbTest : ABTest("yirReadingList", GROUP_SIZE_2) {
     fun isTestGroupUser(): Boolean {
         return group == GROUP_2
     }
+
+    override fun getGroupName(): String {
+        return if (isTestGroupUser()) "android_yir_2025_b" else "android_yir_2025_a"
+    }
 }
