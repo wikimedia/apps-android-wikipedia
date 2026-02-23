ea33cbcc6b13a9bcc2ee13dbaeddb3f2a07b0447
williamrai <williamrai13@gmail.com>
2026-02-23 10:51:33 -0500

    - merge conflict fixes

--- app/src/main/java/org/wikipedia/feed/wikigames/WikiGamesCard.kt
+++ app/src/main/java/org/wikipedia/feed/wikigames/WikiGamesCard.kt
@@ -1,10 +1,8 @@
 package org.wikipedia.feed.wikigames
 
-import org.wikipedia.R
 import org.wikipedia.dataclient.WikiSite
+import org.wikipedia.feed.model.Card
 import org.wikipedia.feed.model.CardType
-import org.wikipedia.feed.model.WikiSiteCard
-import org.wikipedia.util.L10nUtil
 
 class WikiGamesCard(val wikiSite: WikiSite, val games: List<WikiGame>) : Card() {
     override fun type(): CardType {
