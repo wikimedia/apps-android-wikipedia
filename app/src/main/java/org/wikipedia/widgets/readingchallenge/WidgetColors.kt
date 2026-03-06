c7821c4d6f232fa339cfdce88973cabd43761e26
williamrai <williamrai13@gmail.com>
2026-03-06 16:47:03 -0500

    - adds small widget components - adds colors, gradient background - adds preview images - adds resources

---
+++ app/src/main/java/org/wikipedia/widgets/readingchallenge/WidgetColors.kt
@@ -0,0 +1,27 @@
+package org.wikipedia.widgets.readingchallenge
+
+import androidx.compose.ui.graphics.Color
+import org.wikipedia.R
+
+object WidgetColors {
+    val challengeNotOptInBackground = Color(0xFFFFE49C)
+    val normalReadingBackground = Color(0xFFF5EBF2)
+    val musicReadingBackground = Color(0xFFD9E2FF)
+    val spaceReadingBackground = Color(0xFFE6E0F0)
+    val streakOngoingBackground = Color(0xFFFFEAD4)
+    val completedBackground = Color(0xFFB6D4FB)
+
+    val readingContent = Color(0xFF9B527F)
+    val musicContent = Color(0xFF534FA3)
+    val spaceContent = Color(0xFF3056A9)
+
+    val sleepingContent = Color(0xFFA95226)
+
+    val white = Color(0xFFFFFFFF)
+    val primary = Color(0xFF202122)
+    val progressive = Color(0xFF3366CC)
+}
+
+object WidgetBackground {
+    val challengeNotOptInRadialGradient = R.drawable.widget_radial_background
+}
