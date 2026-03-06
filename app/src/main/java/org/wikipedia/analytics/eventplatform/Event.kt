adeaa69096d169697d30238138e6ef957fa331f9
Dmitry Brant <me@dmitrybrant.com>
2026-03-06 08:55:52 -0500

    No need for dt in meta.

--- app/src/main/java/org/wikipedia/analytics/eventplatform/Event.kt
+++ app/src/main/java/org/wikipedia/analytics/eventplatform/Event.kt
@@ -2,7 +2,6 @@ package org.wikipedia.analytics.eventplatform
 
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.Transient
-import java.time.Instant
 
 // Base class for an Event Platform event.
 // This class MUST be `sealed` for Serialization polymorphism to work automatically.
@@ -12,8 +11,5 @@ sealed class Event(@Transient val stream: String = "") {
     private val meta = Meta(stream)
 
     @Serializable
-    private class Meta(
-        val stream: String,
-        val dt: String = Instant.now().toString()
-    )
+    private class Meta(val stream: String)
 }
