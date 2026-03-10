070d37aad9be93d7f649a8f842acb77b051098dd
williamrai <williamrai13@gmail.com>
2026-03-10 09:26:18 -0400

    - code fixes

--- app/src/main/java/org/wikipedia/widgets/readingchallenge/ReadingChallengeWidgetRepository.kt
+++ app/src/main/java/org/wikipedia/widgets/readingchallenge/ReadingChallengeWidgetRepository.kt
@@ -30,6 +30,7 @@ class ReadingChallengeWidgetRepository(private val context: Context) {
 
             val listener = SharedPreferences.OnSharedPreferenceChangeListener { _, _ -> emit() }
             prefs.registerOnSharedPreferenceChangeListener(listener)
+            emit()
             awaitClose { prefs.unregisterOnSharedPreferenceChangeListener(listener) }
         }.distinctUntilChanged()
     }
@@ -103,7 +104,7 @@ class ReadingChallengeWidgetRepository(private val context: Context) {
     }
 
     companion object {
-        private val START_DATE = LocalDate.of(2026, 5, 1)
+        private val START_DATE = LocalDate.of(2026, 3, 1)
         private val END_DATE = LocalDate.of(2026, 5, 31)
         private val REMOVE_DATE = LocalDate.of(2026, 7, 10)
         private const val READING_STREAK_GOAL = 25
