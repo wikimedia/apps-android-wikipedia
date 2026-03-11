1c07a68eb79dc437a9807001b0cfa1bc0253886b
williamrai <williamrai13@gmail.com>
2026-03-11 13:22:02 -0400

    - adds a worker for scheduling widget update

--- app/src/main/java/org/wikipedia/widgets/readingchallenge/smallwidget/ReadingChallengeSmallWidgetReceiver.kt
+++ app/src/main/java/org/wikipedia/widgets/readingchallenge/smallwidget/ReadingChallengeSmallWidgetReceiver.kt
@@ -1,9 +1,21 @@
 package org.wikipedia.widgets.readingchallenge.smallwidget
 
+import android.appwidget.AppWidgetManager
+import android.content.Context
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
+import org.wikipedia.widgets.readingchallenge.ReadingChallengeWidgetWorker
 
 class ReadingChallengeSmallWidgetReceiver : GlanceAppWidgetReceiver() {
     override val glanceAppWidget: GlanceAppWidget
         get() = ReadingChallengeSmallWidget()
+
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds)
+        ReadingChallengeWidgetWorker.scheduleNextMidnightUpdate(context)
+    }
 }
