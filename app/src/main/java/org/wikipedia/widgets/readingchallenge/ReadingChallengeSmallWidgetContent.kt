8f6cca4d2a02bb5fca09c91217152e09f7bba483
williamrai <williamrai13@gmail.com>
2026-03-18 09:07:38 -0400

    - merge conflic fixes

--- app/src/main/java/org/wikipedia/widgets/readingchallenge/ReadingChallengeSmallWidgetContent.kt
+++ app/src/main/java/org/wikipedia/widgets/readingchallenge/ReadingChallengeSmallWidgetContent.kt
@@ -8,8 +8,8 @@ import androidx.glance.GlanceModifier
 import androidx.glance.Image
 import androidx.glance.ImageProvider
 import androidx.glance.LocalContext
-import androidx.glance.action.clickable
 import androidx.glance.action.actionStartActivity
+import androidx.glance.action.clickable
 import androidx.glance.appwidget.action.actionStartActivity
 import androidx.glance.layout.Alignment
 import androidx.glance.layout.Box
