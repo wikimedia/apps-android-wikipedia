f4aecc965e149a84ff4ba68e1323912ef466971c
williamrai <williamrai13@gmail.com>
2026-03-24 11:21:22 -0400

    - lint fixes

--- app/src/main/java/org/wikipedia/feed/FeedFragment.kt
+++ app/src/main/java/org/wikipedia/feed/FeedFragment.kt
@@ -17,7 +17,6 @@ import org.wikipedia.Constants.InvokeSource
 import org.wikipedia.R
 import org.wikipedia.WikipediaApp
 import org.wikipedia.activity.FragmentUtil.getCallback
-import org.wikipedia.analytics.eventplatform.WikiGamesEvent
 import org.wikipedia.databinding.FragmentFeedBinding
 import org.wikipedia.dataclient.WikiSite
 import org.wikipedia.feed.FeedCoordinatorBase.FeedUpdateListener
