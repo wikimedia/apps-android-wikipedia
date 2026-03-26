43bcfa2a5fd1a0fd5cddea941056ddd652eb08c2
William Rai <48931640+Williamrai@users.noreply.github.com>
2026-03-26 16:39:44 -0400

    New Explore feed onboarding nav (#6456)
    * - adds navigation arch
    
    * - simplify navigation

--- app/src/main/java/org/wikipedia/onboarding/InitialOnboardingActivity.kt
+++ app/src/main/java/org/wikipedia/onboarding/InitialOnboardingActivity.kt
@@ -3,34 +3,83 @@ package org.wikipedia.onboarding
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
-import androidx.activity.addCallback
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import org.wikipedia.activity.BaseActivity
+import org.wikipedia.compose.components.AppTextButton
+import org.wikipedia.compose.theme.BaseTheme
+import org.wikipedia.compose.theme.WikipediaTheme
 import org.wikipedia.settings.Prefs
 
-class InitialOnboardingActivity : BaseActivity(), OnboardingFragment.Callback {
-    // TODO: think about the way of how to handle the navigation between onboarding screens.
-    override fun onSkip() {}
+class InitialOnboardingActivity : BaseActivity() {
 
-    override fun onComplete() {
-        Prefs.isInitialOnboardingEnabled = false
-        finish()
-    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
-        onBackPressedDispatcher.addCallback(this) {
-            setResult(RESULT_OK)
-            finish()
+        setContent {
+            BaseTheme {
+                InitialOnboardingScreen(
+                    isNewUser = Prefs.isInitialOnboardingEnabled,
+                    onFinish = {
+                        Prefs.isInitialOnboardingEnabled = false
+                        setResult(RESULT_OK)
+                        finish()
+                    }
+                )
+            }
         }
-        // TODO: remove this later
-        onComplete()
     }
 
     companion object {
         const val RESULT_LANGUAGE_CHANGED = 1
         fun newIntent(context: Context): Intent {
             return Intent(context, InitialOnboardingActivity::class.java)
+        }
+    }
+}
+
+@Composable
+fun InitialOnboardingScreen(
+    modifier: Modifier = Modifier,
+    isNewUser: Boolean,
+    onFinish: () -> Unit
+) {
+    var showPersonalization by remember { mutableStateOf(isNewUser) }
+    if (showPersonalization) {
+        IntroScreen(onClick = onFinish )
+    } else {
+        // Personalization Screen (interest selection + content preference + language)
+    }
+}
+
+// TODO: remove later with actual screen
+@Composable
+fun IntroScreen(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(WikipediaTheme.colors.paperColor),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        AppTextButton (
+            onClick = onClick
+        ) {
+            Text("Skip", color = WikipediaTheme.colors.progressiveColor)
         }
     }
 }
