d4e49aeea8f135411b37cd85a353d240a5a3b6e1
Cooltey Feng <cfeng@wikimedia.org>
2026-04-14 19:51:30 -0700

    NewExploreFeed: In the news module

--- app/src/main/java/org/wikipedia/feed/news/NewsItem.kt
+++ app/src/main/java/org/wikipedia/feed/news/NewsItem.kt
@@ -2,6 +2,7 @@ package org.wikipedia.feed.news
 
 import android.net.Uri
 import android.os.Parcelable
+import androidx.core.net.toUri
 import kotlinx.parcelize.Parcelize
 import kotlinx.serialization.Serializable
 import org.wikipedia.Constants
@@ -13,7 +14,7 @@ import org.wikipedia.util.ImageUrlUtil
 @Serializable
 class NewsItem(
     val story: String = "",
-    val links: List<PageSummary> = emptyList()
+    val links: List<PageSummary> = emptyList(),
 ) : Parcelable {
 
     fun linkCards(wiki: WikiSite): List<NewsLinkCard> {
@@ -22,12 +23,21 @@ class NewsItem(
 
     fun thumb(): Uri? {
         return getFirstImageUri(links)?.let {
-            Uri.parse(ImageUrlUtil.getUrlForPreferredSize(
-                    it.toString(), Constants.PREFERRED_CARD_THUMBNAIL_SIZE))
+            ImageUrlUtil.getUrlForPreferredSize(
+                it.toString(), Constants.PREFERRED_CARD_THUMBNAIL_SIZE
+            ).toUri()
+        }
+    }
+
+    fun thumbUrl(): String? {
+        return getFirstImageUri(links)?.let {
+            ImageUrlUtil.getUrlForPreferredSize(
+                it.toString(), Constants.PREFERRED_CARD_THUMBNAIL_SIZE
+            )
         }
     }
 
     private fun getFirstImageUri(links: List<PageSummary>): Uri? {
-        return links.firstOrNull { !it.thumbnailUrl.isNullOrEmpty() }?.run { Uri.parse(thumbnailUrl) }
+        return links.firstOrNull { !it.thumbnailUrl.isNullOrEmpty() }?.thumbnailUrl?.toUri()
     }
 }
---
+++ app/src/main/java/org/wikipedia/feed/news/NewsModule.kt
@@ -0,0 +1,142 @@
+package org.wikipedia.feed.news
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
+import org.wikipedia.R
+import org.wikipedia.compose.components.HtmlText
+import org.wikipedia.compose.theme.BaseTheme
+import org.wikipedia.compose.theme.WikipediaTheme
+import org.wikipedia.dataclient.page.PageSummary
+import org.wikipedia.feed.CommunityModuleHeader
+import org.wikipedia.theme.Theme
+import org.wikipedia.views.imageservice.ImageService
+
+@Composable
+fun NewsModule(
+    newsItems: List<NewsItem>,
+    onNewsClick: (item: NewsItem) -> Unit = {},
+    onOverflowClick: () -> Unit = {}
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color = WikipediaTheme.colors.paperColor)
+    ) {
+        CommunityModuleHeader(
+            titleResId = R.string.view_card_news_title,
+            subTitleResId = R.string.explore_feed_in_the_news_subtitle,
+            onOverflowClick = onOverflowClick
+        )
+
+        LazyColumn {
+            items(newsItems.size) { index ->
+                NewsItemContent(
+                    newsItem = newsItems[index],
+                    onItemClick = onNewsClick
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun NewsItemContent(
+    newsItem: NewsItem,
+    onItemClick: (NewsItem) -> Unit = {},
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 16.dp, end = 16.dp, bottom = 16.dp)
+            .clip(RoundedCornerShape(16.dp))
+            .clickable { onItemClick(newsItem) }
+    ) {
+        AsyncImage(
+            model = newsItem.thumbUrl()?.let { ImageService.getRequest(LocalContext.current, url = it) },
+            placeholder = ColorPainter(WikipediaTheme.colors.backgroundColor),
+            error = ColorPainter(WikipediaTheme.colors.backgroundColor),
+            contentDescription = newsItem.story,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(360.dp)
+        )
+
+        if (newsItem.story.isNotEmpty()) {
+            Column(
+                modifier = Modifier
+                    .align(Alignment.BottomStart)
+                    .fillMaxWidth()
+                    .padding(16.dp)
+                    .clip(RoundedCornerShape(8.dp))
+                    .background(WikipediaTheme.colors.paperColor.copy(alpha = 0.90f))
+                    .padding(16.dp)
+            ) {
+                HorizontalDivider(
+                    modifier = Modifier.padding(vertical = 12.dp).width(48.dp),
+                    thickness = 1.dp,
+                    color = WikipediaTheme.colors.secondaryColor.copy(alpha = 0.2f)
+                )
+                HtmlText(
+                    text = newsItem.story,
+                    color = WikipediaTheme.colors.primaryColor,
+                    style = MaterialTheme.typography.bodyMedium,
+                    maxLines = 4,
+                    overflow = TextOverflow.Ellipsis
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun NewsCardPreview() {
+    val pageSummary = PageSummary(
+        displayTitle = "Dog and Cat",
+        prefixTitle = "Dog_and_Cat",
+        description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        extract = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        thumbnail = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Commons-logo.svg/1200px-Commons-logo.svg.png",
+        lang = "en"
+    )
+    BaseTheme(currentTheme = Theme.LIGHT) {
+        NewsModule(
+            newsItems = listOf(
+                NewsItem(
+                    story = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                    links = listOf(pageSummary)
+                ),
+                NewsItem(
+                    story = "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                    links = listOf(pageSummary)
+                ),
+                NewsItem(
+                    story = "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                    links = listOf(pageSummary)
+                )
+            )
+        )
+    }
+}
