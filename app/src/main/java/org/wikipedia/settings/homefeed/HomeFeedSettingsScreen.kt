0dfd966aba870eb8790b496063276aaae92f2e55
Cooltey Feng <cfeng@wikimedia.org>
2026-05-14 11:16:01 -0700

    Make the home setting screen scrollable

--- app/src/main/java/org/wikipedia/settings/homefeed/HomeFeedSettingsScreen.kt
+++ app/src/main/java/org/wikipedia/settings/homefeed/HomeFeedSettingsScreen.kt
@@ -3,6 +3,8 @@ package org.wikipedia.settings.homefeed
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -40,7 +42,8 @@ fun HomeFeedSettingsScreen(
         Column(
             modifier = Modifier
                 .padding(paddingValues)
-                .padding(top = 24.dp),
+                .padding(top = 24.dp)
+                .verticalScroll(rememberScrollState()),
             verticalArrangement = Arrangement.spacedBy(34.dp)
         ) {
             SettingsSection(
