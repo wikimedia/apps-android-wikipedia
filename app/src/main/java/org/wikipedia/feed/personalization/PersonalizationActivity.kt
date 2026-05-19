882ba1736f40a0c7fe0b68df0078d0a9a6c52343
Dmitry Brant <me@dmitrybrant.com>
2026-05-19 19:00:36 -0400

    Home feed: Clear "For you" cache upon toggling any interest.

--- app/src/main/java/org/wikipedia/feed/personalization/PersonalizationActivity.kt
+++ app/src/main/java/org/wikipedia/feed/personalization/PersonalizationActivity.kt
@@ -14,7 +14,6 @@ import org.wikipedia.extensions.parcelableExtra
 import org.wikipedia.feed.onboarding.ExploreFeedBuildingActivity
 import org.wikipedia.page.PageTitle
 import org.wikipedia.search.SearchActivity
-import org.wikipedia.settings.Prefs
 
 class PersonalizationActivity : BaseActivity() {
 
@@ -60,9 +59,6 @@ class PersonalizationActivity : BaseActivity() {
                     },
                     onCompleteOnboardingClick = {
                         startActivity(ExploreFeedBuildingActivity.newIntent(this))
-                        // This implies that the user changed their interests, so we should clear the cache of
-                        // today's "For You" feed cards to ensure that they see the updated feed immediately.
-                        Prefs.homeForYouModulesToday = ""
                         setResult(RESULT_OK)
                         finish()
                     }
