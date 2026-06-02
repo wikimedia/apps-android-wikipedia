724d516ff73765ad4c9bbfed52bf882a8fe862b4
williamrai <williamrai13@gmail.com>
2026-06-02 15:15:47 -0400

    - more cleanups

--- app/src/androidTest/java/org/wikipedia/testsuites/TestSuite.kt
+++ app/src/androidTest/java/org/wikipedia/testsuites/TestSuite.kt
@@ -8,12 +8,12 @@ import org.wikipedia.tests.SearchTest
 
 @RunWith(Suite::class)
 @SuiteClasses(
-    // OfflineTestSuite::class, // TODO: Update tests to support Jetpack Compose-based UI.
+    OfflineTestSuite::class, // TODO: Update tests to support Jetpack Compose-based UI.
     // SuggestedEditScreenTest::class, // TODO: uncomment when login test in CI/CD is resolved
     SettingsTestSuite::class,
-    // ExploreFeedTestSuite::class, // TODO: Update tests to support Jetpack Compose-based UI.
-    SearchTest::class,
+    ExploreFeedTestSuite::class, // TODO: Update tests to support Jetpack Compose-based UI.
+    SearchTest::class, // TODO: Update tests to support Jetpack Compose-based UI.
     DeepLinkingTest::class,
-    ArticlesTestSuite::class
+    ArticlesTestSuite::class // TODO: Update tests to support Jetpack Compose-based UI.
 )
 class TestSuite
