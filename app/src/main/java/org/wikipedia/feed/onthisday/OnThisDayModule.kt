3c7719b64b5e58a1d0c847bdd934cf5b063357b3
voyagerfan <101958321+voyagerfan@users.noreply.github.com>
2026-07-15 17:19:22 -0700

    remove import

--- app/src/main/java/org/wikipedia/feed/onthisday/OnThisDayModule.kt
+++ app/src/main/java/org/wikipedia/feed/onthisday/OnThisDayModule.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
