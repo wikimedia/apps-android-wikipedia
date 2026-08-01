135973148e48b739e65f60c7aa68412c790c5e9a
voyagerfan <101958321+voyagerfan@users.noreply.github.com>
2026-07-31 20:18:56 -0700

    add openInNew icon to dialog

--- app/src/main/java/org/wikipedia/edit/EditorChoiceDialog.kt
+++ app/src/main/java/org/wikipedia/edit/EditorChoiceDialog.kt
@@ -6,16 +6,18 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.RadioButtonDefaults
@@ -30,6 +32,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
@@ -41,7 +44,6 @@ import org.wikipedia.compose.theme.BaseTheme
 import org.wikipedia.compose.theme.WikipediaTheme
 import org.wikipedia.settings.Prefs
 import org.wikipedia.theme.Theme
-import java.nio.file.WatchEvent
 
 const val EDITOR_CHOICE_VE = 0
 const val EDITOR_CHOICE_SOURCE = 1
@@ -97,7 +99,8 @@ private fun EditorChoiceContent(
                 title = stringResource(R.string.editor_select_dialog_ve_title),
                 subtitle = stringResource(R.string.editor_select_dialog_ve_subtitle),
                 selected = selectedEditor == EDITOR_CHOICE_VE,
-                onClick = { selectedEditor = EDITOR_CHOICE_VE }
+                onClick = { selectedEditor = EDITOR_CHOICE_VE },
+                shouldShowOpenInNewIcon = true
             )
 
             HorizontalDivider(
@@ -173,6 +176,7 @@ private fun EditorOption(
     title: String,
     subtitle: String,
     selected: Boolean,
+    shouldShowOpenInNewIcon: Boolean = false,
     onClick: () -> Unit
 ) {
     Row(
@@ -191,11 +195,36 @@ private fun EditorOption(
             .weight(1f)
             .fillMaxHeight()
         ) {
-            Text(
-                text = title,
-                style = MaterialTheme.typography.bodyLarge,
-                color = WikipediaTheme.colors.primaryColor
-            )
+            if(shouldShowOpenInNewIcon) {
+                Row (
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    Text(
+                        text = title,
+                        style = MaterialTheme.typography.bodyLarge,
+                        color = WikipediaTheme.colors.primaryColor
+                    )
+
+                    IconButton(
+                        onClick = { }, // TODO: wire up this click
+                        modifier = Modifier
+                            .size(15.dp)
+                    ) {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_open_in_new_black_24px),
+                            contentDescription = "icon",
+                        )
+                    }
+                }
+            } else {
+                Text(
+                    text = title,
+                    style = MaterialTheme.typography.bodyLarge,
+                    color = WikipediaTheme.colors.primaryColor
+                )
+            }
+
             Text(
                 text = subtitle,
                 style = MaterialTheme.typography.bodyMedium,
