77888595e20be785fb71c318f25c917d8538a85b
Cooltey Feng <cfeng@wikimedia.org>
2026-08-04 13:12:38 -0700

    Use webApiUrl instead of regular wiki URL to avoid Firefox kicking back

--- app/src/main/java/org/wikipedia/page/PageActivity.kt
+++ app/src/main/java/org/wikipedia/page/PageActivity.kt
@@ -491,7 +491,7 @@ class PageActivity : BaseActivity(), PageFragment.Callback, LinkPreviewDialog.Lo
     override fun onPageRequestEditSection(sectionId: Int, sectionAnchor: String?, title: PageTitle, highlightText: String?) {
         val launchEditor = {
             if (Prefs.editorModeChoice == EDITOR_CHOICE_VE && Prefs.visualEditorEnabled) {
-                UriUtil.visitInExternalBrowser(this, (title.uri + "?veaction=edit&section=$sectionId&returntoapp=1").toUri())
+                UriUtil.visitInExternalBrowser(this, title.getWebApiUrl("?veaction=edit&section=$sectionId&returntoapp=1").toUri())
                 // startActivity(SingleWebViewActivity.newIntent(this, title.uri + "?veaction=edit&section=$sectionId"))
             } else {
                 requestEditSectionLauncher.launch(EditSectionActivity.newIntent(this, sectionId, sectionAnchor, title, InvokeSource.PAGE_ACTIVITY, highlightText))
