9dc4ae212d07dd363e4dbc517e3161f4087c5094
Dmitry Brant <me@dmitrybrant.com>
2026-08-04 10:46:34 -0400

    Harden authority checking when buiding cookie list.

--- app/src/main/java/org/wikipedia/dataclient/SharedPreferenceCookieManager.kt
+++ app/src/main/java/org/wikipedia/dataclient/SharedPreferenceCookieManager.kt
@@ -100,7 +100,7 @@ class SharedPreferenceCookieManager(
             val cookiesForDomainSpec = cookieJar[domainSpec]!!
             if (domain.endsWith(domainSpec)) {
                 buildCookieList(cookieList, cookiesForDomainSpec, null)
-            } else if (Service.isWikimediaAuthority(domainSpec)) {
+            } else if (Service.isWikimediaAuthority(domainSpec) && Service.isWikimediaAuthority(domain)) {
                 // Transfer CentralAuth cookies to any subdomains of .wikipedia.org
                 buildCookieList(cookieList, cookiesForDomainSpec, CENTRALAUTH_PREFIX)
             }
