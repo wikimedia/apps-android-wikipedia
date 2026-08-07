7521f2f5d80ab368d79534b9f5eb69fe527f8922
williamrai <williamrai13@gmail.com>
2026-08-07 08:37:23 -0400

    - adds missing collector for ArticleSavedOrDeletedEvent which opened the sync dialogs - shows confirmation dialog when user tries to remove articles on long press from "All Articles" tab - design fixes

--- app/src/main/java/org/wikipedia/readinglist/compose/ReadingListPageRow.kt
+++ app/src/main/java/org/wikipedia/readinglist/compose/ReadingListPageRow.kt
@@ -178,7 +178,7 @@ private fun ListChip(title: String, onClick: () -> Unit, modifier: Modifier = Mo
         label = {
             Text(
                 text = title,
-                style = MaterialTheme.typography.bodyMedium,
+                style = MaterialTheme.typography.labelLarge,
                 maxLines = 1
             )
         },
