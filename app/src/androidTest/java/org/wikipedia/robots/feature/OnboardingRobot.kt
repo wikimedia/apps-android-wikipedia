bea752216d41e95f27ee44f3a88f515793a08bb0
williamrai <williamrai13@gmail.com>
2026-08-24 15:44:10 -0400

    - remove unused classes

--- app/src/androidTest/java/org/wikipedia/robots/feature/OnboardingRobot.kt
+++
@@ -1,7 +0,0 @@
-package org.wikipedia.robots.feature
-
-import BaseRobot
-
-class OnboardingRobot : BaseRobot() {
-    // @TODO: add new functions to interact with jetpack compose onboarding screens
-}
