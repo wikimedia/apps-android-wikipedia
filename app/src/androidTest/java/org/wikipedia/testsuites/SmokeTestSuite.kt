da07eb1bbdad8c1cb33e69a7d53912958b6a2271
williamrai <williamrai13@gmail.com>
2026-09-04 09:45:08 -0400

    - adds CreateAccountEncourageTest to SmokeTestSuite.kt and removes TestSuite.kt

--- app/src/androidTest/java/org/wikipedia/testsuites/SmokeTestSuite.kt
+++ app/src/androidTest/java/org/wikipedia/testsuites/SmokeTestSuite.kt
@@ -3,12 +3,14 @@ package org.wikipedia.testsuites
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
 import org.junit.runners.Suite.SuiteClasses
+import org.wikipedia.createaccount.CreateAccountEncourageTest
 import org.wikipedia.onboarding.InitialOnboardingActivityTest
 import org.wikipedia.personalization.PersonalizationSearchLiveApiTest
 
 @RunWith(Suite::class)
 @SuiteClasses(
     InitialOnboardingActivityTest::class,
-    PersonalizationSearchLiveApiTest::class
+    PersonalizationSearchLiveApiTest::class,
+    CreateAccountEncourageTest::class
 )
 class SmokeTestSuite
