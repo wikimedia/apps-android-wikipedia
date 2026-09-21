38640b59519ec123b100e283af985ee1fc1e75fd
Cooltey Feng <cfeng@wikimedia.org>
2026-09-21 12:21:37 -0700

    Add no results screen and skeleton loader

--- app/src/main/java/org/wikipedia/search/semantic/SemanticSearchResultsScreen.kt
+++ app/src/main/java/org/wikipedia/search/semantic/SemanticSearchResultsScreen.kt
@@ -2,6 +2,7 @@ package org.wikipedia.search.semantic
 
 import android.location.Location
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -52,6 +53,7 @@ import androidx.compose.ui.text.LinkAnnotation
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextLinkStyles
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
@@ -59,6 +61,8 @@ import androidx.compose.ui.unit.sp
 import coil3.compose.AsyncImage
 import org.wikipedia.R
 import org.wikipedia.compose.components.HtmlText
+import org.wikipedia.compose.components.error.WikiErrorClickEvents
+import org.wikipedia.compose.components.error.WikiErrorView
 import org.wikipedia.compose.theme.WikipediaTheme
 import org.wikipedia.page.PageTitle
 import org.wikipedia.search.SearchResult
@@ -110,14 +114,14 @@ fun SemanticSearchResultsScreen(
             when (searchResultsState) {
                 is UiState.Loading -> {
                     onLoading(true)
-                    // TODO: show skeleton loader
+                    SemanticSearchResultsSkeletonLoader()
                 }
 
                 is UiState.Success -> {
                     onLoading(false)
                     val results = searchResultsState.data
                     if (results.isEmpty()) {
-                        // TODO: show empty message
+                        SemanticSearchNoResultsContent()
                         return@CompositionLocalProvider
                     }
                     SemanticSearchResultsContent(
@@ -129,6 +133,14 @@ fun SemanticSearchResultsScreen(
 
                 is UiState.Error -> {
                     onLoading(false)
+                    SemanticSearchErrorContent(
+                        throwable = searchResultsState.error,
+                        wikiErrorClickEvents = WikiErrorClickEvents(
+                            retryClickListener = {
+                                viewModel.loadSemanticSearchResults()
+                            }
+                        )
+                    )
                 }
             }
         }
@@ -185,6 +197,42 @@ fun SemanticSearchResultsHeader(
             )
         }
     }
+}
+
+@Composable
+fun SemanticSearchNoResultsContent(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.padding(16.dp)
+    ) {
+        Image(
+            painter = painterResource(R.drawable.illustration_no_results),
+            contentDescription = null
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = stringResource(R.string.semantic_search_no_results_message),
+            color = WikipediaTheme.colors.primaryColor,
+            style = MaterialTheme.typography.bodyMedium,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+@Composable
+fun SemanticSearchErrorContent(
+    throwable: Throwable?,
+    wikiErrorClickEvents: WikiErrorClickEvents
+) {
+    WikiErrorView(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth(),
+        caught = throwable,
+        errorClickEvents = wikiErrorClickEvents,
+        retryForGenericError = true
+    )
 }
 
 @Composable
