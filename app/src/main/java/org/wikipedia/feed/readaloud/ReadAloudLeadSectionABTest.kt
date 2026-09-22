a832c77e5eddb6c573af6cff5598320c61b4e131
williamrai <williamrai13@gmail.com>
2026-09-22 11:28:31 -0400

    - suppress tooltip when read aloud module is turned off

--- app/src/main/java/org/wikipedia/feed/readaloud/ReadAloudLeadSectionABTest.kt
+++ app/src/main/java/org/wikipedia/feed/readaloud/ReadAloudLeadSectionABTest.kt
@@ -1,10 +1,13 @@
 package org.wikipedia.feed.readaloud
 
+import kotlinx.coroutines.flow.first
 import org.wikipedia.WikipediaApp
 import org.wikipedia.analytics.ABTest
 import org.wikipedia.database.AppDatabase
 import org.wikipedia.settings.Prefs
 import org.wikipedia.settings.RemoteConfig
+import org.wikipedia.settings.SettingsRepository
+import org.wikipedia.settings.homefeed.ForYouModuleType
 
 class ReadAloudLeadSectionABTest : ABTest("readaloudleadsection", GROUP_SIZE_2) {
     override fun getGroupName(): String {
@@ -28,6 +31,11 @@ class ReadAloudLeadSectionABTest : ABTest("readaloudleadsection", GROUP_SIZE_2) 
                 ReadAloudArticlesRepository.isSupported(WikipediaApp.instance.wikiSite) &&
                 !Prefs.readAloudLeadSectionTooltipShown &&
                 Prefs.exploreFeedVisitCount > 0 &&
-                AppDatabase.instance.topicInterestDao().hasAnyTopics()
+                AppDatabase.instance.topicInterestDao().hasAnyTopics() &&
+                !isModuleHidden()
+    }
+
+    private suspend fun isModuleHidden(): Boolean {
+        return SettingsRepository.hiddenModules.first().contains(ForYouModuleType.READ_ALOUD_LEAD_SECTION.name)
     }
 }
