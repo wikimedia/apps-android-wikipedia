c4a36711994fc76b030036954f93bce5a4ef00e8
voyagerfan <101958321+voyagerfan@users.noreply.github.com>
2026-09-24 14:01:41 -0700

    lint

--- app/src/main/java/org/wikipedia/search/SearchResultsScreen.kt
+++ app/src/main/java/org/wikipedia/search/SearchResultsScreen.kt
@@ -8,13 +8,11 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
-
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
